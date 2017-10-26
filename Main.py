65119de36bc82209ca3c7120ee2a200bd05cf811
satyajit.patil <satyajit.patil@ARSWIMSN5DSP364.mlit.wisc.edu>
2017-10-26 11:38:08 -0500

    Updated the names of the global variables to make it more readable

--- Main.py
+++ Main.py
@@ -5,70 +5,69 @@ Created by the USDA-ARS at Madison, Wisconsin
 Kristan, Kass, Jit
 '''
 
-if __name__ == '__main__':
-    pass
-
 """This class is a Simulation Controller object. An instance of this object
 runs a simulation. Functions in the class are divided based on time periods;
 year, month, day"""
 class SimulationController:
-    global state  # an array of length 3; 0=year 1=month 2=day
-    state = [0, 0, 0]
+    
+    'Global variables'
+    global currYear, currMonth, currDay
+    currYear = 1
+    currMonth = 1
+    currDay = 0
 
     'Constructor of a Simulation Controller Object'
-    def __init__(self, units, duration):
-        self.units = units
+    def __init__(self, duration):
         self.duration = duration
     
     'This function marks the start of a new year.'
-    def annualSimStart(self, state):
-        print("Annual simulation starting: " + str(state[0] + 1))
+    def annualSimStart(self, currYear):
+        print("Annual simulation starting: " + str(currYear))
         
     'This function marks the start of a new month'
-    def monthlySim(self, state):
-        print("Starting sim for month: " + str(state[1] + 1))
+    def monthlySim(self, currMonth, currDay):
+        print("Starting sim for month: " + str(currMonth + 1))
         for i in range(30):
-            state[2] = i
-            self.dailySim(state)  # call dailySim for each day
-            print("Ending monthly sim: " + str(state[1]))
+            currDay = i
+            self.dailySim(currDay)  # call dailySim for each day
 
     'This function marks the start of a day.'
-    def dailySim(self, state):
-        print("Daily sim: " + str(state[2] + 1))
+    def dailySim(self, currDay):
+        print("Daily sim: " + str(currDay + 1))
     
     'This function marks the end of a year.'
-    def annualSimEnd(self, state):
-        print("Annual simulation ending: " + str(state[0]))
+    def annualSimEnd(self, currYear):
+        print("Annual simulation ending: " + str(currYear))
 
     """This recursive function iterates through all the years, months, and days 
     for the simulation"""
-    def simulationEngine(self, state):
+    def simulationEngine(self, currYear, currMonth, currDay):
         # IF simulation's years is greater than input, quit
-        if state[0] > self.duration - 1:
-            return state
+        if currYear > self.duration:
+            return 
         
-        #ELSE, begin new year
-        self.annualSimStart(state)
+        # ELSE, begin new year
+        self.annualSimStart(currYear)
         
-        #Iterate through months
+        # Iterate through months
         for i in range(12):
-            state[1] = i
-            self.monthlySim(state)
+            currMonth = i
+            self.monthlySim(currMonth, currDay)
         
-        #End the year, and increment year count
-        self.annualSimEnd(state)
-        state[0] += 1
+        # End the year, and increment year count
+        self.annualSimEnd(currYear)
+        currYear += 1
         
-        return self.simulationEngine(state)
+        return self.simulationEngine(currYear, currMonth, currDay)
 
     'This function runs a simulation by calling the simulation engine.'
     def runSimulation(self):
         print(self.duration)
-        finalState = self.simulationEngine(state)
-        print(finalState)
-
-
-"""Execution of the program begins here. A new simulation controller object is 
-created to run the simulation."""
-simulation = SimulationController(4, 4)
-simulation.runSimulation()
+        finalState = self.simulationEngine(currYear, currMonth, currDay)
+        
+"""Execution of the program begins here."""
+if __name__ == '__main__':
+    
+    'A new simulation object is created and run'
+    simulation = SimulationController(4)
+    simulation.runSimulation()
