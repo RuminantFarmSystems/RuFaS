39ac009931f108cd7013d77dc21761e445c80a5b
Kass Chupongstimun <k_724@live.com>
2017-12-19 13:10:12 -0600

    Updates

---
+++ main.py
@@ -0,0 +1,47 @@
+################################################################################
+#
+# MASM: Modular Agricultural Systems Modeling Environment
+#
+# main.py - Main program routine
+#
+# Authors: Kass Chupongstimun
+#          Jit Patil
+#
+################################################################################
+
+#!/usr/bin/env python3
+
+from MASM.simulation_engine import MASM_Simulate
+from MASM.inputs import MASM_prompt_input
+
+#-------------------------------------------------------------------------------
+# Function: main
+#           Main function of MASM, the whole simulation is executed here
+#-------------------------------------------------------------------------------
+def main():
+ 
+    print("MASM: Modular Agricultural Systems Modeling Environment")
+
+    #
+    # Prompt user for an input
+    # Input could either be a MASM file when doing only 1 simulation
+    # or a directory containing MASM files when doing a batch simulation
+    #
+    input_files_list = MASM_prompt_input()
+    
+    #
+    # Begin the simulation
+    # Runs the simulation for each input file in input_file_path
+    # Runs only 1 simulation in the case of a single input file
+    #
+    for input_file_path in input_files_list:
+        MASM_Simulate(input_file_path)
+
+#-------------------------------------------------------------------------------
+#
+# PROGRAM ENTRY POINT
+#
+#------------------------------------------------------------------------------- 
+if __name__ == '__main__': main()
+        
+    
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='MASM',
+      version='1.0',
+      description='Modular Agricultural Systems Modeling Environment',
+      author='Kass Chupongstimun',
+      author_email='kass_c@hotmail.com',
+      url='xxx',
+      packages=['MASM', 'MASM.data', 'MASM.inputs', 'MASM.outputs'],
+     )
