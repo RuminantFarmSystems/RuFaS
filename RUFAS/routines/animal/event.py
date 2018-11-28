0731c9b7a999f1a2cc0e3872fa6685cb84124ac8
Militsa Sotirova <mas769@cornell.edu>
2018-11-28 17:31:04 -0500

    Changed docstring for event.py

--- RUFAS/routines/animal/event.py
+++ RUFAS/routines/animal/event.py
@@ -5,17 +5,15 @@ import numpy as np
 class cowEvent(object):
 	date = 0
 	event = ''
-	
+
 	# Method: __init__
-	''' 
-		Description:
-			initialize a cow life event
-		Input: 
-			date: the date counter for the cow (from birth)
-			event: the event happened on that day
-		Output:
-			events of the cow's life
-	'''
 	def __init__(self, date, event):
+	'''
+	Initialize a cow life event
+
+	Args:
+		date: the date counter for the cow (from birth)
+		event: string that represents the event happened on that day
+	'''
 		self.date = date
 		self.event = event
