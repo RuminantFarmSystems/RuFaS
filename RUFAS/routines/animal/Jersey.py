6cf11bbe49fef2110aa86cbb34363beb5c0230b5
Militsa Sotirova <mas769@cornell.edu>
2018-12-01 14:13:41 -0500

    Edited Jersey.py docstrings

--- RUFAS/routines/animal/Jersey.py
+++ RUFAS/routines/animal/Jersey.py
@@ -4,28 +4,28 @@ import numpy as np
 # Class for Jersey. Temporarily disabled
 class Jersey(Cow):
 	# Method: __init__
-	''' 
+	def __init__(self, repro_method):
+		'''
 		Description:
 			initialize Jersey
-		Input: 
+		Input:
 			repro_method:
 		Output:
-	'''
-	def __init__(self, repro_method):
+		'''
 		Cow.__init__(self, 27.2, 0.58, repro_method, 'J')
 
 	# Method: update
-	''' 
+	def update(self, feed_price, date):
+		'''
 		Description:
-			updates Jersey's information for each day. Simutes any events happened to the Jersey each day
-		Input: 
+			updates Jersey's information for each day. Simulates any events happened to the Jersey each day
+		Input:
 			feed_price: price of the feed
 			date: the date counter for the entire farm
 		Output:
 			manure:
 			feed:
-	'''
-	def update(self, feed_price, date):
+		'''
 		weight_increase = 0
 		if self.days_born <= 70:
 			weight_increase = np.random.normal(0.46, 0.02)
