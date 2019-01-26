9b73c3da6b499b574a36cb17b5ef187277b95644
Militsa Sotirova <mas769@cornell.edu>
2019-01-26 15:09:29 -0500

    Finished code for stripping comments - added test files to the Inputs directory for examples

--- fileReader.py
+++ fileReader.py
@@ -1,11 +1,27 @@
-def convertToJson(filename):
+
+def convert_to_json(filename):
     '''
-    open the file
-    open a new file that will be the json
-    for each line:
-        if not starts with hashtag
-            add line to new file
-    close the file
-    close the new file
+    Input:
+        filename: a string which is the commented text file to be turned into a json file
+    Returns:
+        a json file
+
+    Strips the commented text file from comments. For a comment to be stripped from the text
+    file, the user must use '#' to begin the comment at the very beginning of the line.
+    Stripping inline comments is not supported, i.e. the comment must be contained within
+    one line. See Inputs/test.txt for an example.
     '''
-    pass
+    file_base = filename.replace('.txt','')
+    json_filename = file_base + '.json'
+    commented_file_reader = open(filename, 'r')
+    json_file_writer = open(json_filename, 'w+')
+
+    lines_to_review = commented_file_reader.readlines()
+    for line in lines_to_review:
+        if not line[0] == '#':
+            json_file_writer.write(line)
+
+    commented_file_reader.close()
+    json_file_writer.close()
+
+convert_to_json('Inputs/test.txt')
