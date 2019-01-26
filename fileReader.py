5e52028210bf77fb573e9a7270d828d61a9c4770
Militsa Sotirova <mas769@cornell.edu>
2019-01-26 14:21:03 -0500

    Created pseudocode for the comment remover

---
+++ fileReader.py
@@ -0,0 +1,11 @@
+def convertToJson(filename):
+    '''
+    open the file
+    open a new file that will be the json
+    for each line:
+        if not starts with hashtag
+            add line to new file
+    close the file
+    close the new file
+    '''
+    pass
