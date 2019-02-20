3ea02c33282e8c376d70c45e4f0704dc5ddf0fb1
Kristan <kfr3@cornell.edu>
2019-02-20 13:52:19 -0500

    Demonstrating a pull request

--- PullDemo.py
+++ PullDemo.py
@@ -4,7 +4,7 @@ Created on Feb 11 2019
 @author: KFosterReed
 '''
 #Testing a Git pull request
-# Trying something a little bit different
 
-#M.Motew adding a line of text and then a line of white space
+
+
 
