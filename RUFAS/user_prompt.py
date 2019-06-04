b145b302716259e9aceabdcd9e3fe49745d4cd42
Jacob Johnson <Jacob8399@gmail.com>
2019-06-04 11:12:00 -0500

    Temporary change to user prompt to simplfy running the program

--- RUFAS/user_prompt.py
+++ RUFAS/user_prompt.py
@@ -49,8 +49,8 @@ def input_prompt():
     while(True):
 
         try:
-            user_input = input("\nEnter RUFAS Input: ")
-
+            # user_input = input("\nEnter RUFAS Input: ")
+            user_input = "Inputs/barnyard.json"  # TODO
             #
             # Handle user exiting program
             #
