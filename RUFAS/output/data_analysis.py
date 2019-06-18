4c4dca9bff287140cfbaae3a03c14862a8198717
Max Donovan <wmdonovan@wisc.edu>
2019-06-18 12:24:51 -0500

    Functional water balance output for daily and annual time steps.

--- RUFAS/output/data_analysis.py
+++ RUFAS/output/data_analysis.py
@@ -23,7 +23,6 @@ def data_analysis(output_csv, show_diagnostics, produce_diagnostics, is_final):
     if produce_diagnostics:
 
         output_full_path = util.get_base_dir() / 'Outputs/Sample_Farm_Outputs' / output_csv
-        save_dir = util.get_base_dir() / 'Outputs/diagnostics/' / output_csv.split('.')[0]
 
         with open(output_full_path) as csvfile:
             read_CSV = csv.reader(csvfile, delimiter=',')
@@ -48,17 +47,40 @@ def data_analysis(output_csv, show_diagnostics, produce_diagnostics, is_final):
                 r += 1
 
         if output_csv.endswith('_annual.csv'):
-            counter = 0
-            stacks = []
 
+            save_dir = util.get_base_dir() / 'Outputs/diagnostics/' / output_csv.split('.')[0].strip('_annual')
+
+            mp.figure()
+            counter = 0
+            years = variables['year']
+            legend = []
+            prev_var = ''
             for variable in variables:
-                if 1 < counter < 6:
-                    stacks.append(variables[variable])
+                if counter == 2:
+                    mp.bar(years, variables[variable])
+                    legend.append(variable)
+                if 2 < counter < 6:
+                    mp.bar(years, variables[variable], bottom=variables[prev_var])
+                    legend.append(variable)
                 if counter == 6:
                     precip = variables[variable]
+                    legend.insert(0, variable)
+                    mp.scatter(years, precip)
+                prev_var = variable
                 counter += 1
+            mp.legend(legend)
+            mp.xlabel('Dates')
+            mp.ylabel('mm H2O')
+            mp.title('Annual Water Balance')
+            path = str(save_dir / 'annual_water_balance')
+            mp.savefig(path + '')
+            if not show_diagnostics:
+                mp.close()
 
         else:
+
+            save_dir = util.get_base_dir() / 'Outputs/diagnostics/' / output_csv.split('.')[0]
+
             start_year = int(variables['year'][0])
             start_day = int(variables['julian day'][0])
             start_date = dt.datetime(int(start_year), 1, 1) + dt.timedelta(start_day - 1)
@@ -72,7 +94,7 @@ def data_analysis(output_csv, show_diagnostics, produce_diagnostics, is_final):
                     mp.plot(dates, variables[variable])
                     mp.title(variable.split()[0].upper())
                     mp.xlabel('Dates')
-                    mp.ylabel(variable + " " + units[counter])
+                    mp.ylabel(variable + ' ' + units[counter])
                     path = str(save_dir / variable)
                     mp.savefig(path + '')
                     if not show_diagnostics:
