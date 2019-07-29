5a5d005b03468a627e977728289caf3987ed1f95
wmdonovan-uwcs <wmdonovan@wisc.edu>
2019-07-29 14:54:33 -0500

    Output handler for phosphorus cycling in old RuFaS style.

--- RUFAS/output/soil_phosphorus.py
+++ RUFAS/output/soil_phosphorus.py
@@ -32,10 +32,27 @@ class SoilPhosphorus(BaseReportHandler):
         #
         self.year = []
         self.julianDay = []
-        self.numSoilLayers = 0
-        self.layersActiveP = []
-        self.layersStableP = []
 
+        self.soil_runoff_DRP = []
+        self.manure_runoff_DRP = []
+        self.fert_runoff_DRP = []
+        self.runoff_DIP = []
+        self.manure_runoff_DOP = []
+        self.manure_runoff_NH4 = []
+        self.PSP = []
+        self.labile_p1 = []
+        self.labile_p2 = []
+        self.labile_p3 = []
+        self.available_fert_P = []
+        self.released_fert_P = []
+        self.manure_WIP = []
+        self.manure_WOP = []
+        self.manure_SIP = []
+        self.manure_SOP = []
+        self.manure_NH4 = []
+        self.manure_SON = []
+        self.manure_mass = []
+        self.manure_cover = []
 
     #---------------------------------------------------------------------------
     # Function: get_header
@@ -48,21 +65,28 @@ class SoilPhosphorus(BaseReportHandler):
         with self.get_fPath().open(mode) as csvfile:
 
             # 1) Initialize the header of the cvsfile
-            fieldnames = ['Year', 'Julian Day', 'ActiveP/L1',
-                          'ActiveP/L2', 'ActiveP/L3', 'StableP/L1',
-                          'StableP/L2', 'StableP/L3']
+            fieldnames = ['Year', 'Julian Day', 'soil_runoff_DRP',
+                          'manure_runoff_DRP', 'fert_runoff_DRP', 'runoff_DIP',
+                          'manure_runoff_DOP', 'manure_runoff_NH4', 'PSP',
+                          'Labile_P1', 'Labile_P2', 'Labile_P3', 'Available_Fert_P',
+                          'Released_Fert_P', 'manure_WIP', 'manure_WOP', 'manure_SIP',
+                          'manure_SOP', 'manure_NH4', 'manure_SON', 'manure_mass',
+                          'manure_cover']
 
             self.fieldNames = fieldnames
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                    lineterminator = '\n')
+                                    lineterminator='\n')
             writer.writeheader()
 
             # 2) Write Units in 2nd row of cvsfile
-            units = {'Year': '', 'Julian Day': ''}
-            for fieldname in fieldnames:
-                if (fieldname.startswith("ActiveP/") or 
-                    fieldname.startswith("StableP/")):
-                    units[fieldname] = 'kg/ha'
+            units = {'Year': '', 'Julian Day': '', 'soil_runoff_DRP': 'mgL',
+                     'manure_runoff_DRP': 'mgL', 'fert_runoff_DRP': 'mgL',
+                     'runoff_DIP': 'mgL', 'manure_runoff_DOP': 'mgL',
+                     'manure_runoff_NH4': 'mgL', 'PSP': '', 'Labile_P1': 'kg/ha',
+                     'Labile_P2': 'kg/ha', 'Labile_P3': 'kg/ha', 'Available_Fert_P': 'kg',
+                     'Released_Fert_P': 'kg', 'manure_WIP': 'kg', 'manure_WOP': 'kg',
+                     'manure_SIP': 'kg', 'manure_SOP': 'kg', 'manure_NH4': 'kg',
+                     'manure_SON': 'kg', 'manure_mass': 'kg', 'manure_cover': 'HA'}
             writer.writerow(units)
 
     #---------------------------------------------------------------------------
@@ -95,11 +119,30 @@ class SoilPhosphorus(BaseReportHandler):
 
         self.year.append(time.cal_year)
         self.julianDay.append(time.day)
-        
-        for x in range(0, len(soil.listOfSoilLayers)):
-            self.layersActiveP[x].append(soil.listOfSoilLayers[x].activeP)
-            self.layersStableP[x].append(soil.listOfSoilLayers[x].stableP)
-            
+
+        # TODO: still needs to be implemented
+
+        self.soil_runoff_DRP.append()
+        self.manure_runoff_DRP.append()
+        self.fert_runoff_DRP.append()
+        self.runoff_DIP.append()
+        self.manure_runoff_DOP.append()
+        self.manure_runoff_NH4.append()
+        self.PSP.append()
+        self.labile_p1.append()
+        self.labile_p2.append()
+        self.labile_p3.append()
+        self.available_fert_P.append()
+        self.released_fert_P.append()
+        self.manure_WIP.append()
+        self.manure_WOP.append()
+        self.manure_SIP.append()
+        self.manure_SOP.append()
+        self.manure_NH4.append()
+        self.manure_SON.append()
+        self.manure_mass.append()
+        self.manure_cover.append()
+
     #---------------------------------------------------------------------------
     # Function: write_annual_report
     #           Appends the annual report to the output file
@@ -115,14 +158,28 @@ class SoilPhosphorus(BaseReportHandler):
             for x in range(0, len(self.julianDay)):
                 dailySoilPhosphorusData = {
                     'Year': str(self.year[x]),
-                    'Julian Day': self.julianDay[x]}
-                
-                for y in range(0, self.numSoilLayers):
-                    dailySoilPhosphorusData["ActiveP/L" + str(y+1)] = str(
-                        round(self.layersActiveP[y][x], 3))
-                    
-                    dailySoilPhosphorusData["StableP/L" + str(y+1)] = str(
-                        round(self.layersStableP[y][x], 3))                
+                    'Julian Day': self.julianDay[x],
+                    'soil_runoff_DRP': self.soil_runoff_DRP[x],
+                    'manure_runoff_DRP': self.manure_runoff_DRP[x],
+                    'fert_runoff_DRP': self.fert_runoff_DRP[x],
+                    'runoff_DIP': self.runoff_DIP[x],
+                    'manure_runoff_DOP': self.manure_runoff_DOP[x],
+                    'manure_runoff_NH4': self.manure_runoff_NH4[x],
+                    'PSP': self.PSP[x],
+                    'Labile_P1': self.labile_p1[x],
+                    'Labile_P2': self.labile_p2[x],
+                    'Labile_P3': self.labile_p3[x],
+                    'Available_Fert_P': self.available_fert_P[x],
+                    'Released_Fert_P': self.released_fert_P[x],
+                    'manure_WIP': self.manure_WIP[x],
+                    'manure_WOP': self.manure_WOP[x],
+                    'manure_SIP': self.manure_SIP[x],
+                    'manure_SOP': self.manure_SOP[x],
+                    'manure_NH4': self.manure_NH4[x],
+                    'manure_SON': self.manure_SON[x],
+                    'manure_mass': self.manure_mass[x],
+                    'manure_cover': self.manure_cover[x]
+                }
 
                 writer = csv.DictWriter(csvfile, fieldnames=self.fieldNames,
                                     lineterminator = '\n')
@@ -136,7 +193,27 @@ class SoilPhosphorus(BaseReportHandler):
 
         self.year = []
         self.julianDay = []
-        
-        for x in range(0, self.numSoilLayers):
-            self.layersActiveP[x] = []
-            self.layersStableP[x] = []
+
+        self.year = []
+        self.julianDay = []
+
+        self.soil_runoff_DRP = []
+        self.manure_runoff_DRP = []
+        self.fert_runoff_DRP = []
+        self.runoff_DIP = []
+        self.manure_runoff_DOP = []
+        self.manure_runoff_NH4 = []
+        self.PSP = []
+        self.labile_p1 = []
+        self.labile_p2 = []
+        self.labile_p3 = []
+        self.available_fert_P = []
+        self.released_fert_P = []
+        self.manure_WIP = []
+        self.manure_WOP = []
+        self.manure_SIP = []
+        self.manure_SOP = []
+        self.manure_NH4 = []
+        self.manure_SON = []
+        self.manure_mass = []
+        self.manure_cover = []
