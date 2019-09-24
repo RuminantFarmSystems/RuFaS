957880723f30bc3d7b5f77f054fc3dffe8f2b12e
Max Donovan <wmdonovan@wisc.edu>
2019-09-24 10:22:56 -0500

    Sediment P reduces active and stable Phosphorus pools

--- RUFAS/routines/soil/phosphorus_cycling/erosion.py
+++ RUFAS/routines/soil/phosphorus_cycling/erosion.py
@@ -11,7 +11,6 @@ Author(s): Jacob Johnson, jacob8399@gmail.com,
 
 
 def update_all(S):
-
     S.runoff_conc = 0
     S.enrichment_P = 0
     if S.runoff != 0:
@@ -19,6 +18,12 @@ def update_all(S):
         S.enrichment_P = 0.78 * (S.runoff_conc ** -0.2468)
 
     S.sed_P_conc = 100 * (S.soil_layers[0].active_P + S.soil_layers[0].stable_P) \
-                        / (S.soil_layers[0].bulk_density * S.soil_layers[0].bottom_depth)
+                   / (S.soil_layers[0].bulk_density * S.soil_layers[0].bottom_depth)
 
     S.sed_P = 0.001 * S.sed_P_conc * (S.sed / S.area) * S.enrichment_P
+
+    S.soil_layers[0].active_P -= S.soil_layers[0].active_P[0] * \
+                                 (S.sed_P / S.soil_layers[0].active_P + S.soil_layers[0].stable_P)
+
+    S.soil_layers[0].stable_P -= S.soil_layers[0].stable_P[0] * \
+                                 (S.sed_P / S.soil_layers[0].active_P + S.soil_layers[0].stable_P)
