04af4b9ff4abdd0141ee3f428bbe2e2043edcdfc
Militsa Sotirova <mas769@cornell.edu>
2019-10-21 22:50:32 -0400

    Manure testing and updated ration testing
    Note that since some of the formulas in the ration calculations were changed from the last time the methods were tested, the expected values have also slightly changed.

--- RUFAS/test/animal/test_lactating_cow.py
+++ RUFAS/test/animal/test_lactating_cow.py
@@ -9,14 +9,15 @@ Author(s): Militsa Sotirova
 
 import unittest
 from RUFAS.routines import Feed
-from RUFAS.routines.animal import ration
+from RUFAS.routines.animal.ration import lactating_cow_ration as ration
+from RUFAS.routines.animal.manure import lactating_cow_manure_excretion as manure_excretion
 from RUFAS.test.animal import animal_inputs_outputs
 
 class LactatingCowTest(unittest.TestCase):
     
     def set_up_feeds(self):
-        feed_info = {
-        "feed_library": "Inputs/testing_ration_feedlib.csv",
+        feed_info_0 = {
+        "feed_library": "Inputs/manure_feed_test.csv",
         
         "available_feeds":
             [
@@ -27,7 +28,7 @@ class LactatingCowTest(unittest.TestCase):
                 "Rye_hay"
             ]
         }
-        self.feeds = [Feed(feed_info)]
+        self.feeds = [Feed(feed_info_0)]
    
     def setUp(self):
         self.inputs_ouputs = animal_inputs_outputs.AnimalInputsOutputs()
@@ -39,7 +40,7 @@ class LactatingCowTest(unittest.TestCase):
         expected_ration = self.inputs_ouputs.lactating_cow_expected_ration_1
         feed = self.feeds[0]
         
-        rqmts = ration.calculate_rqmts(inputs['BW'], inputs['BCS'], 
+        rqmts, _, _ = ration.calculate_rqmts(inputs['BW'], inputs['BCS'], 
                                        inputs['CBW'], inputs['CI'], 
                                        inputs['concentrate'], inputs['CP_Milk'],
                                        inputs['DOP'],inputs['DHD'],
@@ -48,11 +49,24 @@ class LactatingCowTest(unittest.TestCase):
                                        inputs['lactose_milk'], 
                                        inputs['milk'], inputs['parity'], 
                                        inputs['type'], feed.nutrient_rqmts)
-    
+
+        self.assertEqual(rqmts, expected_rqmts)
+        
         formulated_ration = ration.optimize(feed, rqmts)
         
-        self.assertEqual(rqmts, expected_rqmts)
         self.assertEqual(formulated_ration, expected_ration)
+        
+    def test_manure_1(self):
+        inputs = self.inputs_ouputs.lactating_cow_input_1
+        input_ration = self.inputs_ouputs.lactating_cow_input_ration_1
+        expected_manure = self.inputs_ouputs.lactating_cow_expected_manure_1
+        feed = self.feeds[0]
+        
+        manure = manure_excretion.manure_calculations(input_ration, feed,
+                                        inputs['BW'], inputs['DIM'], 
+                                        inputs['mPrt'])
+            
+        self.assertEqual(manure, expected_manure)
 
 if __name__ == '__main__':
     unittest.main()
