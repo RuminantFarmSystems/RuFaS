4fbb90244514e641b9be2ffe50e41c999459a601
Militsa Sotirova <mas769@cornell.edu>
2020-01-26 15:05:09 -0500

    P excreted by animal

--- RUFAS/routines/animal/life_cycle/calf.py
+++ RUFAS/routines/animal/life_cycle/calf.py
@@ -139,6 +139,45 @@ class Calf(AnimalBase):
 
 		return p_retained
 
+	def phosphorus_excreted(self, p_intake, total_manure):
+		"""
+		Calculates the phosphorus excreted by the animal. Note that equation
+		(A.#.C.1) (which deals with the P in milk produced) is omitted because
+		that is a calculation for Cows.
+
+		Args:
+			p_intake: amount of P in formulated ration (g)
+			total_manure: amount of manure excreted by animal (kg)
+
+		Returns:
+			P excreted by animal
+			WIP (water extractable inorganic P) fraction
+			WOP (water extractable organic P) fraction
+		"""
+		# OMITTED: calculation for p_milk (A.#.C.1)
+
+		# P in urine (g) (A.#.C.2)
+		p_urine = 0.000002 * self.body_weight * 1000
+
+		# P in feces (g) (A.#.C.3)
+		p_feces = -2.3 + 0.63 * p_intake
+
+		# P in manure (g) (A.#.C.4)
+		p_manure = p_urine + p_feces
+
+		# Water extractable Inorganic P (WIP) fraction - fraction of manure
+		# compromised of inorganic water extractable P (A.#.C.5)
+		WIP_frac = 0.50 * ((p_feces + p_urine) / (total_manure * 1000))
+
+		# Water extractable Organic P (WOP) fraction - fraction of maure
+		# compromised of organic water extractable P (A.#.C.6)
+		WOP_frac = 0.05 * ((p_feces + p_urine) / (total_manure * 1000))
+
+		# P excreted (g)
+		p_excrt = p_manure
+
+		return p_excrt, WIP_frac, WOP_frac
+
 	def update(self):
 		"""
 		Controls calf's grow with average daily gain based on user's input until
--- RUFAS/routines/animal/life_cycle/cow.py
+++ RUFAS/routines/animal/life_cycle/cow.py
@@ -365,6 +365,44 @@ class Cow(HeiferIII):
 
 		return p_retained
 
+	def phosphorus_excreted(self, p_intake, total_manure):
+		"""
+		Calculates the phosphorus excreted by the animal.
+
+		Args:
+			p_intake: amount of P in formulated ration (g)
+			total_manure: amount of manure excreted by animal (kg)
+
+		Returns:
+			P excreted by animal
+			WIP (water extractable inorganic P) fraction
+			WOP (water extractable organic P) fraction
+		"""
+		# P in milk produced (g) (A.#.C.1)
+		p_milk = 0.0009 * self.estimated_daily_milk_produced * 1000
+
+		# P in urine (g) (A.#.C.2)
+		p_urine = 0.000002 * self.body_weight * 1000
+
+		# P in feces (g) (A.#.C.3)
+		p_feces = -2.3 + 0.63 * p_intake
+
+		# P in manure (g) (A.#.C.4)
+		p_manure = p_urine + p_feces
+
+		# Water extractable Inorganic P (WIP) fraction - fraction of manure
+		# compromised of inorganic water extractable P (A.#.C.5)
+		WIP_frac = 0.50 * ((p_feces + p_urine) / (total_manure * 1000))
+
+		# Water extractable Organic P (WOP) fraction - fraction of maure
+		# compromised of organic water extractable P (A.#.C.6)
+		WOP_frac = 0.05 * ((p_feces + p_urine) / (total_manure * 1000))
+
+		# P excreted (g)
+		p_excrt = p_milk + p_manure
+
+		return p_excrt, WIP_frac, WOP_frac
+
 	def calc_daily_walking_dist(
 			self, vertical_dist_to_parlor, horizontal_dist_to_parlor):
 		"""
