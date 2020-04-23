1bdb75dfcc009688f7e08bfa80a35f69ab237c6a
Max Donovan <wmdonovan@wisc.edu>
2020-04-23 08:37:33 -0500

    clearing up edits made for a tutorial video

--- RUFAS/routines/field/soil/nitrogen_cycling/denitrification.py
+++ RUFAS/routines/field/soil/nitrogen_cycling/denitrification.py
@@ -23,13 +23,13 @@ def denitrification(soil):
         org_C = layer.org_C
         de_N_rate = layer.de_N_rate
         SW = layer.soil_water
-        FieldCapacity = layer.fc_water
+        FC = layer.fc_water
 
         temp_fac = layer.temp_fac
 
         # "pseudocode_soil" S.4.D.1
         denitrified_N = 0
-        if SW > FieldCapacity:
+        if SW > FC:
             exp_part = exp(-de_N_rate * temp_fac * org_C)
             denitrified_N = layer.NO3 * (1 - exp_part)
 
