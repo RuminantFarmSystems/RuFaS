2222ca1654985633ea90570173ba0f0390423849
jacob.johnson <jacob8399@gmail.com>
2020-05-28 11:09:57 -0500

    Fixed base_crop to work when crop is null

--- RUFAS/routines/field/crop/crop_types/base_crop.py
+++ RUFAS/routines/field/crop/crop_types/base_crop.py
@@ -1,8 +1,27 @@
 class BaseCrop:
     def __init__(self):
+
         self.grow_years = []
         self.repeat = 0
         self.planting_date = 0
+        self.harvest_date = 0
+
         self.harvest_type = ''
+
         self.crop_name = 'null'
         self.crop_type = ''
+
+        # Necessary variables for a null crop to run
+        # These variables are used within the soil module and must
+        # be declared and initialized even when a crop is not growing
+        self.fr_PHU = 0
+        self.LAI_actual = 0
+        self.z_root = 0
+        self.biomass_actual = 0
+        self.epco = 0
+        self.N_fix = 0.0
+        self.bio_N = 0
+        self.bio_P = 0
+        self.bio_AG = 0
+        self.yield_actual = 0
+        self.yield_annual = 0
