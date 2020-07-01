c261ebe8af58e61c1c5081d50d03853d6cc88753
josephmerhi <jm2257@cornell.edu>
2020-07-01 15:22:27 -0400

    implementing calf manure excretion

--- RUFAS/routines/animal/manure/calf_manure_excretion.py
+++ RUFAS/routines/animal/manure/calf_manure_excretion.py
@@ -10,13 +10,14 @@ Author(s): Militsa Sotirova, militsasotirova@gmail.com
 from .general_manure import phosphorus_excreted
 
 
-def manure_calculations(p_feces_excrt, p_urine):
+def manure_calculations(BW, p_feces_excrt, p_urine):
     """
     TEMPORARY PLACEHOLDER
     Calculates inputs for manure module with information from the
     ration formulation.
 
     Args:
+        BW: body weight, kg
         p_feces_excrt: amount of P excreted by an animal (g)
         p_urine: amount of P required for urine production (g)
 
@@ -35,16 +36,18 @@ def manure_calculations(p_feces_excrt, p_urine):
             p_excrt_manure: manure P excretion for manure module input (g)
             p_frac: P fraction of manure
     """
-    total_manure = 70.792
+    # Amount of manure, kg [A.3A.A.1]
+    Mkg = 0.0567 * BW
+    
     p_excrt, WIP_frac, WOP_frac, p_excrt_manure, p_frac = \
-        phosphorus_excreted(0, total_manure, p_feces_excrt, p_urine)
+        phosphorus_excreted(0, Mkg, p_feces_excrt, p_urine)
     return p_excrt, \
-           {"U": 0.340,
-            "TAN_s": 0.14,
-            "MN": 532.407,
-            "Mkg": total_manure,
-            "VSd": 7087.413,
-            "VSnd": 859.390,
+           {"U": 0.340, #TODO: Implement with correct equation
+            "TAN_s": 0.14, #TODO: Implement with correct equation
+            "MN": 532.407, #TODO: Implement with correct equation
+            "Mkg": Mkg,
+            "VSd": 7087.413, #TODO: Implement with correct equation
+            "VSnd": 859.390, #TODO: Implement with correct equation
             "WIP_frac": WIP_frac,
             "WOP_frac": WOP_frac,
             "p_excrt_manure": p_excrt_manure,
