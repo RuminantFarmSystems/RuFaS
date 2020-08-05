001f19a4d7d01772c9ce6d631e1c6b540510d0f2
michael.tang <mtang2018@gmail.com>
2020-08-05 15:31:46 -0500

    Recreating jsons and crop python files to initialize the parameters for each crop. Needed to change an erroneous merge in field_management branch that I hastily made switching branches for the meeting.

---
+++ RUFAS/routines/field/crop/crop_types/tall_fescue.py
@@ -0,0 +1,178 @@
+from .base_crop import BaseCrop
+
+
+class tall_fescue(BaseCrop):
+
+    def __init__(self, data):
+        super().__init__()
+
+        """GENERAL PLANT INFO"""
+        tall_fescue_data = data['tall_fescue']
+        self.grow_years = tall_fescue_data['grow_years']
+        self.repeat = tall_fescue_data['repeat']
+        self.planting_date = tall_fescue_data['planting_date']
+        self.harvest_date = tall_fescue_data['harvest_date']
+
+        if tall_fescue_data['harvest_type'] != 'optimal':
+            print('Perennial crops are always optimally harvested')
+
+        self.harvest_type = 'optimal'
+
+        self.crop_name = 'tall_fescue'
+        self.crop_type = 'perennial'
+        self.harvest_quality = 'null'
+        self.feed_id = ''  # need
+
+        self.kill_day = -1
+        self.kill_year = False
+        self.planted = False
+        self.growing = False
+
+        self.fix_nitrogen = True  # TODO: don't know this value
+
+        # ===================================================================
+        ''' HEAT UNIT DATA '''
+
+        # input
+        self.T_base_min = 4  # unknown
+        self.T_base_max = 43.33  # unknown
+        self.PHU = 800  # still unknown
+
+        # Internally calculated input
+        self.accumulated_HU = 0.0
+        self.prev_accumulated_HU = 0.0
+        self.fr_PHU_harvest_min = 0.9
+
+        # output
+        self.fr_PHU = 0.0
+        self.prev_fr_PHU = 0.0
+
+        # ===================================================================
+        ''' LEAF AREA INDEX (LAI) DATA '''
+
+        # input
+        self.fr_PHU_1 = 0.15
+        self.fr_PHU_2 = 0.45
+        self.fr_LAI_1 = 0.01
+        self.fr_LAI_2 = 0.95
+        self.fr_PHU_sen = 0.90
+        self.fr_PHU_harvest = 1.2  # unknown
+        self.LAI_max = 4
+        self.LAI_min = 0.75  # unknown
+
+        # Internally calculated input
+        self.prev_fr_LAI_max = 0
+        self.fr_LAI_max = 0
+
+        # output
+        self.prev_LAI_actual = 0
+        self.LAI_actual = 0
+
+        # ===================================================================
+        ''' ROOT DEPTH DATA '''
+
+        # input
+        self.z_root_max = 3000  # maximum depth of root development
+
+        # Internally calculated input
+        self.fr_root = 0
+
+        # output
+        self.z_root = 0
+
+        # ===================================================================
+        ''' BIOMASS DATA '''
+
+        # input
+        self.kl = 0.65  # unknown
+        self.RUE = 30
+        self.T_opt = 15
+
+        # Internally calculated input
+        self.gamma_reg = 0
+        self.d_biomass_max = 0
+        self.d_biomass_actual = 0.0
+
+        # output
+        self.biomass_actual = 0
+        self.prev_biomass_actual = 0
+
+        # ===================================================================
+        ''' Soil Water Uptake Data '''
+
+        self.beta_w = 10  # water-use distribution parameter  # corn  # unknown
+        self.epco = 1  # unknown
+
+        self.water_actual_up = 0
+        self.water_uptake_each_layer = []
+
+        # ===================================================================
+        ''' Nitrogen Uptake Data '''
+
+        self.N_fix = 0.0  # unknown
+
+        self.beta_n = 10  # unknown
+
+        self.bio_N_opt = 0  # unknown
+        self.bio_N = 0  # unknown
+
+        self.fr_n1 = 0.0560
+        self.fr_n2 = 0.0210
+        self.fr_n3 = 0.0120
+        self.fr_n3ish = 0.02001  # unknown
+
+        self.fr_N = 0
+        self.fr_N_up = 0
+        self.N_up = 0
+        self.act_N_up_each_layer = []
+        self.N_actual_up = 0
+
+        # ===================================================================
+        ''' Phosphorus Uptake Data '''
+
+        self.beta_p = 10  # unknown
+
+        self.bio_P_opt = 0  # unknown
+        self.bio_P = 0  # unknown
+
+        self.fr_PHU_50 = 0.5  # unknown
+        self.fr_PHU_100 = 1.0  # unknown
+        self.fr_p1 = 0.0099
+        self.fr_p2 = 0.0022
+        self.fr_p3 = 0.0019
+        self.fr_p3ish = 0.00201  # unknown
+
+        self.fr_P = 0
+        self.P_up = 0
+        self.act_P_up_each_layer = []
+        self.P_act_up = 0
+
+        # ===================================================================
+        ''' Yields Data '''
+
+        self.HI_max = 0  # unknown
+        self.HI_min = 0.  # unknown
+        self.HI_actual = 0  # unknown
+        self.HI_opt = 0.9  # unknown
+
+        self.harvest_eff = 0.9  # unknown
+
+        self.gamma_wu = 0  # unknown
+
+        self.biomass_dry_down_perc = 0.0
+        self.DM_harvest_perc = 0.15  # TODO: Hard coded dry matter percent at harvest
+        self.NDF_harvest_perc = 0.416
+
+        self.bio_AG = 0  # unknown
+        self.yield_max = 0  # unknown
+        self.yield_actual = 0  # unknown
+        self.DM_yield = 0.0  # unknown
+        self.NDF_yield = 0.0  # unknown
+        self.N_yield = 0  # unknown
+        self.P_yield = 0  # unknown
+
+        self.N_yield_annual = 0.0
+        self.P_yield_annual = 0.0
+        self.DM_yield_annual = 0.0
+        self.NDF_yield_annual = 0.0
+        self.yield_annual = 0
