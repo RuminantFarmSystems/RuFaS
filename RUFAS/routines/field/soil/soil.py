4232428c5962d5df2ce588e3bdd2ff3094fd5215
Jacob Johnson <jacob8399@gmail.com>
2020-09-28 15:59:06 -0500

    *minor commit

--- RUFAS/routines/field/soil/soil.py
+++ RUFAS/routines/field/soil/soil.py
@@ -313,6 +313,7 @@ class Soil:
         # soil carbon attributes
         self.residue_harvest = 0.0
         self.lignin_residue_percent = 17  # tied to the reset of this variable in yields, if changed, change together
+        # TODO Lignin AG and BG are currently not implemented
         self.lignin_residue_AG_percent = 2.6  # tied to the reset of this variable in yields, if changed, change together
         self.lignin_residue_BG_percent = 2.6  # tied to the reset of this variable in yields, if changed, change together
         self.curr_layer_depth = 0
