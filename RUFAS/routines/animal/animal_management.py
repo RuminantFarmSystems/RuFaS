a9bc1272665fc5ad1d1af74b01fe9afc9977bb6f
Militsa Sotirova <mas769@cornell.edu>
2020-11-15 13:18:58 -0500

    adding animals to pens

--- RUFAS/routines/animal/animal_management.py
+++ RUFAS/routines/animal/animal_management.py
@@ -378,7 +378,7 @@ class AnimalManagement:
                 self.heiferIIIs.append(animal)
             else:  # animal is of class Cow
                 animal_p_conc = self.cow_p_comp
-                self.all_pens[pen].animals_in_pen.append(animal)
+                # self.all_pens[pen].animals_in_pen.append(animal)
                 self.cows.append(animal)
 
             self.all_pens[pen].set_up_new_animal(animal, animal_p_conc, feed, temp)
@@ -396,6 +396,7 @@ class AnimalManagement:
             self.id_pen[calf.id] = pen
             self.calves.append(calf)
             self.all_pens[pen].set_up_new_animal(calf, self.pasture_concentrate, feed, temp)
+            # self.all_pens[pen].animals_in_pen.append(calf)
 
     def pen_allocation(self):
         """
