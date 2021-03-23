b25968a1fc0f1d3a6bc178cbe0f3c016f5799785
Chris VanKerkhove <cjv47@cornell.edu>
2021-03-23 16:46:48 -0400

    Indicator Variables Dev (Unfinished)

--- RUFAS/routines/animal/ration/pyomo_solver.py
+++ RUFAS/routines/animal/ration/pyomo_solver.py
@@ -53,6 +53,8 @@ def model_shell():
     model.P_req = pyo.Param(within = pyo.NonNegativeReals)
     model.DMIest = pyo.Param(within = pyo.NonNegativeReals)
     model.BW = pyo.Param(within = pyo.NonNegativeReals)
+    #big M
+    model.bigM = pyo.Param(initialize = 1000000)
 
     ###Variables###
     #bounds function for variables
@@ -65,6 +67,11 @@ def model_shell():
         """
         return(0, m.limit[i, j])
     model.x = pyo.Var(model.feed, model.nrg, domain = pyo.NonNegativeReals, bounds=bnds)
+    #single variable for discount value
+    model.discount = pyo.Var(domain = pyo.NonNegativeReals)
+    #Indicator variables
+    model.Z1 = pyo.Var(domain = pyo.Binary)
+    model.Z2 = pyo.Var(domain = pyo.Binary)
 
     ###Objectives###
     def obj_cost(m):
@@ -79,14 +86,48 @@ def model_shell():
 
     ###Constraints###
     #constraint rules
+    '''
+    def discount_con(m):
+        C = 0.035*(m.BW*0.98)**0.75
+        TDNconc = (pyo.summation(m.TDN, m.x)*0.01) / pyo.summation(m.x)
+        DMI_to_maint = ((pyo.summation(m.TDN, m.x)*0.01)/C)*(1-m.Z1) + m.Z1
+        Discount = (((TDNconc - 0.18*TDNconc - 10.3)*(DMI_to_maint - 1)) / \
+                        TDNconc) * (1-m.Z2) + m.Z2
+        return m.discount == Discount
+        '''
+
     def NDF_constraint_1(m):
         return pyo.summation(m.NDF, m.x)  >= 25 * pyo.summation(m.x)
     def NDF_constraint_2(m):
         return pyo.summation(m.NDF, m.x) <= 45 * pyo.summation(m.x)
 
+    #Indicator Variable Constraint Rules#
+    #These constraints and variables are used in the place of conditional
+    #statements on variable values
+    '''
+    #These constraint ensures that Z1 = 1 if TotalTDN < C and Z1 = 0 else
+    def z_con1(m):
+        C = 0.035*(m.BW*0.98)**0.75
+        return pyo.summation(m.TDN, m.x)*0.01 >= (C - m.bigM*(m.Z1))
+    def z_con11(m):
+        C = 0.035*(m.BW*0.98)**0.75
+        return pyo.summation(m.TDN, m.x)*0.01 <= (C + m.bigM*(1-m.Z1))
+    #These constraint ensures that Z2 = 1 if Totalconc < 60 and Z2 = 0 else
+    def z_con2(m):
+        TDN_tot = (pyo.sum_product(m.TDN, m.x)*0.01)
+        return TDN_tot >= (60 - m.bigM*m.Z2) * pyo.summation(m.x)
+    def z_con22(m):
+        TDN_tot = (pyo.summation(m.TDN, m.x)*0.01)
+        return TDN_tot / (60 + m.bigM*(1-m.Z2)) <= pyo.summation(m.x)
+    '''
     #constrain pyomo objects
     model.NDF_con1 = pyo.Constraint(rule = NDF_constraint_1)
     model.NDF_con2 = pyo.Constraint(rule= NDF_constraint_2)
+    #model.Indcon1 = pyo.Constraint(rule = z_con1)
+    #model.Indcon11 = pyo.Constraint(rule = z_con11)
+    #model.Indcon2 = pyo.Constraint(rule = z_con2)
+    #model.Indcon22 = pyo.Constraint(rule = z_con22)
+    #model.discountcon = pyo.Constraint(rule = discount_con)
 
     return model
 
@@ -116,11 +157,11 @@ def create_model(feeds_data, req_data, feeds):
     #creating an instance of this model withe the populated data
     model = model_shell()
     m1 = model.create_instance(data)
-    results = pyo.SolverFactory('ipopt').solve(m1)
+    results = pyo.SolverFactory('APOPT').solve(m1)
 
-    #print('\nCost = ', m1.cost())
-    #for i,j in m1.feed*m1.nrg:
-    #    print(i, j, m1.x[i,j]())
+    print('\nCost = ', m1.cost())
+    for i,j in m1.feed*m1.nrg:
+        print(i, j, m1.x[i,j]())
 
     #print('\nConstraints')
     #print('NDF Con 1  = ', m1.NDF_con1())
