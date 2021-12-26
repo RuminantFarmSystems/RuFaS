3a9569f4a63cd3f68f52329872d0b3fdd8d06b96
Pooya Hekmati <51127592+PooyaHekmati@users.noreply.github.com>
2021-12-25 23:05:50 -0500

    Created test structure and removed old tests

---
+++ tests/feed.py
@@ -0,0 +1,180 @@
+"""
+RUFAS: Ruminant Farm Systems Model
+File name: feed.py
+Description: Implements test cases
+Author(s): Pooya Hekmati, sh2235@cornell.edu
+"""
+
+import pytest
+
+
+def test_update_all():
+	"""Unit test for function update_all in file routines\feed\carbon_loss.py"""
+	pass
+
+
+def test_harvest_loss():
+	"""Unit test for function harvest_loss in file routines\feed\carbon_loss.py"""
+	pass
+
+
+def test_storage_loss():
+	"""Unit test for function storage_loss in file routines\feed\carbon_loss.py"""
+	pass
+
+
+def test_feed_out_loss():
+	"""Unit test for function feed_out_loss in file routines\feed\carbon_loss.py"""
+	pass
+
+
+def test_update_carbon():
+	"""Unit test for function update_carbon in file routines\feed\carbon_loss.py"""
+	pass
+
+
+def test_daily_feed_routine():
+	"""Unit test for function daily_feed_routine in file routines\feed\feed.py"""
+	pass
+
+
+def test_annual_feed_routine():
+	"""Unit test for function annual_feed_routine in file routines\feed\feed.py"""
+	pass
+
+
+def test_summarize_feed_storage():
+	"""Unit test for function summarize_feed_storage in file routines\feed\feed.py"""
+	pass
+
+
+def test_calibrate_storage():
+	"""Unit test for function calibrate_storage in file routines\feed\feed.py"""
+	pass
+
+
+def test_forage_quality_assessment():
+	"""Unit test for function forage_quality_assessment in file routines\feed\feed.py"""
+	pass
+
+
+def test_store_crop():
+	"""Unit test for function store_crop in file routines\feed\feed.py"""
+	pass
+
+
+def test_calculate_losses():
+	"""Unit test for function calculate_losses in file routines\feed\feed.py"""
+	pass
+
+
+def test_reset_storage():
+	"""Unit test for function reset_storage in file routines\feed\feed.py"""
+	pass
+
+
+def test_required_inventory():
+	"""Unit test for function required_inventory in file routines\feed\feed.py"""
+	pass
+
+
+def test_forage_inventory_plan():
+	"""Unit test for function forage_inventory_plan in file routines\feed\feed.py"""
+	pass
+
+
+def test_daily_feed_storage():
+	"""Unit test for function daily_feed_storage in file routines\feed\feed.py"""
+	pass
+
+
+def test_daily_feed_management():
+	"""Unit test for function daily_feed_management in file routines\feed\feed.py"""
+	pass
+
+
+def test_reset_feed_data():
+	"""Unit test for function reset_feed_data in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_feeds_split_by_maturity():
+	"""Unit test for function get_feeds_split_by_maturity in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_all_feed_units():
+	"""Unit test for function get_all_feed_units in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_purchased_feed_ids():
+	"""Unit test for function get_purchased_feed_ids in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_feed_id():
+	"""Unit test for function get_feed_id in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_feed_quality():
+	"""Unit test for function get_feed_quality in file routines\feed\feed.py"""
+	pass
+
+
+def test_add_to_available_feeds():
+	"""Unit test for function add_to_available_feeds in file routines\feed\feed.py"""
+	pass
+
+
+def test_update_available_feed():
+	"""Unit test for function update_available_feed in file routines\feed\feed.py"""
+	pass
+
+
+def test_remove_from_available_feeds():
+	"""Unit test for function remove_from_available_feeds in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_nutrient_vals():
+	"""Unit test for function get_nutrient_vals in file routines\feed\feed.py"""
+	pass
+
+
+def test_get_calf_feeds():
+	"""Unit test for function get_calf_feeds in file routines\feed\feed.py"""
+	pass
+
+
+def test_update_all():
+	"""Unit test for function update_all in file routines\feed\nitrogen_loss.py"""
+	pass
+
+
+def test_CP_loss():
+	"""Unit test for function CP_loss in file routines\feed\nitrogen_loss.py"""
+	pass
+
+
+def test_NPN_loss():
+	"""Unit test for function NPN_loss in file routines\feed\nitrogen_loss.py"""
+	pass
+
+
+def test_update_CP():
+	"""Unit test for function update_CP in file routines\feed\nitrogen_loss.py"""
+	pass
+
+
+def test_update_all():
+	"""Unit test for function update_all in file routines\feed\protein_degradation.py"""
+	pass
+
+
+def test_degrade_protein():
+	"""Unit test for function degrade_protein in file routines\feed\protein_degradation.py"""
+	pass
+
+
