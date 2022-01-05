7170760b60aa77dab28c81930ab65cb749f36ab5
20anpeng07 <32548097+20anpeng07@users.noreply.github.com>
2022-01-05 15:58:23 -0500

    Added comments for index 0 and 2, removed old commented out code

--- RUFAS/output_handler/reports/pen_report.py
+++ RUFAS/output_handler/reports/pen_report.py
@@ -56,6 +56,8 @@ class PenReport(BaseReportDriver):
             pen = state.animal_management.all_pens[self.pen_id]
 
             for variable in self.daily_variables:
+                # index 2 is the accumulator for the evauated variables
+                # index 0 is the string literal code representation of the variable
                 self.daily_variables[variable][2].append(
                     eval(self.daily_variables[variable][0], globals(), locals()))
 
@@ -65,6 +67,8 @@ class PenReport(BaseReportDriver):
             pen = state.animal_management.all_pens[self.pen_id]
 
             for variable in self.annual_variables:
+                # index 2 is the accumulator for the evauated variables
+                # index 0 is the string literal code representation of the variable
                 self.annual_variables[variable][2] = \
                     eval(self.daily_variables[variable]
                          [0], globals(), locals())
@@ -139,163 +143,3 @@ class PenReport(BaseReportDriver):
         def produce_report_graphics(self):
             super().produce_report_graphics()
             graphics.ration_graphics(self)
-
-
-# """
-# RUFAS: Ruminant Farm Systems Model
-# File name: pen_report.py
-# Description:
-# Author(s): William Donovan, wmdonovan@wisc.edu
-# """
-
-# from .base_report_driver import BaseReportDriver
-# from .base_report import BaseReport
-# from .. import graphics
-
-
-# class PensReport(BaseReportDriver):
-#     def __init__(self, data, state):
-#         super().__init__(data)
-#         for pen in state.animal_management.all_pens:
-#             print(pen)
-#             print(pen.animal_groups)
-#             self.reports['pen_' + str(pen.id)] = PenReport(data, state.feed, pen, pen.id)
-
-#         self.reports['pens_summary'] = PensSummary(data['pens_summary'])
-
-
-# class PensSummary(BaseReport):
-#     def __init__(self, data):
-#         super().__init__(data)
-
-#         self.daily_variables = {
-#             'year': ['time.calendar_year', '', []],
-#             'j_day': ['time.day', '', []]
-#         }
-
-#         self.annual_variables = {
-#             'year': ['time.calendar_year', '', 0]
-#         }
-
-
-# class PenReport(BaseReportDriver):
-#     def __init__(self, data, feed, individual_pen, pen_id):
-#         super().__init__(data)
-#         self.pen_id = pen_id
-#         self.report_name = 'pen_' + str(pen_id)
-#         self.reports = {
-#             'ration_report': self.RationReport(data['ration_report'], feed, individual_pen, self.pen_id),
-#             'growth_report': self.GrowthReport(data['growth_report'], self.pen_id),
-#             'manure_report': self.ManureReport(data['manure_report'], self.pen_id)
-#         }
-
-#     class BasePenReport(BaseReport):
-#         def __init__(self, data, pen_id):
-#             super().__init__(data)
-#             self.pen_id = pen_id
-
-#         def daily_update(self, state, weather, time):
-#             animal_management = state.animal_management
-#             feed = state.feed
-#             pen = state.animal_management.all_pens[self.pen_id]
-
-#             print(self.daily_variables)
-#             for variable in self.daily_variables:
-#                 self.daily_variables[variable][2].append(
-#                     eval(self.daily_variables[variable][0], globals(), locals()))
-
-#         def annual_update(self, state, weather, time):
-#             animal_management = state.animal_management
-#             feed = state.feed
-#             pen = state.animal_management.all_pens[self.pen_id]
-
-#             for variable in self.annual_variables:
-#                 self.annual_variables[variable][2] = \
-#                     eval(self.daily_variables[variable][0], globals(), locals())
-
-#     class GrowthReport(BasePenReport):
-#         def __init__(self, data, pen_id):
-#             super().__init__(data, pen_id)
-
-#             self.daily_variables = {'year': ['time.calendar_year', '', []],
-#                                     'j_day': ['time.day', '', []],
-#                                     'num_animals_in_pen': ['len(pen.animals_in_pen)', '', []],
-#                                     'average_growth': ['pen.avg_growth', 'kg', []],
-#                                     'average_milk': ['pen.avg_milk', 'kg', []],
-#                                     'average_p_animal': ['pen.avg_p_animal', 'g', []],
-#                                     'average_p_req': ['pen.avg_p_req', 'g', []]
-#                                     }
-
-#             self.annual_variables = {'year': ['time.calendar_year', '', 0]
-#                                      }
-
-#     class ManureReport(BasePenReport):
-#         def __init__(self, data, pen_id):
-#             super().__init__(data, pen_id)
-
-#             self.daily_variables = {'year': ['time.calendar_year', '', []],
-#                                     'j_day': ['time.day', '', []],
-#                                     'num_animals': ['len(pen.animals_in_pen)', '', []],
-#                                     'manure': ['pen.manure[\'p_excrt_manure\']', 'kg', []],
-#                                     'urea_conc': ['pen.manure[\'U\']', 'mol/L', []],
-#                                     'ammoniacal_N_conc': ['pen.manure[\'TAN_s\']', 'mol/L', []],
-#                                     'manure_N': ['pen.manure[\'MN\']', 'g', []],
-#                                     'total_solids': ['pen.manure[\'TSd\']', 'kg', []],
-#                                     'manure_VSd': ['pen.manure[\'VSd\']', 'g', []],
-#                                     'manure_VSnond': ['pen.manure[\'VSnd\']', 'g', []],
-#                                     'WIP_frac': ['pen.manure[\'WIP_frac\']', 'g/g total man', []],
-#                                     'WOP_frac': ['pen.manure[\'WOP_frac\']', 'g/g total man', []],
-#                                     'manure_P': ['pen.manure[\'p_excrt_manure\']', 'g', []],
-#                                     'P_frac': ['pen.manure[\'p_frac\']', 'g/g total man', []],
-#                                     'K_manure': ['pen.manure[\'K_manure\']', 'g', []],
-#                                     'enteric_methane': ['pen.manure[\'CH4_manure\']', 'g', []]
-#                                     }
-
-#             self.annual_variables = {'year': ['time.calendar_year', '', 0]}
-
-#             self.manure_info = {}
-
-#     class RationReport(BasePenReport):
-#         def __init__(self, data, feed, individual_pen, pen_id):
-#             super().__init__(data, pen_id)
-#             self.ration_interval = data['ration_interval']
-
-#             self.daily_variables = {'year': ['time.calendar_year', '', []],
-#                                     'j_day': ['time.day', '', []],
-#                                     'num_animals': ['len(pen.animals_in_pen)', '', []]
-#                                     }
-#             # this subsets the pen's allotted feed given the animals type(s)
-#             # that are present within the pen that is currently being
-#             # simulated
-#             individual_pen.animal_class_feed_subsetter(feed)
-
-#             all_feeds = feed.all_feed_ids
-#             #print(all_feeds)
-
-#             #index at 0 because there is a nested list here for some unknown reason
-#             print(individual_pen.feed_ids)
-#             pen_id_list = individual_pen.feed_ids
-#             print(pen_id_list)
-
-#             pen_specific_feeds = {str(x): all_feeds[str(x)] for x in pen_id_list}
-
-
-#             for feed_id in pen_id_list:
-#                 feed_name = pen_specific_feeds[str(feed_id)]['feed_name']
-#                 units = pen_specific_feeds[str(feed_id)]['units']
-
-#                 self.daily_variables[str(feed_id) + "(" + feed_name + ")"] = \
-#                     [
-#                         'pen.ration[\'%s\'] if pen.pen_populated and \'%s\' in pen.feed_ids else 0' % (
-#                             feed_id, feed_id), units,
-#                         []]
-
-#             self.annual_variables = {
-#                 'year': ['time.calendar_year', '', 0]
-#             }
-
-
-#         def produce_report_graphics(self):
-#             super().produce_report_graphics()
-#             graphics.ration_graphics(self)
-# yet another line to test whether my version control is fixed
