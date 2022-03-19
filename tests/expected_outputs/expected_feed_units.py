ae2b8519c5e36999686ad6007d2580278ea1d847
ancheypeng <32548097+20anpeng07@users.noreply.github.com>
2022-03-19 15:15:47 -0400

    set up and added tests for pen class

---
+++ tests/expected_outputs/expected_feed_units.py
@@ -0,0 +1,237 @@
+"""
+RUFAS: Ruminant Farm Systems Model
+File name: expected_feed_units.py
+Description: Stores the expected output dictionaries for test_get_all_feed_units()
+Author(s): Anchey Peng, ap724@cornell.edu, Simon Ucedavelez, sau8@cornell.edu
+"""
+
+expected = [
+    {},
+    {
+        '15': {
+            'feed_name': 'BLOOD',
+            'units': 'kg'
+        }
+    },
+    {
+        '56g': {
+            'feed_name': 'GRASSES, COOL SEASON',
+            'units': 'kg'
+        }
+    },
+    {
+        '15': {
+            'feed_name': 'BLOOD',
+            'units': 'kg'
+        },
+        '56g': {
+            'feed_name': 'GRASSES, COOL SEASON',
+            'units': 'kg'
+        }
+    },
+
+    {
+        '15': {
+            'feed_name': 'BLOOD',
+            'units': 'kg'
+        },
+        '29': {
+            'feed_name': 'CORN, YELLOW',
+            'units': 'kg'
+        },
+        '48': {
+            'feed_name': 'FISH BYPRODUCTS',
+            'units': 'kg'
+        },
+        '85': {
+            'feed_name': 'LEGUMES, FORAGE',
+            'units': 'kg'
+        },
+        '96': {
+            'feed_name': 'LINSEED (FLAX)',
+            'units': 'kg'
+        },
+        '97': {
+            'feed_name': 'MEAT',
+            'units': 'kg'
+        },
+        '104': {
+            'feed_name': 'PEANUT',
+            'units': 'kg'
+        },
+        '106': {
+            'feed_name': 'RICE',
+            'units': 'kg'
+        },
+        '109': {
+            'feed_name': 'SORGHUM, GRAIN TYPE',
+            'units': 'kg'
+        },
+        '123': {
+            'feed_name': 'SUNFLOWER',
+            'units': 'kg'
+        },
+        '125': {
+            'feed_name': 'TRITICALE',
+            'units': 'kg'
+        },
+        '139': {
+            'feed_name': 'Limestone, ground',
+            'units': 'kg'
+        },
+        '144': {
+            'feed_name': 'Sodium Choride',
+            'units': 'kg'
+        },
+        '151': {
+            'feed_name': 'Ammonium Chloride',
+            'units': 'kg'
+        },
+        '34g': {
+            'feed_name': 'CORN, YELLOW',
+            'units': 'kg'
+        },
+        '52g': {
+            'feed_name': 'GRASSES, COOL SEASON',
+            'units': 'kg'
+        },
+        '57g': {
+            'feed_name': 'GRASSES, COOL SEASON',
+            'units': 'kg'
+        },
+        '61g': {
+            'feed_name': 'GRASS-Legume MIXTURES Pr. GR.\xa0 (17-22% Hcel.)',
+            'units': 'kg'
+        },
+        '65g': {
+            'feed_name': 'GRASS-Legume MIXTURES Pr. GR.\xa0 (17-22% Hcel.)',
+            'units': 'kg'
+        },
+        '69g': {
+            'feed_name': 'Mixed grass-legume (12-15% Hcel.)',
+            'units': 'kg'
+        },
+        '73g': {
+            'feed_name': 'Mixed grass-legume (12-15% Hcel.)',
+            'units': 'kg'
+        },
+        '77g': {
+            'feed_name': 'Grass-LEGUME MIXTURES Pr. LG. (10-13% HCEL.)',
+            'units': 'kg'
+        },
+        '81g': {
+            'feed_name': 'Grass-LEGUME MIXTURES Pr. LG. (10-13% HCEL.)',
+            'units': 'kg'
+        },
+        '87g': {
+            'feed_name': 'LEGUMES, FORAGE',
+            'units': 'kg'
+        },
+        '92g': {
+            'feed_name': 'LEGUMES, FORAGE',
+            'units': 'kg'
+        }
+    }, {
+        '15': {
+            'feed_name': 'BLOOD',
+            'units': 'kg'
+        },
+        '29': {
+            'feed_name': 'CORN, YELLOW',
+            'units': 'kg'
+        },
+        '48': {
+            'feed_name': 'FISH BYPRODUCTS',
+            'units': 'kg'
+        },
+        '85': {
+            'feed_name': 'LEGUMES, FORAGE',
+            'units': 'kg'
+        },
+        '96': {
+            'feed_name': 'LINSEED (FLAX)',
+            'units': 'kg'
+        },
+        '97': {
+            'feed_name': 'MEAT',
+            'units': 'kg'
+        },
+        '104': {
+            'feed_name': 'PEANUT',
+            'units': 'kg'
+        },
+        '106': {
+            'feed_name': 'RICE',
+            'units': 'kg'
+        },
+        '109': {
+            'feed_name': 'SORGHUM, GRAIN TYPE',
+            'units': 'kg'
+        },
+        '123': {
+            'feed_name': 'SUNFLOWER',
+            'units': 'kg'
+        },
+        '125': {
+            'feed_name': 'TRITICALE',
+            'units': 'kg'
+        },
+        '139': {
+            'feed_name': 'Limestone, ground',
+            'units': 'kg'
+        },
+        '144': {
+            'feed_name': 'Sodium Choride',
+            'units': 'kg'
+        },
+        '151': {
+            'feed_name': 'Ammonium Chloride',
+            'units': 'kg'
+        },
+        '61g': {
+            'feed_name': 'GRASS-Legume MIXTURES Pr. GR.\xa0 (17-22% Hcel.)',
+            'units': 'kg'
+        },
+        '34g': {
+            'feed_name': 'CORN, YELLOW',
+            'units': 'kg'
+        },
+        '52g': {
+            'feed_name': 'GRASSES, COOL SEASON',
+            'units': 'kg'
+        },
+        '57g': {
+            'feed_name': 'GRASSES, COOL SEASON',
+            'units': 'kg'
+        },
+        '65g': {
+            'feed_name': 'GRASS-Legume MIXTURES Pr. GR.\xa0 (17-22% Hcel.)',
+            'units': 'kg'
+        },
+        '69g': {
+            'feed_name': 'Mixed grass-legume (12-15% Hcel.)',
+            'units': 'kg'
+        },
+        '73g': {
+            'feed_name': 'Mixed grass-legume (12-15% Hcel.)',
+            'units': 'kg'
+        },
+        '77g': {
+            'feed_name': 'Grass-LEGUME MIXTURES Pr. LG. (10-13% HCEL.)',
+            'units': 'kg'
+        },
+        '81g': {
+            'feed_name': 'Grass-LEGUME MIXTURES Pr. LG. (10-13% HCEL.)',
+            'units': 'kg'
+        },
+        '87g': {
+            'feed_name': 'LEGUMES, FORAGE',
+            'units': 'kg'
+        },
+        '92g': {
+            'feed_name': 'LEGUMES, FORAGE',
+            'units': 'kg'
+        }
+    }
+
+]
