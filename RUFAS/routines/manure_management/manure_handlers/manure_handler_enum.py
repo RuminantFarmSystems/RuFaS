f12a1ece499c1da62167abfdd5e8dc75c35b8cf0
Loi Pham <ltp23@cornell.edu>
2022-04-06 02:47:16 -0700

    Simplified ManureHandlerEnum class.

--- RUFAS/routines/manure_management/manure_handlers/manure_handler_enum.py
+++ RUFAS/routines/manure_management/manure_handlers/manure_handler_enum.py
@@ -1,33 +1,21 @@
 from __future__ import annotations
 
-from enum import Enum, auto
-from typing import Dict
-
-from RUFAS.routines.manure_management.helpers.misc_functions import format_enum_name
+from enum import Enum
 
 
 class ManureHandlerEnum(Enum):
-    FLUSH_SYSTEM = auto()
-    MANUAL_SCRAPING = auto()
-    ALLEY_SCRAPER = auto()
-    NULL_MANURE_HANDLER = auto()
-    CUSTOM_MANURE_HANDLER = auto()
+    FLUSH_SYSTEM = 'flush_system'
+    MANUAL_SCRAPING = 'manual_scraping'
+    ALLEY_SCRAPER = 'alley_scraper'
+    NULL_MANURE_HANDLER = 'null'
+    CUSTOM_MANURE_HANDLER = 'custom'
 
     @classmethod
-    def get_enum(cls, manure_handling_method: str) -> ManureHandlerEnum:
-        patternToEnum: Dict[str: ManureHandlerEnum] = {
-            'flush_system': cls.FLUSH_SYSTEM,
-            'alley_scrappers': cls.ALLEY_SCRAPER,
-            'manual_scraping': cls.MANUAL_SCRAPING,
-            'null': cls.NULL_MANURE_HANDLER,
-            'custom': cls.CUSTOM_MANURE_HANDLER,
-            'default': cls.FLUSH_SYSTEM
-        }
-        for pattern, enum_type in patternToEnum.items():
-            if pattern in manure_handling_method.lower():
-                return enum_type
+    def get_enum(cls, manure_handler: str, default='flush_system') -> ManureHandlerEnum:
+        for e in cls:  # Iterate through each enum member
+            if manure_handler.lower() in e.value.lower():
+                return e
         else:
-            default = patternToEnum['default']
-            print(f'{manure_handling_method} is not currently implemented as a handling method. '
-                  f'Setting to {format_enum_name(default)}')
-            return default
+            print(f'{manure_handler} is not currently implemented as a handling method. '
+                  f'Setting to {default}')
+            return cls.get_enum(default)
