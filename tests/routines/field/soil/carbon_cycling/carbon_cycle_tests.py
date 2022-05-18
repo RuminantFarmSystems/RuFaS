015ee239471a76bae2a6ddc21a80809cd6f61685
jtweneboah <jessica_tweneboah@intuit.com>
2022-05-18 14:09:50 -0400

    Refactoring first two test files

--- tests/routines/field/soil/carbon_cycling/carbon_cycle_tests.py
+++ tests/routines/field/soil/carbon_cycling/carbon_cycle_tests.py
@@ -5,77 +5,64 @@ Description: Implements test cases
 Author(s): Jessica Tweneboah, jnt42@cornell.edu
 """
 
-import json
 import logging
+from unittest.mock import MagicMock
+
 import numpy as np
-import sys
-import os 
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(0, os.path.dirname(os.path.realpath(__name__)))
 
 from RUFAS.routines.field.soil import Soil
 from RUFAS.routines.field.soil.carbon_cycling import carbon_cycle
-from config.defintions import ROOT_DIR
 
 LOGGER = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-LOGGER.info("Read json file: ARL_soil_tillage.json")
-soil_file = open(ROOT_DIR + "/input/soil/ARL_soil_tillage.json")
-LOGGER.info("Convert from json file format to Python dictionary")
-soil_data = json.load(soil_file)
-
 
 def test_soil_carbon_aggregation():
-	LOGGER.info("Testing function: soil_carbon_aggregation")
-	test_soil = Soil(soil_data)
-	carbon_cycle.soil_carbon_aggregation(test_soil)
+    LOGGER.info("Testing function: soil_carbon_aggregation")
+    test_soil = MagicMock(Soil)
+    test_soil_layer = MagicMock(Soil.SoilLayer)
+    layer_attributes = {
+        "bottom_depth": 279.4,
+        "bulk_density": 1.34,
+        "C_active": 1250000,
+        "C_slow": 1250000,
+        "C_passive": 1250000,
+        "AG_struct_to_C_active_loss": 0,
+        "AG_met_to_C_active_loss": 0,
+        "AG_struct_to_C_slow_loss": 0,
+        "BG_met_to_C_active_loss": 0,
+        "BG_struct_to_C_active_loss": 0,
+        "BG_struct_to_C_slow_loss": 0,
+        "C_CO2_loss_decomp": 0,
+        "C_active_loss": 0,
+        "C_slow_loss": 0,
+        "C_passive_loss": 0
+    }
+    test_soil_layer.configure_mock(**layer_attributes)
+    soil_attributes = {
+        "soil_layers": [test_soil_layer],
+        "curr_layer_depth": 0,
+        "area": 1.0
+    }
+    test_soil.configure_mock(**soil_attributes)
+    carbon_cycle.soil_carbon_aggregation(test_soil)
 
-	LOGGER.info("Checking number of layers")
-	assert len(test_soil.soil_layers) == 4
+    LOGGER.info("Checking number of layers")
+    assert len(test_soil.soil_layers) == 1
 
-	LOGGER.info("Checking changes to soil layers")
-	LOGGER.info("Checking Layer 0")
-	layer0 = test_soil.soil_layers[0]
-	np.testing.assert_almost_equal(1.0016132650989862, layer0.C_percent)
-	np.testing.assert_almost_equal(1.0016132650989862, layer0.org_C)
-	np.testing.assert_almost_equal(3750000, layer0.C)
-	np.testing.assert_almost_equal(3750, layer0.C_mg)
-	np.testing.assert_almost_equal(375000.0, layer0.C_g)
-	np.testing.assert_almost_equal(0.0, layer0.C_CO2_loss_decomp)
-	np.testing.assert_almost_equal(0.0, layer0.C_CO2_loss)
+    LOGGER.info("Checking changes to soil layers")
+    LOGGER.info("Checking Layer 0")
+    layer0 = test_soil.soil_layers[0]
+    np.testing.assert_almost_equal(1.0016132650989862, layer0.C_percent)
+    np.testing.assert_almost_equal(1.0016132650989862, layer0.org_C)
+    np.testing.assert_almost_equal(3750000, layer0.C)
+    np.testing.assert_almost_equal(3750, layer0.C_mg)
+    np.testing.assert_almost_equal(375000.0, layer0.C_g)
+    np.testing.assert_almost_equal(0.0, layer0.C_CO2_loss_decomp)
+    np.testing.assert_almost_equal(0.0, layer0.C_CO2_loss)
 
-	LOGGER.info("Checking Layer 1")
-	layer1 = test_soil.soil_layers[1]
-	np.testing.assert_almost_equal(0.3465675945436397, layer1.C_percent)
-	np.testing.assert_almost_equal(0.3465675945436397, layer1.org_C)
-	np.testing.assert_almost_equal(3750000, layer1.C)
-	np.testing.assert_almost_equal(3750, layer1.C_mg)
-	np.testing.assert_almost_equal(375000.0, layer1.C_g)
-	np.testing.assert_almost_equal(0.0, layer1.C_CO2_loss_decomp)
-	np.testing.assert_almost_equal(0.0, layer1.C_CO2_loss)
+    assert test_soil.curr_layer_depth == 0
 
-	LOGGER.info("Checking Layer 2")
-	layer2 = test_soil.soil_layers[2]
-	np.testing.assert_almost_equal(1.845472440944882, layer2.C_percent)
-	np.testing.assert_almost_equal(1.845472440944882, layer2.org_C)
-	np.testing.assert_almost_equal(3750000, layer2.C)
-	np.testing.assert_almost_equal(3750, layer2.C_mg)
-	np.testing.assert_almost_equal(375000.0, layer2.C_g)
-	np.testing.assert_almost_equal(0.0, layer2.C_CO2_loss_decomp)
-	np.testing.assert_almost_equal(0.0, layer2.C_CO2_loss)
-
-	LOGGER.info("Checking Layer 3")
-	layer3 = test_soil.soil_layers[3]
-	np.testing.assert_almost_equal(0.2982581722739203, layer3.C_percent)
-	np.testing.assert_almost_equal(0.2982581722739203, layer3.org_C)
-	np.testing.assert_almost_equal(3750000, layer3.C)
-	np.testing.assert_almost_equal(3750, layer3.C_mg)
-	np.testing.assert_almost_equal(375000.0, layer3.C_g)
-	np.testing.assert_almost_equal(0.0, layer3.C_CO2_loss_decomp)
-	np.testing.assert_almost_equal(0.0, layer3.C_CO2_loss)
-
-	assert test_soil.curr_layer_depth == 0
 
 if __name__ == "__main__":
-   test_soil_carbon_aggregation()
+    test_soil_carbon_aggregation()
--- tests/routines/field/soil/carbon_cycling/decomp_factors_tests.py
+++ tests/routines/field/soil/carbon_cycling/decomp_factors_tests.py
@@ -5,74 +5,59 @@ Description: Implements test cases
 Author(s): Jessica Tweneboah, jnt42@cornell.edu
 """
 
-import pytest
-import json
 import logging
+from unittest.mock import MagicMock
+
 import numpy as np
-import sys 
-import os 
 
-sys.path.insert(0, os.path.dirname(os.path.realpath(__name__)))
-
-from RUFAS.routines.field.soil.carbon_cycling import decomp_factors
-from RUFAS.routines.field.soil import Soil
-from config.defintions import ROOT_DIR
-from RUFAS.classes import Weather
-from RUFAS.classes import Config
 from RUFAS.classes import Time
+from RUFAS.classes import Weather
+from RUFAS.routines.field.soil import Soil
+from RUFAS.routines.field.soil.carbon_cycling import decomp_factors
 
 LOGGER = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-LOGGER.info("Read json file: ARL_soil_tillage.json")
-soil_file = open(ROOT_DIR + "/input/soil/ARL_soil_tillage.json")
-path_to_weather_file = ROOT_DIR + "/input/weather/ARL_weather.csv"
-
-LOGGER.info("Convert from json file format to Python dictionary")
-soil_data = json.load(soil_file)
-
 
 def test_temp_factor():
-	LOGGER.info("Testing function: temp_factor")
-	test_soil = Soil(soil_data)
-	test_config_data = {
-		"start_date": "1990:1",
-		"end_date": "2019:365",
-		"csv_dir": "output/CSVs/",
-		"graphic_dir": "output/graphics/",
-		"set_seed": False,
-		"seed": 0
-	}
-	test_config = Config(test_config_data, path_to_weather_file)
-	test_weather = Weather(path_to_weather_file, test_config)
-	test_time = Time(test_config)
-	decomp_factors.temp_factor(test_soil, test_weather, test_time)
+    LOGGER.info("Testing function: temp_factor")
+    test_soil = MagicMock(Soil)
+    weather_attributes = {
+        "T_avg": [[0]]
+    }
+    time_attributes = {
+        "year": 1,
+        "day": 1
+    }
+    test_weather = MagicMock(Weather)
+    test_weather.configure_mock(**weather_attributes)
+    test_time = MagicMock(Time)
+    test_time.configure_mock(**time_attributes)
+    decomp_factors.temp_factor(test_soil, test_weather, test_time)
 
-	LOGGER.info("Checking soil T_d")
-	np.testing.assert_almost_equal(0.06893050947285823, test_soil.T_d)
+    LOGGER.info("Checking soil T_d")
+    np.testing.assert_almost_equal(0.12513139838984882, test_soil.T_d)
 
 
 def test_moisture_factor():
-	LOGGER.info("Testing function: moisture_factor")
-	test_soil = Soil(soil_data)
-	decomp_factors.moisture_factor(test_soil)
+    LOGGER.info("Testing function: moisture_factor")
+    test_soil = MagicMock(Soil)
+    test_soil_layer = MagicMock(Soil.SoilLayer)
+    layer_attributes = {
+        "water_fac": 0.0
+    }
+    test_soil_layer.configure_mock(**layer_attributes)
+    soil_attributes = {
+        "soil_layers": [test_soil_layer]
+    }
+    test_soil.configure_mock(**soil_attributes)
+    decomp_factors.moisture_factor(test_soil)
 
-	LOGGER.info("Checking changes to soil layers")
-	LOGGER.info("Checking Layer 0")
-	layer0 = test_soil.soil_layers[0]
-	np.testing.assert_almost_equal(1.0187946798566629e-05, layer0.M_d)
+    LOGGER.info("Checking changes to soil layers")
+    LOGGER.info("Checking Layer 0")
+    layer0 = test_soil.soil_layers[0]
+    np.testing.assert_almost_equal(1.0187946798566629e-05, layer0.M_d)
 
-	LOGGER.info("Checking Layer 1")
-	layer1 = test_soil.soil_layers[1]
-	np.testing.assert_almost_equal(1.0187946798566629e-05, layer1.M_d)
-
-	LOGGER.info("Checking Layer 2")
-	layer2 = test_soil.soil_layers[2]
-	np.testing.assert_almost_equal(1.0187946798566629e-05, layer2.M_d)
-
-	LOGGER.info("Checking Layer 3")
-	layer3 = test_soil.soil_layers[3]
-	np.testing.assert_almost_equal(1.0187946798566629e-05, layer3.M_d)
 
 if __name__ == "__main__":
-  test_temp_factor()
+    test_temp_factor()
