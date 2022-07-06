06839e3e606f36e66543e51c944715cdd7e6d3e7
Loi Pham <ltp23@cornell.edu>
2022-07-06 02:11:04 -0700

    Added some simple tests for gas emissions

--- tests/test_manure_management.py
+++ tests/test_manure_management.py
@@ -1,12 +1,47 @@
 """
 RUFAS: Ruminant Farm Systems Model
-File name: test_manure_management.py
+File name: old_manure_management.py
 Description: Implements test cases
 Author(s): Sadman Chowdhury, skc86@cornell.edu
 """
+from typing import List
 
-import pytest
+from pytest import approx
+
+from RUFAS.routines.manure_management.gas_emissions.gas_emissions import GasEmissions
 from RUFAS.simulation_engine import SimulationEngine
+
+
+def assert_two_lists_equal(expected_items: List, result_items: List) -> None:
+    """Checks equality of two lists element-wise.
+
+    Args:
+        expected_items: list of expected results.
+        result_items: list of results to be tested.
+
+    Returns:
+        None
+
+    """
+    assert len(expected_items) == len(result_items)
+    for expected, result in zip(expected_items, result_items):
+        assert expected == approx(result)
+
+
+def test_convert_temp_C_to_K():
+    """Unit test for function convert_temp_C_to_K in file gas_emissions.py"""
+    temps_in_C = [-273.15, 0, 100]
+    expected_temps = [0, 273.15, 373.15]
+    result_temps = [GasEmissions.convert_temp_C_to_K(temp) for temp in temps_in_C]
+    assert_two_lists_equal(expected_temps, result_temps)
+
+
+def test_calc_modified_hours():
+    """Unit test for function calc_modified_hours in file gas_emissions.py"""
+    hours = [15, 14, 5, 4]
+    expected_hours = [0.2857129, 0.3999013, -0.3999013, -0.2857141]
+    result_hours = [GasEmissions.calc_modified_hours(hour) for hour in hours]
+    assert_two_lists_equal(expected_hours, result_hours)
 
 
 def test_calc_Kh():
@@ -66,14 +101,12 @@ def test_calc_overall_mass_transfer_coefficient():
     """Unit test for function calc_vmax in file gas_emissions.py"""
     pass
 
+
 def test_calc_concentration_of_ammonia_in_manure():
     """Unit test for function calc_concentration_of_ammonia_in_manure in file gas_emissions.py"""
     pass
 
+
 def test_calc_E_N20_manure():
     """Unit test for function calc_E_N20_manure in file gas_emissions.py"""
-    pass
-
-def test_convert_temp_C_to_K():
-    """Unit test for function calc_vmax in file gas_emissions.py"""
     pass
