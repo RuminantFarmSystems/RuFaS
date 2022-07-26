7f8e8de587ab3fc9ff451572f0d388549a4d467d
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2022-07-26 16:20:07 -0500

    restructured the Corn class, following alfalfa

--- RUFAS/routines/field/crop/crop_types/crop_config.py
+++ RUFAS/routines/field/crop/crop_types/crop_config.py
@@ -25,7 +25,7 @@ Description:
 # alfalfa.py
 ALFALFA = {
         # alfalfa ID variables
-        "harvest_type": 'optimal',
+        "harvest_type": 'optimal',  # ToDo: is this always the type for alfalfa? Corn reads this from data
         "crop_type": 'perennial',
         "raw_id": 1,
         "feed_id": '1g',
@@ -76,9 +76,61 @@ ALFALFA = {
         "gamma_wu": 0,
         "biomass_dry_down_percent": 0.0,
         "DM_harvest_percent": 0.15,  # TODO: Hard coded dry matter percent at harvest - GitHub Issue #155
-        "NDF_harvest_percent": 0.416,
+        "NDF_harvest_percent": 0.416
 }
 
 # corn.py
-CORN = {}
+CORN = {
+        # corn ID variables
+        "crop_type": 'annual',
+        "raw_id": 34,
+        "feed_id": '34g',
+        "fix_nitrogen": False,
+        "kill_year": True,
+        # heat unit variables
+        "T_base_min": 10,
+        "T_base_max": 30,
+        "PHU": 1200,
+        # LAI parameters
+        "fr_PHU_1": 0.15,
+        "fr_PHU_2": 0.50,
+        "fr_LAI_1": 0.05,
+        "fr_LAI_2": 0.95,
+        "fr_PHU_sen": 0.90,
+        "fr_PHU_harvest": 1.2,
+        "fr_PHU_harvest_min": 0.7,
+        "LAI_max": 3,
+        "LAI_min": 0,
+        # root depth
+        "z_root_max": 2000,
+        # biomass
+        "kl": 0.65,
+        "RUE": 39,
+        "T_opt": 25,
+        # water uptake
+        "beta_w": 10,
+        "epco": 0.5,
+        # nitrogen uptake
+        "beta_n": 10,
+        "fr_n1": 0.047,
+        "fr_n2": 0.0177,
+        "fr_n3": 0.0138,
+        "fr_n3ish": 0.01381,
+        # phosphorus
+        "beta_p": 10,
+        "fr_PHU_50": 0.5,
+        "fr_PHU_100": 1.0,
+        "fr_p1": 0.0048,
+        "fr_p2": 0.0018,
+        "fr_p3": 0.0014,
+        "fr_p3ish": 0.00141,
+        # yield
+        "HI_max": 0,
+        "HI_min": 0.3,
+        "HI_actual": 0,
+        "HI_opt": 0.6,
+        "harvest_eff": 0.9,
+        "biomass_dry_down_percent": 0.0,
+        "DM_harvest_percent": 0.35  # TODO: Hard coded dry matter percent at harvest - GitHub Issue #155
+}
 
