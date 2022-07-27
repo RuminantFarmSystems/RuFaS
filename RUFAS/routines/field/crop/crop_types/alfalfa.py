db6ecb684d8e0365e03f73cf737d3dab8dd937fe
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2022-07-27 15:30:47 -0500

    Update alfalfa.py

--- RUFAS/routines/field/crop/crop_types/alfalfa.py
+++ RUFAS/routines/field/crop/crop_types/alfalfa.py
@@ -5,18 +5,18 @@ from RUFAS.routines.field.crop.crop_types import crop_config
 # TODO: add overall description of Alfalfa class, along with 
 #  description of attributes - GitHub Issue #170
 class Alfalfa(BaseCrop):
-
     """``Alfalfa`` is a child of the ``BaseCrop`` class, and one of the main crop types.
        Attributes of ``Alfalfa`` are derived from data collected for the crop.
        The crop-specific attributes for alfala, are located in the crop config variable ``ALFALFA`` in crop_config.py.
        See the inherited class ``BaseCrop`` for all other attributes and member functions.
-       
-       Args: 
-        crop_name (str): the name of the crop
-        data (dict): data used to construct the class
     """
     def __init__(self, crop_name, data):
-        """create an instance of Alfalfa"""
+        """create an instance of Alfalfa
+                    
+           Args: 
+            crop_name (str): the name of the crop
+            data (dict): data used to construct the class
+        """
         super().__init__()
 
         # get alfalfa variables from input data:
