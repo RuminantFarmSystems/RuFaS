e674a5447e4ac8841b39317740f9635066972ab3
Brandon-DeBoer <Brandon-DeBoer@usda.gov>
2022-09-14 16:59:28 -0500

    added tests to test_biomass.py

--- tests/tests_SoilCrop/tests_crop/test_biomass.py
+++ tests/tests_SoilCrop/tests_crop/test_biomass.py
@@ -7,6 +7,7 @@ Author(s): Brandon DeBoer, brdeboer@wisc.edu
 
 from RUFAS.routines.field.crop.crop_types import base_crop
 from RUFAS.routines.field.soil import soil
+from RUFAS.classes import Weather, Time
 from RUFAS import *
 from RUFAS.routines.field.crop.biomass import *
 
@@ -46,12 +47,45 @@ def mock_crop(fr_root=.38, biomass_actual=4.6, kl=2.1, LAI_actual=3.4, RUE=1.5, 
     return mcrop
 
 
-def mock_weather():
-    pass
+def mock_weather(radiation = [[3.5]]):
+    """
+    Description: 
+        Creates a Weather class mocking object for use as input for functions. It is initialized with the
+        arguments provided; arguments are dynamic and can be changed/added to as needed.
+
+    Args:
+        radiation (float): Weather attribute, measured amount of radiation
+
+    Return:
+        a Time mocking object instantiated with the provided arguments 
+    """
+    mweather = MagicMock(Weather)
+
+    mweather.radiation = radiation
+    
+    return mweather
 
 
-def mock_time():
-    pass
+
+def mock_time(day = 1, year = 1):
+    """
+    Description: 
+        Creates a Time class mocking object for use as input for functions. It is initialized with the
+        arguments provided; arguments are dynamic and can be changed/added to as needed.
+
+    Args:
+        year (int): Time attribute, current year in simulation
+        day (int): Time attribute, current day in the simulation
+
+    Return:
+        a Time mocking object instantiated with the provided arguments 
+    """
+    mtime = MagicMock(Time)
+
+    mtime.year = year
+    mtime.day = day
+
+    return mtime
 
 
 def mock_soil(ET_max_annual=1.5, evap_annual=2.1, trans_annual=1.3):
@@ -151,3 +185,85 @@ def test_calc_gamma_wu_returns_zero_with_ET_max_as_zero():
     soil = mock_soil(ET_max_annual=0.0)
 
     assert pytest.approx(calc_gamma_wu(soil, crop)) == 0
+
+
+#calc_intercepted_radiation()
+def test_calc_intercepted_radiation_correctly_calculates_active_radiation():
+    """
+    Description:
+        Unittest for calc_intercepted_radiation() in routines/field/crop/biomass.py. Checks
+        that the function calculates and returns the correct value for intercepted radiation
+        on the provided day and year of the simulation
+    """
+    crop = mock_crop()
+    weather = mock_weather()
+    time = mock_time()
+
+    assert pytest.approx(calc_intercepted_radiation(crop,weather,time)) \
+                     == 0.5 * 3.5 * (1 - exp(-1 * 2.1 * 3.4))
+
+#calc_act_biomass()...assumes dependencies work properly
+def test_calc_act_biomass_correctly_calculates_d_biomass_max():
+    """
+    Description:
+        Unittest for calc_act_biomass in routines/field/crop/biomass.py. Checks
+        that the function correctly updates the BaseCrop attribute d_biomass_max.
+    """
+    crop = mock_crop()
+    weather = mock_weather()
+    time = mock_time()
+
+    H_phosyn = calc_intercepted_radiation(crop,weather,time)
+
+    calc_act_biomass(crop,weather,time)
+
+    assert pytest.approx(crop.d_biomass_max) == 1.5 * H_phosyn
+
+def test_calc_act_biomass_correctly_calculates_d_biomass_actual():
+    """
+    Description:
+        Unittest for calc_act_biomass in routines/field/crop/biomass.py. Checks
+        that the function correctly updates the BaseCrop attribute d_biomass_actual.
+    """
+    crop = mock_crop()
+    weather = mock_weather()
+    time = mock_time()
+
+    H_phosyn = calc_intercepted_radiation(crop,weather,time)
+
+    calc_act_biomass(crop,weather,time)
+
+    assert pytest.approx(crop.d_biomass_actual) == (1.5 * H_phosyn) * .7
+
+def test_calc_act_biomass_correctly_calculates_prev_biomass_actual():
+    """
+    Description:
+        Unittest for calc_act_biomass in routines/field/crop/biomass.py. Checks
+        that the function correctly updates the BaseCrop attribute pre_biomass_actual.
+    """
+    crop = mock_crop()
+    weather = mock_weather()
+    time = mock_time()
+
+    calc_act_biomass(crop,weather,time)
+
+    assert pytest.approx(crop.prev_biomass_actual) == 4.6
+
+def test_calc_biomass_correctly_calculates_biomass_actual():
+    """
+    Description:
+        Unittest for calc_act_biomass in routines/field/crop/biomass.py. Checks
+        that the function correctly updates the BaseCrop attribute biomass_actual.
+    """
+    crop = mock_crop()
+    weather = mock_weather()
+    time = mock_time()
+
+    H_phosyn = calc_intercepted_radiation(crop,weather,time)
+
+    calc_act_biomass(crop,weather,time)
+
+    assert pytest.approx(crop.biomass_actual) == 4.6 + (1.5 * H_phosyn) * .7
+
+
+
