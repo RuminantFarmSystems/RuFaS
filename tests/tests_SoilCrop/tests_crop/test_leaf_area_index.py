d74c7f9915d195cd5ae456c3ba6ca3c73cac4e46
Brandon-DeBoer <Brandon-DeBoer@usda.gov>
2022-10-28 11:16:56 -0500

    parametrized test_leaf_area_index.py

--- tests/tests_SoilCrop/tests_crop/test_leaf_area_index.py
+++ tests/tests_SoilCrop/tests_crop/test_leaf_area_index.py
@@ -7,132 +7,114 @@ Author(s): Brandon DeBoer, brdeboer@wisc.edu
 
 from RUFAS.routines.field.crop.crop_types import base_crop
 from RUFAS.routines.field.crop.leaf_area_index import *
+from tests.tests_SoilCrop.mock_classes import *
 from unittest.mock import MagicMock
 import pytest
 
 from math import exp, log, sqrt
 
 
-def mock_crop(fr_PHU_1=0.15, fr_LAI_1=0.01, fr_PHU_2=0.50, fr_LAI_2=0.95,
-              fr_LAI_max=0.95, fr_PHU=0.75, LAI_actual=4.3, LAI_max=7.0, prev_fr_LAI_max=.87,
-              fr_PHU_sen=0.90, gamma_reg=.7):
+#TODO: add exception raising conditions to original function in leaf_area_index.py
+#to ensure no div by 0 and input for logs is valid
+
+@pytest.mark.parametrize(("fr_PHU_1","fr_LAI_1","fr_PHU_2","fr_LAI_2"),[
+    (.15, .01, .50, .95),
+    (.45, .08, .39,.70), 
+    (1.0, 0.47, .95, 0.5)
+])
+def test_calculate_shape_coefficients(fr_PHU_1: float, fr_LAI_1: float, fr_PHU_2: float, fr_LAI_2: float) -> None:
     """
     Description:
-        Creates a BaseCrop class mocking object for use as input for functions. It is initialized with the
-        arguments provided; arguments are dynamic and can be changed/added to as needed.
-
-    Args:
-        fr_PHU_1 (float)
-        fr_LAI_1 (float)
-        fr_PHU_2 (float)
-        fr_LAI_2 (float)
-        fr_LAI_max (float)
-        fr_PHU (float)
-        LAI_actual (float)
-        LAI_max (float)
-        prev_fr_LAI_max (float)
-        fr_PHU_sen (float)
-        gamma_reg (float)
-
-    Return:
-        returns a BaseCrop mocking object instantiated with the provided arguments 
+        Unit test for calculate_shape_coefficients() in routines/field/crop/leaf_area_index.py
     """
-    mcrop = MagicMock(base_crop.BaseCrop)
 
-    mcrop.fr_PHU_1 = fr_PHU_1
-    mcrop.fr_LAI_1 = fr_LAI_1
-    mcrop.fr_PHU_2 = fr_PHU_2
-    mcrop.fr_LAI_2 = fr_LAI_2
-    mcrop.fr_LAI_max = fr_LAI_max
-    mcrop.fr_PHU = fr_PHU
-    mcrop.LAI_actual = LAI_actual
-    mcrop.LAI_max = LAI_max
-    mcrop.prev_fr_LAI_max = prev_fr_LAI_max
-    mcrop.fr_PHU_sen = fr_PHU_sen
-    mcrop.gamma_reg = gamma_reg
+    crop = mock_crop(fr_PHU_1 = fr_PHU_1, fr_LAI_1 = fr_LAI_1, fr_PHU_2 = fr_PHU_2, fr_LAI_2 = fr_LAI_2)
 
-    return mcrop
+    #L2 expected
+    L2_part1 = (fr_PHU_1 / fr_LAI_1) - fr_PHU_1
+    L2_part2 = (fr_PHU_2 / fr_LAI_2) - fr_PHU_2
 
+    L2 = (log(L2_part1) - log(L2_part2)) / (fr_PHU_2 - fr_PHU_1)
 
-# for testing L1, assume L2 is correct
-def test_calculate_shape_coefficients_correctly_calculates_L1():
-    crop = mock_crop()
-    L1, L2 = calculate_shape_coefficients(crop)
-    assert pytest.approx(L1) == log((0.15 / 0.01) - 0.15) + (L2 * 0.15)
+    #L1 expected, uses L2 in its calculation
+    L1 = log((fr_PHU_1 / fr_LAI_1) - fr_PHU_1) + (L2 * fr_PHU_1)
+
+    #assertion
+    assert pytest.approx(calculate_shape_coefficients(crop)) == [L1,L2]
 
 
-# testing L2 individually
-def test_calculate_shape_coefficients_correctly_calculates_L2():
-    crop = mock_crop()
-    L1, L2 = calculate_shape_coefficients(crop)
-    assert pytest.approx(L2) == (log((0.15 / 0.01) - 0.15) - log((0.50 / 0.95) - 0.50)) / (0.50 - 0.15)
+
+@pytest.mark.parametrize(("fr_PHU_1","fr_LAI_1","fr_PHU_2","fr_LAI_2","fr_LAI_max","fr_PHU","prev_fr_LAI_max"),[
+    (.15, .01, .50, .95, .95, .75, .87),
+    (.45, .08, .39,.70, .62, .81, .64), 
+    (1.0, 0.47, .95, 0.5, .20, .41, .41)
+])
+def test_calc_fr_LAI_max(fr_PHU_1, fr_LAI_1, fr_PHU_2, fr_LAI_2, fr_LAI_max, fr_PHU, prev_fr_LAI_max) -> None:
+    """Ensures that prev_fr_LAI_max and fr_LAI_max are calculated correctly in calc_fr_LAI_max"""
+
+    crop = mock_crop(fr_PHU_1 = fr_PHU_1, fr_LAI_1 = fr_LAI_1, fr_PHU_2 = fr_PHU_2, fr_LAI_2 = fr_LAI_2, \
+        fr_LAI_max = fr_LAI_max,fr_PHU = fr_PHU,prev_fr_LAI_max = prev_fr_LAI_max)
+
+    #get L1,L2
+    L1,L2 = calculate_shape_coefficients(crop)
+
+    #expected
+    previous_fr_max = fr_LAI_max
+
+    exponent = exp(L1 - (L2 * fr_PHU))
+    LAI_max = fr_PHU / (fr_PHU + exponent)
+
+    #assert function output/result is same as expected result
+    calc_fr_LAI_max(crop,L1,L2)
+    PREV = crop.prev_fr_LAI_max
+    MAX = crop.fr_LAI_max
+
+    assert pytest.approx([PREV,MAX]) == [previous_fr_max,LAI_max]
 
 
-def test_calculate_shape_coefficients_correctly_calculates_L1_and_L2():
-    crop = mock_crop()
-    L1, L2 = calculate_shape_coefficients(crop)
-    tests = [
-        pytest.approx(L1) == log((0.15 / 0.01) - 0.15) + (L2 * 0.15),
-        pytest.approx(L2) == (log((0.15 / 0.01) - 0.15) - log((0.50 / 0.95) - 0.50)) / (0.50 - 0.15)
-    ]
-    assert all(tests)
+
+@pytest.mark.parametrize(("d_LAI_max","gamma_reg"),[
+    (.55,.7), #arbitrary
+    (0,0), #zeroes
+    (1,1), #ones
+    #(-1,-1), #negative vals
+    (.8,.1) #arbitrary
+])
+def test_calculuate_d_LAI_actual(d_LAI_max, gamma_reg):
+    """ensures that change in LAI actual (d_LAI_actual) is calculated correctly"""
+
+    crop = mock_crop(gamma_reg = gamma_reg)
+
+    #expected value [C.8.A.5]
+    d_actual = d_LAI_max * sqrt(gamma_reg)
+
+    #assert actual result is same as expected
+    assert pytest.approx(calculate_d_LAI_actual(crop,d_LAI_max)) == d_actual
 
 
-# the following tests are for the calc_fr_LAI_max() function
-# assumes that L1 and L2 (output of shape coefficient function) are correct
-def test_calc_fr_LAI_max_correctly_sets_prev_fr_LAI_max():
-    crop = mock_crop()
-    L1, L2 = calculate_shape_coefficients(crop)
-    calc_fr_LAI_max(crop, L1, L2)
-    assert pytest.approx(crop.prev_fr_LAI_max) == 0.95
+
+@pytest.mark.parametrize(("LAI_actual","LAI_max","fr_LAI_max","prev_fr_LAI_max","fr_PHU","fr_PHU_sen","gamma_reg"),
+[(4.3, 7.0, .95, .87, .9, .7,.1), #fr_PHU > fr_PHU_sen
+ (1 ,1, 1, 1, 1, 1, 1), #all ones
+ (3.7, 5.6, .76, .59, .48, .63, .25), #fr_PHU < fr_PHU_sen
+ (3.7, 5.6, .76, .59, .63, .63, .25) #fr_PHU = fr_PHU_sen
+])
+def test_calculate_LAI_actual(LAI_actual, LAI_max, fr_LAI_max, prev_fr_LAI_max, fr_PHU, fr_PHU_sen, gamma_reg):
+    '''ensures that LAI_actual is correctly calculated in calculate_LAI_actual()'''
+    
+    crop = mock_crop(LAI_actual = LAI_actual, LAI_max = LAI_max,fr_LAI_max = fr_LAI_max, 
+    prev_fr_LAI_max = prev_fr_LAI_max,fr_PHU = fr_PHU, fr_PHU_sen = fr_PHU_sen, gamma_reg = gamma_reg)
+
+    #expected value [C.8.A.5/6]
+    delta_LAI_max = (fr_LAI_max - prev_fr_LAI_max) * LAI_max * (1 - exp(5 * (LAI_actual - LAI_max)))
+    delta_LAI_actual =  calculate_d_LAI_actual(crop, delta_LAI_max)
+
+    if fr_PHU <= fr_PHU_sen:
+        LAI_actual = max(LAI_actual + delta_LAI_actual, 0)
+    else:
+        LAI_actual = max(LAI_actual * ((1-fr_PHU) / (1- fr_PHU_sen)) , 0)
 
 
-def test_calc_fr_LAI_max_correctly_sets_fr_LAI_max():
-    crop = mock_crop()
-    L1, L2 = calculate_shape_coefficients(crop)
-    calc_fr_LAI_max(crop, L1, L2)
-    assert pytest.approx(crop.fr_LAI_max) == 0.75 / (0.75 + exp(L1 - (L2 * 0.75)))
-
-
-def test_calc_fr_LAI_max_all_correct():
-    crop = mock_crop()
-    L1, L2 = calculate_shape_coefficients(crop)
-    calc_fr_LAI_max(crop, L1, L2)
-
-    tests = [
-        pytest.approx(crop.prev_fr_LAI_max) == 0.95,
-        pytest.approx(crop.fr_LAI_max) == 0.75 / (0.75 + exp(L1 - (L2 * 0.75)))
-    ]
-    assert all(tests)
-
-
-# the following tests are for the calculate_d_LAI_actual() function:
-def test_calculate_d_LAI_actual_correctly_calculates_change_in_LAI_actual():
-    crop = mock_crop()
-    assert pytest.approx(calculate_d_LAI_actual(crop, .55)) == 0.55 * sqrt(0.7)
-
-
-# the following tests are for the calculate_LAI_actual() function
-# there are 2 cases:when fr_PHU <= fr_PHU_sen and when fr_PHU > fr_PHU_sen
-# assume calculate_d_LAI_actual works properly and is correct
-
-# case 1: fr_PHU <= fr_PHU_sen
-def test_calculate_LAI_actual_sets_LAI_actual_correctly():
-    crop = mock_crop()
+    #check that actual val is same as expected
     calculate_LAI_actual(crop)
-
-    # first need to calculate d_LAI_actual
-    d_LAI_max = (.95 - .87) * 7.0 * (1 - exp(5 * (4.3 - 7.0)))
-    d_LAI_actual = calculate_d_LAI_actual(crop, d_LAI_max)
-
-    assert pytest.approx(crop.LAI_actual) == max(4.3 + d_LAI_actual, 0)
-
-    # case 2: fr_PHU > fr_PHU_sen
-    crop = mock_crop(fr_PHU=.9, fr_PHU_sen=.7)
-    calculate_LAI_actual(crop)
-
-    # first need to calculate d_LAI_actual
-    d_LAI_max = (.95 - .87) * 7.0 * (1 - exp(5 * (4.3 - 7.0)))
-    d_LAI_actual = calculate_d_LAI_actual(crop, d_LAI_max)
-
-    assert pytest.approx(crop.LAI_actual) == max((4.3 * (1 - .9) / (1 - .7)), 0)
+    assert pytest.approx(crop.LAI_actual) == LAI_actual
