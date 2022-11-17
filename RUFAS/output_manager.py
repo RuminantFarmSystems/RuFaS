fcedb170f0ad2ccd19d16aac38cba11f78636a84
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2022-11-16 22:22:30 -0500

    Update output_manager.py

--- RUFAS/output_manager.py
+++ RUFAS/output_manager.py
@@ -6,7 +6,7 @@ import time
 
 class OutputManager (object):
     """
-    Output manager for Rufas simulation results. Works by collecting variables
+    Output manager for RufaS simulation results. Works by collecting variables
     into the pool, and populates requested output channels from the pool once
     the simulation is done. Warnings and errors are added to their own pools.
 
@@ -18,17 +18,23 @@ class OutputManager (object):
         Contains warnings reported from simulation engine
     errors_pool : Dict[str, Any]
         Contains errors reported from simulation engine
+    logs_pool : Dict[str, Any]
+        Contains logs reported from simulation engine
     """
 
     def __init__(self) -> None:
         self.pool: Dict[str, Any] = {}
         self.warnings_pool: Dict[str, Any] = {}
         self.errors_pool: Dict[str, Any] = {}
+        self.logs_pool: Dict[str, Any] = {}
+        self.add_log("Output Manager instantiated.",
+                     caller_class=self.__class__.__name__,
+                     caller_function=self.__init__.__name__)
 
-    def add_to_pool(self, name: str, value: Any,
+    def add_variable(self, name: str, value: Any,
                     **kwargs: Dict[str, Union[str, bool]]) -> None:
         """
-        Adds a variable to the output pool.
+        Adds a variable to the pool.
 
         Parameters
         ----------
@@ -54,15 +60,15 @@ class OutputManager (object):
             Has no effect on manual suffix overrides.
         kwargs['enforce_override'] : bool, optional
             If present and True, overrides the existing value in the pool if a
-            name collision happens.
+            key collision happens.
         kwargs['fail_silently'] : bool, optional
-            If present and True, suppresses raising error if a name collision
+            If present and True, suppresses raising error if a key collision
             happens.
 
         Raises
         ------
         ValueError
-            If a name collision happens in the pool and either
+            If a key collision happens in the pool and either
             **kwargs['fail_silently'] or **kwargs['enforce_override'] 
             are not set to True.
         """
@@ -80,7 +86,7 @@ class OutputManager (object):
             return
         if kwargs.get('fail_silently', False):
             self.add_warning(
-                "Key collision happened; the value was overriden per given flag.",
+                "Key collision happened; the event was ignored per given flag.",
                 self.__class__.__name__, function_name, [key], kwargs)
             return
         raise ValueError(f"Key {key} already exists in the pool." +
@@ -88,8 +94,29 @@ class OutputManager (object):
                          "or turn the automated prefix/suffix generation on"
                          )
 
-    def add_warning(self, msg: str, caller_class: str, caller_function: str,
-                    *args: List[Any], **kwargs: Dict[Any, Any]) -> None:
+    def add_log(
+            self, msg: str, *args: List[Any], **kwargs: Dict[Any, Any]) -> None:
+        """
+        Adds a log message to the pool of logs.
+
+        Parameters
+        ----------
+        msg : str
+            The log message to be added to the pool
+        *args: List[Any]
+            Contains any additional information to be added to the message  
+        **kwargs: Dict[Any, Any]
+            Additional args, some are non-optional
+        kwargs['caller_class'] : str
+            The name of the class which called this function
+        kwargs['caller_function'] : str
+            The name of the function which called this function 
+        """
+        key = self.__generate_key('log_entry', kwargs)
+        self.logs_pool[key] = {'msg': msg, 'args': args, 'kwargs': kwargs}
+
+    def add_warning(
+            self, msg: str, *args: List[Any], **kwargs: Dict[Any, Any]) -> None:
         """
         Adds a warning message to the pool of warnings.
 
@@ -97,16 +124,17 @@ class OutputManager (object):
         ----------
         msg : str
             The warning message to be added to the pool
-        caller_class : str
-            The name of the class which called this function
-        caller_function : str
-            The name of the function which called this function    
         *args: List[Any]
             Contains any additional information to be added to the message  
         **kwargs: Dict[Any, Any]
-            Contains any additional information to be added to the message
+            Additional args, some are non-optional
+        kwargs['caller_class'] : str
+            The name of the class which called this function
+        kwargs['caller_function'] : str
+            The name of the function which called this function 
         """
-        pass
+        key = self.__generate_key('warning_entry', kwargs)
+        self.warnings_pool[key] = {'msg': msg, 'args': args, 'kwargs': kwargs}
 
     def add_error(self, msg: str, caller_class: str, caller_function: str,
                   *args: List[Any], **kwargs: Dict[Any, Any]) -> None:
@@ -117,16 +145,17 @@ class OutputManager (object):
         ----------
         msg : str
             The error message to be added to the pool
-        caller_class : str
-            The name of the class which called this function
-        caller_function : str
-            The name of the function which called this function    
         *args: List[Any]
             Contains any additional information to be added to the message  
         **kwargs: Dict[Any, Any]
-            Contains any additional information to be added to the message
+            Additional args, some are non-optional
+        kwargs['caller_class'] : str
+            The name of the class which called this function
+        kwargs['caller_function'] : str
+            The name of the function which called this function 
         """
-        pass
+        key = self.__generate_key('error_entry', kwargs)
+        self.errors_pool[key] = {'msg': msg, 'args': args, 'kwargs': kwargs}
 
     def __generate_key(self, name: str,
                        **kwargs: Dict[str, Union[str, bool]]) -> str:
