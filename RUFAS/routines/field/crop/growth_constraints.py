dbf201970cf93bccbe6dd023cff060c0aae9bfb0
rhensen <hensenreid@gmail.com>
2022-12-06 11:15:47 -0700

    adding testing files

--- RUFAS/routines/field/crop/growth_constraints.py
+++ RUFAS/routines/field/crop/growth_constraints.py
@@ -30,12 +30,10 @@ def update_growth_factor(crop, soil, weather, time) -> None:
     n_stress_factor = calc_nutrient_stress_scaling_factor(stored=crop.bio_N, optimal=crop.bio_N_opt)
     #print('n_stress', n_stress_factor)
     #print('time',time.index,'bio_n:',crop.bio_N,'bio_n_opt',crop.bio_N_opt)
-    #n_stress_factor = 100
     n_stress = calc_nutrient_stress(optimal=crop.bio_N_opt, stress_factor=n_stress_factor)
-    n_stress= 1
     p_stress_factor = calc_nutrient_stress_scaling_factor(stored=crop.bio_P, optimal=crop.bio_P_opt)
     p_stress = calc_nutrient_stress(optimal=crop.bio_P_opt, stress_factor=p_stress_factor)
-    #print("time",time.index,"stressors",[w_stress, t_stress, n_stress, p_stress])
+    # print("time",time.index,"stressors",[w_stress, t_stress, n_stress, p_stress])
     crop.gamma_reg = calc_growth_factor(w_stress, t_stress, n_stress, p_stress)
     #crop.gamma_reg =1
 
