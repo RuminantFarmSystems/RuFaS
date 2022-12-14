79fed6e1c416239280e8df094345c7fdec95fb45
Ed Hansen <ed.hansen@mlit.wisc.edu>
2022-12-14 14:39:45 -0600

    small changes in WaterDynamics

--- SC_redesign/Crop_and_Soil/tests_SC/test_water_dynamics.py
+++ SC_redesign/Crop_and_Soil/tests_SC/test_water_dynamics.py
@@ -15,7 +15,7 @@ from SC_redesign.Crop_and_Soil.crop.water_dynamics import *
 ])
 def test_calc_evapotranspiration(evap, trans):
     """ensure that evapotranspiration is correclty calculated"""
-    assert calc_evapotranspiration(evap, trans) == evap + trans
+    assert WaterDynamics.calc_evapotranspiration(evap, trans) == evap + trans
 
 
 @pytest.mark.parametrize("et,max_et", [
@@ -35,7 +35,7 @@ def test_calc_water_deficiency(et, max_et):
         expect = 0
     else:
         expect = 100 * (et / max_et)
-    assert calc_water_deficiency(et, max_et) == expect
+    assert WaterDynamics.calc_water_deficiency(et, max_et) == expect
 
 
 # ---- initialization functions (reusable) ----
@@ -56,20 +56,20 @@ def init_water_dyn(**kwargs):
 
 
 # ---- member function tests ----
-@pytest.mark.parametrize("evap", [0, 1, .5, 0.25, 0.75, 1000, 1563.29])
-def test_set_evaporation(evap):
-    """check that evaporation is properly set"""
-    water_dyn = init_water_dyn()
-    water_dyn.set_evaporation(evap)
-    assert water_dyn.evaporation == evap
-
-
-@pytest.mark.parametrize("trans", [0, 1, .5, 0.25, 0.75, 1000, 1563.29])
-def test_set_transpiration(trans):
-    """check that transpiration is properly set"""
-    water_dyn = init_water_dyn()
-    water_dyn.set_transpiration(trans)
-    assert water_dyn.transpiration == trans
+# @pytest.mark.parametrize("evap", [0, 1, .5, 0.25, 0.75, 1000, 1563.29])
+# def test_set_evaporation(evap):
+#     """check that evaporation is properly set"""
+#     water_dyn = init_water_dyn()
+#     water_dyn.set_evaporation(evap)
+#     assert water_dyn.evaporation == evap
+#
+#
+# @pytest.mark.parametrize("trans", [0, 1, .5, 0.25, 0.75, 1000, 1563.29])
+# def test_set_transpiration(trans):
+#     """check that transpiration is properly set"""
+#     water_dyn = init_water_dyn()
+#     water_dyn.set_transpiration(trans)
+#     assert water_dyn.transpiration == trans
 
 
 @pytest.mark.parametrize("max", [0, 1, .5, 0.25, 0.75, 1000, 1563.29])
@@ -90,7 +90,7 @@ def test_update_evapotranspiration(evap, trans):
     """check that evapotranspiration is properly updated"""
     water_dyn = init_water_dyn(evaporation=evap, transpiration=trans)
     water_dyn.update_evapotranspiration()
-    assert water_dyn.evapotranspiration == calc_evapotranspiration(evap, trans)
+    assert water_dyn.evapotranspiration == WaterDynamics.calc_evapotranspiration(evap, trans)
 
 
 @pytest.mark.parametrize("et,max_et", [
@@ -108,7 +108,7 @@ def test_assess_water_deficiency(et, max_et):
     """check that water deficiency is properly updated"""
     water_dyn = init_water_dyn(evapotranspiration=et, evapotranspiration_max=max_et)
     water_dyn.assess_water_deficiency()
-    assert water_dyn.water_deficiency == calc_water_deficiency(et, max_et)
+    assert water_dyn.water_deficiency == WaterDynamics.calc_water_deficiency(et, max_et)
 
 
 @pytest.mark.parametrize("evap,trans,et_max", [
@@ -125,5 +125,5 @@ def test_cycle_water(evap, trans, et_max):
     water_dyn.cycle_water(evap, trans, et_max)
     expected = [water_dyn.evaporation, water_dyn.transpiration, water_dyn.evapotranspiration,
                 water_dyn.evapotranspiration_max, water_dyn.water_deficiency]
-    observed = [evap, trans, evap+trans, et_max, calc_water_deficiency(evap+trans, et_max)]
+    observed = [evap, trans, evap+trans, et_max, WaterDynamics.calc_water_deficiency(evap+trans, et_max)]
     assert expected == observed
