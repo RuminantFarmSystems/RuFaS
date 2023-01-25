809afbc366b9229f8e5fe5dac4a87c320275c91b
MatthewLiu <matthew7838@gmail.com>
2023-01-25 09:00:12 -0600

    shift_phosphorus_time() done and passed

--- SC_redesign/Crop_and_Soil/tests_SC/test_phosphorus_incorporation.py
+++ SC_redesign/Crop_and_Soil/tests_SC/test_phosphorus_incorporation.py
@@ -1,8 +1,21 @@
+import pytest
+
 from SC_redesign.Crop_and_Soil.crop.phosphorus_incorporation import PhosphorusIncorporation
 from SC_redesign.Crop_and_Soil.crop.crop_data import CropData
 
 
-def test_shift_phosphorus_time():
+@pytest.mark.parametrize("old,new", [
+    (None, 1),  # no start
+    (0, 1),  # start = 0
+    (1, 2),  # start = 0
+    (2, 1),  # start > new
+    (133.26, 149.4)  # arbitrary
+])
+def test_shift_phosphorus_time(old, new):
+    data = CropData(previous_phosphorus=old, phosphorus=new)
+    incorp = PhosphorusIncorporation(data)
+    incorp.shift_phosphorus_time()
+    assert data.previous_phosphorus == new
     pass
 
 
