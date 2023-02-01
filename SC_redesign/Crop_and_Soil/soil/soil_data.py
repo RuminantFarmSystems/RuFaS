8bcb3228ec29f9c8f3c6c53765b54af9ddde98c6
Ed Hansen <ed.hansen@mlit.wisc.edu>
2023-02-01 15:58:44 -0600

    ready for first review

--- SC_redesign/Crop_and_Soil/soil/soil_data.py
+++ SC_redesign/Crop_and_Soil/soil/soil_data.py
@@ -5,6 +5,9 @@ from SC_redesign.Crop_and_Soil.soil.layer_data import LayerData
 
 @dataclass
 class SoilData:
+    soil_layers: Optional[List[LayerData]] = None
+    """list of soil layer data objects, top layer is 0th element, bottom is nth element"""
+
     # ---- evapotranspiration
     potential_evapotranspiration: Optional[float] = None
     """potential evapotranspiration for a given day (mm per day)"""
@@ -17,10 +20,62 @@ class SoilData:
     """maximum amount of evaporation from soil on a given day adjusted for plant use (mm)"""
     maximum_soil_evaporation: Optional[float] = None
     """maximum amount of evaporation from soil on a given day (mm)"""
-    soil_layers: Optional[List[LayerData]] = None
-    """list of soil layer data objects, top layer is 0th, bottom is nth"""
 
     # ---- infiltration
-    curve_number_2: float = 40  # arbitrary
-    """Part of SCS Curve Number Procedure for runoff (SWAT 2:1.1)"""
+    previous_retention_parameter: Optional[float] = None
+    """retention parameter for the previous day (mm) (used in SWAT 2:1.1.9)"""
+    average_slope_fraction: float = 0.05
+    """average slope fraction of the subbasin"""
+    moisture_condition_parameter: Optional[float] = None
+    """curve number value adjusted for moisture content (unitless) (SWAT 2:1.1.11)"""
 
+    @property
+    def profile_soil_water_content(self) -> float:
+        """
+
+        Returns: amount of water in the entire soil profile (excluding the amount of water held in the profile at the
+            wilting point) in mm
+
+        Details: this method for calculating total soil water content assumes the lower bound of water to be 0. It also
+            calculates the soil water content per layer, meaning that if one layer has water content greater than its
+            wilting water point and another layer has water content less than its wilting point, the first layer will
+            not have to compensate for the deficit in the second layer.
+
+        """
+        if self.soil_layers is None:
+            return 0
+        else:
+            water_sum = 0
+            for layer in self.soil_layers:
+                water_sum += min(0, (layer.soil_water_content - layer.wilting_point_water_content))
+            return
+
+    @property
+    def profile_saturation(self) -> float:
+        """
+
+        Returns: amount of water in the soil profile when completely saturated in mm
+
+        """
+        if self.soil_layers is None:
+            return 0
+        else:
+            saturation_sum = 0
+            for layer in self.soil_layers:
+                saturation_sum += layer.saturation
+            return saturation_sum
+
+    @property
+    def profile_field_capacity(self) -> float:
+        """
+
+        Returns: total amount of water contained in the soil profile at field capacity (but not saturated) in mm
+
+        """
+        if self.soil_layers is None:
+            return 0
+        else:
+            field_capacity_sum = 0
+            for layer in self.soil_layers:
+                field_capacity_sum += layer.field_capacity_water_content
+            return field_capacity_sum
