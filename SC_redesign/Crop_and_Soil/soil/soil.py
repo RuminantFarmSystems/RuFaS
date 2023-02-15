af50b32f8e3c2f068cd1f4fa2cb7061335a7b7e7
Ed Hansen <isgotkowitz@gmail.com>
2023-02-15 11:02:15 -0600

    added daily update routines for water and non-water attributes

--- SC_redesign/Crop_and_Soil/soil/soil.py
+++ SC_redesign/Crop_and_Soil/soil/soil.py
@@ -25,5 +25,52 @@ class Soil:
         Warning("create from config file not yet implement, returning default Soil()")
         return Soil()
 
-    def daily_non_water_update(self) -> None:
-        return
+    def daily_soil_routine(self, solar_radiation: float, avg_temp: float, min_temp: float, max_temp: float,
+                           plant_cover: float, snow_cover: float, avg_annual_air_temp: float) -> None:
+        """this method calls all now-water related daily update routines
+
+        Args:
+            solar_radiation: solar radiation reaching the ground on the current day (MJ per square meter per day)
+            avg_temp: average temperature of the current day (degrees C)
+            min_temp: minimum temperature of the current day (degrees C)
+            max_temp: maximum temperature of the current day (degrees C)
+            plant_cover: total aboveground plant biomass and residue on the current day (kg per hectare)
+            snow_cover: water content of the snow cover on the current day (mm)
+            avg_annual_air_temp: average annual air temperature (degrees C)
+
+        """
+        self.soil_temp.daily_soil_temperature_update(solar_radiation, avg_temp, min_temp, max_temp, plant_cover,
+                                                     snow_cover, avg_annual_air_temp)
+
+    def daily_soil_water_routine(self, rainfall: float, weighting_coefficient: float,
+                                 has_seasonal_high_water_table: bool, extraterrestrial_radiation: float,
+                                 max_air_temp: float, min_air_temp: float,
+                                 avg_air_temp: float, above_ground_biomass: float, residue: float,
+                                 snow_water_content: float, initial_canopy_free_water: float,
+                                 minimum_cover_management_factor: float) -> None:
+        """this method calls all water related daily update routines
+
+        Args:
+            rainfall: rainfall depth of current day (mm)
+            weighting_coefficient: weighting coefficient used to calculate retention coefficient for daily curve number
+                calculations dependent on plant evapotranspiration (unitless)
+            has_seasonal_high_water_table: if the HRU has a seasonal high water table (true/false)
+            extraterrestrial_radiation: radiation from the aliens, in MJ per square meter per day
+                TODO: better description
+            max_air_temp: maximum air temperature (degrees C)
+            min_air_temp: minimum air temperature (degrees C)
+            avg_air_temp: average air temperature (degrees C)
+            above_ground_biomass: mass of plant above ground (kg per hectare)
+            residue: biomass separated from plant on the ground (kg per hectare)
+            snow_water_content: amount of water from snow (mm)
+            initial_canopy_free_water: initial amount of free water held in canopy on a given day (mm)
+            minimum_cover_management_factor: minimum value for cover and management factor for water erosion applicable
+                to land cover/plant (unitless)
+
+        """
+        self.infiltration.infiltrate(rainfall, weighting_coefficient)
+        self.percolation.percolate(has_seasonal_high_water_table)
+        self.evapotranspiration.evapotranspirate(extraterrestrial_radiation, max_air_temp, min_air_temp, avg_air_temp,
+                                                 above_ground_biomass, residue, snow_water_content,
+                                                 initial_canopy_free_water)
+        self.soil_erosion.erode(minimum_cover_management_factor, residue)
