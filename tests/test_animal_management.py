10e21b579fca8f7de47ab02e40c23fed43ec33bd
lpham <98855117+ltp23@users.noreply.github.com>
2023-03-01 06:38:34 -0800

    Added more unit tests for allocate pens helper methods

--- tests/test_animal_management.py
+++ tests/test_animal_management.py
@@ -17,6 +17,9 @@ from typing import List, Dict, Union
 import io
 import sys
 
+from RUFAS.routines.animal.life_cycle.cow import Cow
+from RUFAS.routines.animal.pen import Pen
+
 
 def create_mock_object_list(attribute_dicts: List[Dict]) -> List[MagicMock]:
     mock_object_list = []
@@ -476,3 +479,347 @@ def test_get_life_cycle_output():
 def test_get_initialize_db_summary():
     """Unit test for function get_initialize_db_summary in file routines/animal/animal_management.py"""
     pass
+
+
+def test_get_lactating_cows(mocker: MockerFixture) -> None:
+    """Unit test for function _get_lactating_cows in file routines/animal/animal_management.py"""
+    # Arrange
+    num_cows = 10
+    num_lactating_cows = 5
+    cows: List[Cow] = []
+    for i in range(num_cows):
+        cow = mocker.MagicMock(spec=Cow)
+        cow.milking = i % 2 == 0
+        cows.append(cow)
+
+    # Act
+    lactating_cows = AnimalManagement._get_lactating_cows(cows)
+
+    # Assert
+    assert len(lactating_cows) == num_lactating_cows
+    for i in range(num_lactating_cows):
+        assert lactating_cows[i] == cows[i * 2]
+
+
+def test_get_dry_cows(mocker: MockerFixture) -> None:
+    """Unit test for function _get_dry_cows in file routines/animal/animal_management.py"""
+    # Arrange
+    num_cows = 10
+    num_dry_cows = 5
+    cows: List[Cow] = []
+    for i in range(num_cows):
+        cow = mocker.MagicMock(spec=Cow)
+        cow.milking = i % 2 == 0
+        cows.append(cow)
+
+    # Act
+    dry_cows = AnimalManagement._get_dry_cows(cows)
+
+    # Assert
+    assert len(dry_cows) == num_dry_cows
+    for i in range(num_dry_cows):
+        assert dry_cows[i] == cows[i * 2 + 1]
+
+
+def test_group_pens_by_animal_combination(mocker: MockerFixture) -> None:
+    """Unit test for function group_pens_by_animal_combination in file routines/animal/animal_management.py"""
+    # Arrange
+    pens: List[Pen] = []
+    animal_combinations = [
+        Pen.AnimalCombination.CALF,
+        Pen.AnimalCombination.GROWING,
+        Pen.AnimalCombination.GROWING_AND_CLOSE_UP,
+        Pen.AnimalCombination.LAC_COW,
+        Pen.AnimalCombination.NONE,
+        Pen.AnimalCombination.CLOSE_UP,
+    ]
+    num_groups = len(animal_combinations)
+    num_pens_per_group = 3
+    num_pens = num_groups * num_pens_per_group
+    for i in range(num_pens):
+        pen = mocker.MagicMock(spec=Pen)
+        pen.animal_combination = animal_combinations[i % num_groups]
+        pens.append(pen)
+
+    # Act
+    pen_groups = AnimalManagement._group_pens_by_animal_combination(pens)
+
+    # Assert
+    assert len(pen_groups) == num_groups
+    for i in range(num_groups):
+        assert len(pen_groups[animal_combinations[i]]) == num_pens_per_group
+        for j in range(num_pens_per_group):
+            assert pen_groups[animal_combinations[i]][j] == pens[j * num_groups + i]
+
+
+def test_calculate_total_maximum_stocking_density(mocker: MockerFixture) -> None:
+    """Unit test for function calculate_total_maximum_stocking_density in file routines/animal/animal_management.py"""
+    # Arrange
+    num_pens = 10
+    pens: List[Pen] = []
+    expected_total_maximum_stocking_density = 0
+    for i in range(num_pens):
+        pen = mocker.MagicMock(spec=Pen)
+        pen.num_stalls = num_stalls = i + 1
+        pen.max_stocking_density = max_stocking_density = (i + 2) * 1.5
+        pens.append(pen)
+        expected_total_maximum_stocking_density += num_stalls * max_stocking_density
+
+    # Act
+    total_maximum_stocking_density = AnimalManagement._calculate_total_maximum_stocking_density(pens)
+
+    # Assert
+    assert total_maximum_stocking_density == int(expected_total_maximum_stocking_density)
+
+
+@pytest.mark.parametrize(
+    'num_animals, total_max_stocking_density, expected_stall_shortage',
+    [
+        (100, 100, 0),
+        (100, 101, -1),
+        (100, 99, 1)
+    ]
+)
+def test_calculate_stall_shortage(num_animals: int,
+                                  total_max_stocking_density: int,
+                                  expected_stall_shortage: int,
+                                  mocker: MockerFixture) -> None:
+    """Unit test for function calculate_stall_shortage in file routines/animal/animal_management.py"""
+    # Arrange
+    mock_pens = mocker.MagicMock(spec=List[Pen])
+    patch_for_calculate_total_maximum_stocking_density = mocker.patch.object(
+        AnimalManagement,
+        '_calculate_total_maximum_stocking_density',
+        return_value=total_max_stocking_density
+    )
+
+    # Act
+    stall_shortage = AnimalManagement._calculate_stall_shortage(num_animals, mock_pens)
+
+    # Assert
+    assert stall_shortage == expected_stall_shortage
+    patch_for_calculate_total_maximum_stocking_density.assert_called_once_with(mock_pens)
+
+
+def test_calculate_stall_shortages_for_main_combinations(mocker: MockerFixture) -> None:
+    """Unit test for function calculate_stall_shortages_for_main_combinations in file routines/animal/animal_management.py"""
+    mock_calf_pens = mocker.MagicMock(spec=List[Pen])
+    mock_growing_pens = mocker.MagicMock(spec=List[Pen])
+    mock_close_up_pens = mocker.MagicMock(spec=List[Pen])
+    mock_lac_cow_pens = mocker.MagicMock(spec=List[Pen])
+    mock_pens_by_animal_combination = {
+        Pen.AnimalCombination.CALF: mock_calf_pens,
+        Pen.AnimalCombination.GROWING: mock_growing_pens,
+        Pen.AnimalCombination.CLOSE_UP: mock_close_up_pens,
+        Pen.AnimalCombination.LAC_COW: mock_lac_cow_pens,
+    }
+
+    num_calves = 10
+    num_heiferIs = 20
+    num_heiferIIs = 30
+    num_heiferIIIs = 40
+    num_dry_cows = 24
+    num_lactating_cows = 26
+
+    stall_shortages_dict = {
+        (num_calves, mock_calf_pens): 11,
+        (num_heiferIs + num_heiferIIs, mock_growing_pens): 21,
+        (num_heiferIIIs + num_dry_cows, mock_close_up_pens): 31,
+        (num_lactating_cows, mock_lac_cow_pens): 41,
+    }
+
+    expected_stall_shortages = {
+        Pen.AnimalCombination.CALF: stall_shortages_dict[(num_calves, mock_calf_pens)],
+        Pen.AnimalCombination.GROWING: stall_shortages_dict[(num_heiferIs + num_heiferIIs, mock_growing_pens)],
+        Pen.AnimalCombination.CLOSE_UP: stall_shortages_dict[(num_heiferIIIs + num_dry_cows, mock_close_up_pens)],
+        Pen.AnimalCombination.LAC_COW: stall_shortages_dict[(num_lactating_cows, mock_lac_cow_pens)],
+    }
+
+    patch_for_calculate_stall_shortage = mocker.patch.object(
+        AnimalManagement,
+        '_calculate_stall_shortage',
+        side_effect=lambda num_animals, pens: stall_shortages_dict[(num_animals, pens)]
+    )
+
+    # Act
+    actual_stall_storages = \
+        AnimalManagement._calculate_stall_shortages_for_main_animal_combinations(
+            mock_pens_by_animal_combination,
+            num_calves,
+            num_heiferIs,
+            num_heiferIIs,
+            num_heiferIIIs,
+            num_dry_cows,
+            num_lactating_cows,
+        )
+
+    # Assert
+    assert actual_stall_storages == expected_stall_shortages
+    patch_for_calculate_stall_shortage.assert_has_calls([
+        mocker.call(num_calves, mock_calf_pens),
+        mocker.call(num_heiferIs + num_heiferIIs, mock_growing_pens),
+        mocker.call(num_heiferIIIs + num_dry_cows, mock_close_up_pens),
+        mocker.call(num_lactating_cows, mock_lac_cow_pens),
+    ])
+
+
+def test_find_pen_with_max_stalls_for_animal_combination(mocker: MockerFixture) -> None:
+    mock_mixed_type_by_pen_id = {}
+    mock_mixed_type_pen_by_pen_id = {}
+    pass
+
+
+@pytest.mark.parametrize(
+    'animal_combination',
+    [
+        Pen.AnimalCombination.CALF,
+        Pen.AnimalCombination.GROWING,
+        Pen.AnimalCombination.CLOSE_UP,
+        Pen.AnimalCombination.LAC_COW
+    ]
+)
+def test_create_default_pen(animal_combination: Pen.AnimalCombination,
+                            mocker: MockerFixture) -> None:
+    """Unit test for function _create_default_pen() in file routines/animal/animal_management.py"""
+    # Arrange
+    num_stalls = 10
+    pen_id = 1
+
+    mock_pen = mocker.MagicMock(spec=Pen)
+    patch_for_pen_init = mocker.patch(
+        'RUFAS.routines.animal.animal_management.Pen',
+        return_value=mock_pen
+    )
+
+    # Act
+    pen = AnimalManagement._create_default_pen(pen_id, num_stalls, animal_combination)
+
+    # Assert
+    assert pen == mock_pen
+    patch_for_pen_init.assert_called_once_with(
+        pen_id=pen_id,
+        vertical_dist_to_milking_parlor=0.1,
+        horizontal_dist_to_milking_parlor=1.6,
+        number_of_stalls=num_stalls,
+        housing_type='open air barn',
+        bedding_type='straw',
+        pen_type='tiestall',
+        manure_handling='manual_scraping',
+        manure_separator='sedimentation',
+        manure_storage='storage_pit',
+        animal_combination=animal_combination,
+        max_stocking_density=1.2
+    )
+
+
+def test_remove_pen_from_dicts_by_id(mocker: MockerFixture) -> None:
+    """Unit test for function _remove_pen_from_dicts_by_id() in file routines/animal/animal_management.py"""
+    # Case 1: Given empty list of dictionaries, then nothing should be removed
+    # Arrange
+    pen_id = 1
+    dicts = []
+
+    # Act
+    AnimalManagement._remove_pen_from_dicts_by_id(pen_id, dicts)
+
+    # Assert
+    assert dicts == []
+
+    # -------------------------------------------------------------------------
+    # Case 2: Given list of dictionaries, then those dictionaries that contain
+    #         the given pen_id should be removed
+    # Arrange
+    pen_id = 1
+    dicts = [
+        {1: 'a', 2: 'b'},
+        {3: 'c', 4: 'd'},
+        {1: 'e', 5: 'f'},
+    ]
+
+    # Act
+    AnimalManagement._remove_pen_from_dicts_by_id(pen_id, dicts)
+
+    # Assert
+    assert dicts == [
+        {2: 'b'},
+        {3: 'c', 4: 'd'},
+        {5: 'f'},
+    ]
+
+    # -------------------------------------------------------------------------
+    # Case 3: Given list of dictionaries, if no dictionary contains the given
+    #         pen_id, then nothing should be removed
+    # Arrange
+    pen_id = 1
+    dicts = [
+        {2: 'b'},
+        {3: 'c', 4: 'd'},
+        {5: 'f'},
+    ]
+
+    # Act
+    AnimalManagement._remove_pen_from_dicts_by_id(pen_id, dicts)
+
+    # Assert
+    assert dicts == [
+        {2: 'b'},
+        {3: 'c', 4: 'd'},
+        {5: 'f'},
+    ]
+
+
+@pytest.mark.parametrize(
+    'animal_combination',
+    [
+        Pen.AnimalCombination.CALF,
+        Pen.AnimalCombination.GROWING,
+        Pen.AnimalCombination.CLOSE_UP,
+        Pen.AnimalCombination.LAC_COW,
+        Pen.AnimalCombination.NONE,
+        Pen.AnimalCombination.GROWING_AND_CLOSE_UP
+    ]
+)
+def test_update_stall_shortage_for_animal_combination(animal_combination: Pen.AnimalCombination,
+                                                      mocker: MockerFixture) -> None:
+    """Unit test for function _update_stall_shortage_for_animal_combination() in file routines/animal/animal_management.py"""
+    mock_pen = mocker.MagicMock(spec=Pen)
+    current_stall_shortage_for_calf = 20
+    current_stall_shortage_for_growing = 30
+    current_stall_shortage_for_close_up = 40
+    current_stall_shortage_for_lac_cow = 50
+
+    mock_stall_shortages = {
+        Pen.AnimalCombination.CALF: current_stall_shortage_for_calf,
+        Pen.AnimalCombination.GROWING: current_stall_shortage_for_growing,
+        Pen.AnimalCombination.CLOSE_UP: current_stall_shortage_for_close_up,
+        Pen.AnimalCombination.LAC_COW: current_stall_shortage_for_lac_cow,
+    }
+    mock_stall_shortages_copy = mock_stall_shortages.copy()
+
+    total_max_stocking_density = 10
+    patch_for_calculate_total_maximum_stocking_density = mocker.patch(
+        'RUFAS.routines.animal.animal_management.AnimalManagement._calculate_total_maximum_stocking_density',
+        return_value=total_max_stocking_density
+    )
+
+    all_expected_stall_shortages = {
+        Pen.AnimalCombination.CALF: current_stall_shortage_for_calf - total_max_stocking_density,
+        Pen.AnimalCombination.GROWING: current_stall_shortage_for_growing - total_max_stocking_density,
+        Pen.AnimalCombination.CLOSE_UP: current_stall_shortage_for_close_up - total_max_stocking_density,
+        Pen.AnimalCombination.LAC_COW: current_stall_shortage_for_lac_cow - total_max_stocking_density,
+    }
+
+    # Act
+    updated_stall_shortages = AnimalManagement._update_stall_shortage_for_animal_combination(
+        stall_shortages=mock_stall_shortages,
+        animal_combination=animal_combination,
+        pen=mock_pen
+    )
+
+    # Assert
+    if animal_combination in mock_stall_shortages:
+        assert updated_stall_shortages[animal_combination] == all_expected_stall_shortages[animal_combination]
+        patch_for_calculate_total_maximum_stocking_density.assert_called_once_with([mock_pen])
+    else:
+        assert mock_stall_shortages == mock_stall_shortages_copy
+        patch_for_calculate_total_maximum_stocking_density.assert_not_called()
