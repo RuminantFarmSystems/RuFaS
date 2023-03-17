7cea952923ed95f8efe90aca6f5f648b7b30a8a2
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2023-03-17 12:39:05 -0400

    method changes

--- RUFAS/routines/animal/life_cycle/heiferIII.py
+++ RUFAS/routines/animal/life_cycle/heiferIII.py
@@ -115,10 +115,11 @@ class HeiferIII(HeiferII):
             0,
             0,
             ]
-        #with open('C:/Users/jw2574/Documents/data/vm1/MASM/output/test.csv', 'a', newline='') as file:
-        with open('C:/Users/joecw/RUFAS/NRC_10yr.csv', 'a', newline='') as file:
+        with open('C:/Users/jw2574/Documents/data/vm1/MASM/output/NASEM_10yr.csv', 'a', newline='') as file:
+        #with open('C:/Users/joecw/RUFAS/NRC_10yr.csv', 'a', newline='') as file:
             csvout = csv.writer(file)
             csvout.writerow(csvline)
+    
     def calc_manure_excretion(self, feed):
         """
         Calculates and sets the manure excretion components.
