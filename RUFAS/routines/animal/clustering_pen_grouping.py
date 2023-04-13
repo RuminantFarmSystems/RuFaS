398adfc9af163fb0320b5c8863e4fad5861ccf3c
ndyt <ntomlinson@gmail.com>
2023-04-13 11:52:15 -0400

    updated cleanup.sh file with Ed's suggestions

--- RUFAS/routines/animal/clustering_pen_grouping.py
+++ RUFAS/routines/animal/clustering_pen_grouping.py
@@ -15,7 +15,6 @@ import math
 ################################################################################
 import pandas as pd
 import numpy as np
-import math
 from scipy.stats import percentileofscore
 
 
--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,23 @@
 # grep -E filters out all the files from the git status that don't end with .py
 # cut -c 4- makes sure to capture just the names of the files so erases out all the git status info
 
-flake8 $(git status -s | grep -E '\.py$' | cut -c 4-)
+# flake8 $(git status -s | grep -E '\.py$' | cut -c 4-)
 # flake8 $(git diff --name-only | grep -E '\.py$')
+
+display_usage() {
+    echo "Usage: ./runflake8onbranch.sh [BASEBRANCH]"
+    echo "Lint all files different between current branch and BASEBRANCH with Flake8."
+    echo ""
+    echo "With no BASEBRANCH, compare current branch to master."
+}
+
+if [ $# -eq 0 ]; then
+    base_branch="master"
+elif [ $# -gt 1 ]; then
+    display_usage
+    exit 1
+else
+    base_branch=$1
+fi
+
+flake8 $(git diff --name-only $(git merge-base ${base_branch} HEAD) | grep -E '\.py$')
