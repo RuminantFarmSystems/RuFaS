7c6cabeabb42c42ba6be187d0edbcd014bd54e57
Ed Hansen <isgotkowitz@gmail.com>
2023-04-20 13:02:58 -0500

    desorption

--- SC_redesign/Crop_and_Soil/soil/phosphorus_cycling/phosphorus_mineralization.py
+++ SC_redesign/Crop_and_Soil/soil/phosphorus_cycling/phosphorus_mineralization.py
@@ -47,14 +47,22 @@ class PhosphorusMineralization:
             if balance < 0:  # Desorption
                 layer.active_inorganic_unbalanced_counter += 1
                 layer.labile_inorganic_unbalanced_counter = 0
-                pass
+
+                phosphorus_amount_to_transfer = self._calculate_phosphorus_desorption(
+                    layer.active_inorganic_unbalanced_counter, layer.mean_phosphorus_sorption_parameter, balance)
+                phosphorus_amount_to_transfer = min(layer.active_inorganic_phosphorus_content,
+                                                    phosphorus_amount_to_transfer)
             elif balance > 0:  # Sorption
-                layer.labile_inorganic_unbalanced_counter += 1
                 layer.active_inorganic_unbalanced_counter = 0
-                pass
+                layer.labile_inorganic_unbalanced_counter += 1
+
+                phosphorus_amount_to_transfer = self._calculate_phosphorus_sorption()
             else:  # Balanced
                 layer.labile_inorganic_unbalanced_counter = 0
                 layer.active_inorganic_unbalanced_counter = 0
+                phosphorus_amount_to_transfer = 0
+
+            pass
 
     # --- Static methods ---
     @staticmethod
@@ -78,7 +86,7 @@ class PhosphorusMineralization:
 
         References
         ----------
-        Surphos Fortran code, pminrl.f, lines 48 - 51
+        SurPhos Fortran code, pminrl.f, lines 48 - 51
 
         Notes
         -----
@@ -123,3 +131,75 @@ class PhosphorusMineralization:
 
         """
         return labile_phosphorus - (active_phosphorus * (sorption_parameter / (1 - sorption_parameter)))
+
+    @staticmethod
+    def _calculate_phosphorus_desorption(active_inorganic_unbalanced_counter: int, sorption_parameter: float,
+                                         phosphorus_balance: float) -> float:
+        """Calculates how much phosphorus should be desorped in the given soil layer.
+
+        Parameters
+        ----------
+        active_inorganic_unbalanced_counter : int
+            The number of days that the active inorganic phosphorus pool has been greater than it would be when in
+            equilibrium with the labile inorganic phosphorus pool
+        sorption_parameter : float
+            The mean phosphorus sorption parameter that has been adjusted for the current day's amount of labile
+            inorganic phosphorus present (unitless)
+        phosphorus_balance : float
+            A value indicating how unbalanced the labile and active inorganic phosphorus pools are (unitless)
+
+        Returns
+        -------
+        float
+            The amount of phosphorus that should be removed from the active inorganic phosphorus pool to put it in
+            equilibrium with the labile inorganic phosphorus pool (kg phosphorus / ha)
+
+        References
+        ----------
+        SurPhos pminrl.f, lines 69, 70
+        Vadas P.A., Krogstad T., Sharpley A.N. (2006) Modeling phosphorus transfer between labile and nonlabile soil
+            pools: Updating the EPIC model. Soil Science Society of America Journal 70:736-743. DOI:
+            Doi 10.2136/Sssaj2005.0067. (Eqn. [8])
+
+        Notes
+        -----
+        The constants used differ between the old code and the literature, the constants from the old code are used
+        here.
+
+        """
+        base = PhosphorusMineralization._determine_desorption_base(sorption_parameter)
+        desorption_factor = base * (active_inorganic_unbalanced_counter ** -0.32)
+        amount_transferred = desorption_factor * phosphorus_balance * -1
+        return amount_transferred
+
+    @staticmethod
+    def _determine_desorption_base(sorption_parameter: float) -> float:
+        """This method calculates a value used to determine how much phosphorus is desorped from the active inorganic
+            phosphorus pool in the labile organic phosphorus pool.
+
+        Parameters
+        ----------
+        sorption_parameter : float
+            The mean phosphorus sorption parameter that has been adjusted for the current day's amount of labile
+            inorganic phosphorus present (unitless)
+
+        Returns
+        -------
+        float
+            A value (named 'base' in code and literature) that is used to determine how much phosphorus is transferred
+            from the active inorganic phosphorus pool to the labile inorganic phosphorus pool.
+
+        References
+        ----------
+        SurPhos pminrl.f, line 59
+        Vadas P.A., Krogstad T., Sharpley A.N. (2006) Modeling phosphorus transfer between labile and nonlabile soil
+            pools: Updating the EPIC model. Soil Science Society of America Journal 70:736-743. DOI:
+            Doi 10.2136/Sssaj2005.0067. (Eqn. [7])
+
+        Notes
+        -----
+        The constants used differ between the old code and the literature, the constants from the old code are used
+        here.
+
+        """
+        return (-1 * sorption_parameter) + 0.8
