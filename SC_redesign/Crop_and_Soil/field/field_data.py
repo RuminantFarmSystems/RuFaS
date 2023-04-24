9b65a6d8777bfef1cc48ed73e26604d65cfb66db
Clay Morrow <morrowcj@outlook.com>
2023-04-24 15:52:03 -0500

    implemented heat scheduling'

--- SC_redesign/Crop_and_Soil/field/field_data.py
+++ SC_redesign/Crop_and_Soil/field/field_data.py
@@ -1,6 +1,6 @@
 from typing import Optional, List, Dict
 from dataclasses import dataclass
-from SC_redesign.Crop_and_Soil.field.harvest_operations import HarvestOperation
+from SC_redesign.Crop_and_Soil.crop.harvest_operations import HarvestOperation
 from SC_redesign.Crop_and_Soil.crop.dormancy import Dormancy
 
 
