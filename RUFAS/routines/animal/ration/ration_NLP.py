92f4382103f93b1d9587e73b2083f31db9d3eb70
Haowen Hu <hh598@cornell.edu>
2023-04-27 16:43:05 -0400

    Set the lower limit for CS instead and updated the price for it

--- RUFAS/routines/animal/ration/ration_NLP.py
+++ RUFAS/routines/animal/ration/ration_NLP.py
@@ -520,7 +520,7 @@ def optimize(id_key):
     bnds = []
     # Dividing limit by 3 for tri-decision variables for farm grown feeds
     for key in id_key:
-        if key == '26':
+        if key == '36':
             bnds.append((2, (limit[i] / 3) + 0.0001))
             bnds.append((2, (limit[i] / 3) + 0.0001))
             bnds.append((2, (limit[i] / 3) + 0.0001))
