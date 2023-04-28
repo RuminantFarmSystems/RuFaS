de7684a823a5b6b844498ba0305394b127087470
Ed Hansen <isgotkowitz@gmail.com>
2023-04-28 13:32:06 -0500

    another method another test

--- SC_redesign/Crop_and_Soil/tests_SC/soil_tests/nitrogen_cycling_tests/test_mineralization_decomp.py
+++ SC_redesign/Crop_and_Soil/tests_SC/soil_tests/nitrogen_cycling_tests/test_mineralization_decomp.py
@@ -1,4 +1,5 @@
 import pytest
+from math import exp
 
 from SC_redesign.Crop_and_Soil.soil.nitrogen_cycling.mineralization_decomp import MineralizationDecomposition
 
@@ -12,5 +13,22 @@ from SC_redesign.Crop_and_Soil.soil.nitrogen_cycling.mineralization_decomp impor
 def test_calculate_residue_nutrient_ratio(carbon: float, organic: float, inorganic: float) -> float:
     """Tests that the correct carbon-residue ratio is calculated."""
     observed = MineralizationDecomposition._calculate_residue_nutrient_ratio(carbon, organic, inorganic)
-    expected = carbon / (organic + inorganic)
+    if organic + inorganic == 0:
+        expected = carbon / 0.000001
+    else:
+        expected = carbon / (organic + inorganic)
+    assert observed == expected
+
+
+@pytest.mark.parametrize("ratio,constant", [
+    (1.334, 25),
+    (0.4465, 25),
+    (0.234, 200),
+    (0, 200)
+])
+def test_calculate_nutrient_term_for_residue_composition_factor(ratio: float, constant: float) -> float:
+    """Tests that the nitrogen and phosphorus ratio terms are calculated correctly for use in calculating the nutrient
+        cycling residue composition factor."""
+    observed = MineralizationDecomposition._calculate_nutrient_term_for_residue_composition_factor(ratio, constant)
+    expected = exp(-0.693 * ((ratio - constant) / constant))
     assert observed == expected
