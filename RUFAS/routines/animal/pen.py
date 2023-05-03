c1ca335fc6937a252e3e4e1555232f0a9c606a3b
KFosterReed <kfr3@cornell.edu>
2023-05-03 13:14:14 -0400

    Removed calving interval from milking update info map, and made ration info map more specific

--- RUFAS/routines/animal/pen.py
+++ RUFAS/routines/animal/pen.py
@@ -433,7 +433,7 @@ class Pen:
 
         info_map = {"class": self.__class__.__name__,
                     "function": self.calc_ration.__name__,
-                    "available_feeds": available_feeds, }
+                    "available_feeds": available_feeds['feed_id'], }
         om.add_variable("ration_nutrient_amount", nutrient_amount, info_map)
         om.add_variable("ration_nutrient_conc", nutrient_conc, info_map)
         om.add_variable("MEdiet", self.MEdiet, info_map)
