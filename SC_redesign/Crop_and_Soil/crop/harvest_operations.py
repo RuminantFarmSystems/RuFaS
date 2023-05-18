320e8ad3f9b5fdc40bbe6a276bb902424d5cbd53
morrowcj <morrowcj@outlook.com>
2023-05-18 15:43:32 -0500

    more updates

--- SC_redesign/Crop_and_Soil/crop/harvest_operations.py
+++ SC_redesign/Crop_and_Soil/crop/harvest_operations.py
@@ -7,7 +7,7 @@ class HarvestOperation(Enum):
     HARVEST_NOKILL = "no_kill"
 
 
-FINAL_HARVEST_OPERATIONS = [HarvestOperation.HARVEST]
+FINAL_HARVEST_OPERATIONS = ["default"]
 """This variable is a list of all the HarvestOperation instances that will terminate the Crop instance (either through
  via death. This list should be appended with any operation added to the HarvestOperation class that will ultimately
  kill the crop before another harvest operation can occur (a non-obvious example would be cutting the crop a final
