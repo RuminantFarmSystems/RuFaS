2098c6d76d7352ebf550acb1ddb77675fc6ffef0
Ed Hansen <isgotkowitz@gmail.com>
2023-05-18 16:17:37 -0500

    added test for canopy storage

--- SC_redesign/Crop_and_Soil/tests_SC/crop_tests/test_crop_data.py
+++ SC_redesign/Crop_and_Soil/tests_SC/crop_tests/test_crop_data.py
@@ -22,3 +22,18 @@ def test_given_harvest_index_property(usr_index, expect):
     """test the class knows if harvest index override is specified"""
     data = CropData(user_harvest_index=usr_index)
     assert data.do_harvest_index_override == expect
+
+
+@pytest.mark.parametrize("max_capacity,lai,max_lai", [
+    (1.445, 0.55, 1.88),
+    (2.88, 3.445, 4.5),
+    (0.0, 1.8, 2.1),
+    (2.3, 0.0, 2.9),
+    (4.33, 3.7, 3.7)
+])
+def test_water_canopy_storage_capacity(max_capacity: float, lai: float, max_lai: float) -> None:
+    """Tests that the current storage capacity of the canopy is correctly calculated."""
+    data = CropData(max_canopy_water_capacity=max_capacity, leaf_area_index=lai, max_leaf_area_index=max_lai)
+    actual = data.water_canopy_storage_capacity
+    expected = max_capacity * lai / max_lai
+    assert pytest.approx(actual) == expected
