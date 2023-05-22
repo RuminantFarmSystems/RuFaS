c9ad0ece91d224188449883b6554ebd4214289a6
Ed Hansen <isgotkowitz@gmail.com>
2023-05-22 17:04:12 -0500

    initial commit

---
+++ SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_output_gatherer.py
@@ -0,0 +1,23 @@
+import pytest
+from typing import List
+
+from SC_redesign.Crop_and_Soil.field.field import Field
+from SC_redesign.Crop_and_Soil.field.field_data import FieldData
+from SC_redesign.Crop_and_Soil.manager.output_gatherer import OutputGatherer
+
+
+@pytest.mark.parametrize("runoff_values", [
+    [1.3, 2.4, 1.22]
+])
+def test_send_daily_variables(runoff_values: List[float]) -> None:
+    field_data_1 = FieldData(name="name 1")
+    field_data_2 = FieldData(name="name 2")
+    field_1 = Field(field_data_1)
+    field_2 = Field(field_data_2)
+    og = OutputGatherer([field_1, field_2])
+    for i in range(len(runoff_values)):
+        field_1.soil.data.accumulated_runoff = runoff_values[i]
+        field_2.soil.data.accumulated_runoff = runoff_values[i]
+        og.send_daily_variables()
+    print(og.om.variables_pool)
+    assert False
