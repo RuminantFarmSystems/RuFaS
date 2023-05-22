e8abd7bc2f748ef3fba5f928adf13191cf9965e1
Ed Hansen <isgotkowitz@gmail.com>
2023-05-22 09:38:42 -0500

    halfway through docstring update

--- SC_redesign/Crop_and_Soil/field/field.py
+++ SC_redesign/Crop_and_Soil/field/field.py
@@ -356,8 +356,7 @@ class Field:
             current_weather: a CurrentWeather object, containing a collection of today's weather variables needed
                 for field processes.
 
-        Details: Water cycling is intimately linked to both soil and crops and, as such, is a property of the
-            whole-field. Therefore, it makes most sense for this process to take place within the field class rather
+        Details: Water processes are linked to both  Therefore, it makes most sense for this process to take place within the field class rather
             than in both the crop and soil classes. This method coordinates the evapotranspiration from the field on any
             given day, by first calculating the evapotranspirative demand, the removing water from the elements in the
             field in the order listed below. The removal of water does not stop until either all elements have had water
@@ -369,11 +368,19 @@ class Field:
                 - Transpiration from crops (amount of water taken up by plants is equal to the amount they transpirate,
                     and this amount depends on the evapotranspirative demand after water has been removed from canopies)
         """
-        total_precipitation = current_weather.rainfall
+        watering_amount = self._determine_watering_amount(current_weather.rainfall)
+        total_precipitation = current_weather.rainfall + watering_amount
         precipitation_reaching_soil = self._handle_water_in_crop_canopies(total_precipitation)
-        # TODO: figure out how to determine weighting coefficient when there are multiple crops in the field - issue 519
+
+        # TODO: figure out how to determine weighting coefficient when there are multiple crops in the field - issue
+        #  #519
         self.soil.infiltration(precipitation_reaching_soil, 1)
         self.soil.percolation.percolate(self.field_data.seasonal_high_water_table)
+        # TODO: find reasonable values/way to set minimum cover management factor - issue #520
+        self.soil.soil_erosion.erode(self.field_data.field_size, 0.02, self.field_data.current_residue)
+        self.soil.phosphorus_cycling.cycle_phosphorus(precipitation_reaching_soil, self.soil.data.accumulated_runoff,
+                                                      self.field_data.field_size, current_weather.mean_air_temperature)
+        self.soil.nitrogen_cycling.cycle_nitrogen(self.field_data.field_size)
 
         full_evapotranspirative_demand = self._determine_potential_evapotranspiration(
             current_weather.incoming_light, current_weather.max_air_temperature, current_weather.min_air_temperature,
@@ -399,8 +406,8 @@ class Field:
 
         # TODO: sublimate and adjust soil_evaporation_and_sublimation_amount here - issue #317
 
-        amount_evaporated_from_soil = self.soil.evaporation.evaporate(soil_evaporation_and_sublimation_amount)
-        remaining_evapotranspirative_demand -= amount_evaporated_from_soil
+        self.soil.evaporation.evaporate(soil_evaporation_and_sublimation_amount)
+        remaining_evapotranspirative_demand -= self.soil.data.water_evaporated
 
         actual_evaporation = full_evapotranspirative_demand - remaining_evapotranspirative_demand
 
