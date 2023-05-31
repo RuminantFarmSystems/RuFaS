759b9b6bc69bd8b788c61bbf438b83dd5012d943
MatthewLiu <matthew7838@gmail.com>
2023-06-01 02:05:22 +0900

    added assertions to check field data collection

--- SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_output_gatherer.py
+++ SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_output_gatherer.py
@@ -10,8 +10,8 @@ from SC_redesign.Crop_and_Soil.manager.output_gatherer import OutputGatherer
     [1.3, 2.4, 1.22]
 ])
 def test_send_daily_variables(runoff_values: List[float]) -> None:
-    field_data_1 = FieldData(name="name 1")
-    field_data_2 = FieldData(name="name 2")
+    field_data_1 = FieldData(name=" name 1 ")
+    field_data_2 = FieldData(name=" name 2 ")
     field_1 = Field(field_data_1)
     field_2 = Field(field_data_2)
     og = OutputGatherer([field_1, field_2])
@@ -20,4 +20,14 @@ def test_send_daily_variables(runoff_values: List[float]) -> None:
         field_2.soil.data.accumulated_runoff = runoff_values[i]
         og.send_daily_variables()
     print(og.om.variables_pool)
-    assert False
+    pool = og.om.variables_pool
+    assert 'field name 1 .current_residue' in pool.keys()
+    assert len(pool['field name 1 .current_residue']['info_maps']) == 3
+    assert len(pool['field name 1 .current_residue']['values']) == 3
+    for prefix_dict in pool['field name 1 .current_residue']['info_maps']:
+        assert prefix_dict['prefix'] == 'field name 1 '
+    assert 'field name 2 .current_residue' in pool.keys()
+    assert len(pool['field name 2 .current_residue']['info_maps']) == 3
+    assert len(pool['field name 2 .current_residue']['values']) == 3
+    for prefix_dict in pool['field name 2 .current_residue']['info_maps']:
+        assert prefix_dict['prefix'] == 'field name 2 '
