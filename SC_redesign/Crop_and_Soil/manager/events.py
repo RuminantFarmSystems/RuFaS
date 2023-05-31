f0db0e97ec24c358ac900eb956393dc46dfd54a7
Ed Hansen <isgotkowitz@gmail.com>
2023-05-31 12:40:19 -0500

    got tests passing with new method

--- SC_redesign/Crop_and_Soil/manager/events.py
+++ SC_redesign/Crop_and_Soil/manager/events.py
@@ -76,98 +76,6 @@ class Event:
         return Event(self.year + years, self.day + days)
 
     @staticmethod
-    def project_sequence(start: int | List[int] = 0, repeat: int = 0, skip: int = 0, cycles: int = 1) -> List[int]:
-        """Generates a sequence of integers from a starting point, based on pattern rules. This method was created with
-        temporal sequences specifically in mind.
-
-        Parameters
-        ----------
-        start : int | list[int], default: 0
-            the starting point of the sequence: either a single number or a sequence of number that will be projected
-            forward. If multiple values are given, it is expected that the values are in ascending order; other formats
-            may yield unexpected behavior.
-        repeat : int, default: 0
-            the number of times `start` should be repeated sequentially within a cycle
-        skip : int, default: 0
-            the number of steps to skip between cycles (if applicable)
-        cycles : int, default: 1
-            the number of cycles in the final sequence
-
-        Returns
-        -------
-        seq : list[int]
-            the resulting sequence.
-
-        Raises
-        ------
-        Exception
-            if repeat is negative
-        Exception
-            if cycles is negative
-
-        Examples
-        --------
-        >>> project_sequence(start=0, repeat=1, skip=1, cycles=3)
-        [0, 1, 3, 4, 6, 7]
-
-        >>> project_sequence(start=[1, 2, 4], skip=1, cycles=3)
-        [1, 2, 4, 6, 7, 9, 11, 12, 14]
-
-        >>> project_sequence(start=[1, 2, 4], repeat=1, skip=2, cycles=3)
-        [1, 2, 4, 5, 6, 8, 11, 12, 14, 15, 16, 18, 21, 22, 24, 25, 26, 28]
-
-        >>> project_sequence(start=1, cycles=10)
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        Equivalently:
-
-        >>> project_sequence(start=[1, 2, 3, 4, 5], repeat=1)
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        Equivalently:
-
-        >>> project_sequence(start=[1, 2, 3, 4, 5], cycles=2)
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        >>> project_sequence(start=1, repeat=3, skip=3, cycles=4)
-        [1, 2, 3, 4, 8, 9, 10, 11, 15, 16, 17, 18, 22, 23, 24, 25]
-
-        Equivalently:
-
-        >>> project_sequence(start=[1, 2, 3, 4], skip=3, cycles=4)
-        [1, 2, 3, 4, 8, 9, 10, 11, 15, 16, 17, 18, 22, 23, 24, 25]
-
-        Notes
-        -----
-        Unlike `repeat` and `cycles`, `skip` can be negative, but the behaviour may not be as expected.
-        """
-        if repeat < 0:
-            raise Exception("repeat must not be negative")
-        if cycles < 0:
-            raise Exception("cycles must not be negative")
-
-        if type(start) is int:
-            start = [start]
-            span = 0
-        else:
-            span = max(start) - min(start)
-
-        increment = span + 1
-        pattern_base = start.copy()
-        if repeat > 0:
-            for i in range(repeat):
-                replicate = i + 1
-                pattern_base += [val + (increment * replicate) for val in start]
-            span = max(pattern_base) - min(pattern_base)
-
-        out_list = pattern_base.copy()
-        for j in range(cycles - 1):
-            cycle = j + 1
-            out_list += [val + (cycle * span) + (cycle * (skip + 1)) for val in pattern_base]
-
-        return out_list
-
-    @staticmethod
     def repeat_pattern(pattern: List[int], skip: int = 0, repeat: int = 0) -> List[int]:
         """
         Takes a pattern of numbers and repeats it a specified number of times, skipping over specified gaps between
@@ -205,18 +113,20 @@ class Event:
             raise ValueError(f"Expected repeat to be >= 0, received '{repeat}'.")
 
         differences = [skip + 1]
-        for i in range(1, len(pattern[1:]) + 1):
-            if pattern[i] <= pattern[i - 1]:
+        number_of_differences = range(1, len(pattern[1:]) + 1)
+        for difference in number_of_differences:
+            if pattern[difference] <= pattern[difference - 1]:
                 raise ValueError(f"Values in pattern must be strictly ascending., received '{pattern}'.")
-            differences.append(pattern[i] - pattern[i - 1])
+            differences.append(pattern[difference] - pattern[difference - 1])
 
-        out = deepcopy(pattern)
-        j = 0
-        for i in range(repeat * len(pattern)):
-            out.append(out[-1] + differences[j])
-            j += 1
-            j %= len(pattern)
-        return out
+        full_pattern = deepcopy(pattern)
+        differences_index = 0
+        number_of_new_values = range(repeat * len(pattern))
+        for _new_value in number_of_new_values:
+            full_pattern.append(full_pattern[-1] + differences[differences_index])
+            differences_index += 1
+            differences_index %= len(pattern)
+        return full_pattern
 
 
 class PlantingEvent(Event):
--- SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_events.py
+++ SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_events.py
@@ -4,45 +4,6 @@ from typing import List
 from SC_redesign.Crop_and_Soil.manager.events import Event
 
 
-def test_project_sequence():
-    """ensure that all examples are tested for project_sequence"""
-    assert Event.project_sequence(start=0, repeat=1, skip=1, cycles=3) == [0, 1, 3, 4, 6, 7]
-    assert Event.project_sequence(start=[1, 2, 4], skip=1, cycles=3) == [1, 2, 4, 6, 7, 9, 11, 12, 14]
-    assert Event.project_sequence(start=[1, 2, 4], repeat=1, skip=2, cycles=3) == [1, 2, 4, 5, 6, 8,
-                                                                                   11, 12, 14, 15, 16, 18,
-                                                                                   21, 22, 24, 25, 26, 28]
-    assert Event.project_sequence(start=1, cycles=10) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    assert Event.project_sequence(start=[1, 2, 3, 4, 5], repeat=1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    assert Event.project_sequence(start=[1, 2, 3, 4, 5], cycles=2) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    assert Event.project_sequence(start=1, repeat=3, skip=3, cycles=4) == [1, 2, 3, 4,
-                                                                           8, 9, 10, 11,
-                                                                           15, 16, 17, 18,
-                                                                           22, 23, 24, 25]
-    assert Event.project_sequence(start=[1, 2, 3, 4], skip=3, cycles=4) == [1, 2, 3, 4,
-                                                                            8, 9, 10, 11,
-                                                                            15, 16, 17, 18,
-                                                                            22, 23, 24, 25]
-    assert Event.project_sequence([1, 2, 3, 4]) == [1, 2, 3, 4]
-    assert Event.project_sequence(0) == [0]
-    assert Event.project_sequence(1, repeat=4) == [1, 2, 3, 4, 5]
-
-    assert Event.project_sequence()
-
-    # Old tests (for "repeat_pattern" - replaced by project sequence)
-    assert Event.project_sequence([1, 2, 3], skip=0, cycles=3) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    assert Event.project_sequence([1, 2, 3], skip=1, cycles=2) == [1, 2, 3, 5, 6, 7]
-    assert Event.project_sequence([1, 2, 4], skip=2, cycles=3) == [1, 2, 4, 7, 8, 10, 13, 14, 16]
-    assert Event.project_sequence([1], skip=0, cycles=5) == [1, 2, 3, 4, 5]
-    assert Event.project_sequence([1], skip=-1, cycles=5) == [1, 1, 1, 1, 1]
-    assert Event.project_sequence([2, 4], skip=1, cycles=3) == [2, 4, 6, 8, 10, 12]
-    assert Event.project_sequence([2, 4], skip=2, cycles=3) == [2, 4, 7, 9, 12, 14]
-    assert Event.project_sequence([1], skip=2, cycles=4) == [1, 4, 7, 10]
-    assert Event.project_sequence([3, 4, 6], skip=0, cycles=3) == [3, 4, 6, 7, 8, 10, 11, 12, 14]
-
-    # This test demonstrates there is a bug in project_sequence()
-    assert Event.project_sequence([1, 3, 4], skip=1, repeat=2) != Event.project_sequence([1, 3, 4], skip=0, repeat=2)
-
-
 def test_repeat_pattern() -> None:
     """Tests that repeat_pattern correctly repeats patterns."""
     assert Event.repeat_pattern([1, 3, 5], 1, 3) == [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]
