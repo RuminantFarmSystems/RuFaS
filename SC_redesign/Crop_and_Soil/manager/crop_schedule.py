65b0424b7926b36cb1a886f36174563092c4ae8b
Ed Hansen <isgotkowitz@gmail.com>
2023-05-31 15:57:31 -0500

    partway through harvest events

--- SC_redesign/Crop_and_Soil/manager/crop_schedule.py
+++ SC_redesign/Crop_and_Soil/manager/crop_schedule.py
@@ -1,7 +1,7 @@
 from typing import List, Any
 
 from SC_redesign.Crop_and_Soil.crop.harvest_operations import FINAL_HARVEST_OPERATIONS
-from SC_redesign.Crop_and_Soil.manager.events import PlantingEvent
+from SC_redesign.Crop_and_Soil.manager.events import PlantingEvent, HarvestEvent
 
 
 class CropSchedule:
@@ -87,7 +87,7 @@ class CropSchedule:
         Returns
         -------
         List[PlantingEvent]
-            List of planting events all planting events that will happen for this crop schedule.
+            List of all planting events that will happen for this crop schedule.
 
         """
         all_planting_years = PlantingEvent.repeat_pattern(self.planting_years, self.pattern_skip, self.pattern_repeat)
@@ -99,6 +99,31 @@ class CropSchedule:
             new_planting_event = PlantingEvent(self.crop_reference, date[0], date[1], self.heat_scheduled)
             planting_events.append(new_planting_event)
         return planting_events
+
+    def generate_harvest_events(self) -> List[HarvestEvent]:
+        """
+        Generates a list of all harvest events that will occur in the crop schedule.
+
+        Returns
+        -------
+        List[HarvestEvent]
+            List of harvesting events that will happen for this crop schedule.
+
+        """
+        all_harvesting_years = HarvestEvent.repeat_pattern(self.harvest_years, self.pattern_skip, self.pattern_repeat)
+        all_harvesting_days = self.harvest_days * (self.pattern_repeat + 1)
+        all_harvesting_operations = self.harvest_operations * (self.pattern_repeat + 1)
+        all_harvesting_dates = list(zip(all_harvesting_years, all_harvesting_days, all_harvesting_operations))
+
+        if self.heat_scheduled:
+            all_harvesting_dates[:] = \
+                [harvest for harvest in all_harvesting_dates if harvest[2] in FINAL_HARVEST_OPERATIONS]
+
+        harvest_events = []
+        for date in all_harvesting_dates:
+            new_harvest_event = HarvestEvent(self.crop_reference, date[0], date[1], date[2])
+            harvest_events.append(new_harvest_event)
+        return harvest_events
 
     @staticmethod
     def _convert_to_list(to_be_converted: Any) -> List:
