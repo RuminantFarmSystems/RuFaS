0efbcf4eff0a5e061d15430cb0083b3de99c7f86
ndyt <ntomlinson@gmail.com>
2023-05-31 13:45:38 -0400

    alters naming of saved variables file to indicate inclusion/exclusion

--- RUFAS/output_manager.py
+++ RUFAS/output_manager.py
@@ -422,7 +422,12 @@ class OutputManager(object):
                 final_pool = filtered_pool
                 if exclude_info_maps:
                     final_pool = self._exclude_info_maps(filtered_pool)
-                file_path = os.path.join(path, self._generate_file_name(f"saved_variables_{input_file}", "json"))
+                if "inclusion" in dir_path:
+                    file_path = os.path.join(path, self._generate_file_name(f"saved_variables_inclusion_{input_file}",
+                                                                            "json"))
+                if "exclusion" in dir_path:
+                    file_path = os.path.join(path, self._generate_file_name(f"saved_variables_exclusion_{input_file}",
+                                                                            "json"))
                 self._dict_to_file_json(final_pool, file_path)
 
     def dump_variables(self, path: str, exclude_info_maps: bool = False) -> None:
