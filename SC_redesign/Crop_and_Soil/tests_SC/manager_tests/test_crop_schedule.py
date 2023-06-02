38f2cab02780b2081f86bacf818d9f298f534b66
Ed Hansen <isgotkowitz@gmail.com>
2023-06-02 14:37:26 -0500

    added name field to CropSchedule

--- SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_crop_schedule.py
+++ SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_crop_schedule.py
@@ -5,31 +5,34 @@ from SC_redesign.Crop_and_Soil.manager.crop_schedule import CropSchedule
 from SC_redesign.Crop_and_Soil.manager.events import PlantingEvent, HarvestEvent
 
 
-@pytest.mark.parametrize("crop_ref,plant_years,plant_days,harvest_years,harvest_days,harvest_ops,heat_sched,pat_skip,"
-                         "pat_repeat,expected", [
-                             ("corn", [1990, 1991], 120, [1990, 1991], [255, 255], ["default", "default"], False, 2, 3,
-                              {"plant_years": [1990, 1991], "plant_days": [120, 120],
-                               "harvest_years": [1990, 1991],
-                               "harvest_days": [255, 255],
-                               "harvest_ops": ["default", "default"]}),
-                             ("beans", [1990, 1991, 1992], [120, 121, 121], [1990, 1991, 1992], [255, 255, 260],
-                              ["no_kill", "no_kill", "default"], True, 2, 3,
+@pytest.mark.parametrize("name,crop_ref,plant_years,plant_days,harvest_years,harvest_days,harvest_ops,heat_sched,"
+                         "pat_skip,pat_repeat,expected", [
+                             ("test_1", "corn", [1990, 1991], 120, [1990, 1991], [255, 255], ["default", "default"],
+                              False, 2, 3, {"plant_years": [1990, 1991], "plant_days": [120, 120],
+                                            "harvest_years": [1990, 1991],
+                                            "harvest_days": [255, 255],
+                                            "harvest_ops": ["default", "default"]}),
+                             ("test_2", "beans", [1990, 1991, 1992], [120, 121, 121], [1990, 1991, 1992],
+                              [255, 255, 260], ["no_kill", "no_kill", "default"], True, 2, 3,
                               {"plant_years": [1990, 1991, 1992], "plant_days": [120, 121, 121],
                                "harvest_years": [1990, 1991, 1992],
                                "harvest_days": [255, 255, 260],
                                "harvest_ops": ["no_kill", "no_kill", "default"]}),
-                             ("greens", 1999, 130, 2000, 220, "default", False, 0, 10, {"plant_years": [1999],
-                                                                                        "plant_days": [130],
-                                                                                        "harvest_years": [2000],
-                                                                                        "harvest_days": [220],
-                                                                                        "harvest_ops": ["default"]})
+                             ("test_3", "greens", 1999, 130, 2000, 220, "default", False, 0, 10, {"plant_years": [1999],
+                                                                                                  "plant_days": [130],
+                                                                                                  "harvest_years": [
+                                                                                                      2000],
+                                                                                                  "harvest_days": [220],
+                                                                                                  "harvest_ops": [
+                                                                                                      "default"]})
                          ])
-def test_crop_schedule_init(crop_ref: str, plant_years: int | List[int], plant_days: int | List[int],
+def test_crop_schedule_init(name: str, crop_ref: str, plant_years: int | List[int], plant_days: int | List[int],
                             harvest_years: int | List[int], harvest_days: int | List[int], harvest_ops: str | List[str],
                             heat_sched: bool, pat_skip: int, pat_repeat: int, expected: Dict) -> None:
     """Tests that CropSchedule's get initialized correctly."""
-    crop_schedule = CropSchedule(crop_ref, plant_years, plant_days, harvest_years, harvest_days, harvest_ops,
+    crop_schedule = CropSchedule(name, crop_ref, plant_years, plant_days, harvest_years, harvest_days, harvest_ops,
                                  heat_sched, pat_skip, pat_repeat)
+    assert crop_schedule.name == name
     assert crop_schedule.crop_reference == crop_ref
     assert crop_schedule.planting_years == expected.get("plant_years")
     assert crop_schedule.planting_days == expected.get("plant_days")
@@ -57,21 +60,24 @@ def test_crop_schedule_init_error(plant_years: int | List[int], plant_days: int 
                                   harvest_ops: str | List[str], pat_skip: int, pat_repeat: int, expected: str) -> None:
     """Tests that errors are correctly raised when invalid input is passed."""
     with pytest.raises(ValueError) as e:
-        CropSchedule("test", plant_years, plant_days, harvest_years, harvest_days, harvest_ops, False, pat_skip,
-                     pat_repeat)
+        CropSchedule("test_name", "test_crop", plant_years, plant_days, harvest_years, harvest_days, harvest_ops, False,
+                     pat_skip, pat_repeat)
     assert str(e.value) == expected
 
 
 @pytest.mark.parametrize("years,days,heat_scheduled,skip,repeat,expected", [
-    ([1, 3, 4], [120, 100, 100], False, 1, 1, [PlantingEvent("test", 1, 120, False),
-                                               PlantingEvent("test", 3, 100, False),
-                                               PlantingEvent("test", 4, 100, False),
-                                               PlantingEvent("test", 6, 120, False),
-                                               PlantingEvent("test", 8, 100, False),
-                                               PlantingEvent("test", 9, 100, False)]),
-    ([2, 4], [115, 115], True, 2, 2, [PlantingEvent("test", 2, 115, True), PlantingEvent("test", 4, 115, True),
-                                      PlantingEvent("test", 7, 115, True), PlantingEvent("test", 9, 115, True),
-                                      PlantingEvent("test", 12, 115, True), PlantingEvent("test", 14, 115, True)]),
+    ([1, 3, 4], [120, 100, 100], False, 1, 1, [PlantingEvent("test_crop", 1, 120, False),
+                                               PlantingEvent("test_crop", 3, 100, False),
+                                               PlantingEvent("test_crop", 4, 100, False),
+                                               PlantingEvent("test_crop", 6, 120, False),
+                                               PlantingEvent("test_crop", 8, 100, False),
+                                               PlantingEvent("test_crop", 9, 100, False)]),
+    ([2, 4], [115, 115], True, 2, 2, [PlantingEvent("test_crop", 2, 115, True),
+                                      PlantingEvent("test_crop", 4, 115, True),
+                                      PlantingEvent("test_crop", 7, 115, True),
+                                      PlantingEvent("test_crop", 9, 115, True),
+                                      PlantingEvent("test_crop", 12, 115, True),
+                                      PlantingEvent("test_crop", 14, 115, True)]),
     ([1, 2, 5], [120, 120, 110], False, 2, 0, [PlantingEvent("test", 1, 120, False),
                                                PlantingEvent("test", 2, 120, False),
                                                PlantingEvent("test", 5, 110, False)])
@@ -79,7 +85,8 @@ def test_crop_schedule_init_error(plant_years: int | List[int], plant_days: int 
 def test_generate_planting_events(years: List[int], days: List[int], heat_scheduled: bool, skip: int,
                                   repeat: int, expected: List[PlantingEvent]) -> None:
     """Tests that planting events are correctly generated by CropSchedule objects."""
-    crop_sched = CropSchedule("test", years, days, [1], [240], ["default"], heat_scheduled, skip, repeat)
+    crop_sched = CropSchedule("test_name", "test_crop", years, days, [1], [240], ["default"], heat_scheduled, skip,
+                              repeat)
     actual = crop_sched.generate_planting_events()
     assert actual == expected
 
@@ -102,7 +109,8 @@ def test_generate_planting_events(years: List[int], days: List[int], heat_schedu
 def test_generate_harvest_events(years: List[int], days: List[int], harvest_ops: List[str], heat_scheduled: bool,
                                  skip: int, repeat: int, expected: List[HarvestEvent]) -> None:
     """Tests that harvest events are correctly generated by CropSchedule objects."""
-    crop_sched = CropSchedule("test", [1], [120], years, days, harvest_ops, heat_scheduled, skip, repeat)
+    crop_sched = CropSchedule("test_name", "test_crop", [1], [120], years, days, harvest_ops, heat_scheduled, skip,
+                              repeat)
     actual = crop_sched.generate_harvest_events()
     assert actual == expected
 
