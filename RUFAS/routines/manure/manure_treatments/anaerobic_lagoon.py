259c6c3477a091984e610046a90c7b0fd05f6565
lpham <98855117+ltp23@users.noreply.github.com>
2023-06-08 23:48:01 -0700

    Update anaerobic_lagoon.py

--- RUFAS/routines/manure/manure_treatments/anaerobic_lagoon.py
+++ RUFAS/routines/manure/manure_treatments/anaerobic_lagoon.py
@@ -240,7 +240,16 @@ class AnaerobicLagoon(BaseManureTreatment):
         abc = self._calc_abc()
         a, b, c = abc[0], abc[1], abc[2]
         discriminant = b ** 2 - 4 * a * c
-        return (-b + discriminant ** 0.5) / (2 * a)
+
+        if discriminant < 0:
+            # raise ValueError("No real solution exists for these parameters.")
+            return 0.0
+        else:
+            solution = (-b + discriminant ** 0.5) / (2 * a)
+            if solution < 0:
+                solution = (-b - discriminant ** 0.5) / (2 * a)
+            # Ensuring that the solution is a float
+            return float(solution.real)
 
     @property
     def lagoon_length(self):
