4f73c6f0ce4d6ef7f496fa547a1cbc5fb0d0fcaa
Ed Hansen <isgotkowitz@gmail.com>
2023-06-14 10:10:35 -0500

    changed call to validate days

--- SC_redesign/Crop_and_Soil/manager/manure_schedule.py
+++ SC_redesign/Crop_and_Soil/manager/manure_schedule.py
@@ -97,7 +97,7 @@ class ManureSchedule(Schedule):
             raise ValueError(error_header + f"expected all years to be > 0 and in non-descending order, received "
                                             f"'{self.years}'.")
 
-        valid_days = self._validate_days(self.days)
+        valid_days = self._validate_days(self.years, self.days)
         if not valid_days:
             raise ValueError(error_header + f"expected all days to be in range [1, 366], received '{self.days}'.")
 
