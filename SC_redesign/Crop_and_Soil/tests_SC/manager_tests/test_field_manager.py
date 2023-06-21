a9760557438d337bbe66697941abf7ef70d14455
MatthewLiu <matthew7838@gmail.com>
2023-06-22 05:51:48 +0800

    Finished annual

--- SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_field_manager.py
+++ SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_field_manager.py
@@ -3,8 +3,6 @@ from SC_redesign.Crop_and_Soil.field.field_data import FieldData
 from SC_redesign.Crop_and_Soil.field.field import Field
 from RUFAS.classes import Time
 import pytest
-from typing import List, Any
-from RUFAS.output_manager import OutputManager
 from SC_redesign.Crop_and_Soil.manager.current_weather import CurrentWeather
 from unittest.mock import MagicMock
 
@@ -19,7 +17,32 @@ def test_daily_update_routine(fields: Field):
     mocked_weather = MagicMock(CurrentWeather)
     setattr(mocked_time, "calendar_year", 1998)
     setattr(mocked_time, "day", 5)
-    Field.manage_field = MagicMock()
+    for field in fields:
+        field.manage_field = MagicMock()
     fm = FieldManager(fields=fields)
+    fm.om.send_daily_variables = MagicMock()
     fm.daily_update_routine(current_weather=mocked_weather, time=mocked_time)
-    assert Field.manage_field.call_count == len(fields)
+    for field in fields:
+        assert field.manage_field.call_count == 1
+    assert fm.om.send_daily_variables.call_count == 1
+
+
+@pytest.mark.parametrize("fields", [
+    [Field(field_data=FieldData(name="field1")), Field(field_data=FieldData(name="field2")),
+     Field(field_data=FieldData(name="field3"))],
+    []
+])
+def test_annual_update_routine(fields: Field):
+    mocked_time = MagicMock(Time)
+    mocked_weather = MagicMock(CurrentWeather)
+    setattr(mocked_time, "calendar_year", 1998)
+    setattr(mocked_time, "day", 5)
+    for field in fields:
+        field.perform_annual_reset = MagicMock()
+    fm = FieldManager(fields=fields)
+    fm.om.send_annual_variables = MagicMock()
+    fm.annual_update_routine()
+    for field in fields:
+        assert field.perform_annual_reset.call_count == 1
+    assert fm.om.send_annual_variables.call_count == 1
+
