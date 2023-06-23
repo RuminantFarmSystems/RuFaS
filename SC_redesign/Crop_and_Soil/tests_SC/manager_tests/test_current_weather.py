9a6468d3cd88eff2158d53f7f0c1f438ca5d6c17
MatthewLiu <matthew7838@gmail.com>
2023-06-23 18:59:38 +0800

    weather test

--- SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_current_weather.py
+++ SC_redesign/Crop_and_Soil/tests_SC/manager_tests/test_current_weather.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock
 import pytest
+
 from SC_redesign.Crop_and_Soil.manager.current_weather import CurrentWeather
 from RUFAS.classes import Weather
 
@@ -8,7 +9,7 @@ from RUFAS.classes import Weather
     (1, 2, 3, 4, 5, 6, 7, 2018, 9, 10)
 ])
 def test_check_current_weather(radiation: float, T_min: float, T_avg: float, T_max: float, T_avg_annual: float,
-                               rainfall: float, latitude: float, year: int, day: int, month: int) -> None:
+                               rainfall: float, latitude: float, year: int, day: int, month: int, mocker) -> None:
     """Tests that a weather object was successfully translated into a current weather object"""
 
     mocked_weather = MagicMock(Weather)
@@ -18,6 +19,8 @@ def test_check_current_weather(radiation: float, T_min: float, T_avg: float, T_m
     setattr(mocked_weather, "T_max", T_max)
     setattr(mocked_weather, "T_avg_annual", T_avg_annual)
     setattr(mocked_weather, "rainfall", rainfall)
+    patch_determine_daylength = mocker.patch('SC_redesign.Crop_and_Soil.manager.current_weather._determine_daylength',
+                                             return_value=12.0)
     CurrentWeather.check_current_weather(weather=mocked_weather, latitude=latitude, year=year,
                                          month=month, day=day)
 
@@ -27,3 +30,6 @@ def test_check_current_weather(radiation: float, T_min: float, T_avg: float, T_m
     assert CurrentWeather.mean_air_temperature == T_avg
     assert CurrentWeather.max_air_temperature == T_max
     assert CurrentWeather.annual_mean_air_temperature == T_avg_annual
+    assert patch_determine_daylength.call_count == 1
+    assert CurrentWeather.daylength == 12
+
