145d9321034459644a91c46fc1d94daf1f7cd544
lpham <98855117+ltp23@users.noreply.github.com>
2023-07-03 19:14:02 -0700

    Create manure_types.py

---
+++ RUFAS/routines/manure/manure_treatments/manure_types.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ManureType(Enum):
+    """
+    This is an Enum class that represents different types of manure.
+
+    The Enum class contains the following members:
+    LIQUID: Represents liquid manure.
+    SLURRY: Represents manure in a semi-liquid, slurry state.
+    SOLID: Represents manure in solid form.
+
+    """
+    LIQUID = 'liquid'
+    SLURRY = 'slurry'
+    SOLID = 'solid'
