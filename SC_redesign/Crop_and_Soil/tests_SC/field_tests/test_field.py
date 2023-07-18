67d66d88705c888751339ee0155dfb33f688d50e
Ed Hansen <isgotkowitz@gmail.com>
2023-07-18 10:52:22 -0500

    shifted values from info maps to values

--- SC_redesign/Crop_and_Soil/tests_SC/field_tests/test_field.py
+++ SC_redesign/Crop_and_Soil/tests_SC/field_tests/test_field.py
@@ -305,17 +305,17 @@ def test_plant_crop(crop_reference: str, heat_scheduled: bool, custom_crop_specs
 @pytest.mark.parametrize("crop_reference,heat_scheduled,species,year,day,field_name,field_size,expected_info_map,"
                          "expected_value", [
                              ("ref_1", False, "species_1", 1993, 100, "name_1", 1.3,
-                              {"prefix": "field:'name_1'", "field_size": 1.3, "date": {"year": 1993, "day": 100},
-                               "species": "species_1"},
-                              {"crop_reference": "ref_1", "heat_scheduled_harvest": False}),
+                              {"prefix": "field:'name_1'", "field_size": 1.3, "species": "species_1"},
+                              {"crop_reference": "ref_1", "heat_scheduled_harvest": False,
+                               "date": {"year": 1993, "day": 100}}),
                              ("ref_2", True, "custom_alien_species", 1996, 120, "name_2", 2.55,
-                              {"prefix": "field:'name_2'", "field_size": 2.55, "date": {"year": 1996, "day": 120},
-                               "species": "custom_alien_species"},
-                              {"crop_reference": "ref_2", "heat_scheduled_harvest": True}),
+                              {"prefix": "field:'name_2'", "field_size": 2.55, "species": "custom_alien_species"},
+                              {"crop_reference": "ref_2", "heat_scheduled_harvest": True,
+                               "date": {"year": 1996, "day": 120}}),
                              ("ref_3", False, "custom_corn", 2008, 122, "name_3", 0.95,
-                              {"prefix": "field:'name_3'", "field_size": 0.95, "date": {"year": 2008, "day": 122},
-                               "species": "custom_corn"},
-                              {"crop_reference": "ref_3", "heat_scheduled_harvest": False})
+                              {"prefix": "field:'name_3'", "field_size": 0.95, "species": "custom_corn"},
+                              {"crop_reference": "ref_3", "heat_scheduled_harvest": False,
+                               "date": {"year": 2008, "day": 122}})
                          ])
 def test_record_planting(crop_reference: str, heat_scheduled: bool, species: str, year: int, day: int, field_name: str,
                          field_size: float, expected_info_map: Dict, expected_value: Dict) -> None:
