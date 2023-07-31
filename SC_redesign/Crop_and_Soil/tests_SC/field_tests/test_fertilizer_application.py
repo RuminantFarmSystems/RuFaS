b0ee808a46f0a2c6e8a43e24218ed2966a1e4b50
Ed Hansen <isgotkowitz@gmail.com>
2023-07-31 11:11:21 -0500

    fixed apply fertilizer test

--- SC_redesign/Crop_and_Soil/tests_SC/field_tests/test_fertilizer_application.py
+++ SC_redesign/Crop_and_Soil/tests_SC/field_tests/test_fertilizer_application.py
@@ -5,13 +5,14 @@ from unittest.mock import MagicMock
 from SC_redesign.Crop_and_Soil.field.fertilizer_application import FertilizerApplication
 
 
-@pytest.mark.parametrize("phosphorus,fertilizer,inorganic_nitrogen_frac,ammonium_frac,organic_nitrogen_frac,expected", [
-    (15, 90, 0.15, 0.44, 0.09, [7.56, 5.94, 4.05]),
-    (22.1, 144, 0.33, 0.2, 0.06, [38.016, 9.504, 4.32]),
-    (0, 0, 0, 0, 0, [0, 0, 0])
-])
+@pytest.mark.parametrize("phosphorus,fertilizer,inorganic_nitrogen_frac,ammonium_frac,organic_nitrogen_frac,depth,"
+                         "remainder,expected", [
+                             (15, 90, 0.15, 0.44, 0.09, 0.0, 1.0, [7.56, 5.94, 4.05]),
+                             (22.1, 144, 0.33, 0.2, 0.06, 40.0, 0.88, [38.016, 9.504, 4.32]),
+                             (0, 0, 0, 0, 0, 0.0, 1.0, [0, 0, 0])
+                         ])
 def test_apply_fertilizer(phosphorus: float, fertilizer: float, inorganic_nitrogen_frac: float, ammonium_frac: float,
-                          organic_nitrogen_frac: float, expected: List[float]) -> None:
+                          organic_nitrogen_frac: float, depth: float, remainder: float, expected: List[float]) -> None:
     """Tests that fertilizer is applied correctly."""
     field_size = 1.7
     fert_app = FertilizerApplication(field_size=field_size)
@@ -23,7 +24,7 @@ def test_apply_fertilizer(phosphorus: float, fertilizer: float, inorganic_nitrog
     fert_app.soil.phosphorus_cycling.fertilizer.add_fertilizer_phosphorus = MagicMock()
 
     fert_app.apply_fertilizer(phosphorus, fertilizer, inorganic_nitrogen_frac, ammonium_frac, organic_nitrogen_frac,
-                              field_size)
+                              depth, remainder, field_size)
 
     fert_app.soil.phosphorus_cycling.fertilizer.add_fertilizer_phosphorus.assert_called_once_with(phosphorus)
     assert pytest.approx(fert_app.soil.data.soil_layers[0].nitrate_content) == expected[0] / field_size
