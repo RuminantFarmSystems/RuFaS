162aa7b8964ab43ea7e1fc775f5148fc80f408b6
Ed Hansen <isgotkowitz@gmail.com>
2023-08-03 10:52:05 -0500

    new files in RUFAS/rountines/field

---
+++ RUFAS/routines/field/soil/nitrogen_cycling/denitrification.py
@@ -0,0 +1,96 @@
+from typing import Optional
+from math import exp
+
+from SC_redesign.Crop_and_Soil.soil.soil_data import SoilData
+
+"""
+This module handles the denitrification of nitrogen in the nitrates pool, based on SWAT section 3:1.4.
+"""
+
+
+class Denitrification:
+
+    def __init__(self, soil_data: Optional[SoilData] = None, field_size: Optional[float] = None):
+        """Initializes the SoilData object that this module will work with, or creates one if none is provided.
+
+        Parameters
+        ----------
+        soil_data : SoilData, optional
+            The SoilData object used by this module to track the denitrification of nitrates in the soil profile,
+            creates new one if one is not provided.
+        field_size : float, optional
+            Size of the field (ha)
+
+        Notes
+        -----
+        The field size is used to initialize a SoilData object for this module to work with, if a pre-configured
+        SoilData object is not provided.
+
+        """
+        self.data = soil_data or SoilData(field_size=field_size)
+
+    def denitrify(self) -> None:
+        """Conducts the daily denitrification operations.
+
+        References
+        ----------
+        SWAT Theoretical documentation section 3:1.4, eqn. 3:1.4.1, 2
+
+        Notes
+        -----
+        The SWAT Theoretical documentation defines denitrification as "the bacterial reduction of nitrate, NO3-, to N2
+        or N2O gases" (page 194). This method conducts denitrification by calculating the amount of nitrate that is
+        denitrified, then removing that amount from the nitrate pool and adding it to denitrification emissions tracker.
+
+        """
+        for layer in self.data.soil_layers:
+            nutrient_is_below_threshold = layer.nutrient_cycling_water_factor < \
+                                          layer.denitrification_threshold_water_content
+            if nutrient_is_below_threshold:
+                continue
+
+            # TODO: check that soil_overall_carbon_fraction is being used correctly in this context - issue #475
+            nitrified_nitrates = self._calculate_denitrification_amount(layer.nitrate_content,
+                                                                        layer.denitrification_rate_coefficient,
+                                                                        layer.nutrient_cycling_temp_factor,
+                                                                        layer.soil_overall_carbon_fraction * 100)
+            layer.nitrate_content -= nitrified_nitrates
+            layer.annual_denitrified_nitrogen_total += nitrified_nitrates
+
+    @staticmethod
+    def _calculate_denitrification_amount(nitrate_content: float, denitrification_rate_coefficient: float,
+                                          temp_factor: float, percent_organic_carbon_content: float) -> float:
+        """Calculates the amount of nitrate lost to denitrification.
+
+        Parameters
+        ----------
+        nitrate_content : float
+            The nitrate content of this soil layer (kg / ha)
+        denitrification_rate_coefficient : float
+            Rate coefficient that regulates denitrification in this layer of soil (unitless)
+        temp_factor : float
+            The nutrient cycling temperature factor of this soil layer (unitless)
+        percent_organic_carbon_content : float
+            The percent of this soil layer that is made up of organic carbon, in the range [0, 100] (unitless)
+
+        Returns
+        -------
+        float
+            The amount of nitrate that is denitrified in this soil layer (kg / ha)
+
+        References
+        ----------
+        SWAT Theoretical documentation eqn. 3:1.4.1
+
+        Notes
+        -----
+        This calculates the fraction of nitrates lost to nitrification as nitrification factor, and bounds it to be in
+        the range [0.0, 1.0]. 0 is the minimum because a negative denitrification factor would indicate nitrous gases
+        turning back into nitrate, which is not an operation that is handled by this module. 1 is the maximum because it
+        is physically impossible to remove more nitrate than there is in the soil.
+
+        """
+        exponential_term = exp(-1 * denitrification_rate_coefficient * temp_factor * percent_organic_carbon_content)
+        denitrification_factor = 1 - exponential_term
+        bounded_denitrification_factor = max(min(1.0, denitrification_factor), 0.0)
+        return nitrate_content * bounded_denitrification_factor
