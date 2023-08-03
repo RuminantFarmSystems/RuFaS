f657a13a2b141aa4c27013a555051de1a44df9b4
Ed Hansen <isgotkowitz@gmail.com>
2023-08-03 10:52:05 -0500

    new files in RUFAS/rountines/field

---
+++ RUFAS/routines/field/manager/fertilizer_schedule.py
@@ -0,0 +1,172 @@
+from typing import List, Any
+
+from SC_redesign.Crop_and_Soil.manager.schedule import Schedule
+from SC_redesign.Crop_and_Soil.manager.events import FertilizerEvent
+from RUFAS.output_manager import OutputManager
+
+"""
+This module contains `FertilizerSchedule`, a `Schedule` child class that defines when and how much fertilizer will be
+applied to a field.
+"""
+
+om = OutputManager()
+
+
+class FertilizerSchedule(Schedule):
+
+    def __init__(self, name: str, mix_names: List[str], years: List[int], days: List[int], nitrogen_masses: List[float],
+                 phosphorus_masses: List[float], application_depths: List[float] = None,
+                 surface_remainder_fractions: List[float] = None, pattern_skip: int = 0, pattern_repeat: int = 0):
+        """
+        Creates and validates a fertilizer application schedule.
+
+        Parameters
+        ----------
+        name : str
+            Name of this fertilizer application schedule.
+        mix_names : List[str]
+            Name(s) of the specific fertilizer mix that fertilizer application is composed of.
+        years : List[int]
+            Year(s) in which fertilizer will be applied.
+        days : List[int]
+            Julian day(s) in which fertilizer will be applied.
+        nitrogen_masses : List[float]
+            Minimum mass(es) of nitrogen in applications (kg)
+        phosphorus_masses : List[float]
+            Minimum mass(es) of phosphorus in applications (kg)
+        application_depths : List[float], default=None
+            Bottom depth(s) of fertilizer injection applications (mm)
+        surface_remainder_fractions : List[float], default=None
+            Fraction(s) of fertilizer application that remain on the soil surface (unitless)
+
+        Notes
+        -----
+        Application depths and surface remainder fractions actually have defaults of [0.0] and [1.0] respectively, but
+        there are not specified in the signature because parameters cannot have mutable defaults.
+
+        """
+        super().__init__(name, years, days, pattern_skip, pattern_repeat)
+
+        self.mix_names = self._elongate_list(mix_names, len(years))
+        self.nitrogen_masses = self._elongate_list(nitrogen_masses, len(years))
+        self.phosphorus_masses = self._elongate_list(phosphorus_masses, len(years))
+
+        if application_depths is None:
+            application_depths = [0.0]
+        self.application_depths = self._elongate_list(application_depths, len(years))
+
+        if surface_remainder_fractions is None:
+            surface_remainder_fractions = [1.0]
+        self.surface_remainder_fractions = self._elongate_list(surface_remainder_fractions, len(years))
+
+        self._validate_fertilizer_parameters()
+
+        self._validate_pattern_parameters()
+
+    def _validate_fertilizer_parameters(self) -> None:
+        """
+        Checks that all fields defining a fertilizer application schedule are valid, raises errors if not.
+
+        Raises
+        ------
+        ValueError
+            If not all fertilizer application years are valid.
+        ValueError
+            If not all fertilizer application days are valid.
+        ValueError
+            If not all fertilizer nitrogen masses are valid.
+        ValueError
+            If not all fertilizer phosphorus masses are valid.
+        ValueError
+            If not all fertilizer application depths are valid.
+        ValueError
+            If not all fertilizer surface retention fractions are valid.
+        ValueError
+            If not all fertilizer application parameters have the same length.
+
+        """
+        error_header = f"'{self.name}': "
+
+        valid_years = self._validate_years(self.years)
+        if not valid_years:
+            raise ValueError(error_header + f"expected all years to be > 0 and in non-descending order, received "
+                                            f"'{self.years}'.")
+
+        valid_days = self._validate_days(self.years, self.days)
+        if not valid_days:
+            raise ValueError(error_header + f"expected all days to be in range [1, 366], received '{self.days}'.")
+
+        valid_nitrogen_masses = self._determine_if_all_non_negative_values(self.nitrogen_masses)
+        if not valid_nitrogen_masses:
+            raise ValueError(error_header + f"expected all nitrogen masses to be in >= 0, received "
+                                            f"'{self.nitrogen_masses}'.")
+
+        valid_phosphorus_masses = self._determine_if_all_non_negative_values(self.phosphorus_masses)
+        if not valid_phosphorus_masses:
+            raise ValueError(error_header + f"expected all phosphorus masses to be >= 0, received "
+                                            f"'{self.phosphorus_masses}'.")
+
+        valid_depths = self._determine_if_all_non_negative_values(self.application_depths)
+        if not valid_depths:
+            raise ValueError(error_header + f"expected all application depths to be >= 0, received "
+                                            f"'{self.application_depths}'.")
+
+        valid_fractions = all(0.0 <= fraction <= 1.0 for fraction in self.surface_remainder_fractions)
+        if not valid_fractions:
+            raise ValueError(error_header + f"expected all surface remainder fractions to be in range [0.0, 1.0], "
+                                            f"received '{self.surface_remainder_fractions}'.")
+
+        equal_fertilizer_parameters = len(self.years) == len(self.days) == len(self.mix_names) == \
+            len(self.nitrogen_masses) == len(self.phosphorus_masses) == len(self.application_depths) == \
+            len(self.surface_remainder_fractions)
+        if not equal_fertilizer_parameters:
+            raise ValueError(error_header + f"expected equal numbers of fertilizer application parameters, received "
+                                            f"'{self.years}' years, '{self.days}' days, '{self.mix_names}' mix names, "
+                                            f"'{self.nitrogen_masses}' nitrogen masses, '{self.phosphorus_masses}' "
+                                            f"phosphorus masses, '{self.application_depths}' application depths, and "
+                                            f"'{self.surface_remainder_fractions}' surface remainder fractions.")
+
+    def generate_fertilizer_events(self) -> List[FertilizerEvent]:
+        """
+        Creates a list of all fertilizer application events that will occur as dictated by this fertilizer schedule.
+
+        Returns
+        -------
+        List[FertilizerEvent]
+            List of all fertilizer events that occur over the course of this fertilizer schedule.
+
+        """
+        all_years = self._repeat_pattern(self.years, self.pattern_skip, self.pattern_repeat)
+        all_days = self.days * (self.pattern_repeat + 1)
+        all_mix_names = self.mix_names * (self.pattern_repeat + 1)
+        all_nitrogen_masses = self.nitrogen_masses * (self.pattern_repeat + 1)
+        all_phosphorus_masses = self.phosphorus_masses * (self.pattern_repeat + 1)
+        all_depths = self.application_depths * (self.pattern_repeat + 1)
+        all_surface_fractions = self.surface_remainder_fractions * (self.pattern_repeat + 1)
+        all_events = list(zip(all_mix_names, all_years, all_days, all_nitrogen_masses, all_phosphorus_masses,
+                              all_depths, all_surface_fractions))
+
+        fertilizer_events = []
+        for event in all_events:
+            new_event = FertilizerEvent(mix_name=event[0], year=event[1], day=event[2], nitrogen_mass=event[3],
+                                        phosphorus_mass=event[4], depth=event[5], surface_remainder_fraction=event[6])
+            fertilizer_events.append(new_event)
+        return fertilizer_events
+
+    @staticmethod
+    def _determine_if_all_non_negative_values(values: List[Any]) -> bool:
+        """
+        Checks that all values in a list are >= 0.
+
+        Parameters
+        ----------
+        values : List[Any]
+            List of values to be checked.
+
+        Returns
+        -------
+        bool
+            True if all values are >= 0, False otherwise.
+
+        """
+        return all(value >= 0 for value in values)
---
+++ RUFAS/routines/field/soil/soil.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+from typing import Optional
+
+from SC_redesign.Crop_and_Soil.soil.carbon_cycling.carbon_cycle import CarbonCycling
+from SC_redesign.Crop_and_Soil.soil.soil_data import SoilData
+from SC_redesign.Crop_and_Soil.soil.evaporation import Evaporation
+from SC_redesign.Crop_and_Soil.soil.infiltration import Infiltration
+from SC_redesign.Crop_and_Soil.soil.percolation import Percolation
+from SC_redesign.Crop_and_Soil.soil.soil_temp import SoilTemp
+from SC_redesign.Crop_and_Soil.soil.soil_erosion import SoilErosion
+from SC_redesign.Crop_and_Soil.soil.phosphorus_cycling.phosphorus_cycling import PhosphorusCycling
+from SC_redesign.Crop_and_Soil.soil.nitrogen_cycling.nitrogen_cycling import NitrogenCycling
+
+
+class Soil:
+    def __init__(self, soil_data: Optional[SoilData] = None, field_size: Optional[float] = None):
+        """Creates a Soil object based on a SoilData object.
+
+        Parameters
+        ----------
+        soil_data: a SoilData object containing initial attribute values as well as attributes tracked and updated
+            throughout the simulation
+        field_size : float, optional
+            Used to initialize a SoilData object for this module to work with, if a pre-configured SoilData object is
+            not provided (ha)
+
+        Notes
+        -----
+        If no SoilData object is passed, default configuration is used.
+
+        """
+        self.data = soil_data or SoilData(field_size=field_size)
+        """object that tracks all soil variable throughout the simulation"""
+
+        # Process components
+        self.soil_temp = SoilTemp(self.data)
+        """Process component that tracks and updates the temperatures within the soil profile"""
+        self.phosphorus_cycling = PhosphorusCycling(self.data)
+        """Process component managing phosphorus on top of and in the soil profile"""
+        self.carbon_cycling = CarbonCycling(self.data)
+        """Process component that handles carbon cycling (through decomposition) in the soil."""
+        # TODO: need to add phosphorus, manure, and carbon cycling main methods methods to the soil methods.
+        #   It is unclear to me how best to do that.
+        self.nitrogen_cycling = NitrogenCycling(self.data)
+        """Process component for managing nitrogen within the soil profile."""
+
+        # Water components
+        self.evaporation = Evaporation(self.data)
+        """Process component that controls evaporation from the soil"""
+        self.infiltration = Infiltration(self.data)
+        """Process component that controls water infiltration from the soil surface into the profile"""
+        self.percolation = Percolation(self.data)
+        """Process component that controls percolation of water from upper layers to lower layers"""
+        self.soil_erosion = SoilErosion(self.data)
+        """Process component that track erosion from the soil profile"""
+
+    def daily_soil_routine(self, solar_radiation: float, avg_temp: float, min_temp: float, max_temp: float,
+                           plant_cover: float, snow_cover: float, avg_annual_air_temp: float) -> None:
+        """this method calls all now-water related daily update routines
+
+        Args:
+            solar_radiation: solar radiation reaching the ground on the current day (MJ per square meter per day)
+            avg_temp: average temperature of the current day (degrees C)
+            min_temp: minimum temperature of the current day (degrees C)
+            max_temp: maximum temperature of the current day (degrees C)
+            plant_cover: total above-ground plant biomass and residue on the current day (kg per hectare)
+            snow_cover: water content of the snow cover on the current day (mm)
+            avg_annual_air_temp: average annual air temperature (degrees C)
+        """
+        # TODO: if no other daily update methods are added here, this method should be removed and Field should call
+        #       this method directly
+        self.soil_temp.daily_soil_temperature_update(solar_radiation, avg_temp, min_temp, max_temp, plant_cover,
+                                                     snow_cover, avg_annual_air_temp)
+
+    def daily_soil_water_routine(self, rainfall: float, weighting_coefficient: float,
+                                 potential_evapotranspiration: float, has_seasonal_high_water_table: bool,
+                                 maximum_soil_evaporation: float, avg_air_temp: float, residue: float,
+                                 minimum_cover_management_factor: float, field_size: float) -> None:
+        """this method calls all water related daily update routines
+
+        Args:
+            rainfall: rainfall depth of current day (mm)
+            weighting_coefficient: weighting coefficient used to calculate retention coefficient for daily curve number
+                calculations dependent on plant evapotranspiration (unitless)
+            potential_evapotranspiration: total potential evaporation and transpiration that can occur on the current
+                day (mm)
+            has_seasonal_high_water_table: if the HRU has a seasonal high water table (true/false)
+            maximum_soil_evaporation: maximum amount of water that can be evaporated from the soil profile on the
+                current day (mm)
+            avg_air_temp: average air temperature (degrees C)
+            residue: biomass separated from plant on the ground (kg per hectare)
+            minimum_cover_management_factor: minimum value for cover and management factor for water erosion applicable
+                to land cover/plant (unitless)
+            field_size: size of the field (ha)
+
+        Notes
+        -----
+        The daily phosphorus cycling method is called here because in large part the phosphorus dynamics of the soil
+        profile depend on how much water enters and move through the soil profile.
+
+        """
+        self.infiltration.infiltrate(rainfall, weighting_coefficient, potential_evapotranspiration)
+        self.percolation.percolate(has_seasonal_high_water_table)
+        self.evaporation.evaporate(maximum_soil_evaporation)
+        self.soil_erosion.erode(field_size, minimum_cover_management_factor, residue, rainfall)
+        self.phosphorus_cycling.cycle_phosphorus(rainfall, self.data.accumulated_runoff, field_size, avg_air_temp)
+        self.nitrogen_cycling.cycle_nitrogen(field_size)
+        self.carbon_cycling.cycle_carbon(rainfall, avg_air_temp, field_size)
