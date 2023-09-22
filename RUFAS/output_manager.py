85526411778bb55ee9a4719a9a42e55541a28a29
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-09-22 10:22:44 -0400

    Brought dump_errors() call to the last

--- RUFAS/output_manager.py
+++ RUFAS/output_manager.py
@@ -789,12 +789,12 @@ class OutputManager(object):
         self, path: str, exclude_info_maps: bool = False
     ) -> None:
         """
-        Dumps all non-data pool into the given path to a directory.
+        Dumps all non-data pools into the given path to a directory.
         """
         self.dump_variable_names_and_contexts(path, exclude_info_maps)
-        self.dump_errors(path)
         self.dump_logs(path)
         self.dump_warnings(path)
+        self.dump_errors(path)
 
     def flush_pools(self) -> None:
         """
