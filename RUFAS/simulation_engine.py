6a868ebf516e44e91a25f8ce90f2f09060d9a8d9
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-10-13 14:15:35 -0400

    call recording methods in sim engine, tested

--- RUFAS/simulation_engine.py
+++ RUFAS/simulation_engine.py
@@ -55,6 +55,9 @@ class SimulationEngine:
         self.state.field_manager.daily_update_routine(self.weather, self.time)
         routines.daily_feed_routine(self.state.feed, self.state.field_manager, self.state.animal_manager)
 
+        self.time.record_time()
+        self.weather.record_weather(self.time.year, self.time.day)
+
         self._advance_time()
 
     def _advance_time(self, print_day: Optional[bool] = False) -> None:
