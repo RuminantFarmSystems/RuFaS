be88f8f6d2ea5866fde2dabf7328dabf59466430
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-10-13 13:07:20 -0400

    added weather test

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,6 +1,54 @@
 import pytest
+from unittest.mock import patch, MagicMock
 
-from RUFAS.classes import Weather
+from RUFAS.classes import Weather, Config
+from RUFAS.output_manager import OutputManager
+
+
+@pytest.fixture
+def mock_output_manager() -> OutputManager:
+    output_manager = OutputManager()
+    return output_manager
+
+
+@pytest.fixture
+def mock_weather_input() -> dict:
+    weather_data = {
+        "year": [1],
+        "jday": [1, 2, 3, 4, 5],
+        "precip": [0.0] * 5,
+        "high": [0.0] * 5,
+        "low": [0.0] * 5,
+        "avg": [0.0] * 5,
+        "Hday": [0.0] * 5,
+        "irrigation": [0.0] * 5,
+    }
+    return weather_data
+
+
+@pytest.fixture
+def mock_config() -> Config:
+    mock_config = MagicMock(Config)
+    setattr(mock_config, "years", [[1]])
+    setattr(mock_config, "w_start_year", 0)
+    setattr(mock_config, "w_start_day", 1)
+    setattr(mock_config, "start_year", 1)
+    setattr(mock_config, "start_day", 1)
+    setattr(mock_config, "end_year", 1)
+    setattr(mock_config, "end_day", 5)
+    setattr(mock_config, "year_length", 365)
+    setattr(mock_config, "leap_year_length", 366)
+    return mock_config
+
+
+def test_annual_average_temperature_recording(mock_output_manager: OutputManager, mock_weather_input: dict,
+                                              mock_config: Config) -> None:
+    """Tests that the annual average temperature is recorded correctly to the OutputManager when Weather is created."""
+    with patch("RUFAS.classes.Weather._calculate_average_annual_temperature") as avg, \
+            patch("RUFAS.output_manager.OutputManager.add_variable") as record:
+        _weather = Weather(mock_weather_input, mock_config)
+        avg.assert_called_once()
+        record.assert_called_once()
 
 
 @pytest.mark.parametrize("avg_daily_temperatures,expected", [
