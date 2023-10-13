56fcdccaba241a4365cc89d4705b9b9dc4c2528a
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-10-13 13:43:58 -0400

    moved record weather to weather, tests

--- RUFAS/classes.py
+++ RUFAS/classes.py
@@ -247,6 +247,28 @@ class Weather:
         info_map = {"class": self.__class__.__name__, "function": self.__init__.__name__, "prefix": "Weather"}
         om.add_variable("average_annual_temperature(C)", self.T_avg_annual, info_map)
 
+    def record_weather(self, year: int, day: int) -> None:
+        """
+        Records the current weather conditions in the OutputManager.
+
+        Parameters
+        ----------
+        year: int
+            Current simulated year.
+        day: int
+            Current simulated julian day.
+
+        """
+        info_map = {"class": self.__class__.__name__, "function": self.record_weather.__name__, "prefix": "Weather"}
+        year_index = year - 1
+        day_index = day - 1
+        om.add_variable("precipitation(mm)", self.rainfall[year_index][day_index], info_map)
+        om.add_variable("maximum_temperature(C)", self.T_max[year_index][day_index], info_map)
+        om.add_variable("minimum_temperature(C)", self.T_min[year_index][day_index], info_map)
+        om.add_variable("average_temperature(C)", self.T_avg[year_index][day_index], info_map)
+        om.add_variable("radiation(MJ/square_meter/day)", self.radiation[year_index][day_index], info_map)
+        om.add_variable("irrigation(mm)", self.irrigation[year_index][day_index], info_map)
+
     @staticmethod
     def _calculate_average_annual_temperature(daily_average_temperatures: list[float]) -> float:
         """
