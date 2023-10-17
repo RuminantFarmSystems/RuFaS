878869f70830ad8802de48ba2cc2c7a9aa319854
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-10-17 13:08:10 -0400

    changed inputs and scenario run

--- RUFAS/scenario_manager.py
+++ RUFAS/scenario_manager.py
@@ -46,6 +46,6 @@ class MetadataPaths(TypedDict):
     path: Path
 
 
-METADATA_PATHS: List[MetadataPaths] = [{"prefix": "default_scenario",
-                                        "path": Path('input/metadata/default_metadata.json')},
+METADATA_PATHS: List[MetadataPaths] = [{"prefix": "arl",
+                                        "path": Path('input/metadata/ARL_metadata.json')},
                                        ]
