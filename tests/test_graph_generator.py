fbc3a07bfbf6e8ec3e7bd1391195d11afda14ad5
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-10-18 15:53:59 -0400

    fix test_generate_graph_path_with_title

--- tests/test_graph_generator.py
+++ tests/test_graph_generator.py
@@ -93,7 +93,7 @@ def test_generate_graph_path_with_title(graph_generator: GraphGenerator) -> None
             mock_mkdir.assert_called_once()
             assert (
                 result
-                == "/path/to/save\\graphics\\test-graph-13-Oct-2023_Fri_11-41-23.png"
+                == r"/path/to/save\graphics\test-graph-13-Oct-2023_Fri_11-41-23.png"
             )
 
 
