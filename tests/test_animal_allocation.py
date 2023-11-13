be57725107a431e90bd29b546b85050d98f9e603
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2023-11-13 13:32:20 -0500

    new unit test

--- tests/test_animal_allocation.py
+++ tests/test_animal_allocation.py
@@ -545,3 +545,24 @@ def test_allocate_animals_to_pens(mocker: MockerFixture) -> None:
     assert patch_for_allocate_animals_to_pens_helper.call_count == len(animals_by_combination)
     patch_for_fully_update_animal_to_pen_id_map.assert_called_once()
     patch_for_sort_animals_before_allocation.assert_called_once()
+
+
+def test_set_animal_grouping_scenario(mocker: MockerFixture):
+    """Unit test for function set_animal_grouping_scenario() in file animal_manager.py."""
+    scenario = mocker.MagicMock()
+    mocker.patch(
+        'RUFAS.routines.animal.animal_manager.AnimalManager.__init__',
+        return_value=None
+    )
+    animal_manager = AnimalManager(
+        data=mocker.MagicMock(),
+        config=mocker.MagicMock(),
+        feed=mocker.MagicMock(),
+        weather=mocker.MagicMock(),
+        time=mocker.MagicMock(),
+        )
+    # act
+    animal_manager.set_animal_grouping_scenario(scenario)
+
+    # assert
+    animal_manager.ANIMAL_GROUPING_SCENARIO = scenario
