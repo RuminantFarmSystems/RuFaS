0338b0adc690047354e8ba1e12de34ed67b376cd
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-11-16 12:50:57 -0500

    applied Niko's suggestions

--- RUFAS/input_manager.py
+++ RUFAS/input_manager.py
@@ -454,7 +454,7 @@ class InputManager:
                     element_counter_and_validity["fixed_elements"] += 1
                 else:
                     om.add_warning("Validation: invalid unfixable element found",
-                                   f"Variable '{var_name}' was invalid and could not be fixed", info_map)
+                                   f"Variable: '{var_name}' was invalid and could not be fixed", info_map)
                     element_counter_and_validity["invalid_elements"] += 1
                     element_counter_and_validity["is_valid"] = False
                 return element_counter_and_validity
@@ -465,10 +465,12 @@ class InputManager:
         info_map = {"class": self.__class__.__name__,
                     "function": self._array_type_validator.__name__,
                     }
-        properties_violation_message = f"Violates properties defined in '{properties_blob_key}'."
+        properties_violation_message = f"Violates properties defined in metadata properties section" \
+                                       f" '{properties_blob_key}'."
         if type(input_data_value) is not list:
-            warning_string = "Validation: array is not a list."
-            warning_message = f"Variable '{var_name}' is type: {type(input_data_value)}. {properties_violation_message}"
+            warning_string = "Validation: array is not a list"
+            warning_message = f"Variable: '{var_name}' is type: {type(input_data_value)}. " \
+                              f"{properties_violation_message}"
             om.add_warning(warning_string, warning_message, info_map)
             return False
 
@@ -477,16 +479,16 @@ class InputManager:
         if minimum_length is not None:
             is_in_range = variable_properties["minimum_length"] <= len(input_data_value)
             if not is_in_range:
-                warning_name = "Validation: array length less than minimum."
-                warning_message = f"Variable '{var_name}' has length: {len(input_data_value)}, less than minimum " \
+                warning_name = "Validation: array length less than minimum"
+                warning_message = f"Variable: '{var_name}' has length: {len(input_data_value)}, less than minimum " \
                                   f"length: {minimum_length}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_message, info_map)
                 return False
         if maximum_length is not None:
             is_in_range = len(input_data_value) <= variable_properties["maximum_length"]
             if not is_in_range:
-                warning_name = "Validation: array length greater than maximum."
-                warning_message = f"Variable '{var_name}' has length: {len(input_data_value)}, greater than " \
+                warning_name = "Validation: array length greater than maximum"
+                warning_message = f"Variable: '{var_name}' has length: {len(input_data_value)}, greater than " \
                                   f"maximum length: {maximum_length}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_message, info_map)
                 return False
@@ -500,26 +502,27 @@ class InputManager:
                     }
         minimum_value = variable_properties.get("minimum")
         maximum_value = variable_properties.get("maximum")
-        properties_violation_message = f"Violates properties defined in '{properties_blob_key}'."
+        properties_violation_message = f"Violates properties defined in metadata properties section" \
+                                       f" '{properties_blob_key}'."
         if type(input_data_value) is not float and type(input_data_value) is not int:
-            warning_string = "Validation: value is not a number."
-            warning_message = f"Variable '{var_name}' has value: {input_data_value}, is type: " \
+            warning_string = "Validation: value is not a number"
+            warning_message = f"Variable: '{var_name}' has value: {input_data_value}, is type: " \
                               f"{type(input_data_value)}. {properties_violation_message}"
             om.add_warning(warning_string, warning_message, info_map)
             return False
         if minimum_value is not None:
             is_in_range = minimum_value <= input_data_value
             if not is_in_range:
-                warning_name = "Validation: value less than minimum."
-                warning_message = f"Variable '{var_name}' has value: {input_data_value}, less than minimum value: " \
+                warning_name = "Validation: value less than minimum"
+                warning_message = f"Variable: '{var_name}' has value: {input_data_value}, less than minimum value: " \
                                   f"{minimum_value: .2f}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_message, info_map)
                 return False
         if maximum_value is not None:
             is_in_range = input_data_value <= maximum_value
             if not is_in_range:
-                warning_name = "Validation: value greater than maximum."
-                warning_string = f"Variable '{var_name}' has value: {input_data_value}, greater than maximum value: " \
+                warning_name = "Validation: value greater than maximum"
+                warning_string = f"Variable: '{var_name}' has value: {input_data_value}, greater than maximum value: " \
                                  f"{maximum_value: .2f}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_string, info_map)
                 return False
@@ -532,10 +535,11 @@ class InputManager:
         info_map = {"class": self.__class__.__name__,
                     "function": self._string_type_validator.__name__,
                     }
-        properties_violation_message = f"Violates properties defined in '{properties_blob_key}'."
+        properties_violation_message = f"Violates properties defined in metadata properties section" \
+                                       f" '{properties_blob_key}'."
         if type(input_data_value) is not str:
-            warning_name = "Validation: string variable is not a string."
-            warning_message = f"Variable '{var_name}' has value: {input_data_value}, is type: " \
+            warning_name = "Validation: string variable is not a string"
+            warning_message = f"Variable: '{var_name}' has value: {input_data_value}, is type: " \
                               f"{type(input_data_value)}. {properties_violation_message}"
             om.add_warning(warning_name, warning_message, info_map)
             return False
@@ -544,8 +548,8 @@ class InputManager:
         if pattern_check is not None:
             is_valid_string = bool(re.match(pattern_check, input_data_value))
             if not is_valid_string:
-                warning_name = "Validation: string variable does not match pattern."
-                warning_message = f"Variable '{var_name}' has value: '{input_data_value}', does not match pattern: " \
+                warning_name = "Validation: string variable does not match pattern"
+                warning_message = f"Variable: '{var_name}' has value: '{input_data_value}', does not match pattern: " \
                                   f"{pattern_check}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_message, info_map)
                 return False
@@ -555,16 +559,16 @@ class InputManager:
         if minimum_length is not None:
             is_valid_string = variable_properties["minimum_length"] <= len(input_data_value)
             if not is_valid_string:
-                warning_name = "Validation: string length less than minimum."
-                warning_message = f"Variable '{var_name}' has value: '{input_data_value}', length is less than " \
+                warning_name = "Validation: string length less than minimum"
+                warning_message = f"Variable: '{var_name}' has value: '{input_data_value}', length is less than " \
                                   f"minimum length: {minimum_length}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_message, info_map)
                 return False
         if maximum_length is not None:
             is_valid_string = len(input_data_value) <= variable_properties["maximum_length"]
             if not is_valid_string:
-                warning_name = "Validation: string length greater than maximum."
-                warning_message = f"Variable '{var_name}' has value: '{input_data_value}', length is greater than " \
+                warning_name = "Validation: string length greater than maximum"
+                warning_message = f"Variable: '{var_name}' has value: '{input_data_value}', length is greater than " \
                                   f"maximum length: {maximum_length}. {properties_violation_message}"
                 om.add_warning(warning_name, warning_message, info_map)
                 return False
@@ -577,10 +581,11 @@ class InputManager:
         info_map = {"class": self.__class__.__name__,
                     "function": self._bool_type_validator.__name__,
                     }
-        properties_violation_message = f"Violates properties defined in '{properties_blob_key}'."
+        properties_violation_message = f"Violates properties defined in metadata properties section" \
+                                       f" '{properties_blob_key}'."
         if type(input_data_value) is not bool:
-            warning_name = "Validation: bool variable is not a bool."
-            warning_message = f"Variable '{var_name}' has value: '{input_data_value}', is type: " \
+            warning_name = "Validation: bool variable is not a bool"
+            warning_message = f"Variable: '{var_name}' has value: '{input_data_value}', is type: " \
                               f"'{type(input_data_value)}'. {properties_violation_message}"
             om.add_warning(warning_name, warning_message, info_map)
             return False
@@ -619,15 +624,16 @@ class InputManager:
                                  input_data)
 
         element_path = ".".join([str(element) for element in element_hierarchy])
-        properties_violation_message = f"Violates properties defined in '{properties_blob_key}'."
+        properties_violation_message = f"Violates properties defined in metadata properties section" \
+                                       f" '{properties_blob_key}'."
         if 'default' not in variable_properties.keys():
-            error_message = f"Variable '{element_path}' has invalid value: {variable_parent[element_hierarchy[-1]]}, " \
-                            f"and cannot be changed to a default value. {properties_violation_message}"
-            om.add_error("Validation: invalid data not able to be fixed.", error_message, info_map)
+            error_message = f"Variable: '{element_path}' has invalid value: {variable_parent[element_hierarchy[-1]]}" \
+                            f", and cannot be changed to a default value. {properties_violation_message}"
+            om.add_error("Validation: invalid data not able to be fixed", error_message, info_map)
             return False
 
         original_invalid_value = variable_parent[element_hierarchy[-1]]
-        warning_message = f"Variable '{element_path}' has value: {original_invalid_value}. " \
+        warning_message = f"Variable: '{element_path}' has value: {original_invalid_value}. " \
                           f"{properties_violation_message}"
         om.add_warning("Validation: invalid data found",
                        warning_message,
