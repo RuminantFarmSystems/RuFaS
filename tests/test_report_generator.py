9c29a816a4bc3c1ebb59db54caf26adc7c7b8a0b
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-11-19 13:46:23 -0500

    Update test_report_generator.py

--- tests/test_report_generator.py
+++ tests/test_report_generator.py
@@ -16,4 +16,11 @@ def test_average_aggregator():
     assert average_aggregator([]) == 0
 
 
+def test_division_aggregator():
+    assert division_aggregator([100, 2, 5]) == 10
+    assert division_aggregator([100, -2, 5]) == -10
+    assert division_aggregator([]) is None
+    assert division_aggregator([10]) is None
+    assert division_aggregator([10, 0]) is None
+
 
