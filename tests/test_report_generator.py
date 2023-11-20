c0b89ec6f839701f03ea03df0494540fc3cc5258
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-11-20 16:34:02 -0500

    Update test_report_generator.py

--- tests/test_report_generator.py
+++ tests/test_report_generator.py
@@ -120,24 +120,6 @@ def test_generate_report_no_aggregation(
         report_generator.generate_report(sample_filtered_pool, filter_content)
 
 
-# def test_generate_report_valid_horizontal_vertical(
-#     report_generator: ReportGenerator,
-# ) -> None:
-#     filtered_pool: Dict[str, Dict[str, List[Dict[str, int]]]] = {
-#         "data1": {"values": [{"a": 1, "b": 2, "c": 10}, {"a": 3, "b": 4, "c": 10}]},
-#         "data2": {"values": [{"a": 5, "b": 6, "c": 10}, {"a": 7, "b": 8, "c": 10}]},
-#     }
-#     filter_content: Dict[str, Any] = {
-#         "variables": ["a", "b"],
-#         "slice_start": 0,
-#         "slice_end": 0,
-#         "horizontal_aggregation": "sum",
-#         "vertical_aggregation": "average",
-#         "horizontal_first": True,
-#     }
-#     assert report_generator.generate_report(filtered_pool, filter_content) == [9]
-
-
 def test_generate_report_invalid_empty_data(report_generator: ReportGenerator) -> None:
     filtered_pool: Dict[str, Dict[str, List[Any]]] = {}
     filter_content: Dict[str, Any] = {
