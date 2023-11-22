63714c75b1757ac03d1095ad69b3893e498b10e2
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2023-11-22 09:55:20 -0500

    removed daily from var names

--- RUFAS/routines/animal/life_cycle/cow.py
+++ RUFAS/routines/animal/life_cycle/cow.py
@@ -114,9 +114,9 @@ class Cow(HeiferIII):
         self.days_in_milk = 0
         self.estimated_daily_milk_produced = 0
         # Milk production as estimated from the lactation curve, kg/day.
-        self.daily_milk_fat_kg = 0
+        self.milk_fat_kg = 0
         # Milk fat content estimate, kg/day.
-        self.daily_milk_protein_kg = 0
+        self.milk_protein_kg = 0
         # Milk protein content estimate, kg/day.
         self.milk_production_reduction = 0.0
         self.single_acc_milk_prod = 0
@@ -344,13 +344,13 @@ class Cow(HeiferIII):
             daily_fat_correct_milk_production = \
                 0.4 * estimated_daily_milk_produced + \
                 0.15 * fat_percent * estimated_daily_milk_produced
-            self.daily_milk_fat_kg = fat_percent*estimated_daily_milk_produced
-            self.daily_milk_protein_kg = self.mPrt * self.estimated_daily_milk_produced
+            self.milk_fat_kg = fat_percent*estimated_daily_milk_produced
+            self.milk_protein_kg = self.mPrt * self.estimated_daily_milk_produced
         else:
             fat_percent = 0
             daily_fat_correct_milk_production = 0
-            self.daily_milk_fat_kg = 0
-            self.daily_milk_protein_kg = 0
+            self.milk_fat_kg = 0
+            self.milk_protein_kg = 0
 
         self.daily_growth = self.get_bw_change(calving_interval)
 
