4eb7736e44f0305ad5bdd54eaceca64b591ba048
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-11-30 10:40:46 -0500

    add harvested crop

--- RUFAS/routines/feed_storage/harvested_crop.py
+++ RUFAS/routines/feed_storage/harvested_crop.py
@@ -60,3 +60,57 @@ class CropType(Enum):
     MEADOW_FESCUE = "Meadow Fescue"
 
 
+@dataclass
+class HarvestedCrop:
+    """
+    A class to represent a harvested crop with its attributes.
+
+    Attributes
+    ----------
+    category : CropCategory
+        The category of the crop (enum).
+    type : CropType
+        The type of the crop (enum), a subdivision of crop category.
+    fresh_mass : float
+        The fresh mass of the crop in kg.
+    dry_matter_percentage : float
+        Percent of mass that is not water.
+    dry_matter_digestibility : float
+        Percent of mass that is digestible.
+    crude_protein_percent : float
+        Percent of mass that is dietary crude protein.
+    non_protein_nitrogen : float
+        Percent of mass that is non-protein nitrogen.
+    starch : float
+        Percent of mass that is starch.
+    adf : float
+        Percent of mass that is acid detergent fiber.
+    ndf : float
+        Percent of mass that is neutral detergent fiber.
+    lignin : float
+        Percent of mass that is lignin.
+    sugar : float
+        Percent of mass that is labile carbohydrate.
+    ash : float
+        Percent of mass that is ash.
+
+    Methods
+    -------
+    __post_init__():
+        Validates the category and type relationship.
+    """
+
+    category: CropCategory
+    type: CropType
+    fresh_mass: float
+    dry_matter_percentage: float
+    dry_matter_digestibility: float
+    crude_protein_percent: float
+    non_protein_nitrogen: float
+    starch: float
+    adf: float
+    ndf: float
+    lignin: float
+    sugar: float
+    ash: float
+
