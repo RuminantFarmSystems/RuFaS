c2cac2a36644a5fb82783fa71a88b791ddcc90c3
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-12-01 15:48:52 -0500

    changed crop management, unchanged tests breaking

--- RUFAS/routines/field/manager/output_gatherer.py
+++ RUFAS/routines/field/manager/output_gatherer.py
@@ -242,7 +242,7 @@ class OutputGatherer:
                 om.add_variable("canopy_water", crop.data.canopy_water, info_map)
                 om.add_variable("cut_biomass", crop.data.cut_biomass, info_map)
                 om.add_variable("fresh_yield_collected", crop.data.fresh_yield_collected, info_map)
-                om.add_variable("yield_residue", crop.data.yield_residue, info_map)
+                om.add_variable("dry_matter_yield_residue", crop.data.yield_residue, info_map)
                 om.add_variable("yield_nitrogen", crop.data.yield_nitrogen, info_map)
                 om.add_variable("yield_phosphorus", crop.data.yield_phosphorus, info_map)
                 om.add_variable("residue_nitrogen", crop.data.residue_nitrogen, info_map)
