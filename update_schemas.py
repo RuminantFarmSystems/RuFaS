2b404b79d7291a4e54d5177a86ef59991a36f730
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-12-04 15:57:53 -0500

    created script and have method for number schema setup

---
+++ update_schemas.py
@@ -0,0 +1,60 @@
+import json
+# import pprint
+from typing import Any
+
+PROPERTIES_WITH_SCHEMA = {
+    'crop_schedule': "Crop Specifications"
+}
+
+def _setup_number_schema(title: str, properties: dict[str, Any]) -> dict[str, Any]:
+    schema = {
+        "title": title,
+        "type": "number"
+    }
+    minimum = properties.get("minimum")
+    maximum = properties.get("maximum")
+    default = properties.get("default")
+    description = properties.get("description")
+
+    if minimum is not None:
+        schema["minimum"] = minimum
+    if maximum is not None:
+        schema["maximum"] = maximum
+    if minimum is not None and maximum is not None:
+        schema["format"] = "range"
+    if default is not None:
+        schema["default"] = default
+
+    schema["options"] = {
+                            "grid_columns": 12,
+                            "inputAttributes": {
+                                "class": "text-primary form-control"
+                            }
+                        }
+    if description:
+        schema["options"]["infoText"] = description
+
+    return schema
+
+# def _setup_schema(title: str, structure: dict[str, Any]) -> dict[str, Any]:
+#     if
+#     schema = {"title": title,
+#               "type": "object",
+#               "format": "grid",
+#               "properties": {}
+#               }
+#     for key in structure.keys():
+#
+#     return schema
+
+
+with open("input/metadata/default_metadata.json") as metadata:
+    metadata_dict = json.load(metadata)
+
+properties = metadata_dict["properties"]
+
+for key in properties.keys():
+    if key in PROPERTIES_WITH_SCHEMA.keys():
+        new_schema = _setup_schema(PROPERTIES_WITH_SCHEMA[key], properties[key])
+    print(key)
+# pprint.pprint(json.dumps(properties, indent=4))
