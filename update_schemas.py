8a057535f82f88e96e6d436fb60339eb7f0bec4a
Ed, WSL Ubuntu <isgotkowitz@gmail.com>
2023-12-04 17:24:47 -0500

    added array schema setup method, untested

--- update_schemas.py
+++ update_schemas.py
@@ -1,6 +1,6 @@
-import json
+# import json
 # import pprint
-from typing import Any
+from typing import Any, Callable
 import re
 
 PROPERTIES_WITH_SCHEMA = {
@@ -8,95 +8,136 @@ PROPERTIES_WITH_SCHEMA = {
 }
 
 
-def _setup_number_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
-    schema = {
-        "title": title,
-        "type": "number",
-        "options": {
-            "grid_columns": 12,
-            "inputAttributes": {
-                "class": "text-primary form-control"
+class SchemaSetupMethods:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def _setup_number_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
+        schema = {
+            "title": title,
+            "type": "number",
+            "options": {
+                "grid_columns": 12,
+                "inputAttributes": {
+                    "class": "text-primary form-control"
+                }
             }
         }
-    }
-    minimum = input_properties.get("minimum")
-    maximum = input_properties.get("maximum")
-    default = input_properties.get("default")
-    description = input_properties.get("description")
+        minimum = input_properties.get("minimum")
+        maximum = input_properties.get("maximum")
+        default = input_properties.get("default")
+        description = input_properties.get("description")
 
-    if minimum is not None:
-        schema["minimum"] = minimum
-    if maximum is not None:
-        schema["maximum"] = maximum
-    if minimum is not None and maximum is not None:
-        schema["format"] = "range"
-    if default is not None:
-        schema["default"] = default
-    if description:
-        schema["options"]["infoText"] = description
+        if minimum is not None:
+            schema["minimum"] = minimum
+        if maximum is not None:
+            schema["maximum"] = maximum
+        if minimum is not None and maximum is not None:
+            schema["format"] = "range"
+        if default is not None:
+            schema["default"] = default
+        if description:
+            schema["options"]["infoText"] = description
 
-    return schema
+        return schema
 
-
-def _setup_bool_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
-    schema = {
-        "title": title,
-        "type": "boolean",
-        "options": {
-            "grid_columns": 12,
-            "format": "select2",
-            "inputAttributes": {
-                "class": "text-primary form-control"
+    @staticmethod
+    def _setup_bool_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
+        schema = {
+            "title": title,
+            "type": "boolean",
+            "options": {
+                "grid_columns": 12,
+                "format": "select2",
+                "inputAttributes": {
+                    "class": "text-primary form-control"
+                }
             }
         }
-    }
-    default = input_properties.get("default")
-    description = input_properties.get("description")
+        default = input_properties.get("default")
+        description = input_properties.get("description")
 
-    if default is not None:
-        schema["default"] = default
-    if description is not None:
-        schema["options"]["infoText"] = description
+        if default is not None:
+            schema["default"] = default
+        if description is not None:
+            schema["options"]["infoText"] = description
 
-    return schema
+        return schema
 
-
-def _setup_string_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
-    schema = {
-        "title": title,
-        "type": "string",
-        "options": {
-            "grid_columns": 12,
-            "inputAttributes": {
-                "class": "text-primary form-control"
+    @staticmethod
+    def _setup_string_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
+        schema = {
+            "title": title,
+            "type": "string",
+            "options": {
+                "grid_columns": 12,
+                "inputAttributes": {
+                    "class": "text-primary form-control"
+                }
             }
         }
+        default = input_properties.get("default")
+        pattern = input_properties.get("pattern")
+        description = input_properties.get("description")
+
+        if default is not None:
+            schema["default"] = default
+        if pattern is not None:
+            enum = self._get_enum_of_options(pattern)
+            schema["enum"] = enum
+            schema["format"] = "select2"
+        if description is not None:
+            schema["options"]["infoText"] = description
+
+        return schema
+
+    @staticmethod
+    def _get_enum_of_options(string: str) -> list[str]:
+        pattern = "\\^\\(.*\\)\\$"
+        is_valid_pattern = bool(re.match(pattern, string))
+        if not is_valid_pattern:
+            raise ValueError(f"'{string}' is not a valid pattern. Cannot create schema")
+
+        unsplit_list = string[2:-2]
+        split_list = unsplit_list.split("|")
+        return split_list
+
+    @staticmethod
+    def _setup_array_schema(title: str, input_properties: dict[str, Any]) -> dict[str, Any]:
+        schema = {
+            "title": title,
+            "type": "array",
+            "format": "grid",
+            "options": {
+                "inputAttributes": {
+                    "class": "text-primary form-control"
+                }
+            }
+        }
+        default = input_properties.get("default")
+        description = input_properties.get("description")
+
+        if default is not None:
+            schema["default"] = default
+        if description is not None:
+            schema["options"]["infoText"] = description
+
+        element_properties = input_properties["properties"]
+        element_schema_creator = SchemaSetupMethods.DATA_TYPE_TO_SCHEMA_SETUP_MAP[element_properties["type"]]
+        element_title = title + "_element"
+        element_property_dictionary = element_schema_creator(element_title, element_properties)
+        schema["items"] = element_property_dictionary
+
+        return schema
+
+    DATA_TYPE_TO_SCHEMA_SETUP_MAP: dict[str, Callable[[str, dict[str, Any]], dict[str, Any]]] = {
+        "number": _setup_number_schema,
+        "bool": _setup_bool_schema,
+        "string": _setup_bool_schema,
+        "array": _setup_array_schema,
     }
-    default = input_properties["default"]
-    pattern = input_properties["pattern"]
-    description = input_properties["description"]
 
-    if default is not None:
-        schema["default"] = default
-    if pattern is not None:
-        enum = _get_enum_of_options(pattern)
-        schema["enum"] = enum
-        schema["format"] = "select2"
-    if description is not None:
-        schema["options"]["infoText"] = description
-
-    return schema
-
-
-def _get_enum_of_options(string: str) -> list[str]:
-    pattern = "\\^\\(.*\\)\\$"
-    is_valid_pattern = bool(re.match(pattern, string))
-    if not is_valid_pattern:
-        raise ValueError(f"'{string}' is not a valid pattern. Cannot create schema")
-
-    unsplit_list = string[2:-2]
-    split_list = unsplit_list.split("|")
-    return split_list
 
 # def _setup_schema(title: str, structure: dict[str, Any]) -> dict[str, Any]:
 #     if
