078cfe87dd6c1afb54700fbc6a37560b05708469
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-12-19 10:51:34 -0500

    Update grain.py

--- RUFAS/routines/feed_storage/grain.py
+++ RUFAS/routines/feed_storage/grain.py
@@ -3,6 +3,12 @@ from .enums import CropCategory
 
 
 class Grain(Storage):
+    """
+    Represents grain storage and manages its specific attributes and behaviors.
+
+    Inherits from Storage.
+    """
+
     def __init__(self, capacity: float = float("inf")):
         super().__init__(capacity)
         self.acceptable_crops = [
@@ -10,14 +16,6 @@ class Grain(Storage):
             CropCategory.SMALL_GRAIN,
             CropCategory.SOY,
         ]
-
-    """
-    Represents grain storage and manages its specific attributes and behaviors.
-
-    Inherits from Storage.
-    """
-
-    pass
 
 
 class Dry(Grain):
