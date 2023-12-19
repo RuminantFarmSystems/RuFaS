02b028d34258dd62d899c882c282dcb42af96036
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2023-12-19 10:49:18 -0500

    add acceptable_crops to grain

--- RUFAS/routines/feed_storage/grain.py
+++ RUFAS/routines/feed_storage/grain.py
@@ -1,9 +1,15 @@
 from .storage import Storage
+from .enums import CropCategory
 
 
 class Grain(Storage):
     def __init__(self, capacity: float = float("inf")):
         super().__init__(capacity)
+        self.acceptable_crops = [
+            CropCategory.CORN,
+            CropCategory.SMALL_GRAIN,
+            CropCategory.SOY,
+        ]
 
     """
     Represents grain storage and manages its specific attributes and behaviors.
