2f8ab49c58fbab983f326a2eda34494a3a57787a
ndyt <ntomlinson@gmail.com>
2023-12-20 11:16:04 -0500

    updates unit testing

--- RUFAS/routines/manure/manure_nutrients/nutrient_request.py
+++ RUFAS/routines/manure/manure_nutrients/nutrient_request.py
@@ -32,14 +32,11 @@ class NutrientRequest:
                 if value < 0:
                     raise ValueError(f"Field {field.name} must be non-negative.")
             else:
-                if not isinstance(value, ManureType):
+                if field.name == "manure_type" and not isinstance(value, ManureType):
                     raise ValueError(f"Field {field.name} must be an instance of ManureType.")
 
-        if any(isinstance(getattr(self, field.name), (int, float)) and getattr(self, field.name) > 0.0 for field in fields(self)):
+        if any(isinstance(getattr(self, field.name), (int, float)) and getattr(self, field.name) > 0.0
+               for field in fields(self)):
             return
         else:
-            for field in fields(self):
-                value = getattr(self, field.name)
-                print(value)
-                print(field.name)
             raise ValueError("At least one nutrient must be requested and positive.")
