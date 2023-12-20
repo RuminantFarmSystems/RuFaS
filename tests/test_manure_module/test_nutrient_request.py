d5f105bb6ecc06c987152f9561855a28893f896e
ndyt <ntomlinson@gmail.com>
2023-12-20 17:17:52 -0500

    updates coverage to 100% on nutrient_request.py

--- tests/test_manure_module/test_nutrient_request.py
+++ tests/test_manure_module/test_nutrient_request.py
@@ -12,6 +12,7 @@ from RUFAS.routines.manure.manure_nutrients.nutrient_request import NutrientRequ
         (0.0, 0.0, ManureType.LIQUID),  # No nutrients requested
         (-1.0, 0.0, ManureType.SOLID),  # Negative nitrogen requested
         (1.0, -2.0, ManureType.LIQUID),  # Negative phosphorus requested
+        (1.0, 1.0, ""),  # Invalid ManureType
     ]
 )
 def test_nutrient_request_invalid_init(nitrogen: float, phosphorus: float, manure_type: ManureType):
