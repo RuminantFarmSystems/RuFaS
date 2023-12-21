b6ba81018d2e4c7b989358a92dd508ffff54fb2e
ndyt <ntomlinson@gmail.com>
2023-12-21 13:52:17 -0500

    updates ManureType class docstrings to match other Enum classes in RuFaS

--- RUFAS/routines/manure/manure_treatments/manure_types.py
+++ RUFAS/routines/manure/manure_treatments/manure_types.py
@@ -5,9 +5,12 @@ class ManureType(Enum):
     """
     This is an Enum class that represents different types of manure.
 
-    The Enum class contains the following members:
-    LIQUID: Represents liquid manure.
-    SOLID: Represents manure in solid form.
+    Attributes
+    ----------
+    LIQUID : str
+        Represents liquid manure.
+    SOLID : str
+        Represents manure in solid form.
 
     """
 
