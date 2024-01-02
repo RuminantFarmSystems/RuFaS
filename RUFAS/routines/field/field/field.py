28af8ace6678bb8aab7ed903e81bb26abd0b5a2b
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-01-02 12:39:14 -0500

    changed harvest operations parsing and options

--- RUFAS/routines/field/field/field.py
+++ RUFAS/routines/field/field/field.py
@@ -787,8 +787,8 @@ class Field:
         ----------
         crop_reference : str
             Name used to get the specifications for the crop to be harvested.
-        harvest_operation : str
-            Name of the harvest operation to be performed on the referenced crop.
+        harvest_operation : HarvestOperation
+            Harvest operation to be performed on the referenced crop.
         time : Time
             Object containing the current day and year of the simulation.
         current_conditions : CurrentDayConditions
@@ -815,8 +815,7 @@ class Field:
             om.add_warning("harvest_warning", "No crop found to be harvested by a HarvestEvent.", info_map)
 
         for crop in crops_to_be_harvested:
-            harvest_operation_enum = HarvestOperation(harvest_operation)
-            crop.crop_management.manage_harvest(harvest_operation_enum, self.field_data.name,
+            crop.crop_management.manage_harvest(harvest_operation, self.field_data.name,
                                                 self.field_data.field_size, time.calendar_year, time.day,
                                                 self.soil.data)
             self.soil.carbon_cycling.residue_partition.add_residue_to_pools(current_conditions.rainfall)
