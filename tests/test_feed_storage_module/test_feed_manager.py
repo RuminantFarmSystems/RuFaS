4f96c104a164dc3418285091ece52098d51e8ef2
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-01-05 12:26:10 -0500

    add test_receive_crop_multiple

--- tests/test_feed_storage_module/test_feed_manager.py
+++ tests/test_feed_storage_module/test_feed_manager.py
@@ -70,6 +70,32 @@ def test_receive_crop_success(feed_manager: FeedManager, harvested_crop: Harvest
             harvested_crop=harvested_crop,
             storage_type=StorageType.DRY,
         )
+        pass
+    except ValueError:
+        pytest.fail("Unexpected ValueError raised")
+
+
+def test_receive_crop_multiple(
+    feed_manager: FeedManager, harvested_crop: HarvestedCrop
+):
+    try:
+        feed_manager.receive_crop(
+            harvested_crop=harvested_crop,
+            storage_type=StorageType.DRY,
+        )
+        feed_manager.receive_crop(
+            harvested_crop=harvested_crop,
+            storage_type=StorageType.DRY,
+        )
+        feed_manager.receive_crop(
+            harvested_crop=harvested_crop,
+            storage_type=StorageType.BUNKER,
+        )
+        assert StorageType.DRY in feed_manager.active_storages.keys()
+        assert StorageType.BUNKER in feed_manager.active_storages.keys()
+        assert len(feed_manager.active_storages[StorageType.BUNKER].stored) ==1
+        len(feed_manager.active_storages[StorageType.DRY].stored)==2
+        pass
     except ValueError:
         pytest.fail("Unexpected ValueError raised")
 
