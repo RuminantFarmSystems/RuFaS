72c50bd1cc2bc9392a6fba71104905321a1ef074
lpham <98855117+ltp23@users.noreply.github.com>
2024-01-05 05:09:35 -0800

    Update output_manager.py

--- RUFAS/output_manager.py
+++ RUFAS/output_manager.py
@@ -297,33 +297,6 @@ class OutputManager(object):
         """Sets the __log_verbose attribute"""
         self.__log_verbose = log_verbose
 
-    def _get_timestamp(self, include_millis: bool = False) -> str:
-        """
-        Produces the current system time as a timestamp string.
-
-        Parameters
-        ----------
-        include_millis : bool
-            If True, adds milliseconds to the timestamp.
-
-        Returns
-        -------
-        str
-            The current time's timestamp string.
-
-        Example
-        --------
-        >>> self._get_timestamp(include_millis=True)
-        28-Jun-2023_Wed_15-48-21.406585
-        >>> self._get_timestamp(include_millis=False)
-        28-Jun-2023_Wed_15-48-21
-        """
-        base_timestamp_str: str = "%d-%b-%Y_%a_%H-%M-%S"
-        timestamp_format_string: str = (
-            f"{base_timestamp_str}.%f" if include_millis else base_timestamp_str
-        )
-        return datetime.datetime.now().strftime(timestamp_format_string)
-
     def _generate_key(self, name: str, info_map: Dict[str, Union[str, bool]]) -> str:
         """
         Generates key for the pool.
