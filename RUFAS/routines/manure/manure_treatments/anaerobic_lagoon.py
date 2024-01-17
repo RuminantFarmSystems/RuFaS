42acce0ebe55b14a29408bf2d5555cc2145803e9
ndyt <ntomlinson@gmail.com>
2024-01-17 10:58:05 -0500

    updates daily helper methods in an-dig, an-lag, comp-bed-pac

--- RUFAS/routines/manure/manure_treatments/anaerobic_lagoon.py
+++ RUFAS/routines/manure/manure_treatments/anaerobic_lagoon.py
@@ -116,7 +116,6 @@ class AnaerobicLagoon(BaseManureTreatment):
         """
         daily_input = self._current_manure_treatment_daily_input
         daily_output = self._initialize_daily_output_during_update(daily_input)
-        self._accumulate_daily_output(daily_output)
 
         adjusted_daily_final_manure_volume = self._adjust_final_manure_volume(
             daily_output.daily_final_manure_volume
