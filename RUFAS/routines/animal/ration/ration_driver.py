55607bc8d4df7efbce2b7a03467908ca91b29ef2
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2024-01-19 17:54:11 -0500

    Update RUFAS/routines/animal/ration/ration_driver.py
    Co-authored-by: Niko <70217952+ew3361zh@users.noreply.github.com>

--- RUFAS/routines/animal/ration/ration_driver.py
+++ RUFAS/routines/animal/ration/ration_driver.py
@@ -1029,7 +1029,7 @@ class AvailableFeeds:
                     info_map = {"class": self.__class__.__name__,
                                 "function": self.get_feed_data_from_feed_ids.__name__,
                                 }
-                    om.add_error("KeyError", f"Key {feed_id} not found in AvailableFeeds. "
+                    om.add_error("KeyError", f"Feed ID {feed_id} not found in AvailableFeeds. "
                                  f"Check that price was set in purchased_feeds in input feed json, "
                                  f"and that it is included in the specified NRC or NASEM csv.",
                                  info_map)
