1b9ffec16a610c5e4265c2838345130679dfb4f4
ndyt <ntomlinson@gmail.com>
2024-01-25 10:44:27 -0500

    moves slurry outdoor methane emission calculation before ammonia calculation to match discussed emission calculation order

--- RUFAS/routines/manure/manure_treatments/slurry_storage_outdoor.py
+++ RUFAS/routines/manure/manure_treatments/slurry_storage_outdoor.py
@@ -268,6 +268,18 @@ class SlurryStorageOutdoor(BaseManureTreatment):
         self._accumulate_daily_output(daily_output)
 
         (
+            methane_loss,
+            new_accumulated_liquid_manure_total_solids,
+        ) = self.calc_methane_emission(
+            self._accumulated_output.liquid_manure_total_solids
+        )
+        daily_output.storage_methane = methane_loss
+        self._accumulated_output.storage_methane += methane_loss
+        self._accumulated_output.liquid_manure_total_solids = (
+            new_accumulated_liquid_manure_total_solids
+        )
+
+        (
             ammonia_loss,
             new_accumulated_liquid_manure_total_ammoniacal_nitrogen,
         ) = self.calc_ammonia_emission(
@@ -280,18 +292,6 @@ class SlurryStorageOutdoor(BaseManureTreatment):
         )
         daily_output.storage_ammonia = ammonia_loss
         self._accumulated_output.storage_ammonia += ammonia_loss
-
-        (
-            methane_loss,
-            new_accumulated_liquid_manure_total_solids,
-        ) = self.calc_methane_emission(
-            self._accumulated_output.liquid_manure_total_solids
-        )
-        daily_output.storage_methane = methane_loss
-        self._accumulated_output.storage_methane += methane_loss
-        self._accumulated_output.liquid_manure_total_solids = (
-            new_accumulated_liquid_manure_total_solids
-        )
 
         self._accumulated_output.liquid_manure_total_ammoniacal_nitrogen = (
             new_accumulated_liquid_manure_total_ammoniacal_nitrogen
