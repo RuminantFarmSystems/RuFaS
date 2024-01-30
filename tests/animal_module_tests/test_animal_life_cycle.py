c77807ccca072cc6dc5aa1061a00a537cf29db0f
0luchi <103719875+0luchi@users.noreply.github.com>
2024-01-30 09:30:41 -0500

    Update tests/animal_module_tests/test_animal_life_cycle.py
    Co-authored-by: Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>

--- tests/animal_module_tests/test_animal_life_cycle.py
+++ tests/animal_module_tests/test_animal_life_cycle.py
@@ -1688,7 +1688,6 @@ def test_update_calf(mocker: MockerFixture, days_born: int, wean_day: int) -> No
 
     # act
     sim_day = 2
-    weaned = False
     weaned = Calf.update(calf, sim_day)
     calf.days_born += 1
     calf.update_body_weight_history.assert_called()
