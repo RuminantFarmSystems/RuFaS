ce408da481cc088f897ebff48f2bcf6658edb96f
oluchi <oco6@cornell.edu>
2024-01-31 11:43:20 -0500

    made changes to animal combination

--- RUFAS/routines/animal/animal_combinations.py
+++ RUFAS/routines/animal/animal_combinations.py
@@ -4,6 +4,18 @@ from enum import Enum
 class AnimalCombination(Enum):
     """
     Enumeration that represents the valid combinations of animals in a pen.
+    Attributes
+    ----------
+    CALF: int
+        Represents a calf-only pen
+    GROWING: int
+        Represents a pen with growing heiferI and heiferII only
+    CLOSE_UP: int
+        Represents a pen with heiferIII and dry cows only
+    LAC_COW: int
+        Represents a pen with lactating cows only
+    GROWING_AND_CLOSE_UP: int
+        Represents a pen with all heifers and dry cows only
     """
     CALF = 0  # calves
     GROWING = 1  # heiferIs, heiferIIs
