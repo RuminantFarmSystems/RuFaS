bca0959596d2972278c9daed44d91429a38db8e7
lpham <98855117+ltp23@users.noreply.github.com>
2024-02-02 14:29:20 -0800

    Updated unit tests

--- RUFAS/routines/manure/manure_manager.py
+++ RUFAS/routines/manure/manure_manager.py
@@ -206,9 +206,6 @@ class ManureManager:
                     custom_manure_separator_config=custom_manure_separator_config,  # type: ignore
                 )
 
-            if mm_pen.manure_treatment == "slurry storage underfloor":
-                print()
-
             custom_manure_treatment_config = (
                 self.manure_manager_config_handler.get_custom_manure_treatment_config(
                     mm_pen.manure_treatment
