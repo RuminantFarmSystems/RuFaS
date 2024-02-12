dea98a8f8916054dbac79ec9fa66e4d70d1feb21
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-02-12 12:55:13 -0500

    init estimate_all

--- RUFAS/routines/EEE/energy.py
+++ RUFAS/routines/EEE/energy.py
@@ -2,6 +2,17 @@ class EnergyEstimator:
     """Class to esitmate energy consumption for various operations on the farm"""
 
     @classmethod
+    def estimate_all() -> None:
+        """Runs all estimation functions and performs pre/post processing for them."""
+        crop_yield = 0  # TODO get the correct value
+        field_production_size = 0  # TODO get the correct value
+        diesel_consumption_tractor_implement_liter_per_ton = (
+            EnergyEstimator.calculate_diesel_consumption(
+                crop_yield, field_production_size
+            )
+        )
+
+    @classmethod
     def calculate_diesel_consumption(
         crop_yield: float, field_production_size: float
     ) -> float:
