d9d40bb4a8c11314516cedf8a92da9de2c4787a5
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-02-12 13:14:16 -0500

    report to om

--- RUFAS/routines/EEE/energy.py
+++ RUFAS/routines/EEE/energy.py
@@ -1,15 +1,31 @@
+from RUFAS.output_manager import OutputManager
+
+om = OutputManager()
+
+
 class EnergyEstimator:
     """Class to esitmate energy consumption for various operations on the farm"""
 
     @classmethod
     def estimate_all() -> None:
         """Runs all estimation functions and performs pre/post processing for them."""
+        base_info_map = {
+            "class": EnergyEstimator.__name__,
+            "function": EnergyEstimator.estimate_all.__name__,
+            "unit": "unitless",
+        }
         crop_yield = 0  # TODO get the correct value
         field_production_size = 0  # TODO get the correct value
         diesel_consumption_tractor_implement_liter_per_ton = (
             EnergyEstimator.calculate_diesel_consumption(
                 crop_yield, field_production_size
             )
+        )
+        variable_info_map = {"unit": "liter/tone"}
+        om.add_variable(
+            "diesel_consumption_tractor_implement",
+            diesel_consumption_tractor_implement_liter_per_ton,
+            {**base_info_map, **variable_info_map},
         )
 
     @classmethod
