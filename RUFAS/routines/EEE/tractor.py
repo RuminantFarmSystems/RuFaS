dc5d0237db7373754ef53f2fc081f1c7e95b43e2
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-02-12 14:19:40 -0500

    Update tractor.py

--- RUFAS/routines/EEE/tractor.py
+++ RUFAS/routines/EEE/tractor.py
@@ -13,33 +13,33 @@ class TractorSize(Enum):
 
 class TractorSpecs:
     def __init__(self, tractor_size: TractorSize | None, herd_size: int | None) -> None:
-        if tractor_size:
-            self.tractor_size = tractor_size
-        elif herd_size:
-            self.tractor_size = self.herd_size_to_tractor_size(herd_size)
-        else:
+        if not tractor_size and not herd_size:
             raise ValueError(
-                "At least one of `tractor_size` or `herd_size` has to be given."
+                "At least one of `tractor_size` or `herd_size` must be given."
             )
+        self.tractor_size = tractor_size or self.herd_size_to_tractor_size(herd_size)
 
     def herd_size_to_tractor_size(self, herd_size: int) -> TractorSize:
         "Assign a Tractor Size based on number of cows"
+        if herd_size < 0:
+            raise ValueError("Herd size must be a positive integer.")
         if herd_size < 500:
             return TractorSize.SMALL
-        if herd_size < 2000:
+        elif herd_size < 2000:
             return TractorSize.MEDIUM
-        return TractorSize.LARGE
+        else:
+            return TractorSize.LARGE
 
     @property
     def PTO_kw(self) -> float:
         # TODO get these values from IM
         """Constants 589, 592, 595 in EEE Functions file"""
-        if self.tractor_size == TractorSize.SMALL:
-            return 55.93
-        if self.tractor_size == TractorSize.MEDIUM:
-            return 208.42
-        if self.tractor_size == TractorSize.LARGE:
-            return 328.11
+        pto_mapping = {
+            TractorSize.SMALL: 55.93,
+            TractorSize.MEDIUM: 208.42,
+            TractorSize.LARGE: 328.11,
+        }
+        return pto_mapping[self.tractor_size]
 
     @property
     def power_available_kw(self) -> float:
@@ -50,12 +50,12 @@ class TractorSpecs:
     def mass_kg(self) -> float:
         # TODO get these values from IM
         """Constants 591, 594, 597 in EEE Functions file"""
-        if self.tractor_size == TractorSize.SMALL:
-            return 8_400.0
-        if self.tractor_size == TractorSize.MEDIUM:
-            return 12_700.0
-        if self.tractor_size == TractorSize.LARGE:
-            return 20_856.0
+        mass_mapping = {
+            TractorSize.SMALL: 8_400.0,
+            TractorSize.MEDIUM: 12_700.0,
+            TractorSize.LARGE: 20_856.0,
+        }
+        return mass_mapping[self.tractor_size]
 
     @property
     def speed_km_hr(self) -> float:
