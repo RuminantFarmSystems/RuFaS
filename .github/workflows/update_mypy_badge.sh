612f00d35f45080c7c3048a8af32517f824e893c
lpham <98855117+ltp23@users.noreply.github.com>
2024-02-21 01:18:47 -0800

    Update update_mypy_badge.sh

--- .github/workflows/update_mypy_badge.sh
+++ .github/workflows/update_mypy_badge.sh
@@ -7,13 +7,13 @@ echo "Mypy errors: $error_count"
 # Assign color accordingly
 if [ "$error_count" -eq 0 ]; then
   color="brightgreen"
-  elif [ "$error_count" -le 5 ]; then
+elif [ "$error_count" -le 5 ]; then
   color="green"
-  elif [ "$error_count" -le 10 ]; then
+elif [ "$error_count" -le 10 ]; then
   color="yellowgreen"
-  elif [ "$error_count" -le 20 ]; then
+elif [ "$error_count" -le 20 ]; then
   color="yellow"
-  elif [ "$error_count" -le 30 ]; then
+elif [ "$error_count" -le 30 ]; then
   color="orange"
 else
   color="red"
