7fe2c7bb605e6bdc072d247628b5e1218b8b1e72
Matthew Liu <matthew7838@gmail.com>
2024-02-26 13:34:37 +0900

    deleted original description

--- RUFAS/routines/field/soil/layer_data.py
+++ RUFAS/routines/field/soil/layer_data.py
@@ -274,299 +274,165 @@ class LayerData:
     """
 
     field_size: InitVar[float] = None
-    """Size of the field (ha)
-        Note: this attribute is only used for initialization. After that it cannot be used.
-    """
     residue: InitVar[float] = 0
-    """Amount of residue on the soil surface when this soil layer is initialized (kg / ha)
-        Note: this attribute is only used for initialization. After that it cannot be used.
-    """
     top_depth: Optional[float] = None
-    """top depth of the layer (mm)"""
     bottom_depth: Optional[float] = None
-    """bottom depth of the layer (mm)"""
 
     # --- Water
     soil_water_concentration: float = 0.25  # arbitrary
-    """soil water concentration of the layer (mm)"""
     water_content: float = field(init=False)
-    """water present in the layer (mm)"""
     field_capacity_water_concentration: float = 0.3  # arbitrary
-    """water concentration of soil layer at field capacity (mm water / mm soil)"""
     wilting_point_water_concentration: float = 0.2  # arbitrary
-    """water concentration of soil layer at wilting point (mm water / mm soil)"""
     saturation_point_water_concentration: float = 0.5
-    """water concentration of soil layer at saturation point (mm water / mm soil)"""
 
     # --- Evaporation
     evaporated_water_content: float = 0.0
-    """Amount of water that evaporated out of the layer on the current day (mm)."""
     soil_evaporation_compensation_coefficient: float = 1
-    """coefficient that allows user to modify depth distribution used to meet the soil evaporative demand (unitless)
-        (SWAT 2:2.3.17)"""
 
     # --- Percolation
     temperature: float = 15.05
-    """current temperature of this soil layer (degrees Celsius)"""
     saturated_hydraulic_conductivity: float = 9.5
-    """saturated hydraulic conductivity for this layer of soil (mm per hour)"""
     percolated_water: float = 0
-    """Amount of water that percolated out of the soil layer on the current day (mm)"""
 
     # --- Temperature
     bulk_density: float = 1.4
-    """bulk density of the soil layer (Mg per cubic meter) (provided by user, but SWAT 2:3.1.1 has an equation for
-        calculating this field as well)"""
     previous_day_temperature: Optional[float] = None
-    """temperature of soil layer on the previous day (degrees C)"""
     decomposition_temperature_effect: Optional[float] = None
-    """temperature effect on decomposition factor (unitless) (pseudocode_soil S.6.A.1)"""
 
     # --- Erosion
     percent_organic_carbon_content: float = 1.2
-    """organic carbon content expressed as percent of soil in this layer (unitless)"""
     percent_clay_content: float = 18.7
-    """clay content expressed as percent of soil in this layer (unitless)"""
     percent_sand_content: float = 14.5
-    """sand content expressed as percent of soil in this layer (unitless)"""
     percent_silt_content: float = 64.5
-    """silt content expressed as percent of soil in this layer (unitless)"""
     percent_rock_content: float = 1
-    """rock content expressed as percent of soil in this layer (unitless)"""
 
     # --- Decomposition
     decomposition_moisture_effect: float = 0.0
-    """moisture effect on decomposition factor (unitless) (pseudocode_soil S.6.A.2)"""
 
     # --- pool_gas_partition
     # (pseudocode_soil S.6.A.1)
     plant_metabolic_active_carbon_usage: float = 0.0
-    """plant metabolic carbon decomposed into active carbon (kg/ha) (pseudocode_soil S.6.B.I.)"""
     plant_metabolic_active_carbon_loss: float = 0.0
-    """plant metabolic carbon being lost as carbon dioxide during decomposition into active carbon (kg/ha)"""
     plant_metabolic_active_carbon_remaining: float = 0.0
-    """plant metabolic carbon decomposed to active carbon after accounting for carbon dioxide loss (kg/ha)"""
 
     plant_structural_active_carbon_usage: float = 0.0
-    """plant structural carbon decomposed into active carbon (kg/ha) (pseudocode_soil S.6.B.I.11)"""
     plant_structural_active_carbon_loss: float = 0.0
-    """plant structural carbon being lost as carbon dioxide during decomposition into active carbon (kg/ha)"""
     plant_structural_active_carbon_remaining: float = 0.0
-    """plant metabolic carbon decomposed to active carbon after accounting for carbon dioxide loss (kg/ha)"""
 
     plant_structural_slow_carbon_usage: float = 0.0
-    """plant structural carbon decomposed into slow carbon (kg/ha) (pseudocode_soil S.6.B.I.11)"""
     plant_structural_slow_carbon_loss: float = 0.0
-    """plant structural carbon being lost as carbon dioxide during decomposition into slow carbon (kg/ha)"""
     plant_structural_slow_carbon_remaining: float = 0.0
-    """plant metabolic carbon decomposed to slow carbon after accounting for carbon dioxide loss (kg/ha)"""
 
     soil_metabolic_active_carbon_usage: float = 0.0
-    """soil metabolic carbon decomposed into active carbon (kg/ha) (pseudocode_soil S.6.B.II.8)"""
     soil_metabolic_active_carbon_loss: float = 0.0
-    """soil metabolic carbon being lost as carbon dioxide during decomposition into active carbon (kg/ha)"""
     soil_metabolic_active_carbon_remaining: float = 0.0
-    """soil metabolic carbon decomposed to active carbon after accounting for carbon dioxide loss (kg/ha)"""
 
     soil_structural_active_carbon_usage: float = 0.0
-    """soil structural carbon decomposed into active carbon (kg/ha) (pseudocode_soil S.6.B.II.11)"""
     soil_structural_active_carbon_loss: float = 0.0
-    """soil structural carbon being lost as carbon dioxide during decomposition into active carbon (kg/ha)"""
     soil_structural_active_carbon_remaining: float = 0.0
-    """soil structural carbon decomposed to active carbon after accounting for carbon dioxide loss (kg/ha)"""
 
     soil_structural_slow_carbon_usage: float = 0.0
-    """soil structural carbon decomposed into slow carbon (kg/ha) (pseudocode_soil S.6.B.II.11)"""
     soil_structural_slow_carbon_loss: float = 0.0
-    """soil structural carbon being lost as carbon dioxide during decomposition into slow carbon (kg/ha)"""
     soil_structural_slow_carbon_remaining: float = 0.0
-    """soil structural carbon decomposed to slow carbon after accounting for carbon dioxide loss (kg/ha)"""
 
     active_carbon_decomposition_rate: float = 0.0
-    """rate at which active carbon is decomposed into slow or passive carbon and CO2 (%) (pseudocode_soil S.6.C.2)"""
     carbon_lost_adjusted_factor: float = 0.0
-    """adjusted factor of CO2 loss from the decomposition of active carbon (pseudocode_soil S.6.C.6)"""
 
     # pseudocode_soil S.6.C.3
     active_carbon_decomposition_amount: float = 0.0
-    """active carbon decomposed into slow or passive carbon and CO2 (kg/ha)"""
     active_carbon_amount: Optional[float] = None
-    """active carbon stored in the layer (kg/ha)"""
 
     # pseudocode_soil S.6.C.4
     slow_carbon_amount: Optional[float] = None
-    """slow carbon stored in the soil (kg/ha)"""
     slow_carbon_decomposition_amount: float = 0.0
-    """slow carbon decomposed into active or passive carbon and CO2 (kg/ha)"""
 
     # pseudocode_soil S.6.C.5
     passive_carbon_decomposition_amount: float = 0.0
-    """passive carbon decomposed into active or passive carbon and CO2 (kg/ha)"""
     passive_carbon_amount: Optional[float] = None
-    """passive carbon stored in the soil (kg/ha)"""
 
     # pseudocode_soil S.6.C.7
     active_carbon_to_slow_amount: float = 0.0
-    """active carbon decomposed into slow carbon (kg/ha)"""
     active_carbon_to_slow_loss: float = 0.0
-    """active carbon lost as CO2 during decomposition into slow carbon (kg/ha)"""
 
     # pseudocode_soil S.6.C.8
     active_carbon_to_passive_amount: float = 0.0
-    """active carbon decomposed into passive carbon (kg/ha)"""
 
     # pseudocode_soil S.6.C.9
     slow_to_active_carbon_amount: float = 0.0
-    """slow carbon decomposed into active carbon (kg/ha)"""
     slow_carbon_co2_lost_amount: float = 0.0
-    """slow carbon lost as CO2 during decomposition (kg/ha)"""
     slow_to_passive_carbon_amount: float = 0.0
-    """slow carbon decomposed into passive carbon (kg/ha)"""
 
     # pseudocode_soil S.6.C.10
     passive_to_active_carbon_amount: float = 0.0
-    """passive carbon decomposed into active carbon (kg/ha)"""
     passive_carbon_co2_lost_amount: float = 0.0
-    """passive carbon lost as CO2 during decomposition (kg/ha)"""
 
     # pseudocode_soil S.6.C.11
     plant_active_decompose_carbon: float = 0.0
-    """plant carbon decomposed into the active carbon pool (kg/ha)"""
     soil_active_decompose_carbon: float = 0.0
-    """soil carbon decomposed into the active carbon pool (kg/ha)"""
 
     # --- Phosphorus
     initial_labile_inorganic_phosphorus_concentration: float = None
-    """Concentration of labile inorganic phosphorus at the beginning of the simulation (mg / kg soil)
-        Note: default = 25, is from page 208 (bottom paragraph) of the SWAT theoretical documentation, and is reasonable
-        for soil in the plow layer of cropland.
-    """
     mean_phosphorus_sorption_parameter: float = None
-    """Parameter that determines the equilibria of the different inorganic phosphorus pools and has been adjusted so it
-        is not sensitive to large immediate changes in the soil chemistry (unitless).
-        Note: This value is very important, and is used a lot in both SurPhos and SWAT (SurPhos theoretical
-        documentation refers to it as the "Phosphorus Sorption Coefficient" - see eqn. [18], and SWAT theoretical
-        documentation as the "Phosphorus Availability Index" - section 3:2.1). In SWAT this value is entered by the
-        user, but as Pete Vadas found this was not a well understood or easily measured parameter, so SurPhos uses an
-        equation to compute it based off other soil attributes.
-    """
     labile_inorganic_phosphorus_content: float = 0
-    """Labile inorganic phosphorus content of this soil layer (kg / ha)"""
     active_inorganic_phosphorus_content: float = 0
-    """Active inorganic phosphorus content of this soil layer (kg / ha)"""
     stable_inorganic_phosphorus_content: float = 0
-    """Stable inorganic phosphorus content of this soil layer (kg / ha)"""
     fresh_organic_phosphorus_content: float = 0
-    """Fresh organic phosphorus content of this soil layer (kg / ha)"""
     # TODO: organic phosphorus still needs to be implemented - issue #444
 
     active_inorganic_unbalanced_counter: int = 0
-    """The number of days that the active inorganic phosphorus pool has been greater than it would be when in
-        equilibrium with the labile inorganic phosphorus pool."""
     labile_inorganic_unbalanced_counter: int = 0
-    """The number of days that the labile inorganic phosphorus pool has been greater than the it would be when in
-            equilibrium with the active inorganic phosphorus pool."""
     previous_phosphorus_balance: float = None
-    """The phosphorus balance on the previous day (unitless)"""
 
     percolated_phosphorus: float = 0.0
-    """Amount of phosphorus removed from the layer by water percolating out (kg / ha)."""
 
     # --- Residue partition
     plant_metabolic_to_soil_carbon_amount: float = 0.0
-    """metabolic carbon incorporated into soil during tillage (kg/ha)"""
     structural_litter_amount: float = 0.0
-    """amount of plant structural carbon (kg/ha)"""
     metabolic_litter_amount: float = 0.0
-    """plant metabolic carbon amount (hg/ha)"""
     tillage_fraction: float = 0.0
-    """Fraction of metabolic carbon incorporated into soil during tillage (unitless)"""
     structural_carbon_transfer_amount: float = 0.0
-    """the amount of transfer of structural carbon during tillage (kg/ha)"""
     soil_dry_matter_residue_amount: float = 0.0
-    """the amount of soil dry matter residue at harvest (kg/ha)"""
     plant_dry_matter_residue_amount: float = 0.0
-    """the amount of plant dry matter residue at harvest (kg/ha)"""
     plant_residue_metabolic_fraction: float = 0.0
-    """fraction of plant residue that is metabolic (unitless)"""
     plant_structural_to_slow_or_active_rate: float = 0.0
-    """the rate at which above ground structural carbon decomposes into slow or active carbon (unitless)"""
     weighted_residue_dry_matter_lignin_fraction: float = 0.0
-    """the weighted fractional of lignin amount in residue dry matter (unitless)"""
     soil_residue_lignin_fraction: float = 0.17
-    """the fraction of soil residue that's comprised of lignin (unitless)"""
     soil_lignin_to_nitrogen_fraction: float = 0.0
-    """soil lignin to nitrogen fraction(unitless)"""
     soil_residue_metabolic_fraction: float = 0.0
-    """the fraction of soil residue that is metabolic(unitless)"""
     soil_metabolic_carbon_amount: float = 0.0
-    """soil metabolic carbon amount (kg/ha)"""
     soil_structural_carbon_amount: float = 0.0
-    """amount of soil structural carbon decomposed into slow or active carbon (kg/ha)"""
     soil_structural_to_slow_or_active_rate: float = 0.0
-    """the rate at which below ground structural carbon decomposes into slow or active carbon (unitless)"""
 
     # ---- Nitrogen
     initial_soil_nitrate_concentration: Optional[float] = None
-    """Concentration of nitrates in this soil layer at beginning of the simulation (mg / kg soil)"""
     initial_soil_ammonium_concentration: Optional[float] = None
-    """Concentration of ammonium in this soil layer at beginning of the simulation (mg / kg soil)"""
     nitrate_content: Optional[float] = None
-    """Nitrate (NO3) content of this soil layer (kg / ha)"""
     ammonium_content: Optional[float] = None
-    """Ammonium (NH4+) content of this soil layer (kg / ha)"""
     active_organic_nitrogen_content: float = field(init=False)
-    """Active organic nitrogen content of this soil layer (kg / ha)"""
     stable_organic_nitrogen_content: float = field(init=False)
-    """Stable organic nitrogen content of this soil layer (kg / ha)"""
     fresh_organic_nitrogen_content: float = 0
-    """Fresh organic nitrogen content of this soil layer (kg / ha)
-        Note: all layers except the top layer are initialized with 0 fresh organic nitrogen."""
 
     nitrous_oxide_emissions: float = 0.0
-    """Amount of nitrous oxide emitted from this soil layer on the current day (kg / ha)."""
     annual_nitrous_oxide_emissions_total: float = 0.0
-    """Cumulative total amount of nitrates that have denitrified in a year (kg / ha)"""
 
     humus_mineralization_rate_factor: float = 0.0003
-    """Rate factor for humus mineralization of active organic nutrients (nitrogen and phosphorus) (unitless)
-        Reference: SWAT Input .BSN file, see "CMN" on page 101."""
     denitrification_rate_coefficient: float = 1.4
-    """Controls the rate of denitrification in this layer of soil (unitless)
-        Note: acceptable values for this attribute are in the range [0.0, 3.0].
-        Reference: SWAT Input .BSN file, see "CDN" on page 101."""
     denitrification_threshold_water_content: float = 1.10
-    """Fraction of field capacity water content above which denitrification takes place (unitless)
-        Reference: SWAT Input .BSN file, see "SDNCO" on page 102."""
     residue_fresh_organic_mineralization_rate: float = 0.05
-    """Rate coefficient for mineralization of residue fresh organic nutrients (nitrogen and phosphorus) (unitless)
-        Reference: SWAT Input .BSN file (see "RSDCO" on page 101) and SWAT Input CROP.DAT file (see "RSDCO_PL" on page
-        205)"""
     ammonium_volatilization_cation_exchange_factor: float = 0.15
-    """Exchange factor that accounts for the soil's cation exchange capacity, default = 0.15 (unitless)
-        Reference: SWAT Theoretical documentation eqn. 3:1.3.5"""
 
     ammonia_emissions: float = 0.0
-    """Amount of ammonium that volatilized out of the soil layer on the current day (kg / ha)."""
     annual_ammonia_emissions_total: float = 0.0
-    """Cumulative total of ammonium volatilized in this year (kg / ha)"""
 
     percolated_nitrates: float = 0.0
-    """Amount of nitrates removed from the soil layer by water percolating out (kg / ha)."""
     percolated_ammonium: float = 0.0
-    """Amount of ammonium removed from the soil layer by water percolating out (kg / ha)."""
     percolated_active_organic_nitrogen: float = 0.0
-    """Amount of active organic nitrogen removed from the soil layer by water percolating out (kg / ha)."""
 
     # --- Carbon cycling
     soil_overall_carbon_fraction: Optional[float] = None
-    """the total fraction of carbon in the soil (unitless)"""
     total_soil_carbon_amount: Optional[float] = None
-    """the total amount of soil carbon (kg/ha)"""
     annual_decomposition_carbon_CO2_lost: Optional[float] = None
-    """amount of total carbon lost as CO2 during decomposition(kg/ha)"""
     annual_carbon_CO2_lost: Optional[float] = None
-    """total amount of carbon lost as CO2 (kg/ha)"""
 
     def __post_init__(self, field_size: float, residue: float):
         """Initialize all attributes in the dataclass that depend on other attributes
