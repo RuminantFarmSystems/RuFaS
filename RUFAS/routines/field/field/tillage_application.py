c64f2157c42ceb1d4b4925979b77f2398aa50c6a
Allister Liu <allisterakun@gmail.com>
2024-03-04 12:23:17 -0500

    fixed all tests

--- RUFAS/routines/field/field/tillage_application.py
+++ RUFAS/routines/field/field/tillage_application.py
@@ -322,7 +322,7 @@ class TillageApplication:
             "year": "year",
             "day": "day",
             "field_size": "ha",
-            "average_clay_percent": "percent",
+            "average_clay_percent": "percentage",
         }
         info_map = {
             "class": self.__class__.__name__,
