00275bcb57ebff1465cbfae6f4fda9afa16c4076
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-03-06 16:25:47 -0500

    removed validate tabular data call from populate pool

--- RUFAS/input_manager.py
+++ RUFAS/input_manager.py
@@ -264,7 +264,6 @@ class InputManager:
             file_path = file_details["path"]
 
             try:
-                file_type = file_details["type"]
                 data_loader = data_type_to_loader_map[file_details["type"]]
                 input_data = data_loader(file_path)
             except KeyError:
@@ -291,22 +290,13 @@ class InputManager:
                     "invalid_elements": 0,
                     "is_valid": True,
                 }
-                if file_type == "json":
-                    element_counter_and_validity = self._validate_dict_element(
-                        [metadata_property],
-                        properties_blob_key,
-                        filtered_input_data,
-                        eager_termination,
-                        element_counter_and_validity,
-                    )
-                if file_type == "csv":
-                    element_counter_and_validity = self._validate_tabular_element(
-                        metadata_property,
-                        properties_blob_key,
-                        filtered_input_data,
-                        eager_termination,
-                        element_counter_and_validity,
-                    )
+                element_counter_and_validity = self._validate_dict_element(
+                    [metadata_property],
+                    properties_blob_key,
+                    filtered_input_data,
+                    eager_termination,
+                    element_counter_and_validity,
+                )
 
                 fixed_elements_counter += element_counter_and_validity["fixed_elements"]
                 valid_elements_counter += element_counter_and_validity["valid_elements"]
