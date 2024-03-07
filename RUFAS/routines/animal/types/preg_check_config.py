d3844e25d63d44e8ed788ab1333c1af02489fbbc
lpham <98855117+ltp23@users.noreply.github.com>
2024-03-07 15:55:34 -0800

    Update preg_check_config.py

--- RUFAS/routines/animal/types/preg_check_config.py
+++ RUFAS/routines/animal/types/preg_check_config.py
@@ -6,12 +6,13 @@ class PregCheckConfig(TypedDict, total=False):
     List of expected keys for preg check configuration dictionary,
     used in daily pregnancy check routine for HeiferII and Cow classes.
 
-    Each dictionary contains:
-        - 'day': The number of days from the AI day when the pregnancy check should occur.
-        - 'loss_rate': The probability of pregnancy loss at this check.
-        - 'on_preg_loss': The event message to log if pregnancy loss occurs at this check.
-        - 'on_preg': The event message to log if the cow remains pregnant at this check.
-        - 'on_not_preg' (optional): The event message to log if the cow is not pregnant, due to a failed conception,
+    Attributes
+    ----------
+        day (int): The number of days from the artificial insemination day when the pregnancy check should occur.
+        loss_rate (float): The probability of pregnancy loss at this check.
+        on_preg_loss (str): The event message to log if pregnancy loss occurs at this check.
+        on_preg (str): The event message to log if the cow remains pregnant at this check.
+        on_not_preg (str): The event message to log if the cow is not pregnant, due to a failed conception,
          at the start of the first check.
     """
 
