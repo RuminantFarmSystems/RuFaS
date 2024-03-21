aba5bcfde132be11153cf02001cb26433c95920f
Allister Liu <allisterakun@gmail.com>
2024-03-21 14:37:19 -0400

    Added unit tests and mypy type hints

--- RUFAS/input_manager.py
+++ RUFAS/input_manager.py
@@ -353,17 +353,17 @@ class InputManager:
         if len(input_array_data) == 0:
             return input_array_data
 
-        filtered_array_data = []
+        filtered_array_data: List[Any] = []
 
         for array_element in input_array_data:
             if isinstance(array_element, dict):
-                nested_input_data = self._filter_input_data_by_metadata(
+                nested_input_data: Dict[str, Any] = self._filter_input_data_by_metadata(
                     array_element, metadata_properties["properties"]
                 )
                 if nested_input_data:
                     filtered_array_data.append(nested_input_data)
             elif isinstance(array_element, list):
-                nested_input_data = self._filter_input_array_data_by_metadata(
+                nested_input_data: List[Any] = self._filter_input_array_data_by_metadata(
                     array_element, metadata_properties["properties"]
                 )
                 if nested_input_data:
@@ -391,15 +391,17 @@ class InputManager:
         metadata_properties : Dict[str, Any]
             The dictionary containing metadata properties used as a filter for input_data.
         """
-        filtered_input_data = {}
+        filtered_input_data: Dict[str, Any] = {}
         for key, value in input_data.items():
             if key in metadata_properties:
                 if isinstance(metadata_properties[key], dict) and isinstance(value, dict):
-                    nested_input_data = self._filter_input_data_by_metadata(value, metadata_properties[key])
+                    nested_input_data: Dict[str, Any] = self._filter_input_data_by_metadata(
+                        value, metadata_properties[key])
                     if nested_input_data:
                         filtered_input_data[key] = nested_input_data
                 elif isinstance(metadata_properties[key], dict) and isinstance(value, list):
-                    array_input_data = self._filter_input_array_data_by_metadata(value, metadata_properties[key])
+                    array_input_data: List[Any] = self._filter_input_array_data_by_metadata(
+                        value, metadata_properties[key])
                     if array_input_data:
                         filtered_input_data[key] = array_input_data
                 else:
