d72b2a4d3eb84bb2221305cf28605dcbae08288a
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-03-25 09:08:54 -0400

    changed docstrings, added constants

--- RUFAS/routines/feed_storage/feed_storage_constants.py
+++ RUFAS/routines/feed_storage/feed_storage_constants.py
@@ -4,27 +4,32 @@ class FeedStorageConstants:
 
     Attributes
     ----------
-    ALFALFA_GASEOUS_LOSS_LOWER_TEMP_THRESHOLD : float
+    ALFALFA_GASEOUS_LOSS_LOWER_TEMP_LIMIT : float
         Temperature below which ensiled alfalfa does not lose dry matter to fermentation (degrees C).
-    ALFALFA_GASEOUS_LOSS_UPPER_TEMP_THRESHOLD : float
+    ALFALFA_GASEOUS_LOSS_UPPER_TEMP_LIMIT : float
         Temperature above which ensiled alfalfa does not lose dry matter to fermentation (degrees C).
-    NON_ALFALFA_GASEOUS_LOSS_LOWER_TEMP_THRESHOLD : float
+    NON_ALFALFA_GASEOUS_LOSS_LOWER_TEMP_LIMIT : float
         Temperature below which ensiled non-alfalfa crops does not lose dry matter to fermentation (degrees C).
-    NON_ALFALFA_GASEOUS_LOSS_UPPER_TEMP_THRESHOLD : float
+    NON_ALFALFA_GASEOUS_LOSS_UPPER_TEMP_LIMIT : float
         Temperature above which ensiled non-alfalfa crops does not lose dry matter to fermentation (degrees C).
-    ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_THRESHOLD : float
-        Dry matter percentage below which ensiled alfalfa does not lose dry matter to fermentation.
-    NON_ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_THRESHOLD : float
-        Dry matter percentage below which ensiled non-alfalfa crops does not lose dry matter to fermentation.
-    GASEOUS_LOSS_UPPER_DRY_MATTER_THRESHOLD : float
-        Dry matter percentage below which ensiled crops do not lose dry matter to fermentation.
+    ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_LIMIT : float
+        Dry matter percentage of fresh mass below which ensiled alfalfa does not lose dry matter to fermentation.
+    ALFALFA_GASEOUS_LOSS_UPPER_DRY_MATTER_LIMIT : float
+        Dry matter percentage of fresh mass above which ensiled alfalfa does not lose dry matter to fermentation.
+    NON_ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_LIMIT : float
+        Dry matter percentage of fresh mass below which ensiled non-alfalfa crops does not lose dry matter to
+        fermentation.
+    NON_ALFALFA_GASEOUS_LOSS_UPPER_DRY_MATTER_LIMIT : float
+        Dry matter percentage of fresh mass above which ensiled non-alfalfa crops do not lose dry matter to
+        fermentation.
 
     """
 
-    ALFALFA_GASEOUS_LOSS_LOWER_TEMP_THRESHOLD = 5.0
-    ALFALFA_GASEOUS_LOSS_UPPER_TEMP_THRESHOLD = 45.0
-    NON_ALFALFA_GASEOUS_LOSS_LOWER_TEMP_THRESHOLD = 0.0
-    NON_ALFALFA_GASEOUS_LOSS_UPPER_TEMP_THRESHOLD = 40.0
-    ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_THRESHOLD = 20.0
-    NON_ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_THRESHOLD = 15.0
-    GASEOUS_LOSS_UPPER_DRY_MATTER_THRESHOLD = 60.0
+    ALFALFA_GASEOUS_LOSS_LOWER_TEMP_LIMIT = 5.0
+    ALFALFA_GASEOUS_LOSS_UPPER_TEMP_LIMIT = 45.0
+    NON_ALFALFA_GASEOUS_LOSS_LOWER_TEMP_LIMIT = 0.0
+    NON_ALFALFA_GASEOUS_LOSS_UPPER_TEMP_LIMIT = 40.0
+    ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_LIMIT = 20.0
+    ALFALFA_GASEOUS_LOSS_UPPER_DRY_MATTER_LIMIT = 60.0
+    NON_ALFALFA_GASEOUS_LOSS_LOWER_DRY_MATTER_LIMIT = 15.0
+    NON_ALFALFA_GASEOUS_LOSS_UPPER_DRY_MATTER_LIMIT = 60.0
