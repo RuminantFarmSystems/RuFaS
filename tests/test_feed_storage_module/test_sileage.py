c6e30dcfea41825182df6bdc160701f738f38e38
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-03-24 20:06:19 -0400

    moved effluent tests to silage

--- tests/test_feed_storage_module/test_sileage.py
+++ tests/test_feed_storage_module/test_sileage.py
@@ -18,24 +18,44 @@ def test_acceptable_crops(sileage: Sileage) -> None:
 
 
 @pytest.mark.parametrize(
-    "dry_matter,percentage,category,temp,expected",
+    "dry_matter,mass,expected",
     [
-        (100.0, 25.0, CropCategory.ALFALFA, 20.0, 1.378),
-        (40.0, 20.0, CropCategory.ALFALFA, 6.0, 0.624),
-        (150.0, 19.0, CropCategory.ALFALFA, 10.0, 0.0),
-        (200.0, 23.0, CropCategory.ALFALFA, 46.0, 0.0),
-        (140.0, 15.0, CropCategory.CORN, 30.0, 1.2096),
-        (55.0, 66.0, CropCategory.GRASS, 25.0, 0.0),
-        (120.0, 4.0, CropCategory.SMALL_GRAIN, 15.0, 0.0),
+        (31, 100.0, 0.0),
+        (30, 100.0, 0.0),
+        (25, 200.0, 10.0),
+        (1, 150.0, 43.5),
+        (0, 250.0, 75.0),
     ],
 )
-def test_calculate_dry_matter_loss_to_gas(
-    sileage: Sileage, dry_matter: float, percentage: float, category: CropCategory, temp: float, expected: float
-) -> None:
-    """Tests calculate_dry_matter_loss_to_gas in Sileage."""
-    actual = sileage.calculate_dry_matter_loss_to_gas(dry_matter, percentage, category, temp)
+def test_estimate_maximum_effluent(sileage: Sileage, dry_matter: float, mass: float, expected: float) -> None:
+    """
+    Test the estimate_maximum_effluent method of the Sileage class.
+    """
+    actual = sileage.estimate_maximum_effluent(dry_matter, mass)
 
     assert pytest.approx(actual) == expected
+
+
+@pytest.mark.parametrize(
+    "dry_matter,max_effluent,days,expected",
+    [
+        (100.0, 25.0, 8, 0.0207),
+        (350.0, 40.0, 2, 0.002365714),
+        (30.0, 55.0, 12, 0.2277),
+        (400.0, 12.0, 1, 0.0003105),
+        (100.0, 0.0, 4, 0.0),
+    ],
+)
+def test_calculate_dry_matter_loss_to_effluent(
+    sileage: Sileage, dry_matter: float, max_effluent: float, days: int, expected: float
+) -> None:
+    """
+    Test the calculate_dry_matter_loss_to_effluent method of the Sileage class.
+    """
+    actual = sileage.calculate_dry_matter_loss_to_effluent(dry_matter, max_effluent, days)
+
+    assert pytest.approx(actual) == expected
+
 
 
 @pytest.mark.parametrize(
