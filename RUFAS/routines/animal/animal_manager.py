2068b295f39b2a1ef91f2c4c01d7555865e5fc7a
harry-samuels <harrybsamuels@ymail.com>
2024-03-26 17:33:51 -0400

    remove old call location from animal manager and verify order that code is run w print statement

--- RUFAS/routines/animal/animal_manager.py
+++ RUFAS/routines/animal/animal_manager.py
@@ -129,7 +129,7 @@ class AnimalManager:
 
         AnimalBase.set_config(animal_config)
         AnimalBase.set_nutrient_list(feed.nutrient_rqmts)
-        AnimalBase.set_lactation_curve_parameters()
+        print("OLD LOCATION")
 
         # if False, there are no animals being simulated on the farm
         self.simulate_animals = config_data.get("simulate_animals", True)
