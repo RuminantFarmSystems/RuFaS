8d67a4711e7e3108f15b4de80847bd2584467be7
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-04-01 21:53:56 -0400

    add getter setter

--- RUFAS/input_manager.py
+++ RUFAS/input_manager.py
@@ -76,9 +76,33 @@ class InputManager:
             InputManager.__instance = self
         self.__metadata: Dict[str, Any] = {}
         self.__pool: Dict[str, Any] = {}
-        self.__properties_used: Dict[str, Any] = {}
         self.__get_data_logs_pool: Dict[str, str] = {}
         self.elements_counter = ElementsCounter()
+        self.__original_data_exists: bool = False
+
+    @property
+    def meta_data(self) -> Dict[str, Any]:
+        """The getter method for __metadata"""
+        return self.__metadata
+
+    @meta_data.setter
+    def meta_data(self, incoming_metadata: Dict[str, Any]) -> None:
+        """The setter method for __metadata"""
+        if self.__original_data_exists:
+            raise AttributeError("This instance of Input Manager is already populated.")
+        self.__metadata = incoming_metadata
+
+    @property
+    def pool(self) -> Dict[str, Any]:
+        """The getter method for __pool"""
+        return self.__pool
+
+    @pool.setter
+    def pool(self, incoming_pool: Dict[str, Any]) -> None:
+        """The setter method for __pool"""
+        if self.__original_data_exists:
+            raise AttributeError("This instance of Input Manager is already populated.")
+        self.__pool = incoming_pool
 
     def start_data_processing(self, metadata_path: str, eager_termination: bool = True) -> bool:
         """
@@ -100,6 +124,7 @@ class InputManager:
         self._load_metadata(metadata_path)
         self._load_properties()
         is_input_data_valid = self._populate_pool(eager_termination)
+        self.__original_data_exists = True
         return is_input_data_valid
 
     def _load_metadata(self, metadata_path: str) -> None:
@@ -168,7 +193,6 @@ class InputManager:
             if not os.path.exists(properties_path):
                 raise FileNotFoundError(f"Properties file not found at {properties_path}")
 
-            self.__properties_used = self.__metadata["files"]["properties"]
             del self.__metadata["files"]["properties"]
 
             self.__metadata["properties"] = self._load_data_from_json(properties_path)
