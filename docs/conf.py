109f3513bc3cdcfcfeb0f09baa807c040bd5ed18
Matthew Liu <matthew7838@gmail.com>
2024-04-02 17:29:35 +0800

    Edit setting to show static methods in Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,14 @@ extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': True,
+    'special-members': '__init__',
+    'static-members': True,
+    'member-order': 'bysource',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
