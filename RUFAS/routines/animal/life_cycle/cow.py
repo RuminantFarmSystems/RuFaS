53ea3acc8c345ed73248565b09bec58b3be0891a
Katherine Li <146815187+ktthr@users.noreply.github.com>
2024-04-04 11:53:14 -0400

    Update RUFAS/routines/animal/life_cycle/cow.py
    Co-authored-by: Niko <70217952+ew3361zh@users.noreply.github.com>

--- RUFAS/routines/animal/life_cycle/cow.py
+++ RUFAS/routines/animal/life_cycle/cow.py
@@ -355,7 +355,7 @@ class Cow(HeiferIII):
             calving_interval is an int (user input calving interval) if the
             value of animal_config["use_input_calving_interval"] exists
             and is True; otherwise, it is a float (average herd calving
-            interval), see logic in _set_avg_CI in lifecycle.py.
+            interval). See logic in _set_avg_CI in lifecycle.py.
 
         Returns
         -------
