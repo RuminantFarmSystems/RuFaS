9f1fa5c3402550cd5b50a314cb9f5cec7007236b
ndyt <ntomlinson@gmail.com>
2024-04-18 17:18:52 -0400

    updates soil layer erosion attributes from percents to fractions

--- RUFAS/routines/field/soil/phosphorus_cycling/phosphorus_mineralization.py
+++ RUFAS/routines/field/soil/phosphorus_cycling/phosphorus_mineralization.py
@@ -58,9 +58,9 @@ class PhosphorusMineralization:
                 field_size,
             )
             current_phosphorus_sorption_parameter = layer.calculate_phosphorus_sorption_parameter(
-                layer.percent_clay_content,
+                layer.clay_proportion_content,
                 soil_phosphorus_content,
-                layer.percent_organic_carbon_content,
+                layer.organic_carbon_proportion_content,
             )
             layer.mean_phosphorus_sorption_parameter = self._recompute_mean_phosphorus_sorption_parameter(
                 layer.mean_phosphorus_sorption_parameter,
