de69ee0ebb352be7fed573192bd1e75d2c971170
lpham <98855117+ltp23@users.noreply.github.com>
2024-04-22 15:06:18 -0700

    Updated unit tests

--- RUFAS/graph_generator.py
+++ RUFAS/graph_generator.py
@@ -103,7 +103,7 @@ class GraphGenerator:
     NOTE: This class is not multi-thread safe!!!
     """
 
-    def __init__(self, metadata_prefix: str = "", time= None) -> None:
+    def __init__(self, metadata_prefix: str = "", time=None) -> None:
         self.metadata_prefix = metadata_prefix
         self.time = time
 
