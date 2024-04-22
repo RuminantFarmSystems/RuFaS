206d0973f81b7059d1226b8ac979c7804b2b0911
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-04-22 16:35:47 -0400

    TODOs

--- RUFAS/task_manager.py
+++ RUFAS/task_manager.py
@@ -50,7 +50,7 @@ class TaskType(Enum):
 class TaskManager:
     def __init__(self):
         self.input_manager = InputManager()
-        self.output_manager = OutputManager()
+        self.output_manager = OutputManager()  # TODO use to log
         self.parsed_task_args: List[Dict[str, Any]] = []
 
     def start(self, metadata_path: str) -> None:
@@ -60,14 +60,13 @@ class TaskManager:
             workers, maxtasksperchild=1
         )  # maxtasksperchild=1 to maintain isolation between tasks and ensure no memory leaks happens in IO Managers
         self._parse_input_tasks()
-        print(f"{self.input_manager=}")
         task_with_args = partial(self.task, const_var=1)  # TODO remove or fix const_var
         results = self.pool.imap_unordered(task_with_args, self.parsed_task_args)
         for _ in results:
             pass
 
     def _parse_input_tasks(self) -> None:
-        tasks_from_input: Dict[str, Any] = self.input_manager.get_data("tasks.tasks")
+        tasks_from_input: Dict[str, Any] = self.input_manager.get_data("tasks.tasks")  # TODO imeplement
         for input_task in tasks_from_input:
             parsed_task_arg = {
                 "task_type": TaskType.from_string(input_task["task_type"]),
@@ -75,15 +74,7 @@ class TaskManager:
             self.parsed_task_args.append(parsed_task_arg)
 
     @staticmethod
-    def bar1(args: Dict[str, Any], const_var: int, input_manager: InputManager) -> None:
-        print(f"bar 1 {args['task_type']=}, {const_var=}, {input_manager=}")
-
-    @staticmethod
-    def bar2(args: Dict[str, Any], const_var: int, input_manager: InputManager) -> None:
-        print(f"bar 2 {args['task_type']=}, {const_var=}, {input_manager=}")
-
-    @staticmethod
-    def task(args: Dict[str, Any], const_var: int) -> None:
+    def task(args: Dict[str, Any], const_var: int) -> None:  # TODO imeplement
         input_manager = InputManager()
         if args["task_type"] == TaskType.SIMULATION_SIGNLE_RUN:
             TaskManager.bar1(args, const_var, input_manager)
@@ -91,3 +82,11 @@ class TaskManager:
             TaskManager.bar2(args, const_var, input_manager)
         else:
             print("error")
+
+    @staticmethod
+    def bar1(args: Dict[str, Any], const_var: int, input_manager: InputManager) -> None:  # TODO remove
+        print(f"bar 1 {args['task_type']=}, {const_var=}, {input_manager=}")
+
+    @staticmethod
+    def bar2(args: Dict[str, Any], const_var: int, input_manager: InputManager) -> None:  # TODO remove
+        print(f"bar 2 {args['task_type']=}, {const_var=}, {input_manager=}")
