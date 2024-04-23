6811bb1d81f6ee8144c939ecdf24e6e64e069138
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-04-23 14:20:02 -0400

    added name to bedding

--- RUFAS/routines/manure/beddings/bedding_classes.py
+++ RUFAS/routines/manure/beddings/bedding_classes.py
@@ -5,10 +5,8 @@ from typing import Dict
 from typing import Optional
 from typing import Type
 
-from RUFAS.routines.manure.default_enum.default_enum import DefaultEnum
 
-
-class BeddingType(DefaultEnum):
+class BeddingType(Enum):
     """
     Enumerate the different types of bedding.
 
@@ -29,8 +27,6 @@ class BeddingType(DefaultEnum):
         Represent the 'sand' type of bedding.
     NONE : str
         Represent no bedding.
-    DEFAULT : str
-        The default type of bedding is 'sand' if none is specified.
 
     """
 
@@ -40,7 +36,6 @@ class BeddingType(DefaultEnum):
     STRAW = "straw"
     SAND = "sand"
     NONE = "none"
-    DEFAULT = SAND
 
 
 @dataclass
@@ -91,6 +86,8 @@ class BaseBedding(ABC):
 
     Attributes
     ----------
+    name : str
+        The name of the bedding.
     bedding_mass_per_day : float
         Quantity of bedding required per animal per day (kg/animal/day).
     bedding_density : float
@@ -119,16 +116,19 @@ class BaseBedding(ABC):
 
     """
 
-    def __init__(self, bedding_config: BeddingConfig) -> None:
+    def __init__(self, name: str, bedding_config: BeddingConfig) -> None:
         """
         Initialize the base bedding class with specific configuration data.
 
         Parameters
         ----------
+        name : str
+            Identifier for the bedding configuration being used here.
         bedding_config : BeddingConfig
             A BeddingConfig object that specifies configuration data specific to the choice of bedding.
 
         """
+        self.name = name
         self.bedding_mass_per_day = bedding_config.bedding_mass_per_day
         self.bedding_density = bedding_config.bedding_density
         self.bedding_dry_matter_content = bedding_config.bedding_dry_matter_content
