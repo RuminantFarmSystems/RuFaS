80f858f1563fc3c8b83cb332e25cb2e494e239bb
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-04-24 02:22:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -51,58 +51,6 @@ def main() -> None:
         )
 
 
-def run_load_vars_pool(
-    vars_file_path: Path,
-    exclude_info_maps: bool,
-    format_option: str,
-    produce_graphics: bool,
-    graphics_dir: Path,
-    clear_output: bool,
-    output_dir: Path,
-    filters_dir: Path,
-    csv_dir: Path,
-) -> None:
-    """Instantiates Output Manager and triggers loading of the variables pool from the provided file path
-    for post-processing.
-
-    Parameters
-    ----------
-    vars_file_path : Path
-        The path to the json file to load into Output Manager variables pool for processing.
-    exclude_info_maps : bool
-        Flag for whether or not the user wants to inlcude info_maps data in their results files.
-    format_option : str
-        Format for variable_names.txt output file.
-    produce_graphics : bool
-        Flag for whether or not the user wants to produce graphs at after the simulation.
-    graphics_dir : Path
-        The directory for saving graphics.
-    clear_output : bool
-        Flag for whether or not the user wants to clear the output directory.
-    output_dir : Path
-        The directory for saving output.
-    filters_dir : Path
-        The directory for the files containing the keys for filtering.
-    csv_dir : Path
-        The directory for the csv output files to be saved.
-    """
-    output_manager = OutputManager()
-    if clear_output:
-        output_manager.clear_output_dir(vars_file_path, output_dir)
-    output_manager.flush_pools()
-    output_manager.load_variables_pool_from_file(vars_file_path)
-    output_manager.set_metadata_prefix("reload")
-    output_manager.save_results(
-        output_dir,
-        filters_dir,
-        exclude_info_maps,
-        produce_graphics,
-        graphics_dir,
-        csv_dir,
-    )
-    output_manager.dump_all_nondata_pools(output_dir, exclude_info_maps, format_option)
-
-
 class CaseInsensitiveArgumentAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None) -> None:
         for action in self.option_strings:
