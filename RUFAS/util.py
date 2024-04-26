4b77ff5b21573f9248427de410e7f2a1008923e8
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-04-26 16:31:12 -0400

    wrote time series gen method

--- RUFAS/util.py
+++ RUFAS/util.py
@@ -457,3 +457,45 @@ class Utility:
             return True
         else:
             return False
+
+    @staticmethod
+    def generate_time_series(date: datetime.date, days_before: int, days_after: int) -> list[datetime.date]:
+        """
+        Generates a list of dates based on a given date and when the dates should start and end relative to the given
+        date.
+
+        Parameters
+        ----------
+        date : datetime.date
+            Date around which the time sequence will be generated.
+        days_before : int
+            Number of days before the given date to start the time sequence, the minimum for this value is 0.
+        days_after : int
+            Number of days after the given date to end the time sequence, the minimum for this value is 0.
+
+        Raises
+        ------
+        ValueError
+            If the days_before value is negative.
+            If the days after value is negative.
+
+        Examples
+        --------
+        >>> Utility.generate_time_series(datetime.date(2024, 6, 1), 0, 0)
+        [datetime.date(2024, 6, 1)]
+        >>> Utility.generate_time_series(datetime.date(2024, 6, 1), 2, 0)
+        [datetime.date(2024, 5, 30), datetime.date(2024, 5, 31), datetime.date(2024, 6, 1)]
+        >>> Utility.generate_time_series(datetime.date(2024, 6, 1), 0, 2)
+        [datetime.date(2024, 6, 1), datetime.date(2024, 6, 2), datetime.date(2024, 6, 3)]
+        >>> Utility.generate_time_series(datetime.date(2024, 6, 1), 1, 1)
+        [datetime.date(2024, 5, 31), datetime.date(2024, 6, 1), datetime.date(2024, 6, 2)]
+
+        """
+        if days_before < 0:
+            raise ValueError(f"Cannot have negative value for 'days_before', passed {days_before}.")
+        if days_after < 0:
+            raise ValueError(f"Cannot have negative value for 'days_after', passed {days_after}.")
+
+        time_series = [date + datetime.timedelta(day) for day in range(-days_before, days_after + 1)]
+
+        return time_series
