a19df42cd46a33cacd15bb4dff8d73b70351712d
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-05-16 15:00:24 -0400

    wrote first test

--- tests/test_feed_storage_module/test_hay.py
+++ tests/test_feed_storage_module/test_hay.py
@@ -1,6 +1,10 @@
 import pytest
+from pytest_mock import MockerFixture
+from RUFAS.time import Time
+from RUFAS.routines.feed_storage.harvested_crop import HarvestedCrop
 from RUFAS.routines.feed_storage.hay import Hay
-from RUFAS.routines.feed_storage.enums import CropCategory
+from RUFAS.routines.feed_storage.enums import CropCategory, CropType
+from .sample_crop_data import sample_crop_data
 
 
 @pytest.fixture
@@ -16,9 +20,44 @@ def hay() -> Hay:
     return Hay()
 
 
+@pytest.fixture
+def harvested_crop() -> HarvestedCrop:
+    """
+    Pytest fixture to create a HarvestedCrop instance for testing.
+
+    Returns
+    -------
+    HarvestedCrop
+        An instance of the HarvestedCrop class.
+    """
+    category = CropCategory.SMALL_GRAIN
+    crop_type = CropType.WHEAT
+    return HarvestedCrop(category=category, type=crop_type, **sample_crop_data)  # type: ignore[arg-type]
+
+
 def test_acceptable_crops(hay: Hay):
     assert hay.acceptable_crops == [
         CropCategory.ALFALFA,
         CropCategory.GRASS,
         CropCategory.SMALL_GRAIN,
     ]
+
+
+@pytest.mark.parametrize("stored_day,current_day,expect_loss", [(1, 1, False), (1, 10, True)])
+def test_calculate_dry_matter_loss_to_gas(hay: Hay, harvested_crop: HarvestedCrop, mocker: MockerFixture, stored_day: int, current_day: int, expect_loss: bool) -> None:
+    """Tests calculate_dry_matter_loss_to_gas in Hay."""
+    mock_storage_time = mocker.MagicMock(autospec=Time)
+    mock_storage_time.simulation_day = stored_day
+    harvested_crop.storage_time = mock_storage_time
+    mock_current_time = mocker.MagicMock(autospec=Time)
+    mock_current_time.simulation_day = current_day
+    mock_initial_loss = mocker.patch.object(hay, "_calculate_initial_dry_matter_loss_to_gas", return_value=20.0)
+    mock_subsequent_loss = mocker.patch.object(hay, "_calculate_subsequent_dry_matter_loss_to_gas", return_value=10.0)
+    expected_loss = (30.0 if expect_loss else 0.0)
+    expected_call_count = (1 if expect_loss else 0)
+
+    actual = hay.calculate_dry_matter_loss_to_gas(harvested_crop, [], mock_current_time)
+
+    assert actual == expected_loss
+    assert mock_initial_loss.call_count == expected_call_count
+    assert mock_subsequent_loss.call_count == expected_call_count
