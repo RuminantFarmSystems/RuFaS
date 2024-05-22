9ce318712133d85259c4b7627c7835f182cdb730
Haowen Hu <hh598@cornell.edu>
2024-05-22 09:16:02 -0400

    Update animal_module_constants.py
    Updated the minimum intake of both lactating and dry cows to 2.0 kg/day

--- RUFAS/routines/animal/animal_module_constants.py
+++ RUFAS/routines/animal/animal_module_constants.py
@@ -76,11 +76,11 @@ class AnimalModuleConstants:
     MINIMUM_DAILY_DMI_RATIO: float = 0.01
     """Minimum estimated DMI (kg/day), as a percentage of body_weight in kg"""
 
-    MINIMUM_DMI_LACT: float = 3.94
-    """Minimum estimated DMI instituted for lactating cows, kg/day (Reed et al. 2015)"""
+    MINIMUM_DMI_LACT: float = 2.0
+    """Minimum estimated DMI instituted for lactating cows, kg/day. Note that the mimimum intake in the dataset used to generate the equation is 3.94 kg/day (Reed et al. 2015)"""
 
-    MINIMUM_DMI_DRY: float = 7.1
-    """Minimum estimated DMI instituted for dry cows, kg/day (Appuhamy 2018)"""
+    MINIMUM_DMI_DRY: float = 2.0
+    """Minimum estimated DMI instituted for dry cows, kg/day. Note that the minimum intake in the dataset used to generate the equation is 7.1 kg/day (Appuhamy 2018)"""
 
     MINIMUM_PHOSPHORUS: float = 0.0
     """Minimum phosphorus estimate, g/day"""
