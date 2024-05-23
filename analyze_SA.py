4a617704b54811e47935b14d4949ae4f8f554b04
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2024-05-23 17:49:06 -0400

    outputs_to_collate

--- analyze_SA.py
+++ analyze_SA.py
@@ -72,7 +72,7 @@ for analysis in config_json['analyses']:
         writer = csv.writer(f)
         writer.writerows(new_whole_output)
 
-    for input in inputs_to_collate:
+    if inputs_to_collate or outputs_to_collate:
         new_whole_output_pd = pd.DataFrame(new_whole_output)
         column_names = new_whole_output_pd.iloc[0]
         row_names = new_whole_output_pd[0]
@@ -80,10 +80,12 @@ for analysis in config_json['analyses']:
         new_whole_output_pd.columns = column_names[1:]
         new_whole_output_pd.index = row_names[1:]
         new_whole_output_pd = new_whole_output_pd.loc[:, ~new_whole_output_pd.columns.duplicated(keep="last")]
+        column_names = list(new_whole_output_pd.columns)
 
+    for input in inputs_to_collate:
         if sampler == "fractional_factorial":
             # get the input column starts with ME:
-            input_col = new_whole_output_pd["ME:" + input]
+            main_effects = new_whole_output_pd["ME:" + input]
 
             # get the total column sum of all that include it and start with IE:
             IEnames = [name for name in column_names if input in name and "IE:" in name]
@@ -92,43 +94,21 @@ for analysis in config_json['analyses']:
             IEsums = IEtable.sum(axis=1)
             IEsums.columns = ["IE"]
 
-            total_effect = IEsums + input_col
-            threethings = pd.concat([input_col, IEsums, total_effect], axis=1)
+            total_effect = IEsums + main_effects
+            threethings = pd.concat([main_effects, IEsums, total_effect], axis=1)
             threethings.rename({0: "interaction_effects", 1: "total_effects"}, axis=1, inplace=True)
-
-            newcols = []
-            for rowname in row_names[1:]:
-                print(rowname)
-                # get the list of inputs (X)
-                input_reformatted = input.replace(" ", ".")
-                input_index = parsed_SA_input_variables["names"].index(input_reformatted)
-                input_values = [value[input_index] for value in sampled_values]
-                # get the list of outputs (Y)
-                output_values = collated_outputs[rowname]
-                slope, r2_value, p_value = SA_helpers.regression_stuff(
-                    X=input_values, xname=input, Y=output_values, yname=rowname, plot_inputs=plot_inputs
-                )
-                newcols.append([slope, r2_value, p_value])
-            newcols_pd = pd.DataFrame(newcols)
-            newcols_pd.index = new_whole_output_pd.index
-
-            output_pd = pd.concat([threethings, newcols_pd], axis=1)
-            output_pd.sort_values(by="total_effects", axis=0, ascending=False, inplace=True)
-            output_pd.rename({0: "slope", 1: "r2_value", 2: "p_value"}, axis=1, inplace=True)
-            filenameout = output_path + output_prefix + input + "_summarytable.csv"
-            output_pd.to_csv(filenameout)
-
         elif (sampler == "sobol" or sampler == "saltelli_sobol"):
             # get the input column main effect: starts with S1:
-            input_col = new_whole_output_pd["S1:" + input]
+            main_effects = new_whole_output_pd["S1:" + input]
             total_effect = new_whole_output_pd["ST:" + input]
-            interaction_effects = total_effect - input_col
-            threethings = pd.concat([input_col, interaction_effects, total_effect], axis=1)
-            threethings.rename({0: "interaction_effects", threethings.columns[-1]: "total_effects"}, axis=1, inplace=True)
+            interaction_effects = total_effect - main_effects
+            threethings = pd.concat([main_effects, interaction_effects, total_effect], axis=1)
+            threethings.rename({0: "interaction_effects",
+                               threethings.columns[-1]: "total_effects"},
+                               axis=1, inplace=True)
 
             newcols = []
             for rowname in row_names[1:]:
-                print(rowname)
                 # get the list of inputs (X)
                 input_reformatted = input.replace(" ", ".")
                 input_index = parsed_SA_input_variables["names"].index(input_reformatted)
@@ -145,19 +125,65 @@ for analysis in config_json['analyses']:
             output_pd = pd.concat([threethings, newcols_pd], axis=1)
             output_pd.sort_values(by="total_effects", axis=0, ascending=False, inplace=True)
             output_pd.rename({0: "slope", 1: "r2_value", 2: "p_value"}, axis=1, inplace=True)
-            filenameout = output_path + output_prefix + input + "_summarytable.csv"
+            filenameout = output_path + output_prefix + "_input_META_" + input + "_summarytable.csv"
             output_pd.to_csv(filenameout)
-        # get the total column starts with ST:
-        # determine the order
-        # assign rank for each - based on total
-        # reorder the inputs based on their rank
-        # add in the single effect column
 
-        # run a simple linear regression, add slope, R2, and p to each
-        if plot_inputs:
-            # plot the input
-            pass
-        pass
+    # here for a single output, we sort and explore all the inputs
+    for output in outputs_to_collate:
+
+        just_output = pd.DataFrame(new_whole_output_pd.loc[output])
+        MEnames = [name for name in column_names if "ME:" in name or "S1:" in name]
+        temp_output = just_output.loc[MEnames]
+
+        # get the input column starts with ME:
+        # MEnames = [name for name in column_names if "ME:" in name]
+        # main_effects = new_whole_output_pd[MEnames].loc[output]
+
+        # get the total column sum of all that include it and start with IE:
+        interaction_effects = []
+        total_effects = []
+        for MEname in MEnames:
+            if sampler == "fractional_factorial":
+                IEnames_temp = [
+                    name
+                    for name in column_names
+                    if MEname.replace(".", " ").replace("ME:", "") in name and "IE:" in name
+                ]
+                IEtable = just_output.loc[IEnames_temp]
+                interaction_effects.append(IEtable.sum(axis=0).values[0])
+                total_effects.append(interaction_effects[-1] + just_output.loc[MEname].values[0])
+            else:
+                total_effects.append(just_output.loc[MEname.replace("S1:", "ST:")].values[0])
+                interaction_effects.append(total_effects[-1] - just_output.loc[MEname].values)
+
+        inteffs = pd.DataFrame(interaction_effects)
+        inteffs.index = temp_output.index
+        inteffs.rename({0: "interaction_effects"}, axis=1, inplace=True)
+        toteffs = pd.DataFrame(total_effects)
+        toteffs.index = temp_output.index
+        toteffs.rename({0: "total_effects"}, axis=1, inplace=True)
+        output_temp = pd.concat([temp_output, inteffs, toteffs], axis=1)
+
+        input_names = [name for name in output_temp.index]
+        newcols = []
+        for input_name in input_names:
+            input_reformatted = input_name.replace(" ", ".").replace("ME:", "").replace("S1:", "")
+            # Append the output for that input to the single_output
+            input_index = parsed_SA_input_variables["names"].index(input_reformatted)
+            input_values = [value[input_index] for value in sampled_values]
+            # get the list of outputs (Y)
+            output_values = collated_outputs[output]
+            slope, r2_value, p_value = SA_helpers.regression_stuff(
+                X=input_values, xname=input, Y=output_values, yname=output, plot_inputs=plot_inputs)
+            newcols.append([slope, r2_value, p_value])
+        newcols_pd = pd.DataFrame(newcols)
+
+        newcols_pd.index = output_temp.index
+        output_pd = pd.concat([output_temp, newcols_pd], axis=1)
+        output_pd.sort_values(by="total_effects", axis=0, ascending=False, inplace=True)
+        output_pd.rename({0: "slope", 1: "r2_value", 2: "p_value"}, axis=1, inplace=True)
+        filenameout = output_path + output_prefix + "_output_META_" + output + "_summarytable.csv"
+        output_pd.to_csv(filenameout)
 
     if plot_whole_new:
         SA_helpers.plot_whole_new(output_path=output_path,
