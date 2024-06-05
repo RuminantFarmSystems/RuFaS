a6e3b0f8b9f9ed01715361adff9ef4a6b32101de
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-06-05 17:55:26 -0400

    added method to calc land use change emissions

--- tests/animal_module_tests/test_purchased_feed_emissions_estimator.py
+++ tests/animal_module_tests/test_purchased_feed_emissions_estimator.py
@@ -9,13 +9,19 @@ def test_purchased_feed_emissions_estimator(mocker: MockerFixture) -> None:
     get_data = mocker.patch("RUFAS.routines.animal.purchased_feed_emissions_estimator.im.get_data")
     add_var = mocker.patch("RUFAS.routines.animal.purchased_feed_emissions_estimator.om.add_variable")
     setup_emissions = mocker.patch.object(PurchasedFeedEmissionsEstimator, "_setup_feed_emissions")
+    expected_get_data_calls = [
+        mocker.call("config.FIPS_county_code"),
+        mocker.call("purchased_feeds_emissions"),
+        mocker.call("purchased_feed_land_use_change_emissions")
+    ]
 
     estimator = PurchasedFeedEmissionsEstimator()
 
-    get_data.assert_called_once_with("config.FIPS_county_code")
-    assert add_var.call_count == 2
-    setup_emissions.assert_called_once()
-    assert estimator.missing_feed_ids == []
+    get_data.assert_has_calls(expected_get_data_calls)
+    assert add_var.call_count == 3
+    assert setup_emissions.call_count == 2
+    assert estimator.missing_purchased_feed_ids == []
+    assert estimator.missing_land_use_change_feed_ids == []
 
 
 def test_create_daily_purchased_feed_emissions_report(mocker: MockerFixture) -> None:
@@ -27,8 +33,8 @@ def test_create_daily_purchased_feed_emissions_report(mocker: MockerFixture) -> 
         return_value=None,
     )
     mock_estimator = PurchasedFeedEmissionsEstimator()
-    mock_estimator.missing_feed_ids = ["2"]
-    mock_estimator.feed_emissions = {"1": 0.15}
+    mock_estimator.missing_purchased_feed_ids = ["2"]
+    mock_estimator.purchased_feed_emissions = {"1": 0.15}
     expected_emissions_report = {"1": 3.0, "feed_emissions_total": 3.0}
     expected_warning_call = mocker.call(
         "Missing Purchased Feed Emissions",
@@ -40,7 +46,7 @@ def test_create_daily_purchased_feed_emissions_report(mocker: MockerFixture) -> 
 
     assert actual == expected_emissions_report
     add_warning.assert_has_calls([expected_warning_call])
-    assert mock_estimator.missing_feed_ids == ["2", "3"]
+    assert mock_estimator.missing_purchased_feed_ids == ["2", "3"]
 
 
 @pytest.mark.parametrize(
@@ -54,7 +60,6 @@ def test_create_daily_purchased_feed_emissions_report(mocker: MockerFixture) -> 
 def test_setup_feed_emissions(mocker: MockerFixture, code: int, expected: dict[str, float]) -> None:
     """Tests that feed emissions data is gathered and selected correctly."""
     mock_data = {"county_code": [1000, 1001, 1002], "1": [0.3, 0.4, 0.5], "2": [1.33, 2.33, 3.33], "3": [0.6, 0.7, 0.8]}
-    mocker.patch("RUFAS.routines.animal.purchased_feed_emissions_estimator.im.get_data", return_value=mock_data)
     mocker.patch(
         "RUFAS.routines.animal.purchased_feed_emissions_estimator.PurchasedFeedEmissionsEstimator.__init__",
         return_value=None,
@@ -62,6 +67,6 @@ def test_setup_feed_emissions(mocker: MockerFixture, code: int, expected: dict[s
     mock_estimator = PurchasedFeedEmissionsEstimator()
     mock_estimator.FIPS_county_code = code
 
-    actual = mock_estimator._setup_feed_emissions()
+    actual = mock_estimator._setup_feed_emissions(mock_data)
 
     assert actual == expected
