1f7aeb2af8c7a582e29ae9aa51ccebf6d66c9b8a
Allister Liu <39185942+allisterakun@users.noreply.github.com>
2024-06-11 13:06:06 -0400

    Update tests/test_time.py
    Co-authored-by: Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>

--- tests/test_time.py
+++ tests/test_time.py
@@ -27,8 +27,9 @@ def test_time_initialization(mock_config: Dict[str, Any], mocker: MockerFixture)
 
     mock_im_get_data.assert_called_once_with("config")
     assert time.config_data == mock_config
-    assert time.start_date == datetime.datetime.strptime("1999:2", "%Y:%j")
-    assert time.end_date == datetime.datetime.strptime("2000:1", "%Y:%j")
+    assert time.start_date == datetime.datetime(year=1999, month=1, day=2)
+    assert time.end_date == datetime.datetime(year=2000, month=1, day=1)
+
 
     assert time.current_date == time.start_date
     assert time.simulation_length_days == (time.end_date - time.start_date).days
