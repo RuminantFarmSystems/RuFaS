f1448f0bae3c2651347d974c1f72f054ba336cec
Matthew Liu <matthew7838@gmail.com>
2024-06-18 21:59:40 +0800

    removed unused imports

--- RUFAS/routines/animal/life_cycle/cow.py
+++ RUFAS/routines/animal/life_cycle/cow.py
@@ -7,7 +7,6 @@ from typing import Dict, Any
 
 import numpy as np
 
-from RUFAS.input_manager import InputManager
 from RUFAS.output_manager import OutputManager
 from RUFAS.routines.animal.animal_module_constants import AnimalModuleConstants
 from RUFAS.routines.animal.life_cycle import animal_constants as const
