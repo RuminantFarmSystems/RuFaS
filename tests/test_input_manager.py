3b2a5a17d3eceb2423b719dc6c6ffce8fd4b492d
Matthew Liu <matthew7838@gmail.com>
2024-07-10 00:54:46 +0800

    cleaned up and linting issue

--- tests/test_input_manager.py
+++ tests/test_input_manager.py
@@ -4934,7 +4934,7 @@ def test_check_modifiability_error(
     "variable_name,metadata_properties,eager_termination,info_map,modifiable",
     [("test", {"test": 12}, False, {"class": "aa", "function": "bb"}, False)],
 )
-def test_check_modifiability_error(
+def test_check_modifiability_warning(
     variable_name: str,
     metadata_properties: dict,
     eager_termination: bool,
@@ -5007,7 +5007,7 @@ def test_validate_data(
         input_manager,
         "_validate_input_by_type",
         side_effect=lambda variable_path, variable_properties, input_data, eager_termination, properties_blob_key,
-                           elements_counter, called_during_initialization: input_data.get(variable_path[0]) is not None
+                    elements_counter, called_during_initialization: input_data.get(variable_path[0]) is not None
     )
 
     validated_data = input_manager._validate_data(
