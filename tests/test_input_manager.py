792abdb6ba6c3cbc1a6eb4d7914a1ffb1ed1feb2
Matthew Liu <matthew7838@gmail.com>
2024-07-12 02:03:55 +0800

    linting issues

--- tests/test_input_manager.py
+++ tests/test_input_manager.py
@@ -4984,9 +4984,8 @@ def test_validate_data(
     mock_validate_input_by_type = mocker.patch.object(
         input_manager,
         "_validate_input_by_type",
-        side_effect=lambda variable_path, variable_properties, input_data, eager_termination, properties_blob_key, elements_counter, called_during_initialization: input_data.get(
-            variable_path[0]
-        )
+        side_effect=lambda variable_path, variable_properties, input_data, eager_termination, properties_blob_key,
+        elements_counter, called_during_initialization: input_data.get(variable_path[0])
         is not None,
     )
 
