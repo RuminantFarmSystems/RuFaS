54efe0f756f779c499f536ffb91566b9b46051fc
Kristan <41019131+KFosterReed@users.noreply.github.com>
2024-07-17 20:54:45 -0400

    MP units update
    reformatting unit conversions to use constant variables for clarity

--- RUFAS/routines/animal/ration/ration_driver.py
+++ RUFAS/routines/animal/ration/ration_driver.py
@@ -1102,8 +1102,9 @@ class RationReporter:
         RDP_diet = []
         RUP_diet = []
         for i, kg_fed in enumerate(ration.values()):
-            RDP_diet.append(RDP_list[i] * kg_fed * 0.01)
-            RUP_diet.append(kg_fed * RUP_list[i] * dRUP_diet[i])
+            RDP_diet.append(GeneralConstants.KG_TO_GRAMS * RDP_list[i] * kg_fed * GeneralConstants.PERCENTAGE_TO_FRACTION)
+            RUP_diet.append(GeneralConstants.KG_TO_GRAMS * kg_fed * RUP_list[i] * GeneralConstants.PERCENTAGE_TO_FRACTION * 
+                            dRUP_diet[i] * GeneralConstants.PERCENTAGE_TO_FRACTION)
 
         TDN_total_actual = TDNtotal * RationReporter.get_TDN_discount(
             ration_report, body_weight
@@ -1111,10 +1112,10 @@ class RationReporter:
 
         # MP bact calcs
         MP_bact = 0.64 * min(
-            1000 * 0.13 * TDN_total_actual, 1000 * 0.85 * sum(RDP_diet)
+            GeneralConstants.KG_TO_GRAMS * 0.13 * TDN_total_actual, 0.85 * sum(RDP_diet)
         )
 
-        MP_supply = MP_bact + sum(RUP_diet) * 0.1 + 0.4 * 11.8 * DMI_estimate
+        MP_supply = MP_bact + sum(RUP_diet) + 0.4 * 11.8 * DMI_estimate
         return float(MP_supply)
 
 
