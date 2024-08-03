08dfd882f28e9f72b4a534079c5b1d4e7a0bd9df
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-08-03 15:58:52 -0400

    tested get milk yield

--- tests/animal_module_tests/life_cycle/test_lactation_curve.py
+++ tests/animal_module_tests/life_cycle/test_lactation_curve.py
@@ -207,9 +207,20 @@ def test_calculate_adjusted_wood_parameters(
     assert actual == expected
 
 
-def test_get_milk_yield_values_wood_curve() -> None:
+@pytest.mark.parametrize(
+    "day,l_param,m_param,n_param,expected",
+    [
+        (1, 17.8, 0.229, 0.00331, 17.7411794),
+        (40, 17.8, 0.229, 0.00331, 36.2903495),
+        (200, 17.8, 0.229, 0.00331, 30.8924906),
+        (305, 17.8, 0.229, 0.00331, 24.0371332),
+    ]
+)
+def test_get_milk_yield_values_wood_curve(lactation_curve: LactationCurve, day: int, l_param: float, m_param: float, n_param: float, expected: float) -> None:
     """Test that milk yield on a given day is estimated correctly."""
-    pass
+    actual = lactation_curve.get_milk_yield_values_wood_curve(day, l_param, m_param, n_param)
+
+    assert pytest.approx(actual) == expected
 
 
 @pytest.mark.parametrize(
