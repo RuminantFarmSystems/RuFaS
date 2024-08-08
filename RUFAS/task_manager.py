2c486c5cce790925a6622578cf8f5cdd0bfd81ea
Matthew Liu <matthew7838@gmail.com>
2024-08-08 23:43:35 +0800

    try catch at the caller

--- RUFAS/task_manager.py
+++ RUFAS/task_manager.py
@@ -485,38 +485,35 @@ class TaskManager:
             Whether to load data pool from file.
 
         """
-        try:
-            info_map = {
-                "class": TaskManager.__name__,
-                "function": TaskManager.handle_post_processing.__name__,
-                "units": MeasurementUnits.UNITLESS,
-            }
-            output_manager.add_log("Validation counts", f"{str(input_manager.elements_counter)}", info_map)
+        info_map = {
+            "class": TaskManager.__name__,
+            "function": TaskManager.handle_post_processing.__name__,
+            "units": MeasurementUnits.UNITLESS,
+        }
+        output_manager.add_log("Validation counts", f"{str(input_manager.elements_counter)}", info_map)
 
-            if load_pool_from_file:
-                output_manager.flush_pools()
-                output_manager.load_variables_pool_from_file(args["output_pool_path"])
-                output_manager.set_metadata_prefix("reload")
+        if load_pool_from_file:
+            output_manager.flush_pools()
+            output_manager.load_variables_pool_from_file(args["output_pool_path"])
+            output_manager.set_metadata_prefix("reload")
 
-            output_manager.print_errors_warnings_logs_counts(task_id)
-            if save_results:
-                output_manager.save_results(
-                    args["filters_directory"],
-                    args["exclude_info_maps"],
-                    produce_graphics,
-                    args["report_directory"],
-                    args["graphics_directory"],
-                    args["csv_output_directory"],
-                    args["json_output_directory"],
-                )
+        output_manager.print_errors_warnings_logs_counts(task_id)
+        if save_results:
+            output_manager.save_results(
+                args["filters_directory"],
+                args["exclude_info_maps"],
+                produce_graphics,
+                args["report_directory"],
+                args["graphics_directory"],
+                args["csv_output_directory"],
+                args["json_output_directory"],
+            )
 
-            if not args["suppress_log_files"]:
-                input_manager.dump_get_data_logs(args["logs_directory"])
-                output_manager.dump_all_nondata_pools(
-                    args["logs_directory"], args["exclude_info_maps"], args["variable_name_style"]
-                )
-        except Exception:
-            pass
+        if not args["suppress_log_files"]:
+            input_manager.dump_get_data_logs(args["logs_directory"])
+            output_manager.dump_all_nondata_pools(
+                args["logs_directory"], args["exclude_info_maps"], args["variable_name_style"]
+            )
 
     @staticmethod
     def set_random_seed(random_seed: int | None, output_manager: OutputManager) -> None:
@@ -545,8 +542,11 @@ class TaskManager:
         produce_grahics: bool,
     ) -> None:
         """Handler for all methods related to metadata property comparison."""
-        TaskManager.handle_input_data_audit(args, input_manager, output_manager, False)
-        TaskManager.handle_post_processing(args, input_manager, output_manager, task_id)
+        try:
+            TaskManager.handle_input_data_audit(args, input_manager, output_manager, False)
+            TaskManager.handle_post_processing(args, input_manager, output_manager, task_id)
+        except Exception:
+            pass
 
     @staticmethod
     def _handle_compare_metadata_properties_tasks(
@@ -570,9 +570,12 @@ class TaskManager:
         produce_grahics: bool,
     ) -> None:
         """Handler for all methods related to herd initialization."""
-        args["init_herd"] = True
-        TaskManager.handle_herd_initializaition(args, output_manager)
-        TaskManager.handle_post_processing(args, input_manager, output_manager, task_id)
+        try:
+            args["init_herd"] = True
+            TaskManager.handle_herd_initializaition(args, output_manager)
+            TaskManager.handle_post_processing(args, input_manager, output_manager, task_id)
+        except Exception:
+            pass
 
     @staticmethod
     def _handle_simulation_engine_run_tasks(
@@ -601,4 +604,7 @@ class TaskManager:
         produce_graphics: bool,
     ) -> None:
         """Handler for all methods related to postprocessing."""
-        TaskManager.handle_post_processing(args, input_manager, output_manager, task_id, produce_graphics, True, True)
+        try:
+            TaskManager.handle_post_processing(args, input_manager, output_manager, task_id, produce_graphics, True, True)
+        except Exception:
+            pass
