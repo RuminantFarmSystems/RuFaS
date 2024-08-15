886ae127b01aa77c340205569a076760ceebb360
Matthew Liu <matthew7838@gmail.com>
2024-08-16 03:01:03 +0800

    Addressed Ed's comments

--- RUFAS/routines/manure/manure_treatments/anaerobic_lagoon.py
+++ RUFAS/routines/manure/manure_treatments/anaerobic_lagoon.py
@@ -118,13 +118,8 @@ class AnaerobicLagoon(BaseManureTreatment):
         )
 
         if self.config.manure_cover == ManureCoverEnum.COVER_AND_FLARE.value:
-            daily_output.storage_methane_burned = (
-                methane_loss * ManureConstants.METHANE_DESTRUCTION_EFFICIENCY * GeneralConstants.PERCENTAGE_TO_FRACTION
-            )
-            methane_loss = methane_loss * (
-                1 - ManureConstants.METHANE_DESTRUCTION_EFFICIENCY * GeneralConstants.PERCENTAGE_TO_FRACTION
-            )
-            daily_output.storage_methane = methane_loss
+            daily_output.storage_methane_burned, daily_output.storage_methane = self.calculate_cover_and_flare_methane(
+                methane_loss)
 
         methane_emission_from_non_degradable_volatile_solids = (
             methane_loss - methane_emission_from_degradable_volatile_solids
