73ce94f1d51b05ad890ad883f4fbbdcc2309602f
Allister Liu <allisterakun@gmail.com>
2024-08-15 08:28:39 -0700

    mypy errors

--- RUFAS/routines/animal/genetics/animal_genetics.py
+++ RUFAS/routines/animal/genetics/animal_genetics.py
@@ -2,12 +2,14 @@ from RUFAS.time import Time
 
 
 class AnimalGenetics:
-    def __init__(self):
+    def __init__(self) -> None:
         # read and save a lookup table for top-semen and net-merit percentile
         pass
 
     def assign_net_merit_value_to_animals_entering_herd(self, birth_date: str) -> float:
         pass
+        return 0.0
 
     def assign_net_merit_value_to_newborn_calf(self, time: Time, dam_net_merit_value: float) -> float:
         pass
+        return 0.0
