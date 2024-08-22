591776eb4338cbe66c0ae7ba98500739ff9f1900
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-08-22 13:35:02 -0400

    fixed test

--- tests/soil_crop_tests/manager_tests/test_field_manager.py
+++ tests/soil_crop_tests/manager_tests/test_field_manager.py
@@ -10,7 +10,6 @@ from RUFAS.routines.field.manager.fertilizer_schedule import FertilizerSchedule
 from RUFAS.routines.field.manager.manure_schedule import ManureSchedule
 from RUFAS.routines.manure.manure_treatments.manure_types import ManureType
 from RUFAS.routines.field.manager.tillage_schedule import TillageSchedule
-from RUFAS.routines.manure.field_manure_supplier import FieldManureSupplier
 from RUFAS.routines.field.field.field_data import FieldData
 from RUFAS.routines.field.field.field import Field
 from RUFAS.routines.field.soil.layer_data import LayerData
@@ -56,10 +55,6 @@ def test_field_manager_init(mocker: MockerFixture, field_blob_names: list[str]) 
     data_keys_by_properties = mocker.patch(
         "RUFAS.input_manager.InputManager.get_data_keys_by_properties", return_value=field_blob_names
     )
-    get_manure_supplier = mocker.patch(
-        "RUFAS.routines.field.manager.field_manager.FieldManager._get_manure_supplier",
-        return_value=mocked_manure_manager,
-    )
     field_setup = mocker.patch(
         "RUFAS.routines.field.manager.field_manager.FieldManager._setup_field", return_value=MagicMock(Field)
     )
@@ -70,7 +65,6 @@ def test_field_manager_init(mocker: MockerFixture, field_blob_names: list[str]) 
     assert len(field_manager.fields) == len(field_blob_names)
     assert len(field_manager.output_gatherer.fields) == len(field_blob_names)
     data_keys_by_properties.assert_called_once()
-    get_manure_supplier.assert_called_once_with(mocked_manure_manager)
     if len(field_blob_names) > 0:
         field_setup.assert_has_calls(expected_field_setup_calls)
         add_warning.assert_not_called()
@@ -177,29 +171,6 @@ def test_annual_update_routine(fields: List[Field]):
         for field in fields:
             assert field.perform_annual_reset.call_count == 1
         assert fm.output_gatherer.send_annual_variables.call_count == 1
-
-
-@pytest.mark.parametrize("animals", [True, False])
-def test_get_manure_supplier(mocker: MockerFixture, animals: bool) -> None:
-    """Tests that the correct manure supplier is provided for setting up Fields."""
-    mock_manure_manager = mocker.MagicMock(autospec=ManureManager)
-    mocker.patch("RUFAS.routines.field.manager.field_manager.FieldManager.__init__", return_value=None)
-
-    field_manager = FieldManager()
-    field_manager.im = mocker.MagicMock()
-    field_manager.im.get_data = mocker.MagicMock(return_value=animals)
-    field_manager.om = mocker.MagicMock()
-    add_log = mocker.patch.object(field_manager.om, "add_log")
-
-    actual = field_manager._get_manure_supplier(mock_manure_manager)
-
-    field_manager.im.get_data.assert_called_once_with("config.simulate_animals")
-    if animals:
-        assert actual == mock_manure_manager
-        add_log.assert_not_called()
-    else:
-        assert isinstance(actual, FieldManureSupplier)
-        add_log.assert_called_once()
 
 
 @pytest.mark.parametrize(
