88c80de4dac93f8c35629bdc81bed359a7cc14f1
Matthew Liu <matthew7838@gmail.com>
2024-09-09 12:14:11 +0800

    Niko's comment

--- tests/soil_crop_tests/soil_tests/test_soil_erosion.py
+++ tests/soil_crop_tests/soil_tests/test_soil_erosion.py
@@ -2,7 +2,10 @@ import pytest
 from math import exp, log, atan, sin
 from unittest.mock import MagicMock, patch
 
+from pytest_mock import MockerFixture
+
 from RUFAS.general_constants import GeneralConstants
+from RUFAS.output_manager import OutputManager
 from RUFAS.routines.field.soil.soil_data import SoilData
 from RUFAS.routines.field.soil.soil_erosion import SoilErosion
 
@@ -41,8 +44,22 @@ def test_determine_clay_silt_ratio_factor(silt: float, clay: float) -> None:
     """Tests _determine_clay_silt_ratio_factor() in soil_erosion.py"""
     observe = SoilErosion._determine_clay_silt_ratio_factor(silt, clay)
     expect = silt / (clay + silt)
-    expect = expect**0.3
+    expect = expect ** 0.3
     assert observe == expect
+
+
+def test_determine_clay_silt_ratio_factor_zero(mocker: MockerFixture) -> None:
+    """Tests the case when silt and clay are zero."""
+    mock_add = mocker.patch.object(OutputManager, "add_warning")
+    observed = SoilErosion._determine_clay_silt_ratio_factor(0, 0)
+    assert observed == 1
+    info_map = {'class': 'SoilErosion', 'function': '_determine_clay_silt_ratio_factor'}
+    mock_add.assert_called_once_with("Silt and clay fractions are 0",
+                                     f"Both silt and clay fractions are 0 which will lead to unreliable predictions "
+                                     f"of erosion and soil"
+                                     f" emissions",
+                                     info_map
+                                     )
 
 
 @pytest.mark.parametrize(
@@ -285,7 +302,7 @@ def test_determine_rainfall_intensity(
 )
 def test_determine_time_of_concentration(slope: float, manning: float, average_slope: float) -> None:
     """Tests that the time of concentration is determined correctly."""
-    expected = ((slope**0.6) * (manning**0.6)) / (18 * (average_slope**0.3))
+    expected = ((slope ** 0.6) * (manning ** 0.6)) / (18 * (average_slope ** 0.3))
     actual = SoilErosion._determine_time_of_concentration(slope, manning, average_slope)
     assert actual == expected
 
