d44df4a52feb59b7e0dfdd3ee84bacf23e792c10
Matthew Liu <matthew7838@gmail.com>
2024-09-11 01:59:50 +0800

    reverting changes accidentally made

--- RUFAS/routines/EEE/emissions.py
+++ RUFAS/routines/EEE/emissions.py
@@ -366,7 +366,6 @@ class EmissionsEstimator:
                 aggregated_manure_apps[field],
                 aggregated_manure_requests[field],
             )
-            print("a")
             crops_with_emissions.extend(crops)
 
         info_map = {
