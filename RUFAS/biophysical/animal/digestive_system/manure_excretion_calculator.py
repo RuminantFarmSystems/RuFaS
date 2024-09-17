bd948de4e3c2952352af8b330eeccb14ae0a2125
Matthew Liu <matthew7838@gmail.com>
2024-09-17 22:40:53 +0800

    changes to prevent errors

---
+++ RUFAS/biophysical/animal/digestive_system/manure_excretion_calculator.py
@@ -0,0 +1,6 @@
+from RUFAS.data_structures.animal_manure_excretions import AnimalManureExcretions
+
+
+class ManureExcretionCalculator:
+    @staticmethod
+    def calf_manure() -> AnimalManureExcretions:
