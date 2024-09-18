c51f4ee5720d9945fa457c37f95c62f7898ba466
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-09-18 13:14:35 -0400

    fix time tests

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,5 +1,4 @@
-import datetime
-from datetime import date
+from datetime import date, datetime, timedelta
 from typing import Dict, Any
 
 import pytest
@@ -7,7 +6,6 @@ from unittest.mock import patch
 from pytest_mock import MockerFixture
 
 from RUFAS.time import Time
-
 
 @pytest.fixture
 def mock_config() -> Dict[str, Any]:
@@ -17,18 +15,13 @@ def mock_config() -> Dict[str, Any]:
     }
     return config
 
-
-def test_time_initialization(mock_config: Dict[str, Any], mocker: MockerFixture) -> None:
+def test_time_initialization() -> None:
     """Tests that Time instances are created correctly."""
 
-    mock_im_get_data = mocker.patch("RUFAS.input_manager.InputManager.get_data", return_value=mock_config)
+    time = Time(datetime(year=1999, month=1, day=2), datetime(year=2000, month=1, day=1))
 
-    time = Time()
-
-    mock_im_get_data.assert_called_once_with("config")
-    assert time.config_data == mock_config
-    assert time.start_date == datetime.datetime(year=1999, month=1, day=2)
-    assert time.end_date == datetime.datetime(year=2000, month=1, day=1)
+    assert time.start_date == datetime(year=1999, month=1, day=2)
+    assert time.end_date == datetime(year=2000, month=1, day=1)
 
     assert time.current_date == time.start_date
     assert time.simulation_length_days == (time.end_date - time.start_date).days
@@ -42,7 +35,7 @@ def test_advance(mock_config: Dict[str, Any], mocker: MockerFixture) -> None:
     for n in range(365):
         time.advance()
         assert time.simulation_day == n + 1
-        assert time.current_date == time.start_date + datetime.timedelta(days=n + 1)
+        assert time.current_date == time.start_date + timedelta(days=n + 1)
 
 
 def test_current_julian_day(mock_config: Dict[str, Any], mocker: MockerFixture) -> None:
@@ -154,12 +147,12 @@ def test_record_time(mock_config: Dict[str, Any], mocker: MockerFixture) -> None
     "start_date_str, simulation_day, expected_date",
     [
         # Year 2023
-        ("2023:1", 1, datetime.datetime(2023, 1, 1)),
-        ("2023:1", 365, datetime.datetime(2023, 12, 31)),
+        ("2023:1", 1, datetime(2023, 1, 1)),
+        ("2023:1", 365, datetime(2023, 12, 31)),
         # Year 2024 (Leap Year)
-        ("2024:1", 1, datetime.datetime(2024, 1, 1)),
-        ("2024:1", 366, datetime.datetime(2024, 12, 31)),
-        ("2024:15", 17, datetime.datetime(2024, 1, 31)),
+        ("2024:1", 1, datetime(2024, 1, 1)),
+        ("2024:1", 366, datetime(2024, 12, 31)),
+        ("2024:15", 17, datetime(2024, 1, 31)),
     ],
 )
 def test_convert_simulation_day_to_date(
@@ -172,7 +165,7 @@ def test_convert_simulation_day_to_date(
     # Arrange
     mocker.patch("RUFAS.time.Time.__init__", return_value=None)
     time = Time()
-    time.start_date = datetime.datetime.strptime(start_date_str, "%Y:%j")
+    time.start_date = datetime.strptime(start_date_str, "%Y:%j")
 
     # Act
     actual_date = time.convert_simulation_day_to_date(simulation_day)
@@ -195,9 +188,9 @@ def test_str(mock_config: Dict[str, Any], mocker: MockerFixture) -> None:
 @pytest.mark.parametrize(
     "year,jday,expected",
     [
-        (2001, 3, datetime.datetime(2001, 1, 3)),
-        (2005, 350, datetime.datetime(2005, 12, 16)),
-        (2020, 60, datetime.datetime(2020, 2, 29)),
+        (2001, 3, datetime(2001, 1, 3)),
+        (2005, 350, datetime(2005, 12, 16)),
+        (2020, 60, datetime(2020, 2, 29)),
     ],
 )
 def test_convert_year_jday_to_date(
