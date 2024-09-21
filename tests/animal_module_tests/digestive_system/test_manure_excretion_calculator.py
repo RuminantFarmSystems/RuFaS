d892adc68dcdd92ff9d1a0734ab34bd3226b260b
Matthew Liu <matthew7838@gmail.com>
2024-09-21 14:50:34 +0800

    added heifer tests

--- tests/animal_module_tests/digestive_system/test_manure_excretion_calculator.py
+++ tests/animal_module_tests/digestive_system/test_manure_excretion_calculator.py
@@ -82,15 +82,95 @@ def test_calf_manure(body_weight: float,
 
     assert observed == expected
 
-def test_heifer_manure(
-    body_weight: float,
-                     fecal_phosphorus: float,
-                     urine_phosphorus_required: float,
-                     nutrient_amounts: dict[str, float],
-                     nutrient_concentrations: dict[str, float],
-                     inorganic_phosphorus_fraction: float,
-                     total_phosphorus_excreted: float,
-                     organic_phosphorus_fraction: float,
-                     manure_phosphorus_excreted: float,
-                     manure_phosphorus_fraction: float,
-                     mocker: MockerFixture) -> None:
+
+@pytest.mark.parametrize(
+    "body_weight,fecal_phosphorus,urine_phosphorus_required,nutrient_amounts,nutrient_concentrations,"
+    "inorganic_phosphorus_fraction,total_phosphorus_excreted,organic_phosphorus_fraction,manure_phosphorus_excreted,"
+    "manure_phosphorus_fraction",
+    [
+        (500000,
+         36.5,
+         22.8,
+         {"dm": 24.3},
+         {"CP": 92.4, "potassium": 23.6},
+         26.3,
+         56.2,
+         58.3,
+         77.7,
+         0.35)
+    ]
+)
+def test_heifer_manure(body_weight: float,
+                       fecal_phosphorus: float,
+                       urine_phosphorus_required: float,
+                       nutrient_amounts: dict[str, float],
+                       nutrient_concentrations: dict[str, float],
+                       inorganic_phosphorus_fraction: float,
+                       total_phosphorus_excreted: float,
+                       organic_phosphorus_fraction: float,
+                       manure_phosphorus_excreted: float,
+                       manure_phosphorus_fraction: float,
+                       mocker: MockerFixture) -> None:
+    """Tests manure excretion values for a growing heifer is calculated correctly."""
+    total_manure_excreted = 4.158 * nutrient_amounts["dm"] - 0.0246 * body_weight
+    total_solids = 0.178 * nutrient_amounts["dm"] + 2.733
+    degradable_volatile_solids = 0.9 * 0.0073 * body_weight
+    non_degradable_volatile_solids = 0.0073 * body_weight - 0.9 * 0.0073 * body_weight
+    manure_nitrogen = (
+                          15.1
+                          + 0.83
+                          * (nutrient_amounts["dm"] * GeneralConstants.KG_TO_GRAMS)
+                          * (nutrient_concentrations["CP"] * GeneralConstants.PROTEIN_TO_NITROGEN)
+                          / GeneralConstants.FRACTION_TO_PERCENTAGE
+                      ) * GeneralConstants.GRAMS_TO_KG
+    fecal_nitrogen = (
+                         0.345
+                         + 0.317
+                         * (nutrient_amounts["dm"] * GeneralConstants.KG_TO_GRAMS)
+                         * (nutrient_concentrations["CP"] * GeneralConstants.PROTEIN_TO_NITROGEN)
+                         / GeneralConstants.FRACTION_TO_PERCENTAGE
+                     ) * GeneralConstants.GRAMS_TO_KG
+    urine_nitrogen = manure_nitrogen - fecal_nitrogen
+    urinary_nitrogen_concentration = (urine_nitrogen * GeneralConstants.KG_TO_GRAMS) / 9.0
+    urine_urea_nitrogen_concentration = -1.16 + 0.86 * urinary_nitrogen_concentration
+    potassium = (
+        nutrient_amounts["dm"]
+        * (nutrient_concentrations["potassium"] / GeneralConstants.FRACTION_TO_PERCENTAGE)
+        * GeneralConstants.KG_TO_GRAMS
+    )
+    mock_phosphorus_excretion = mocker.patch.object(ManureExcretionCalculator,
+                                                    "_calculate_phosphorus_excretion_values",
+                                                    return_value=(total_phosphorus_excreted,
+                                                                  inorganic_phosphorus_fraction,
+                                                                  organic_phosphorus_fraction,
+                                                                  manure_phosphorus_excreted,
+                                                                  manure_phosphorus_fraction))
+    expected = (total_phosphorus_excreted,
+                AnimalManureExcretions(
+                    urea=urine_urea_nitrogen_concentration,
+                    urine=9.0,
+                    manure_total_ammoniacal_nitrogen=urine_nitrogen,
+                    urine_nitrogen=urine_nitrogen,
+                    manure_nitrogen=manure_nitrogen,
+                    manure_mass=total_manure_excreted,
+                    total_solids=total_solids,
+                    degradable_volatile_solids=degradable_volatile_solids,
+                    non_degradable_volatile_solids=non_degradable_volatile_solids,
+                    inorganic_phosphorus_fraction=inorganic_phosphorus_fraction,
+                    organic_phosphorus_fraction=organic_phosphorus_fraction,
+                    non_water_inorganic_phosphorus_fraction=0.0,
+                    non_water_organic_phosphorus_fraction=0.0,
+                    phosphorus=manure_phosphorus_excreted,
+                    phosphorus_fraction=manure_phosphorus_fraction,
+                    potassium=potassium,
+                ))
+    observed = ManureExcretionCalculator.heifer_manure(body_weight, fecal_phosphorus, urine_phosphorus_required,
+                                                       nutrient_amounts,
+                                                       nutrient_concentrations)
+
+    mock_phosphorus_excretion.assert_called_once_with(daily_milk_production=0,
+                                                      total_manure_excreted=total_manure_excreted,
+                                                      fecal_phosphorus=fecal_phosphorus,
+                                                      urine_phosphorus_required=urine_phosphorus_required)
+
+    assert observed == expected
