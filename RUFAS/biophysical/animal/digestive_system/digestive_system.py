9013027c8754d7f75cf8dc021bbc5bac84dbeda4
Matthew Liu <matthew7838@gmail.com>
2024-09-25 04:58:51 +0800

    started updates

--- RUFAS/biophysical/animal/digestive_system/digestive_system.py
+++ RUFAS/biophysical/animal/digestive_system/digestive_system.py
@@ -1,2 +1,11 @@
+from RUFAS.biophysical.animal.animal_properties.digestive_system_properties import DigestiveSystemProperties
+from RUFAS.biophysical.animal.animal_properties.general_properties import GeneralProperties
+
+
 class DigestiveSystem:
+    @staticmethod
+    def daily_update(general_properties: GeneralProperties,
+                     digestive_properties: DigestiveSystemProperties,
+                     animal_phosphorus_property: AnimalPhosphorusProperties,
+                     ):
     pass
