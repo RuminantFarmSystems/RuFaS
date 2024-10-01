eb0f66c6a4f8aecbcdb314f50aaf6d966baae806
Matthew Liu <matthew7838@gmail.com>
2024-10-02 01:38:00 +0800

    added tests

--- tests/test_EEE_module/test_emissions.py
+++ tests/test_EEE_module/test_emissions.py
@@ -452,3 +452,7 @@ def test_get_feed_emissions_data(feed_emission_data: dict[str, list[float]], cou
     observed = em._get_feed_emissions_data(county_code, feed_emission_data)
     assert observed == expected
 
+
+def test_calculate_homegrown_feed_emissions(mocker: MockerFixture) -> None:
+    """Tests the result of calculated homegrown feed emissions."""
+
