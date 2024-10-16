57e892137c082ad930204c75a31264354cbd0120
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-10-16 11:26:40 -0400

    added docstrings to new data types

--- RUFAS/data_structures/runtime_purchase_allowance.py
+++ RUFAS/data_structures/runtime_purchase_allowance.py
@@ -1,2 +1,6 @@
 class RuntimePurchaseAllowance:
+    """
+    Upper limits on the masses of feed that can be purchased and the monetary amounts that can be spent on purchased
+    feed nn a single day.
+    """
     pass
--- RUFAS/data_structures/total_inventory.py
+++ RUFAS/data_structures/total_inventory.py
@@ -1,6 +1,3 @@
 class TotalInventory:
-    """
-    Contains the total inventory of purchased and farm grown feeds projected to be available for feeding animals.
-    """
-
+    """Contains the total inventory of purchased and farm grown feeds projected to be available for feeding animals."""
     pass
