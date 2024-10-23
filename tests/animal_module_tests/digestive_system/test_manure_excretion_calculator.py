2b928d9025d0a3b15566b2ee42234d7ae91f0d86
Matthew Liu <matthew7838@gmail.com>
2024-10-24 00:47:34 +0800

    update tests with manual expected result ine test_manure_excretion_calculator.py

--- tests/animal_module_tests/digestive_system/test_manure_excretion_calculator.py
+++ tests/animal_module_tests/digestive_system/test_manure_excretion_calculator.py
@@ -37,35 +37,25 @@ def test_calf_manure(
             manure_phosphorus_fraction,
         ),
     )
-    total_manure_excreted = 3.45 * nutrient_amounts["dm"]
-    total_solids = 0.393 * nutrient_amounts["dm"]
-    degradable_volatile_solids = 0.9 * 0.0023 * body_weight
-    non_degradable_volatile_solids = 0.0023 * body_weight - 0.9 * 0.0023 * body_weight
-    manure_nitrogen = (
-        112.55 * nutrient_amounts["dm"] * (nutrient_concentrations["CP"] / 100)
-    ) * GeneralConstants.GRAMS_TO_KG
-    urine_nitrogen = 0.45 * (
-        (112.55 * nutrient_amounts["dm"] * (nutrient_concentrations["CP"] / 100)) * GeneralConstants.GRAMS_TO_KG
-    )
 
     expected = (
-        total_phosphorus_excreted,
+        56.2,
         AnimalManureExcretions(
             urea=9.52,
             urine=2.0,
-            manure_total_ammoniacal_nitrogen=urine_nitrogen,
-            urine_nitrogen=urine_nitrogen,
-            manure_nitrogen=manure_nitrogen,
-            manure_mass=total_manure_excreted,
-            total_solids=total_solids,
-            degradable_volatile_solids=degradable_volatile_solids,
-            non_degradable_volatile_solids=non_degradable_volatile_solids,
-            inorganic_phosphorus_fraction=inorganic_phosphorus_fraction,
-            organic_phosphorus_fraction=organic_phosphorus_fraction,
+            manure_total_ammoniacal_nitrogen=1.137198447,
+            urine_nitrogen=1.137198447,
+            manure_nitrogen=2.52710766,
+            manure_mass=83.83500000000001,
+            total_solids=9.549900000000001,
+            degradable_volatile_solids=1035.0,
+            non_degradable_volatile_solids=115.0,
+            inorganic_phosphorus_fraction=26.3,
+            organic_phosphorus_fraction=58.3,
             non_water_inorganic_phosphorus_fraction=0.0,
             non_water_organic_phosphorus_fraction=0.0,
-            phosphorus=manure_phosphorus_excreted,
-            phosphorus_fraction=manure_phosphorus_fraction,
+            phosphorus=77.7,
+            phosphorus_fraction=0.35,
             potassium=0,
         ),
     )
@@ -76,7 +66,7 @@ def test_calf_manure(
 
     mock_phosphorus_excretion.assert_called_once_with(
         daily_milk_production=0,
-        total_manure_excreted=total_manure_excreted,
+        total_manure_excreted=83.83500000000001,
         fecal_phosphorus=fecal_phosphorus,
         urine_phosphorus_required=urine_phosphorus_required,
     )
@@ -104,32 +94,6 @@ def test_heifer_manure(
     mocker: MockerFixture,
 ) -> None:
     """Tests manure excretion values for a growing heifer is calculated correctly."""
-    total_manure_excreted = 4.158 * nutrient_amounts["dm"] - 0.0246 * body_weight
-    total_solids = 0.178 * nutrient_amounts["dm"] + 2.733
-    degradable_volatile_solids = 0.9 * 0.0073 * body_weight
-    non_degradable_volatile_solids = 0.0073 * body_weight - 0.9 * 0.0073 * body_weight
-    manure_nitrogen = (
-        15.1
-        + 0.83
-        * (nutrient_amounts["dm"] * GeneralConstants.KG_TO_GRAMS)
-        * (nutrient_concentrations["CP"] * GeneralConstants.PROTEIN_TO_NITROGEN)
-        / GeneralConstants.FRACTION_TO_PERCENTAGE
-    ) * GeneralConstants.GRAMS_TO_KG
-    fecal_nitrogen = (
-        0.345
-        + 0.317
-        * (nutrient_amounts["dm"] * GeneralConstants.KG_TO_GRAMS)
-        * (nutrient_concentrations["CP"] * GeneralConstants.PROTEIN_TO_NITROGEN)
-        / GeneralConstants.FRACTION_TO_PERCENTAGE
-    ) * GeneralConstants.GRAMS_TO_KG
-    urine_nitrogen = manure_nitrogen - fecal_nitrogen
-    urinary_nitrogen_concentration = (urine_nitrogen * GeneralConstants.KG_TO_GRAMS) / 9.0
-    urine_urea_nitrogen_concentration = -1.16 + 0.86 * urinary_nitrogen_concentration
-    potassium = (
-        nutrient_amounts["dm"]
-        * (nutrient_concentrations["potassium"] / GeneralConstants.FRACTION_TO_PERCENTAGE)
-        * GeneralConstants.KG_TO_GRAMS
-    )
     mock_phosphorus_excretion = mocker.patch.object(
         ManureExcretionCalculator,
         "_calculate_phosphorus_excretion_values",
@@ -141,25 +105,26 @@ def test_heifer_manure(
             manure_phosphorus_fraction,
         ),
     )
+
     expected = (
         total_phosphorus_excreted,
         AnimalManureExcretions(
-            urea=urine_urea_nitrogen_concentration,
+            urea=176.35486046222226,
             urine=9.0,
-            manure_total_ammoniacal_nitrogen=urine_nitrogen,
-            urine_nitrogen=urine_nitrogen,
-            manure_nitrogen=manure_nitrogen,
-            manure_mass=total_manure_excreted,
-            total_solids=total_solids,
-            degradable_volatile_solids=degradable_volatile_solids,
-            non_degradable_volatile_solids=non_degradable_volatile_solids,
-            inorganic_phosphorus_fraction=inorganic_phosphorus_fraction,
-            organic_phosphorus_fraction=organic_phosphorus_fraction,
+            manure_total_ammoniacal_nitrogen=1.8577136560000003,
+            urine_nitrogen=1.8577136560000003,
+            manure_nitrogen=2.9968849600000005,
+            manure_mass=-12198.9606,
+            total_solids=7.058400000000001,
+            degradable_volatile_solids=3285.0,
+            non_degradable_volatile_solids=365.0,
+            inorganic_phosphorus_fraction=26.3,
+            organic_phosphorus_fraction=58.3,
             non_water_inorganic_phosphorus_fraction=0.0,
             non_water_organic_phosphorus_fraction=0.0,
-            phosphorus=manure_phosphorus_excreted,
-            phosphorus_fraction=manure_phosphorus_fraction,
-            potassium=potassium,
+            phosphorus=77.7,
+            phosphorus_fraction=0.35,
+            potassium=5734.800000000001,
         ),
     )
     actual = ManureExcretionCalculator.calculate_heifer_manure(
@@ -168,7 +133,7 @@ def test_heifer_manure(
 
     mock_phosphorus_excretion.assert_called_once_with(
         daily_milk_production=0,
-        total_manure_excreted=total_manure_excreted,
+        total_manure_excreted=-12198.9606,
         fecal_phosphorus=fecal_phosphorus,
         urine_phosphorus_required=urine_phosphorus_required,
     )
@@ -213,43 +178,6 @@ def test_lactating_cow_manure(
     mocker: MockerFixture,
 ) -> None:
     """Test the manure excretion values for a lactating cow with information from the ration formulation."""
-    dry_matter_intake = nutrient_amounts["dm"]
-    ASH_diet_content = nutrient_amounts["ash"]
-    dry_matter_concentration = nutrient_concentrations["dm"]
-    ADF_concentration = nutrient_concentrations["ADF"]
-    CP_concentration = nutrient_concentrations["CP"]
-    NDF_concentration = nutrient_concentrations["NDF"]
-    potassium_concentration = nutrient_concentrations["potassium"]
-    fecal_solids = -0.576 + 0.370 * dry_matter_intake - 0.075 * CP_concentration + 0.059 * ADF_concentration
-    urine = -7.742 + 0.388 * dry_matter_intake + 0.726 * CP_concentration + 2.066 * milk_protein
-    total_manure_excreted = (
-        (
-            1.987 * dry_matter_intake
-            + 0.348 * ADF_concentration
-            - 0.412 * CP_concentration
-            - 0.074 * dry_matter_concentration
-            - 0.0057 * days_in_milk
-        )
-        + fecal_solids
-        + urine
-    )
-    manure_nitrogen = (20.3 + 0.654 * (dry_matter_intake * 1000) * (CP_concentration * 0.16) / 100) * 0.001
-    dry_matter_intake = max(dry_matter_intake, 2.0)
-    fecal_nitrogen = (-18.5 + 10.1 * dry_matter_intake) * 0.001
-    urine_nitrogen = manure_nitrogen - fecal_nitrogen
-    organic_matter_intake = dry_matter_intake - ASH_diet_content
-    degradable_volatile_solids = (
-        -1.017 + 0.364 * organic_matter_intake + 0.029 * NDF_concentration - 0.023 * CP_concentration
-    )
-    total_volatile_solids = (
-        -1.201 + 0.402 * organic_matter_intake + 0.036 * NDF_concentration - 0.024 * CP_concentration
-    )
-    non_degradable_volatile_solids = total_volatile_solids - degradable_volatile_solids
-    urinary_nitrogen_concentration = (urine_nitrogen * 1000) / urine
-    urine_urea_nitrogen_concentration = -1.16 + 0.86 * urinary_nitrogen_concentration
-    potassium = (
-        7.21 * dry_matter_intake + 15944 * potassium_concentration / 100 - 164.5
-    )
     mock_phosphorus_excretion = mocker.patch.object(
         ManureExcretionCalculator,
         "_calculate_phosphorus_excretion_values",
@@ -264,22 +192,22 @@ def test_lactating_cow_manure(
     expected = (
         total_phosphorus_excreted,
         AnimalManureExcretions(
-            urea=urine_urea_nitrogen_concentration,
-            urine=urine,
-            manure_total_ammoniacal_nitrogen=urine_nitrogen,
-            urine_nitrogen=urine_nitrogen,
-            manure_nitrogen=manure_nitrogen,
-            manure_mass=total_manure_excreted,
-            total_solids=fecal_solids,
-            degradable_volatile_solids=degradable_volatile_solids,
-            non_degradable_volatile_solids=non_degradable_volatile_solids,
-            inorganic_phosphorus_fraction=inorganic_phosphorus_fraction,
-            organic_phosphorus_fraction=organic_phosphorus_fraction,
+            urea=11.62193070833374,
+            urine=144.1778,
+            manure_total_ammoniacal_nitrogen=2.1428728480000006,
+            urine_nitrogen=2.1428728480000006,
+            manure_nitrogen=2.3698028480000004,
+            manure_mass=147.8078,
+            total_solids=1.7327999999999986,
+            degradable_volatile_solids=-17.1197,
+            non_degradable_volatile_solids=-1.4015000000000057,
+            inorganic_phosphorus_fraction=26.3,
+            organic_phosphorus_fraction=58.3,
             non_water_inorganic_phosphorus_fraction=0.0,
             non_water_organic_phosphorus_fraction=0.0,
-            phosphorus=manure_phosphorus_excreted,
-            phosphorus_fraction=manure_phosphorus_fraction,
-            potassium=potassium,
+            phosphorus=77.7,
+            phosphorus_fraction=0.35,
+            potassium=3773.487,
         ),
     )
 
@@ -292,11 +220,10 @@ def test_lactating_cow_manure(
         nutrient_amounts,
         nutrient_concentrations,
     )
-
     assert expected == actual
     mock_phosphorus_excretion.assert_called_once_with(
         daily_milk_production=daily_milk_production,
-        total_manure_excreted=total_manure_excreted,
+        total_manure_excreted=147.8078,
         fecal_phosphorus=fecal_phosphorus,
         urine_phosphorus_required=urine_phosphorus_required,
     )
@@ -318,6 +245,7 @@ def test_cow_manure_lactating(
     nutrient_concentrations: dict[str, float],
     mocker: MockerFixture,
 ) -> None:
+    """Tests that the lactating function is being called in the cow manure excretion routine."""
     mock_lactating_cow_manure = mocker.patch.object(ManureExcretionCalculator, "_calculate_lactating_cow_manure")
     ManureExcretionCalculator.calculate_cow_manure(
         True,
@@ -357,6 +285,7 @@ def test_cow_manure_dry(
     nutrient_concentrations: dict[str, float],
     mocker: MockerFixture,
 ) -> None:
+    """Tests that the dry cow function is being called in the cow manure excretion routine."""
     mock_dry_cow_manure = mocker.patch.object(ManureExcretionCalculator, "_calculate_dry_cow_manure")
     ManureExcretionCalculator.calculate_cow_manure(
         False,
