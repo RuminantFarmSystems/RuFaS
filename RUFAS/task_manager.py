7a58a9979647255fb3b03d03dce72b6f208d1ad1
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2024-10-23 12:12:49 -0400

    N_sampler to sampler_n

--- RUFAS/task_manager.py
+++ RUFAS/task_manager.py
@@ -277,19 +277,19 @@ class TaskManager:
         elif multi_run_args["sampler"] == "saltelli":
             sampled_values = saltelli_sampler.sample(
                 parsed_SA_input_variables,
-                multi_run_args["N_sampler"],
+                multi_run_args["sampler_n"],
                 skip_values=multi_run_args["skip_values"],
             )
         elif multi_run_args["sampler"] == "sobol":
             sampled_values = sobol_sampler.sample(
                 parsed_SA_input_variables,
-                multi_run_args["N_sampler"],
+                multi_run_args["sampler_n"],
                 skip_values=multi_run_args["skip_values"],
                 seed=multi_run_args["random_seed"],
             )
         elif multi_run_args["sampler"] == "morris":
             sampled_values = morris_sampler.sample(
-                parsed_SA_input_variables, multi_run_args["N_sampler"], seed=multi_run_args["random_seed"]
+                parsed_SA_input_variables, multi_run_args["sampler_n"], seed=multi_run_args["random_seed"]
             )
         else:
             self.output_manager.add_log(
