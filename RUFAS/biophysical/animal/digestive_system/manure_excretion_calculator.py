af247ea06e22f9efb45b94683326239cd9f9da5f
Matthew Liu <49083256+matthew7838@users.noreply.github.com>
2024-10-26 01:42:13 +0800

    Update RUFAS/biophysical/animal/digestive_system/manure_excretion_calculator.py
    Co-authored-by: Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>

--- RUFAS/biophysical/animal/digestive_system/manure_excretion_calculator.py
+++ RUFAS/biophysical/animal/digestive_system/manure_excretion_calculator.py
@@ -186,7 +186,7 @@ class ManureExcretionCalculator:
             + 0.83
             * (dry_matter_intake * GeneralConstants.KG_TO_GRAMS)
             * (crude_protein_concentration * GeneralConstants.PROTEIN_TO_NITROGEN)
-            / GeneralConstants.FRACTION_TO_PERCENTAGE
+            * GeneralConstants.PERCENTAGE_TO_FRACTION
         ) * GeneralConstants.GRAMS_TO_KG
 
         # Nitrogen excretion in feces, kg [A.3B.B.2]
