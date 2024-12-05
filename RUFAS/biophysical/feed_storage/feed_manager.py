f874f4916ea69b72f4965f052d51de7afd4e3e62
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-12-05 12:35:40 -0500

    cp'd feed storage into biophysical

---
+++ RUFAS/biophysical/feed_storage/feed_manager.py
@@ -0,0 +1,379 @@
+from datetime import date
+from typing import Any, Dict, List
+
+from RUFAS.data_structures.crop_soil_to_feed_storage_connection import (
+    CropCategory,
+    CropType,
+    HarvestedCrop,
+    StorageType,
+)
+from RUFAS.data_structures.feed_storage_to_animal_connection import (
+    IdealFeeds,
+    PlanningCycleAllowance,
+    RuntimePurchaseAllowance,
+    Feed,
+    NASEMFeed,
+    NRCFeed,
+    NutrientStandard,
+    Type,
+    Category,
+    RequestedFeed,
+    TotalInventory,
+)
+from RUFAS.input_manager import InputManager
+from RUFAS.time import Time
+from RUFAS.util import Utility
+from RUFAS.units import MeasurementUnits
+from RUFAS.weather import Weather
+
+from .baleage import Baleage
+from .grain import Dry, Grain, HighMoisture
+from .hay import Hay, ProtectedIndoors, ProtectedTarped, ProtectedWrapped, Unprotected
+from .silage import Bag, Bunker, Pile, Silage
+from .storage import Storage
+
+""""Defines the compatilibty between Crop Categories and Storage Types."""
+CROP_TO_STORAGE_MAPPING: Dict[CropCategory, type[List[Storage]]] = {
+    CropCategory.ALFALFA: [Hay, Silage, Baleage],
+    CropCategory.CORN: [Grain, Silage],
+    CropCategory.GRASS: [Hay, Silage, Baleage],
+    CropCategory.SMALL_GRAIN: [Hay, Grain, Silage, Baleage],
+    CropCategory.SOY: [Grain],
+}
+
+
+"""Maps each StorageType enum element to the associated Storage subclass."""
+STORAGE_TYPE_TO_CLASS_MAP: dict[StorageType, type[Storage]] = {
+    StorageType.PROTECTED_INDOORS: ProtectedIndoors,
+    StorageType.PROTECTED_WRAPPED: ProtectedWrapped,
+    StorageType.PROTECTED_TARPED: ProtectedTarped,
+    StorageType.UNPROTECTED: Unprotected,
+    StorageType.BALEAGE: Baleage,
+    StorageType.DRY: Dry,
+    StorageType.HIGH_MOISTURE: HighMoisture,
+    StorageType.BUNKER: Bunker,
+    StorageType.PILE: Pile,
+    StorageType.BAG: Bag,
+}
+
+
+QUERY_RESULT_DATA_TYPE = Dict[str, CropCategory | CropType | float]
+
+
+class FeedManager:
+    """
+    Manages the feed storage, handling crop reception, purchasing, degradation processing, feed distribution,
+    and querying available feeds.
+
+    Attributes
+    ----------
+    Dict[StorageType, Storage]
+        Containts the list of active storage units in the simulation and their mapping from StorageType(Enum).
+    """
+
+    def __init__(self, feed_config: dict[str, Any], nutrient_standard: NutrientStandard) -> None:
+        self.active_storages: Dict[StorageType, Storage] = {}
+        self._available_feeds: list[Feed] = self._setup_available_feeds(feed_config, nutrient_standard)
+        self.planning_cycle_allowance: PlanningCycleAllowance = PlanningCycleAllowance(feed_config)
+        self.runtime_purchase_allowance: RuntimePurchaseAllowance = RuntimePurchaseAllowance(feed_config)
+
+    def _query_result_factory(
+        self, crop_category: CropCategory, crop_type: CropType, amount: float
+    ) -> QUERY_RESULT_DATA_TYPE:
+        return {
+            "category": crop_category,
+            "type": crop_type,
+            "amount": amount,
+        }
+
+    def receive_crop(
+        self,
+        harvested_crop: HarvestedCrop,
+        storage_type: StorageType,
+    ) -> None:
+        """
+        Receives a harvested crop and assigns it to a storage unit.
+
+        Parameters
+        ----------
+        harvested_crop : HarvestedCrop
+            The harvested crop to be stored.
+        storage_type : StorageType
+            The type of storage to use for this crop.
+
+        Raises
+        ------
+        ValueError
+            If the crop type is not compatible with the storage type.
+        """
+        compatible_storage_classes = CROP_TO_STORAGE_MAPPING.get(harvested_crop.category, [])
+        is_crop_compatible_with_storage = any(
+            issubclass(STORAGE_TYPE_TO_CLASS_MAP[storage_type], storage_class)
+            for storage_class in compatible_storage_classes
+        )
+
+        if not is_crop_compatible_with_storage:
+            raise ValueError(
+                f"Crop of category '{harvested_crop.category}' is not compatible with storage type '{storage_type}'. "
+                f"Compatible storage types are: {', '.join([cls.__name__ for cls in compatible_storage_classes])}"
+            )
+
+        if storage_type not in self.active_storages:
+            self.active_storages[storage_type] = STORAGE_TYPE_TO_CLASS_MAP[storage_type]()
+
+        self.active_storages[storage_type].receive_crop(harvested_crop)
+
+    def process_degradations(self, weather: Weather, time: Time) -> None:
+        """
+        Processes the degradation of all stored feeds over time.
+        """
+        for _, storage in self.active_storages.items():
+            storage.process_degradations(weather, time)
+
+    def give_feed(self, amount: float, crop_type: CropType) -> float:
+        """
+        Distributes feed to the Animal module based on the FIFO principle.
+
+        Parameters
+        ----------
+        amount : float
+            The amount of feed to distribute.
+        crop_type : CropType
+            The type of crop to distribute.
+
+        Returns
+        -------
+        float
+            The actual amount of feed distributed.
+        """
+        pass
+
+    def manage_daily_feed_request(self, requested_feed: RequestedFeed) -> bool:
+        """Returns true if requested feeds can be provided, either through on-farm feeds or by purchasing."""
+        current_feed_totals = self._query_available_feed_totals(requested_feed.requested_feed.keys())
+
+        feeds_to_remove_from_inventory = {id: 0.0 for id in requested_feed.requested_feed.keys()}
+        feeds_to_purchase = {id: 0.0 for id in requested_feed.requested_feed.keys()}
+        for feed_id, amount_requested in requested_feed.requested_feed.items():
+            is_fulfillable_with_inventory: bool = amount_requested <= current_feed_totals[feed_id]
+            is_fulfillable_with_purchase: bool = (
+                (amount_requested - current_feed_totals[feed_id]) <= self.runtime_purchase_allowance[feed_id]
+            )
+            is_request_unfulfillable = not is_fulfillable_with_inventory and not is_fulfillable_with_purchase
+            if is_request_unfulfillable:
+                return False
+            feeds_to_remove_from_inventory[feed_id] = amount_requested
+            if not is_fulfillable_with_inventory:
+                feeds_to_purchase[feed_id] = amount_requested - current_feed_totals[feed_id]
+
+        self.purchase_feed(feeds_to_purchase)
+        self._deduct_feeds_from_inventory(feeds_to_remove_from_inventory)
+
+        return True
+
+    def get_total_inventory(self, inventory_date: date) -> TotalInventory:
+        """Gets the inventory expected to be held in storage at the specified date."""
+        # TODO: project losses at given inventory date, then assess feeds in storage
+        available_feed_rufas_ids = [feed.rufas_id for feed in self._available_feeds]
+
+        available_feed_totals = self._query_available_feed_totals(available_feed_rufas_ids)
+
+        for feed in self._available_feeds:
+            feed.amount_available = available_feed_totals.get(feed.rufas_id, 0.0)
+
+        return TotalInventory(available_feeds=self._available_feeds, date=inventory_date)
+
+    def manage_planning_cycle_purchases(self, ideal_feeds: IdealFeeds) -> None:
+        """
+        Purchases as much of the ideal feeds as possible, while respecting the Planning Allowance, storage capacity,
+        future harvests, budget, etc.
+        """
+        # TODO: respect things other than the Planning Allowance
+        feeds_to_purchase = {
+            rufas_id: min(
+                ideal_feeds[rufas_id], self.planning_cycle_allowance.allowances.get(rufas_id, 0.0)
+            ) for rufas_id in ideal_feeds.ideal_feeds.keys()
+        }
+
+        self.purchase_feed(feeds_to_purchase)
+
+    def _query_available_feed_totals(self, query_feed_ids: list[int]) -> dict[int, float]:
+        """Gets the current dry matter mass of each feed ID currently in storage"""
+        feed_totals = {rufas_id: 0.0 for rufas_id in query_feed_ids}
+
+        for storage in self.active_storages.values():
+            for feed in storage.stored:
+                if feed.rufas_id in feed_totals:
+                    feed_totals[feed.rufas_id] += feed.dry_matter_mass
+
+        return feed_totals
+
+    def query_available_feeds(
+        self,
+        query_crop_types: List[CropType] | None = None,
+        query_crop_categories: List[CropCategory] | None = None,
+        query_storage_types: List[StorageType] | None = None,
+    ) -> List[QUERY_RESULT_DATA_TYPE]:
+        """
+        Queries the available amount of feed in storage.
+
+        Parameters
+        ----------
+        query_crop_types : List[CropType], optional, default=None
+            The types of crop to query (if None, all crop types are queried).
+        query_crop_categories : List[CropCategory], optional, default=None
+            The categories of crop to query (if None, all crop categories are queried).
+        query_storage_types : List[StorageType], optional, default=None
+            The types of storage to query (if None, all storages types are queried).
+
+        Returns
+        -------
+        List[QUERY_RESULT_DATA_TYPE]
+            The amount of available feed, either as a total or for a specific crop type.
+        """
+        query_all_crop_types = query_crop_types is None
+        query_all_crop_categories = query_crop_categories is None
+        query_all_storage_types = query_storage_types is None
+        results: List[QUERY_RESULT_DATA_TYPE] = []
+
+        for storage_type, storage in self.active_storages.items():
+            is_storage_queryable = query_all_storage_types or storage_type in query_storage_types
+            if not is_storage_queryable:
+                continue
+            for stored_crop in storage.stored:
+                is_crop_type_queryable = query_all_crop_types or stored_crop.type in query_crop_types
+                is_crop_category_queryable = query_all_crop_categories or stored_crop.category in query_crop_categories
+                if not (is_crop_type_queryable and is_crop_category_queryable):
+                    continue
+                for previous_result in results:
+                    if (
+                        stored_crop.type == previous_result["type"]
+                        and stored_crop.category == previous_result["category"]
+                    ):
+                        previous_result["amount"] += stored_crop.fresh_mass
+                        break
+                else:
+                    results.append(
+                        self._query_result_factory(
+                            stored_crop.category,
+                            stored_crop.type,
+                            stored_crop.fresh_mass,
+                        )
+                    )
+
+        return results
+
+    def purchase_feed(self) -> None:
+        """The purchase feed logic is currently in the Animal Module. We will move it to here."""
+        # TODO: this function will take in a `RequestedFeed` or dict[int, float] of feeds to purchase, purchase them,
+        # and put them in storage. It will also record the amounts and money spent on feeds.
+        pass  # TODO: implement me!
+
+    def _deduct_feeds_from_inventory(self, feeds_to_deduct: dict[int, float]) -> None:
+        """Removes feeds from storage in a FIFO manner."""
+        pass  # TODO: implement me!
+
+    def _setup_available_feeds(
+        self, feed_config: list[dict[str, Any]], nutrient_standard: NutrientStandard
+    ) -> list[Feed]:
+        feed_library = self._process_feed_library(nutrient_standard)
+
+        feed_representation = NASEMFeed if nutrient_standard is NutrientStandard.NASEM else NRCFeed
+        available_feeds = []
+        for feed in feed_config:
+            rufas_id = feed["purchased_feed"]
+            try:
+                nutritive_properties = feed_library[rufas_id]
+            except KeyError:
+                pass  # TODO: implement me!
+            new_feed = feed_representation(
+                rufas_id=rufas_id,
+                amount_available=0.0,
+                on_farm_cost=feed["on_farm_cost"],
+                purchase_cost=feed["purchased_feed_cost"],
+                **nutritive_properties,
+            )
+            available_feeds.append(new_feed)
+
+        return available_feeds
+
+    def _process_feed_library(self, nutrient_standard: NutrientStandard) -> dict[int, dict[str, Any]]:
+        im = InputManager()
+        feed_library = (
+            im.get_data("NASEM_Comp") if nutrient_standard is NutrientStandard.NASEM else im.get_data("NRC_Comp")
+        )
+
+        feed_library = Utility.convert_dict_of_lists_to_dict_of_lists(feed_library)
+
+        feed_library = {feed["rufas_id"]: feed for feed in feed_library}
+        for feed in feed_library.values():
+            del feed["rufas_id"]
+            feed["feed_type"] = Type(feed["feed_type"])
+            feed["Fd_Category"] = Category(feed["Fd_Category"])
+            feed["units"] = MeasurementUnits(feed["units"])
+        return feed_library
+
+    # TODO: remove this method after Feed Storage and Animal modules are connected - #1878
+    def setup_stored_feeds(self, feeds_info: dict[str, dict[str, str | float]], time: Time) -> None:
+        """Sets up HarvestedCrops for the Feed Manager to degrade, if running end-to-end testing."""
+        reusable_values = feeds_info["reusable_values"]
+        time_copy = Time(start_date=time.start_date, end_date=time.end_date, current_date=time.current_date)
+        reusable_values.update({"harvest_time": time_copy, "storage_time": time_copy})
+
+        hay_values: dict[str, str | float | CropCategory | CropType] = feeds_info[
+            "hay_values"
+        ]  # type: ignore[assignment]
+        hay_values.update(
+            {"category": CropCategory(hay_values["category"]), "type": CropType(hay_values["crop_type"])},
+            **reusable_values,
+        )
+        del hay_values["crop_type"]
+        baleage_values: dict[str, str | float | CropCategory | CropType] = feeds_info[
+            "baleage_values"
+        ]  # type: ignore[assignment]
+        baleage_values.update(
+            {"category": CropCategory(baleage_values["category"]), "type": CropType(baleage_values["crop_type"])},
+            **reusable_values,
+        )
+        del baleage_values["crop_type"]
+        grain_values: dict[str, str | float | CropCategory | CropType] = feeds_info[
+            "grain_values"
+        ]  # type: ignore[assignment]
+        grain_values.update(
+            {"category": CropCategory(grain_values["category"]), "type": CropType(grain_values["crop_type"])},
+            **reusable_values,
+        )
+        del grain_values["crop_type"]
+        silage_values: dict[str, str | float | CropCategory | CropType] = feeds_info[
+            "silage_values"
+        ]  # type: ignore[assignment]
+        silage_values.update(
+            {"category": CropCategory(silage_values["category"]), "type": CropType(silage_values["crop_type"])},
+            **reusable_values,
+        )
+        del silage_values["crop_type"]
+
+        storages: dict[StorageType, Storage] = {
+            StorageType.PROTECTED_INDOORS: ProtectedIndoors(),
+            StorageType.PROTECTED_WRAPPED: ProtectedWrapped(),
+            StorageType.PROTECTED_TARPED: ProtectedTarped(),
+            StorageType.UNPROTECTED: Unprotected(),
+            StorageType.BALEAGE: Baleage(),
+            StorageType.DRY: Dry(),
+            StorageType.HIGH_MOISTURE: HighMoisture(),
+            StorageType.BUNKER: Bunker(),
+            StorageType.PILE: Pile(),
+            StorageType.BAG: Bag(),
+        }
+        storages[StorageType.PROTECTED_INDOORS].receive_crop(HarvestedCrop(**hay_values))  # type: ignore[arg-type]
+        storages[StorageType.PROTECTED_WRAPPED].receive_crop(HarvestedCrop(**hay_values))  # type: ignore[arg-type]
+        storages[StorageType.PROTECTED_TARPED].receive_crop(HarvestedCrop(**hay_values))  # type: ignore[arg-type]
+        storages[StorageType.UNPROTECTED].receive_crop(HarvestedCrop(**hay_values))  # type: ignore[arg-type]
+        storages[StorageType.BALEAGE].receive_crop(HarvestedCrop(**baleage_values))  # type: ignore[arg-type]
+        storages[StorageType.DRY].receive_crop(HarvestedCrop(**grain_values))  # type: ignore[arg-type]
+        storages[StorageType.HIGH_MOISTURE].receive_crop(HarvestedCrop(**grain_values))  # type: ignore[arg-type]
+        storages[StorageType.BUNKER].receive_crop(HarvestedCrop(**silage_values))  # type: ignore[arg-type]
+        storages[StorageType.PILE].receive_crop(HarvestedCrop(**silage_values))  # type: ignore[arg-type]
+        storages[StorageType.BAG].receive_crop(HarvestedCrop(**silage_values))  # type: ignore[arg-type]
+
+        self.active_storages = storages
