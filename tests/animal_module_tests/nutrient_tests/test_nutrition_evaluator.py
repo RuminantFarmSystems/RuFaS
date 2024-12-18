efaa719f866db70e84ae7d03f9d1ae3078d9557e
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2024-12-18 17:14:13 -0500

    added test scaffolding

---
+++ tests/animal_module_tests/nutrient_tests/test_nutrition_evaluator.py
@@ -0,0 +1,61 @@
+def test_evaluate_energy_nutrition_supply() -> None:
+    """Tests that energy and nutrition supplied in ration is correctly compared against an animal's requirements."""
+    pass
+
+
+def test_check_total_energy_supplied() -> None:
+    """Tests that total energy supplied in ration is correctly compared against an animal's total energy requirement."""
+    pass
+
+
+def test_check_activity_maintenance_energy_supplied() -> None:
+    """
+    Tests that maintenance energy supplied in ration is correctly compared against an animal's maintenance energy
+    requirement.
+    """
+    pass
+
+
+def test_check_lactation_energy_supplied() -> None:
+    """
+    Tests that lactation energy supplied in ration is correctly compared against an animal's lactation energy
+    requirement.
+    """
+    pass
+
+
+def test_check_growth_energy_supplied() -> None:
+    """
+    Tests that growth energy supplied in ration is correctly compared against an animal's growth energy requirement.
+    """
+    pass
+
+
+def test_check_calcium_supplied() -> None:
+    """Tests that calcium supplied in ration is correctly compared against an animal's calcium requirement."""
+    pass
+
+
+def test_check_phosphorus_supplied() -> None:
+    """Tests that phosphorus supplied in ration is correctly compared against an animal's phosphorus requirement."""
+    pass
+
+
+def test_check_protein_supplied() -> bool:
+    """Tests that protein supplied in ration is correctly compared against an animal's protein requirement."""
+    pass
+
+
+def test_check_neutral_detergent_fiber_supplied() -> None:
+    """Tests that NDF supplied in ration is correctly compared against an animal's NDF requirement."""
+    pass
+
+
+def test_check_fat_content() -> None:
+    """Tests that fat supplied in ration is correctly compared against an animal's fat requirement."""
+    pass
+
+
+def test_check_dry_matter_intake() -> None:
+    """Tests that dry matter supplied in ration is correctly compared against an animal's dry matter requirement."""
+    pass
