015bd8ef7d1de3bb253ecd5d3a1e6511318fa8c2
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-01-08 14:15:39 -0500

    fixed mypy errors, base class no longer has interface

--- tests/animal_module_tests/nutrient_tests/test_nasem_requirements_calculator.py
+++ tests/animal_module_tests/nutrient_tests/test_nasem_requirements_calculator.py
@@ -135,7 +135,7 @@ def test_calculate_dry_matter_intake(
     [(450.0, "Barn", 100.0, 0.01575), (475.0, "Grazing", 120.0, 41.895), (500.0, "Other", 50.0, 0.0)],
 )
 def test_calculate_activity_energy_requirements(
-    weight: float, housing: float, distance: float, expected: float
+    weight: float, housing: str, distance: float, expected: float
 ) -> None:
     """Test that net energy requirement for activity is calculated correctly."""
     actual = NASEMRequirementsCalculator._calculate_activity_energy_requirements(weight, housing, distance)
