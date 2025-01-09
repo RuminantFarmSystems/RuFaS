4f47816d52690ea0c46f05df0d9b25dea0098ed7
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-01-09 16:46:45 -0500

    updated names of unit tests

--- tests/animal_module_tests/nutrient_tests/test_nutrition_evaluator.py
+++ tests/animal_module_tests/nutrient_tests/test_nutrition_evaluator.py
@@ -3,12 +3,12 @@ def test_evaluate_nutrition_supply() -> None:
     pass
 
 
-def test_check_total_energy_supplied() -> None:
+def test_calculate_total_energy_supplied() -> None:
     """Tests that total energy supplied in ration is correctly compared against an animal's total energy requirement."""
     pass
 
 
-def test_check_activity_maintenance_energy_supplied() -> None:
+def test_calculate_activity_maintenance_energy_supplied() -> None:
     """
     Tests that maintenance energy supplied in ration is correctly compared against an animal's maintenance energy
     requirement.
@@ -16,7 +16,7 @@ def test_check_activity_maintenance_energy_supplied() -> None:
     pass
 
 
-def test_check_lactation_energy_supplied() -> None:
+def test_calculate_lactation_energy_supplied() -> None:
     """
     Tests that lactation energy supplied in ration is correctly compared against an animal's lactation energy
     requirement.
@@ -24,38 +24,38 @@ def test_check_lactation_energy_supplied() -> None:
     pass
 
 
-def test_check_growth_energy_supplied() -> None:
+def test_calculate_growth_energy_supplied() -> None:
     """
     Tests that growth energy supplied in ration is correctly compared against an animal's growth energy requirement.
     """
     pass
 
 
-def test_check_calcium_supplied() -> None:
+def test_calculate_calcium_supplied() -> None:
     """Tests that calcium supplied in ration is correctly compared against an animal's calcium requirement."""
     pass
 
 
-def test_check_phosphorus_supplied() -> None:
+def test_calculate_phosphorus_supplied() -> None:
     """Tests that phosphorus supplied in ration is correctly compared against an animal's phosphorus requirement."""
     pass
 
 
-def test_check_protein_supplied() -> None:
+def test_calculate_protein_supplied() -> None:
     """Tests that protein supplied in ration is correctly compared against an animal's protein requirement."""
     pass
 
 
-def test_check_neutral_detergent_fiber_supplied() -> None:
+def test_calculate_neutral_detergent_fiber_supplied() -> None:
     """Tests that NDF supplied in ration is correctly compared against an animal's NDF requirement."""
     pass
 
 
-def test_check_fat_content() -> None:
+def test_calculate_fat_content() -> None:
     """Tests that fat supplied in ration is correctly compared against an animal's fat requirement."""
     pass
 
 
-def test_check_dry_matter_intake() -> None:
+def test_calculate_dry_matter_intake() -> None:
     """Tests that dry matter supplied in ration is correctly compared against an animal's dry matter requirement."""
     pass
