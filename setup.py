98a1b2be885169478baf2873c4ef84e38f4b5b97
Niko <70217952+ew3361zh@users.noreply.github.com>
2025-01-17 15:49:20 -0500

    removing setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+# from setuptools import setup
 
-from RUFAS.task_manager import RUFAS_VERSION
+# from RUFAS.task_manager import RUFAS_VERSION
 
-setup(
-    name="RUFAS",
-    version=RUFAS_VERSION,
-    description="Ruminant Farm Systems Model",
-    author="Kristan Foster Reed",
-    author_email="kfr3@cornell.edu",
-    url="xxx",
-    packages=["RUFAS"],
-    python_requires=">=3.12",
-)
+# setup(
+#     name="RUFAS",
+#     version=RUFAS_VERSION,
+#     description="Ruminant Farm Systems Model",
+#     author="Kristan Foster Reed",
+#     author_email="kfr3@cornell.edu",
+#     url="xxx",
+#     packages=["RUFAS"],
+#     python_requires=">=3.12",
+# )
