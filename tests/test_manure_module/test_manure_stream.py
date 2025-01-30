ad657edcd172590bdfad90d08745dfab25d160e8
Matthew Liu <matthew7838@gmail.com>
2025-01-31 00:03:21 +0800

    Updated volume setup

--- tests/test_manure_module/test_manure_stream.py
+++ tests/test_manure_module/test_manure_stream.py
@@ -7,7 +7,7 @@ from RUFAS.data_structures.pen_manure_data import PenManureData
 
 @pytest.fixture
 def manure_stream(mocker: MockerFixture) -> ManureStream:
-    return ManureStream(1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, mocker.MagicMock(autospec=PenManureData))
+    return ManureStream(1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10, mocker.MagicMock(autospec=PenManureData))
 
 
 def test_total_volatile_solids(manure_stream: ManureStream) -> None:
@@ -18,10 +18,6 @@ def test_total_volatile_solids(manure_stream: ManureStream) -> None:
 def test_mass(manure_stream: ManureStream) -> None:
     """Checks that the property method correctly calculated mass."""
     assert manure_stream.mass == 11
-
-
-def test_volume(manure_stream: ManureStream) -> None:
-    pass
 
 
 def test_clear_pen_manure_data(manure_stream: ManureStream) -> None:
