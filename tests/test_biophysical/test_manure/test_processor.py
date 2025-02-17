2ec19e7f0ea2845787f7ba239f784f4d34901fe5
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-02-17 16:09:36 -0500

    added empty base digester and test

--- tests/test_biophysical/test_manure/test_processor.py
+++ tests/test_biophysical/test_manure/test_processor.py
@@ -5,10 +5,10 @@ from RUFAS.biophysical.manure.processor import Processor
 def test_processor_init_error() -> None:
     """Test that base Processor class throws appropriate error when initialized."""
     with pytest.raises(TypeError):
-        Processor(name="test_processor", is_housing_emissions_calculator=True)
+        Processor(name="test processor", is_housing_emissions_calculator=True)
 
 
-# TODO: test with a child or grandchild of Processor, either in #2098 or #2099
+# TODO: test with a grandchild of Processor in #2102, #2103, #2104, or #2105
 def test_check_manure_stream_compatibility() -> None:
     """Tests that ManureStreams are correctly checked for compatibility."""
     pass
