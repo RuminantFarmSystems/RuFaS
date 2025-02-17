b19b130aa9749b5a4a0924eab880853d1466aa37
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-02-17 16:00:58 -0500

    fixed mypy error

--- tests/test_biophysical/test_manure/test_processor.py
+++ tests/test_biophysical/test_manure/test_processor.py
@@ -7,7 +7,7 @@ from RUFAS.biophysical.manure.processor import Processor
 def test_processor_init_error() -> None:
     """Test that base Processor class throws appropriate error when initialized."""
     with pytest.raises(TypeError):
-        Processor(is_housing_emissions_calculator=True)
+        Processor(name="test processor", is_housing_emissions_calculator=True)
 
 
 # TODO: test with a grandchild of Processor in #2102, #2103, #2104, or #2105
