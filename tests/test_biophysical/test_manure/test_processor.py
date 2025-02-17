3cf4e20628268f881ff8c577388d6b9740d47eb2
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-02-17 15:59:19 -0500

    updated TODOs for processor tests

--- tests/test_biophysical/test_manure/test_processor.py
+++ tests/test_biophysical/test_manure/test_processor.py
@@ -10,7 +10,7 @@ def test_processor_init_error() -> None:
         Processor(is_housing_emissions_calculator=True)
 
 
-# TODO: test with a child or grandchild of Processor, either in #2098 or #2099
+# TODO: test with a grandchild of Processor in #2102, #2103, #2104, or #2105
 def test_check_manure_stream_compatibility() -> None:
     """Tests that ManureStreams are correctly checked for compatibility."""
     pass
