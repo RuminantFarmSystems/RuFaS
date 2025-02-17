356e18612024099733e98ad0d019a8f3738d6a15
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-02-17 16:04:35 -0500

    added name to processor base class

--- tests/test_biophysical/test_manure/test_processor.py
+++ tests/test_biophysical/test_manure/test_processor.py
@@ -5,7 +5,7 @@ from RUFAS.biophysical.manure.processor import Processor
 def test_processor_init_error() -> None:
     """Test that base Processor class throws appropriate error when initialized."""
     with pytest.raises(TypeError):
-        Processor(is_housing_emissions_calculator=True)
+        Processor(name="test_processor", is_housing_emissions_calculator=True)
 
 
 # TODO: test with a child or grandchild of Processor, either in #2098 or #2099
