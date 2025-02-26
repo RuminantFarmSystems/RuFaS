a93223b729d732aa773354c9dbbe9e02d2124388
Matthew Liu <49083256+matthew7838@users.noreply.github.com>
2025-02-26 21:32:17 +0800

    Update RUFAS/biophysical/manure/handler.py
    Co-authored-by: elle-andreen <148499778+elle-andreen@users.noreply.github.com>

--- RUFAS/biophysical/manure/handler.py
+++ RUFAS/biophysical/manure/handler.py
@@ -273,7 +273,7 @@ class Handler(Processor):
         ):
             self._om.add_error(
                 "Unsupported pen type.",
-                f"Handler only supports freestall and tiestall," f" received {manure_stream.pen_manure_data.pen_type}.",
+                f"Handler should only be used with freestall and tiestall pen types," f" received {manure_stream.pen_manure_data.pen_type}.",
                 info_map,
             )
             return False
