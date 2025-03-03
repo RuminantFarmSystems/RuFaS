6313b55525b8222daf817f217322782580bc058a
Matthew Liu <matthew7838@gmail.com>
2025-03-03 18:55:19 +0800

    change directory

--- tests/test_biophysical/test_manure/test_parlor_cleaning.py
+++ tests/test_biophysical/test_manure/test_parlor_cleaning.py
@@ -3,8 +3,8 @@ from unittest.mock import MagicMock
 import pytest
 from pytest_mock import MockerFixture
 
-from RUFAS.biophysical.manure.handler import HandlerConfig, Handler
-from RUFAS.biophysical.manure.parlor_cleaning import ParlorCleaningHandler
+from RUFAS.biophysical.manure.handler.handler import HandlerConfig, Handler
+from RUFAS.biophysical.manure.handler.parlor_cleaning import ParlorCleaningHandler
 from RUFAS.current_day_conditions import CurrentDayConditions
 from RUFAS.data_structures.animal_to_manure_connection import ManureStream, PenManureData, StreamType
 from RUFAS.enums import AnimalCombination
