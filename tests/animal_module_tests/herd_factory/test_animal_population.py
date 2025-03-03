15ffc5d236da3f10802941a5faf288d66c41b564
Allister Liu <allisterakun@gmail.com>
2025-03-03 11:51:44 -0500

    Implement Niko's comment
    - changed List to list
    - relinked MagicMock to unittest.mock
    - removed MockAnimals class
    - no more mypy or flake8 errors

--- tests/animal_module_tests/herd_factory/test_animal_population.py
+++ tests/animal_module_tests/herd_factory/test_animal_population.py
@@ -1,10 +1,9 @@
 import random
 import sys
-from typing import List
+from unittest.mock import MagicMock
 
-import mock
 import pytest
-from mock.mock import MagicMock
+from pytest_mock import MockerFixture
 
 from RUFAS.biophysical.animal.animal_config import AnimalConfig
 from RUFAS.biophysical.animal.data_types.animal_population import AnimalPopulation
@@ -14,10 +13,61 @@ from RUFAS.biophysical.animal.data_types.animal_types import AnimalType
 ANIMAL_TYPE_COW: list[AnimalType] = [AnimalType.LAC_COW, AnimalType.DRY_COW]
 
 
-@pytest.fixture
-def mock_animal_population() -> AnimalPopulation:
-    """InputManager fixture"""
-    return AnimalPopulation(calves=[], heiferIs=[], heiferIIs=[], heiferIIIs=[], cows=[], replacement=[])
+def mock_animal(animal_type: AnimalType, id: int, mocker: MockerFixture) -> Animal:
+    animal = MagicMock(auto_spec=Animal)
+
+    animal.id = id
+    animal.type = animal_type
+    animal.days_born = random.randint(0, 2000)
+    animal.days_in_pregnancy = random.randint(0, 500) \
+        if animal_type not in [AnimalType.CALF, AnimalType.HEIFER_I] else 0
+    animal.days_in_milk = random.randint(0, 2000) if animal_type.is_cow else 0
+    animal.calves = random.randint(0, 10) if animal_type.is_cow else 0
+    animal.calving_interval = random.randint(250, 1000) \
+        if animal_type.is_cow else AnimalConfig.calving_interval
+
+    mocker.patch.object(animal, "get_animal_values", return_value={"dummy": "animal"})
+
+    return animal
+
+
+def average(data: list[float | int]) -> float:
+    return sum(data) / len(data) if len(data) > 0 else 0.0
+
+
+def mock_herd(
+    num_calf: int,
+    num_heiferI: int,
+    num_heiferII: int,
+    num_heiferIII: int,
+    num_cow: int,
+    num_replacement: int,
+    mocker: MockerFixture,
+) -> tuple[list[Animal], list[Animal], list[Animal], list[Animal], list[Animal], list[Animal]]:
+    starting_id = 0
+    mock_calves: list[Animal] = [mock_animal(AnimalType.CALF, i, mocker) for i in range(num_calf)]
+    starting_id += num_calf
+
+    mock_heiferIs: list[Animal] = [mock_animal(AnimalType.HEIFER_I, i, mocker) for i in range(
+        starting_id, starting_id + num_heiferI)]
+    starting_id += num_heiferI
+
+    mock_heiferIIs: list[Animal] = [mock_animal(AnimalType.HEIFER_II, i, mocker) for i in range(
+        starting_id, starting_id + num_heiferII)]
+    starting_id += num_heiferII
+
+    mock_heiferIIIs: list[Animal] = [mock_animal(AnimalType.HEIFER_III, i, mocker) for i in range(
+        starting_id, starting_id + num_heiferIII)]
+    starting_id += num_heiferIII
+
+    mock_cows: list[Animal] = [mock_animal(AnimalType.LAC_COW, i, mocker) for i in range(
+        starting_id, starting_id + num_cow)]
+    starting_id += num_cow
+
+    mock_replacement: list[Animal] = [mock_animal(AnimalType.HEIFER_III, i, mocker) for i in range(
+        starting_id, starting_id + num_replacement)]
+
+    return mock_calves, mock_heiferIs, mock_heiferIIs, mock_heiferIIIs, mock_cows, mock_replacement
 
 
 @pytest.mark.parametrize("starting_animal_id", [0, 1, 31415, sys.maxsize])
@@ -53,31 +103,28 @@ def test_get_animals(
     num_heiferIII: int,
     num_cow: int,
     num_replacement: int,
-    mock_animal_population: AnimalPopulation,
+    mocker: MockerFixture,
 ) -> None:
     """Unit test for get_animals()"""
-    calves: List[Animal] = [mock.MagicMock(auto_spec=Animal)] * num_calf
-    heiferIs: List[Animal] = [mock.MagicMock(auto_spec=Animal)] * num_heiferI
-    heiferIIs: List[Animal] = [mock.MagicMock(auto_spec=Animal)] * num_heiferII
-    heiferIIIs: List[Animal] = [mock.MagicMock(auto_spec=Animal)] * num_heiferIII
-    cows: List[Animal] = [mock.MagicMock(auto_spec=Animal)] * num_cow
-    replacement: List[Animal] = [mock.MagicMock(auto_spec=Animal)] * num_replacement
-
     (
-        mock_animal_population.calves,
-        mock_animal_population.heiferIs,
-        mock_animal_population.heiferIIs,
-        mock_animal_population.heiferIIIs,
-        mock_animal_population.cows,
-        mock_animal_population.replacement,
-    ) = (calves, heiferIs, heiferIIs, heiferIIIs, cows, replacement)
+        calves,
+        heiferIs,
+        heiferIIs,
+        heiferIIIs,
+        cows,
+        replacement,
+    ) = mock_herd(num_calf, num_heiferI, num_heiferII, num_heiferIII, num_cow, num_replacement, mocker)
 
-    result_calves = mock_animal_population.get_calves()
-    result_heiferIs = mock_animal_population.get_heiferIs()
-    result_heiferIIs = mock_animal_population.get_heiferIIs()
-    result_heiferIIIs = mock_animal_population.get_heiferIIIs()
-    result_cows = mock_animal_population.get_cows()
-    result_replacement = mock_animal_population.get_replacement_cows()
+    animal_population = AnimalPopulation(
+        calves=calves, heiferIs=heiferIs, heiferIIs=heiferIIs, heiferIIIs=heiferIIIs, cows=cows, replacement=replacement
+    )
+
+    result_calves = animal_population.get_calves()
+    result_heiferIs = animal_population.get_heiferIs()
+    result_heiferIIs = animal_population.get_heiferIIs()
+    result_heiferIIIs = animal_population.get_heiferIIIs()
+    result_cows = animal_population.get_cows()
+    result_replacement = animal_population.get_replacement_cows()
 
     assert result_calves == calves
     assert len(result_calves) == num_calf
@@ -97,88 +144,7 @@ def test_get_animals(
     assert result_replacement == replacement
     assert len(result_replacement) == num_replacement
 
-
-class MockAnimals:
-    def __init__(self, num_animal: int, animal_type: AnimalType, starting_id: int = 0) -> None:
-        self.num_animal = num_animal
-        self.animal_type = animal_type
-
-        self.id_list = list(range(starting_id, starting_id + num_animal))
-        self.days_born_list = [random.randint(0, 5000) for _ in range(num_animal)]
-
-        self.days_in_pregnancy_list: list[int] | list[None] = (
-            [random.randint(0, 365) for _ in range(num_animal)] if animal_type in ANIMAL_TYPE_COW else [0] * num_animal
-        )
-        self.days_in_milk_list = (
-            [random.randint(0, 365) for _ in range(num_animal)] if animal_type in ANIMAL_TYPE_COW else [0] * num_animal
-        )
-        self.calves_list = (
-            [random.randint(0, 10) for _ in range(num_animal)] if animal_type in ANIMAL_TYPE_COW else [0] * num_animal
-        )  # Parity
-        self.calving_interval_list = (
-            [random.randint(180, 540) for _ in range(num_animal)] if animal_type in ANIMAL_TYPE_COW
-            else [AnimalConfig.calving_interval] * num_animal
-        )  # Calving Interval
-
-        self.average_days_in_pregnancy = 0.0
-        self.average_days_in_milk = 0.0
-        self.average_parity = 0.0
-        self.average_calving_interval = 0.0
-
-        self.animals = list(
-            map(
-                self.mock_animal,
-                [animal_type] * num_animal,
-                self.id_list,
-                self.days_born_list,
-                self.days_in_pregnancy_list,
-                self.days_in_milk_list,
-                self.calves_list,
-                self.calving_interval_list,
-            )
-        )
-
-        self.__post_init__()
-
-    def __post_init__(self) -> None:
-        self.current_max_id = max(self.id_list) if self.id_list else 0
-        self.average_age = sum(animal.days_born for animal in self.animals) / self.num_animal if self.num_animal else 0
-
-        if self.animal_type in ANIMAL_TYPE_COW:
-            self.average_days_in_pregnancy = (
-                sum(animal.days_in_pregnancy for animal in self.animals) / self.num_animal if self.num_animal else 0
-            )
-            self.average_days_in_milk = (
-                sum(animal.days_in_milk for animal in self.animals) / self.num_animal if self.num_animal else 0
-            )
-            self.average_parity = (
-                sum(animal.calves for animal in self.animals) / self.num_animal if self.num_animal else 0
-            )
-            self.average_calving_interval = sum(
-                animal.calving_interval for animal in self.animals
-            ) / self.num_animal if self.num_animal else 0
-
-    @staticmethod
-    def mock_animal(
-        animal_type: AnimalType,
-        id: int,
-        days_born: int,
-        days_in_pregnancy: int = 0,
-        days_in_milk: int = 0,
-        calves: int = 0,
-        calving_interval: int = AnimalConfig.calving_interval,
-    ) -> Animal:
-        dummy_animal = mock.MagicMock(auto_spec=Animal)
-        dummy_animal.id = id
-        dummy_animal.animal_type = animal_type
-        dummy_animal.days_born = days_born
-        dummy_animal.days_in_pregnancy = days_in_pregnancy if days_in_pregnancy > 0 else 0
-        dummy_animal.days_in_milk = days_in_milk if days_in_milk > 0 else 0
-        dummy_animal.calves = calves if calves > 0 else 0
-        dummy_animal.calving_interval = calving_interval if calving_interval > 0 else AnimalConfig.calving_interval
-
-        dummy_animal.get_animal_values = MagicMock(return_value={"dummy": "animal"})
-        return dummy_animal
+    AnimalPopulation.set_current_max_animal_id(0)
 
 
 @pytest.mark.parametrize(
@@ -196,10 +162,11 @@ class MockAnimals:
         ([]),
     ],
 )
-def test_average(data: List[int | float], mock_animal_population: AnimalPopulation) -> None:
+def test_average(data: list[int | float]) -> None:
     expected_result = sum(data) / len(data) if len(data) else 0
 
-    actual_result = mock_animal_population._average(data=data)
+    animal_population = AnimalPopulation(calves=[], heiferIs=[], heiferIIs=[], heiferIIIs=[], cows=[], replacement=[])
+    actual_result = animal_population._average(data=data)
 
     assert actual_result == expected_result
 
@@ -215,48 +182,28 @@ def test_get_herd_summary(
     num_heiferIII: int,
     num_cow: int,
     num_replacement: int,
-    mock_animal_population: AnimalPopulation,
+    mocker: MockerFixture,
 ) -> None:
     """Unit test for get_herd_summary()"""
     AnimalPopulation.set_current_max_animal_id(0)
 
-    mock_calves = MockAnimals(num_animal=num_calf, animal_type=AnimalType.CALF, starting_id=0)
-    mock_animal_population.calves = mock_calves.animals
+    (
+        calves,
+        heiferIs,
+        heiferIIs,
+        heiferIIIs,
+        cows,
+        replacements,
+    ) = mock_herd(num_calf, num_heiferI, num_heiferII, num_heiferIII, num_cow, num_replacement, mocker)
 
-    mock_heiferIs = MockAnimals(
-        num_animal=num_heiferI,
-        animal_type=AnimalType.HEIFER_I,
-        starting_id=mock_calves.current_max_id + 1,
+    animal_population = AnimalPopulation(
+        calves=calves,
+        heiferIs=heiferIs,
+        heiferIIs=heiferIIs,
+        heiferIIIs=heiferIIIs,
+        cows=cows,
+        replacement=replacements
     )
-    mock_animal_population.heiferIs = mock_heiferIs.animals
-
-    mock_heiferIIs = MockAnimals(
-        num_animal=num_heiferII,
-        animal_type=AnimalType.HEIFER_II,
-        starting_id=mock_heiferIs.current_max_id + 1,
-    )
-    mock_animal_population.heiferIIs = mock_heiferIIs.animals
-
-    mock_heiferIIIs = MockAnimals(
-        num_animal=num_heiferIII,
-        animal_type=AnimalType.HEIFER_III,
-        starting_id=mock_heiferIIs.current_max_id + 1,
-    )
-    mock_animal_population.heiferIIIs = mock_heiferIIIs.animals
-
-    mock_cows = MockAnimals(
-        num_animal=num_cow,
-        animal_type=AnimalType.LAC_COW,
-        starting_id=mock_heiferIIIs.current_max_id + 1,
-    )
-    mock_animal_population.cows = mock_cows.animals
-
-    mock_replacement = MockAnimals(
-        num_animal=num_replacement,
-        animal_type=AnimalType.HEIFER_III,
-        starting_id=mock_cows.current_max_id + 1,
-    )
-    mock_animal_population.replacement = mock_replacement.animals
 
     expected_result = {
         "num_calf": num_calf,
@@ -265,19 +212,19 @@ def test_get_herd_summary(
         "num_heiferIII": num_heiferIII,
         "num_cow": num_cow,
         "num_replacement": num_replacement,
-        "avg_calf_age": mock_calves.average_age,
-        "avg_heiferI_age": mock_heiferIs.average_age,
-        "avg_heiferII_age": mock_heiferIIs.average_age,
-        "avg_heiferIII_age": mock_heiferIIIs.average_age,
-        "avg_cow_age": mock_cows.average_age,
-        "avg_replacement_age": mock_replacement.average_age,
-        "cow_avg_days_in_pregnancy": mock_cows.average_days_in_pregnancy,
-        "cow_avg_days_in_milk": mock_cows.average_days_in_milk,
-        "cow_avg_parity": mock_cows.average_parity,
-        "cow_avg_calving_interval": mock_cows.average_calving_interval,
+        "avg_calf_age": average([calf.days_born for calf in calves]),
+        "avg_heiferI_age": average([heiferI.days_born for heiferI in heiferIs]),
+        "avg_heiferII_age": average([heiferII.days_born for heiferII in heiferIIs]),
+        "avg_heiferIII_age": average([heiferIII.days_born for heiferIII in heiferIIIs]),
+        "avg_cow_age": average([cow.days_born for cow in cows]),
+        "avg_replacement_age": average([replacement.days_born for replacement in replacements]),
+        "cow_avg_days_in_pregnancy": average([cow.days_in_pregnancy for cow in cows]),
+        "cow_avg_days_in_milk": average([cow.days_in_milk for cow in cows]),
+        "cow_avg_parity": average([cow.calves for cow in cows]),
+        "cow_avg_calving_interval": average([cow.calving_interval for cow in cows]),
     }
 
-    result = mock_animal_population.get_herd_summary()
+    result = animal_population.get_herd_summary()
     assert result == expected_result
 
     AnimalPopulation.set_current_max_animal_id(0)
@@ -294,50 +241,22 @@ def test_repr(
     num_heiferIII: int,
     num_cow: int,
     num_replacement: int,
-    mock_animal_population: AnimalPopulation,
+    mocker: MockerFixture
 ) -> None:
     """Unit test for __repr__()"""
     AnimalPopulation.set_current_max_animal_id(0)
 
-    mock_calves = MockAnimals(num_animal=num_calf, animal_type=AnimalType.CALF, starting_id=0)
+    (
+        calves,
+        heiferIs,
+        heiferIIs,
+        heiferIIIs,
+        cows,
+        replacement,
+    ) = mock_herd(num_calf, num_heiferI, num_heiferII, num_heiferIII, num_cow, num_replacement, mocker)
 
-    mock_heiferIs = MockAnimals(
-        num_animal=num_heiferI,
-        animal_type=AnimalType.HEIFER_I,
-        starting_id=mock_calves.current_max_id + 1,
-    )
-
-    mock_heiferIIs = MockAnimals(
-        num_animal=num_heiferII,
-        animal_type=AnimalType.HEIFER_II,
-        starting_id=mock_heiferIs.current_max_id + 1,
-    )
-
-    mock_heiferIIIs = MockAnimals(
-        num_animal=num_heiferIII,
-        animal_type=AnimalType.HEIFER_III,
-        starting_id=mock_heiferIIs.current_max_id + 1,
-    )
-
-    mock_cows = MockAnimals(
-        num_animal=num_cow,
-        animal_type=AnimalType.LAC_COW,
-        starting_id=mock_heiferIIIs.current_max_id + 1,
-    )
-
-    mock_replacement = MockAnimals(
-        num_animal=num_replacement,
-        animal_type=AnimalType.HEIFER_III,
-        starting_id=mock_cows.current_max_id + 1,
-    )
-
-    dummy_animal_population = AnimalPopulation(
-        calves=mock_calves.animals,
-        heiferIs=mock_heiferIs.animals,
-        heiferIIs=mock_heiferIIs.animals,
-        heiferIIIs=mock_heiferIIIs.animals,
-        cows=mock_cows.animals,
-        replacement=mock_replacement.animals,
+    animal_population = AnimalPopulation(
+        calves=calves, heiferIs=heiferIs, heiferIIs=heiferIIs, heiferIIIs=heiferIIIs, cows=cows, replacement=replacement
     )
 
     expected = {
@@ -349,7 +268,7 @@ def test_repr(
         "replacement": [{"dummy": "animal"}] * num_replacement,
     }
 
-    actual = dummy_animal_population.__repr__()
+    actual = animal_population.__repr__()
 
     assert actual == expected
 
@@ -367,48 +286,22 @@ def test_post_init(
     num_heiferIII: int,
     num_cow: int,
     num_replacement: int,
+    mocker: MockerFixture
 ) -> None:
     """Unit test for __post_init__()"""
     AnimalPopulation.set_current_max_animal_id(0)
-    mock_calves = MockAnimals(num_animal=num_calf, animal_type=AnimalType.CALF, starting_id=0)
 
-    mock_heiferIs = MockAnimals(
-        num_animal=num_heiferI,
-        animal_type=AnimalType.HEIFER_I,
-        starting_id=mock_calves.current_max_id + 1,
-    )
-
-    mock_heiferIIs = MockAnimals(
-        num_animal=num_heiferII,
-        animal_type=AnimalType.HEIFER_II,
-        starting_id=mock_heiferIs.current_max_id + 1,
-    )
-
-    mock_heiferIIIs = MockAnimals(
-        num_animal=num_heiferIII,
-        animal_type=AnimalType.HEIFER_III,
-        starting_id=mock_heiferIIs.current_max_id + 1,
-    )
-
-    mock_cows = MockAnimals(
-        num_animal=num_cow,
-        animal_type=AnimalType.LAC_COW,
-        starting_id=mock_heiferIIIs.current_max_id + 1,
-    )
-
-    mock_replacement = MockAnimals(
-        num_animal=num_replacement,
-        animal_type=AnimalType.HEIFER_III,
-        starting_id=mock_cows.current_max_id + 1,
-    )
+    (
+        calves,
+        heiferIs,
+        heiferIIs,
+        heiferIIIs,
+        cows,
+        replacement,
+    ) = mock_herd(num_calf, num_heiferI, num_heiferII, num_heiferIII, num_cow, num_replacement, mocker)
 
     AnimalPopulation(
-        calves=mock_calves.animals,
-        heiferIs=mock_heiferIs.animals,
-        heiferIIs=mock_heiferIIs.animals,
-        heiferIIIs=mock_heiferIIIs.animals,
-        cows=mock_cows.animals,
-        replacement=mock_replacement.animals,
+        calves=calves, heiferIs=heiferIs, heiferIIs=heiferIIs, heiferIIIs=heiferIIIs, cows=cows, replacement=replacement
     )
 
     expected = max(sum([num_calf, num_heiferI, num_heiferII, num_heiferIII, num_cow, num_replacement]) - 1, 0)
