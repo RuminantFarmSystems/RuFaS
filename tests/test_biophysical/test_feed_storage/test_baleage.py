b196c85e7a48cc24f9284af839c3ed97aca6f7dd
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2025-03-04 12:09:21 -0600

    brought baleage coverage back up

--- tests/test_biophysical/test_feed_storage/test_baleage.py
+++ tests/test_biophysical/test_feed_storage/test_baleage.py
@@ -1,4 +1,5 @@
 import pytest
+from dataclasses import replace
 from datetime import datetime
 from pytest_mock import MockerFixture
 
@@ -6,12 +7,13 @@ from RUFAS.input_manager import InputManager
 from RUFAS.biophysical.feed_storage.baleage import Baleage, INITIAL_LOSS_PERIOD
 from RUFAS.data_structures.crop_soil_to_feed_storage_connection import CropCategory, CropType, HarvestedCrop
 from RUFAS.time import Time
+from RUFAS.weather import Weather
 
 from .sample_crop_data import sample_crop_data
 
 
 @pytest.fixture
-def baleage() -> Baleage:
+def baleage(mocker: MockerFixture) -> Baleage:
     """
     Pytest fixture to create a Baleage instance for testing.
 
@@ -19,8 +21,40 @@ def baleage() -> Baleage:
     -------
     Baleage
         An instance of the Baleage class.
+
     """
+    im = InputManager()
+    mocker.patch.object(im, "get_data", return_value=45)
     return Baleage()
+
+
+@pytest.fixture
+def time() -> Time:
+    """
+    Pytest fixture to create a Time instance for testing.
+
+    Returns
+    -------
+    Time
+        An instance of the Time class.
+
+    """
+    return Time(datetime(2022, 12, 20), datetime(2025, 3, 7), datetime(2025, 3, 3))
+
+
+@pytest.fixture
+def weather(mocker: MockerFixture) -> Weather:
+    """
+    Pytest fixture to create a Weather instance for testing.
+
+    Returns
+    -------
+    Weather
+        An instance of the Weather class.
+
+    """
+    mocker.patch.object(Weather, "__init__", return_value=None)
+    return Weather()
 
 
 @pytest.fixture
@@ -48,20 +82,46 @@ def test_acceptable_crops(baleage: Baleage) -> None:
 
 
 def test_process_degradations(
+    baleage: Baleage,
     harvested_crop: HarvestedCrop,
+    time: Time,
+    weather: Weather,
     mocker: MockerFixture,
 ) -> None:
     """Tests process_degradations in Hay."""
-    im = InputManager()
-    mocker.patch.object(im, "get_data", return_value=45)
-    baleage = Baleage()
-    time = Time(datetime(2022, 12, 20), datetime(2025, 3, 7), datetime(2025, 3, 3))
     baleage.stored = [harvested_crop]
+    baleage.post_wilting_moisture_percentage = 45
     mock_moisture_loss = mocker.patch.object(baleage, "_process_moisture_loss")
-    mock_storage_process_degradations = mocker.patch("RUFAS.biophysical.feed_storage.storage.Storage.process_degradations")
-    mock_weather = mocker.MagicMock()
+    mock_storage_process_degradations = mocker.patch(
+        "RUFAS.biophysical.feed_storage.storage.Storage.process_degradations"
+    )
 
-    baleage.process_degradations(mock_weather, time)
+    baleage.process_degradations(weather, time)
 
     mock_moisture_loss.assert_called_once_with(time, INITIAL_LOSS_PERIOD, 45)
-    mock_storage_process_degradations.assert_called_once_with(mock_weather, time)
+    mock_storage_process_degradations.assert_called_once_with(weather, time)
+
+
+def test_project_degradations(
+    baleage: Baleage,
+    harvested_crop: HarvestedCrop,
+    weather: Weather,
+    mocker: MockerFixture,
+) -> None:
+    """Tests project_degradations in Hay."""
+    baleage.stored = [replace(harvested_crop, fresh_mass=1000.0) for _ in range(3)]
+    baleage.post_wilting_moisture_percentage = 43.0
+    crops_with_moisture_loss = [replace(crop, fresh_mass=900.0) for crop in baleage.stored]
+    crops_with_all_loss = [replace(crop, fresh_mass=800.0) for crop in baleage.stored]
+    project_moisture_loss = mocker.patch.object(
+        baleage, "_project_moisture_loss", return_value=crops_with_moisture_loss
+    )
+    project_degradations = mocker.patch(
+        "RUFAS.biophysical.feed_storage.storage.Storage.project_degradations", return_value=crops_with_all_loss
+    )
+
+    actual = baleage.project_degradations(baleage.stored, weather, time)
+
+    assert actual == crops_with_all_loss
+    project_moisture_loss.assert_called_once_with(baleage.stored, time, INITIAL_LOSS_PERIOD, 43.0)
+    project_degradations.assert_called_once_with(crops_with_moisture_loss, weather, time)
