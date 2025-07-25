2675f3ff73edf7a4e1bc5eeb0f8b19d8dca08f6f
Allister Liu <allisterakun@gmail.com>
2025-07-25 10:54:33 -0400

    fix field not running and update e2e results.

--- RUFAS/task_manager.py
+++ RUFAS/task_manager.py
@@ -522,11 +522,8 @@ class TaskManager:
                 TaskManager.handle_post_processing(args, input_manager, output_manager, task_id, False)
                 return None
 
-            input_manager.start_data_processing(metadata_path)
-            task_config: dict[str, Any] = input_manager.get_data("tasks")
-            for task in task_config.get("tasks", []):
-                filters_path = Path(task["filters_directory"])
-                output_manager.validate_filter_constant_content(filters_path)
+            filters_path = Path(args["filters_directory"])
+            output_manager.validate_filter_constant_content(filters_path)
 
             TaskManager.set_random_seed(args["random_seed"], output_manager)
 
