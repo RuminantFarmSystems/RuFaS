4759db8d9e7ee8ed784a743510ae4ff461512751
Allister Liu <allisterakun@gmail.com>
2025-08-05 01:46:37 -0400

    docstring, units, om filter validation

--- RUFAS/user_constants.py
+++ RUFAS/user_constants.py
@@ -3,6 +3,34 @@ from RUFAS.units import MeasurementUnits
 
 
 class UserConstants:
+    """
+    Contains user-overridable constants for RUFAS.
+
+    Attributes
+    ----------
+    WATER_DENSITY_KG_PER_LITER : float
+        Density of water (kg/L).
+    WATER_DENSITY_KG_PER_M3 : float
+        Density of water (kg/m^3).
+    PROTEIN_TO_NITROGEN : float
+        Conversion factor from protein to nitrogen (unitless).
+    NITROGEN_TO_PROTEIN : float
+        Conversion factor from nitrogen to protein (unitless).
+    MILK_FAT_WEIGHT : float
+        Average weight of milk fat (unitless).
+    FRACTION_OF_HUMIC_NITROGEN_IN_ACTIVE_POOL : float
+        Fraction of humic nitrogen in an active pool (unitless).
+        Defined in SWAT Theoretical documentation, page 186 in paragraph beneath eqn. 3:1.1.4.
+    METHANE_FACTOR : float
+        Conversion factor for methane from :math:`m^3` to kg at 20 °C (kg/m³).
+    GENERAL_LOWER_BOUND_TEMPERATURE : float
+        General temperature lower bound (°C).
+    GENERAL_UPPER_BOUND_TEMPERATURE : float
+        General temperature upper bound (°C).
+    CONSTANTS_TO_UNITS : dict[str, MeasurementUnits]
+        A dictionary mapping constant names to their respective measurement units.
+
+    """
     # Density-related
     WATER_DENSITY_KG_PER_LITER = 0.997
     WATER_DENSITY_KG_PER_M3 = WATER_DENSITY_KG_PER_LITER * GeneralConstants.LITERS_TO_CUBIC_METERS
@@ -12,16 +40,11 @@ class UserConstants:
     NITROGEN_TO_PROTEIN = 6.25
     MILK_FAT_WEIGHT = 12.2
     FRACTION_OF_HUMIC_NITROGEN_IN_ACTIVE_POOL = 0.02
-    """Defined in SWAT Theoretical documentation, page 186 in paragraph beneath eqn. 3:1.1.4."""
 
     METHANE_FACTOR: float = 0.67
-    """Unit conversion factor for methane from :math:`m^3` to kg at 20 degrees C (kg/m3)."""
 
     GENERAL_LOWER_BOUND_TEMPERATURE: float = -40.0
-    """General temperature lower bound (degrees C)."""
-
     GENERAL_UPPER_BOUND_TEMPERATURE: float = 60.0
-    """General temperature upper bound (degrees C)."""
 
     CONSTANTS_TO_UNITS = {
         "WATER_DENSITY_KG_PER_LITER": MeasurementUnits.KILOGRAMS_PER_LITER,
