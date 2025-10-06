f035ed4d34e5908233bee1e9eef03bb091c3716d
Niko <70217952+ew3361zh@users.noreply.github.com>
2025-10-06 17:25:27 -0400

    updates storage type coverage

--- tests/test_biophysical/test_feed_storage/test_feed_storage_enum.py
+++ tests/test_biophysical/test_feed_storage/test_feed_storage_enum.py
@@ -1,4 +1,9 @@
+from typing import Type
 import pytest
+from RUFAS.biophysical.feed_storage.baleage import Baleage
+from RUFAS.biophysical.feed_storage.grain import Dry, HighMoisture
+from RUFAS.biophysical.feed_storage.hay import Hay, ProtectedIndoors, ProtectedTarped, ProtectedWrapped, Unprotected
+from RUFAS.biophysical.feed_storage.silage import Bag, Bunker, Pile
 from RUFAS.biophysical.feed_storage.storage import Storage
 from RUFAS.biophysical.feed_storage.feed_storage_enum import StorageType
 
@@ -10,6 +15,25 @@ def test_get_storage_class_returns_enum_value(member) -> None:
     assert cls is member.value
     assert issubclass(cls, Storage)
 
+
+@pytest.mark.parametrize(
+    "input_str, expected_class",
+    [
+        ("Hay", Hay),
+        ("ProtectedIndoors", ProtectedIndoors),
+        ("ProtectedWrapped", ProtectedWrapped),
+        ("Unprotected", Unprotected),
+        ("ProtectedTarped", ProtectedTarped),
+        ("Baleage", Baleage),
+        ("Dry", Dry),
+        ("HighMoisture", HighMoisture),
+        ("Bunker", Bunker),
+        ("Pile", Pile),
+        ("Bag", Bag)   
+    ]
+)
+def test_get_storage_class_valid(input_str: str, expected_class: Type["Storage"]) -> None:
+    assert StorageType.get_storage_class(input_str) is expected_class
 
 @pytest.mark.parametrize("bad_name", ["Unknown", "pile", "BUNKER", "", "Hay "])
 def test_get_storage_class_raises_for_unknown(bad_name: str) -> None:
