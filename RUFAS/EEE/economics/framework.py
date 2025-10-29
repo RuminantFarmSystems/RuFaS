ff19d8ffc166ce5cd109ba99c6d915fcfc32ae15
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2025-10-28 20:30:41 -0400

    make economic framework class

--- RUFAS/EEE/economics/framework.py
+++ RUFAS/EEE/economics/framework.py
@@ -7,34 +7,38 @@ import pandas as pd
 from RUFAS.input_manager import InputManager
 from .dcfror import DCFRORCalculator
 from .partial_budget import calculate_partial_budget, has_partial_budget_activity
+from .preprocessing import EconomicPreprocessor
 
 
-def _capital_cost_present(im: InputManager) -> bool:
-    """Return ``True`` if any capital cost is defined."""
+class EconomicFramework:
+    def __init__(self) -> None:
+        self.im = InputManager()
 
-    cost_df = im.get_data("capital_costs.capital_cost_breakdown")
-    if isinstance(cost_df, pd.DataFrame) and not cost_df.empty:
+    def _capital_cost_present(self, im: InputManager) -> bool:
+        """Return ``True`` if any capital cost is defined."""
+
+        cost_df = im.get_data("capital_costs.capital_cost_breakdown")
+        if isinstance(cost_df, pd.DataFrame) and not cost_df.empty:
+            try:
+                return float(cost_df.get("Cost", cost_df.sum()).sum()) > 0
+            except Exception:
+                return cost_df.sum().sum() > 0
         try:
-            return float(cost_df.get("Cost", cost_df.sum()).sum()) > 0
+            return float(cost_df) > 0
         except Exception:
-            return cost_df.sum().sum() > 0
-    try:
-        return float(cost_df) > 0
-    except Exception:
-        return False
+            return False
 
+    def run_economic_analysis(self) -> None:
+        """Execute economic analysis using the Flexible Economic Framework."""
 
-def run_economic_analysis() -> None:
-    """Execute economic analysis using the Flexible Economic Framework."""
+        inputs = EconomicPreprocessor(self.im).preprocess()
+        capital_present = self._capital_cost_present(self.im)
+        partial_budget_requested = has_partial_budget_activity(self.im)
 
-    im = InputManager()
-    capital_present = _capital_cost_present(im)
-    partial_budget_requested = has_partial_budget_activity(im)
-
-    if capital_present:
-        calculator = DCFRORCalculator()
-        calculator.calculate()
-        if partial_budget_requested:
+        if capital_present:
+            calculator = DCFRORCalculator()
+            calculator.calculate()
+            if partial_budget_requested:
+                calculate_partial_budget()
+        else:
             calculate_partial_budget()
-    else:
-        calculate_partial_budget()
