6a7d38a605dd7732635e0f7dd8ba455b68ff0dac
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2026-02-04 09:12:51 -0500

    changed separator warning to error, removed liquid manure min

--- RUFAS/biophysical/manure/separator/separator.py
+++ RUFAS/biophysical/manure/separator/separator.py
@@ -162,12 +162,11 @@ class Separator(Processor):
 
         liquid_manure_water = self.held_manure.water - solid_manure_water
         if liquid_manure_water <= 0.0:
-            self._om.add_warning(
+            self._om.add_error(
                 f"Liquid manure volume is {liquid_manure_water}.",
-                "Setting liquid manure water to minimum of 0.01 m3.",
+                f"Separator {self.name} attempted to separate more water into manure solids fraction than was present in manure entering separator. {self.separated_solids_dry_matter} value for separator {self.name} may need to be increased.",
                 {**info_map},
             )
-            liquid_manure_water = 0.01
         liquid_manure_total_solids = self.held_manure.total_solids * (1 - self.total_solids_efficiency)
         liquid_manure_volume = (
             liquid_manure_water + liquid_manure_total_solids
