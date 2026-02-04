c0901d74907fc3578c9c0c37d200c9e03896ec1e
Matthew Liu <matthew7838@gmail.com>
2026-02-04 23:33:36 +0900

    checking out

--- RUFAS/data_structures/crop_soil_to_feed_storage_connection.py
+++ RUFAS/data_structures/crop_soil_to_feed_storage_connection.py
@@ -150,7 +150,7 @@ class HarvestedCrop:
         mass_to_remove : float
             Dry-matter to remove. (kg).
         """
-        if mass_to_remove > self.dry_matter_mass + 1e-6:
+        if mass_to_remove > self.fresh_mass + 1e-6:
             raise ValueError(
                 f"Cannot remove {mass_to_remove:.3f} kg DM - "
                 f"only {self.dry_matter_mass:.3f} kg dry matter available."
