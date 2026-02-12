0320c567899c068adb55232d799160c3634d5f7a
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2026-02-12 09:44:50 -0600

    begin eval setup dir

---
+++ eval_CS/alt_main.py
@@ -0,0 +1,15 @@
+from main import main # pycharm thinks it will work, but it doesn't
+# from ..main import main # doesn't work
+# from ... import main # doesn't work
+# from ... import rufas # doesn't work
+# from rufas import main # doesn't work
+# from ..rufas import main # doesn't work
+
+## Other code here ...
+
+if __name__ == '__main__':
+    main()
+    # main.main()
+    # rufas.main.main()
+
+    ## Other code here ...
