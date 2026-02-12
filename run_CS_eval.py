871515a5b9bd9ec605ff7a7aaf680a936eb1775c
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2026-02-12 14:26:35 -0600

    added inputs (unfinished)

--- run_CS_eval.py
+++ run_CS_eval.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
-from main import main
+from main import main, run_rufas
+from pathlib import Path
 
-## extra code
+# Helper functions and classes for evaluation
+## TODO
 
+# Run the script code
 if __name__ == "__main__":
-    main()
+
+    # Pre processing
+    ## TODO
+
+    # Run the RuFaS model within the eval_CS folder.
+    run_rufas(
+        metapath = Path("eval_CS/inputs/metadata.json"),
+        out_dir = Path("eval_CS/outputs/"),
+        log_dir = Path("eval_CS/logs/")
+    )
+
+    # Post-processing
+    ## TODO
