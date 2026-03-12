c69f8c866d632a95f47618db7bd41b64969baef5
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2026-03-12 16:50:21 -0500

    updated script

---
+++ eval_CS/helpful-shell-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+# usage:
+#   source helpful-shell-variables.sh
+
+EVAL_DIR="/home/morrowcj/Documents/projects/rufas-evaluation/"
+RUFAS_DIR="/home/morrowcj/Documents/projects/rufas/"
