feb30b6622f49b9298da1659cfb5646355e3fc90
Niko <70217952+ew3361zh@users.noreply.github.com>
2026-08-05 17:32:40 -0400

    sets up initial ppo classes

---
+++ RUFAS/post_processing/graph_generator.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GraphGeneratorConfig:
+    """
+    Attributes
+    ----------
+    """
+
+class GraphGenerator:
+    """
+    GraphGenerator is used to generate graphs from the simulation results.
+
+    Parameters
+    ----------
+    metadata_prefix : str, optional
+        Prefix applied to graph metadata. Defaults to ``""``.
+    time : RufasTime, optional
+        ``RufasTime`` object used to track simulation time. Defaults to ``None``.
+
+    Attributes
+    ----------
+    metadata_prefix : str, optional
+        Prefix applied to graph metadata. Defaults to ``""``.
+    time : RufasTime, optional
+        ``RufasTime`` object used to track simulation time. Defaults to ``None``.
+
+    Notes
+    -----
+    This class is not multi-thread safe!!!
+    """
+
+    def __init__(self, config: GraphGeneratorConfig) -> None:
+        pass
