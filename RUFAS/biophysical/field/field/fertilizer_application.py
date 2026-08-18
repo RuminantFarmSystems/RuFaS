9c8c560b231e46671f313902aaddcfd919a8602d
JoeWaddell <109082924+JoeWaddell@users.noreply.github.com>
2026-08-18 16:59:06 -0400

    fixed equations in generate_depth_factors

--- RUFAS/biophysical/field/field/fertilizer_application.py
+++ RUFAS/biophysical/field/field/fertilizer_application.py
@@ -124,8 +124,9 @@ class FertilizerApplication:
         injection applications occurred.
 
         """
+        top_depths = self.soil.data.get_vectorized_layer_attribute("top_depth")
         bottom_depths = self.soil.data.get_vectorized_layer_attribute("bottom_depth")
-        depth_factors = self.generate_depth_factors(application_depth, bottom_depths)
+        depth_factors = self.generate_depth_factors(application_depth, top_depths, bottom_depths)
         for index, depth_factor in enumerate(depth_factors):
             self.soil.data.soil_layers[index].labile_inorganic_phosphorus_content += (
                 phosphorus * depth_factor * subsurface_fraction
@@ -134,7 +135,7 @@ class FertilizerApplication:
             self.soil.data.soil_layers[index].ammonium_content += ammonium * depth_factor * subsurface_fraction
 
     @staticmethod
-    def generate_depth_factors(application_depth: float, soil_layer_bottom_depths: list[float]) -> list[float]:
+    def generate_depth_factors(application_depth: float, soil_layer_top_depths: list[float], soil_layer_bottom_depths: list[float]) -> list[float]:
         """
         Generates a list of fractions that partitions sub-surface nutrients between the different soil layers.
 
@@ -163,13 +164,19 @@ class FertilizerApplication:
         """
         depth_factors_sum = 0.0
         depth_factors = []
-        for depth in soil_layer_bottom_depths:
-            if depth < application_depth:
-                depth_factor = depth / application_depth
+        for top_depth, bottom_depth in zip(soil_layer_top_depths, soil_layer_bottom_depths):
+            layer_depth = bottom_depth - top_depth
+            if bottom_depth < application_depth:
+                depth_factor = layer_depth / application_depth
                 depth_factors_sum += depth_factor
                 depth_factors.append(depth_factor)
+                if depth_factor < 0:
+                    print("depth_error")
             else:
                 depth_factor = 1.0 - depth_factors_sum
                 depth_factors.append(depth_factor)
+                if depth_factor < 0:
+                    print("depth_error")
                 break
+        print(depth_factors)
         return depth_factors
